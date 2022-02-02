b67bd80f1f9dee0e552e7ec486c35a1c650ec57b
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-02 17:13:35 +0200

    add more checks

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import os
 def create_directory(dir_names):
     counter = 0
     path_to_file = str(dir_names[counter])
-    print(len(dir_names))
+
     while True:
         try:
             os.mkdir(path_to_file)
@@ -21,29 +21,51 @@ def delete_current_file(file_name):
     os.remove(file_name)
 
 
+def copy_file(old_name, new_name):
+    try:
+        with open(old_name, mode='r') as file_in:
+            with open(new_name, mode='w') as file_out:
+                for line in file_in:
+                    file_out.write(line)
+    except FileNotFoundError:
+        raise
+
+
 def move_file(command: str):
     command_split = command.split()
     command_with_slash = command_split[2].split('/')
     result_directory = '/'.join(command_with_slash)
 
-    if command_split[0] == 'mv' and command_split[1] != command_split[2] and len(command_with_slash) < 2:
+    if command_split[0] != 'mv':
+        print('This function work only for mv(move) command')
+        return None
+
+    if not os.path.exists(command_split[1]):
+        print(f'File {command_split[1]} not found')
+        return None
+
+    if os.path.exists(command_split[2]):
+        print(f'A file with that name {command_split[2]} already exists')
+        return None
+
+    if command_split[1] == command_split[2]:
+        print(f'File {command_split[1]} have already name {command_split[2]}')
+        return None
+
+    if len(command_with_slash) < 2:
         try:
             os.rename(command_split[1], command_split[2])
         except FileNotFoundError:
             print(f'File not found {command_split[1]}')
             return
 
-    elif command_split[0] == 'mv' and command_with_slash:
+    elif len(command_with_slash) > 2:
         create_directory(command_with_slash)
-
-        with open(command_split[1], mode='r') as file_in:
-            with open(result_directory, mode='w') as file_out:
-                for line in file_in:
-                    file_out.write(line)
-
+        copy_file(command_split[1], result_directory)
         delete_current_file(command_split[1])
 
 
 if __name__ == "__main__":
-    move_file("mv file.txt some_dir1/some_dir2/some_dir3/new_file.txt")
-    # move_file("mv file.txt new_file.txt")
+    '''Tests for this task'''
+    move_file("mv new_file.txt some_dir1/some_dir2/some_dir3/new_file.txt")
+    move_file("mv file.txt new_file.txt")
