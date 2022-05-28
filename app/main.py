f741e67f1f9842f5b137ec4474853d6256bf20c2
Taras Nester <tarasnester414@gmail.com>
2022-05-28 21:05:13 +0300

    base version

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    operator, current_file_name, destination = command.split()
+    *folders, new_file_name = destination.split("/")
+
+    if operator != "mv":
+        return
+
+    path = ""
+    for folder_name in folders:
+        path += folder_name + "/"
+        os.mkdir(path)
+
+    with open(current_file_name) as old_file:
+        with open(path + new_file_name, "w") as new_file:
+            new_file.write(old_file.read())
+
+    os.remove(current_file_name)
