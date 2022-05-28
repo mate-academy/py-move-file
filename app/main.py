4966b5ce07dd0f3c54ff415177aa9a7c3c515668
Taras Nester <tarasnester414@gmail.com>
2022-05-28 21:11:51 +0300

    Revert "renamed files"
    This reverts commit 4f46e4bc1966ef6ed9ee18db7c9c8d0f847d44a8.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import os
 
 
 def move_file(command: str):
-    operator, old_file_name, destination = command.split()
+    operator, current_file_name, destination = command.split()
     *folders, new_file_name = destination.split("/")
 
     if operator != "mv":
@@ -13,8 +13,8 @@ def move_file(command: str):
         path += folder_name + "/"
         os.mkdir(path)
 
-    with open(old_file_name) as old_file:
-        with open(destination, "w") as new_file:
+    with open(current_file_name) as old_file:
+        with open(path + new_file_name, "w") as new_file:
             new_file.write(old_file.read())
 
-    os.remove(old_file_name)
+    os.remove(current_file_name)
