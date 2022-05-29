1962904e99d0ee4029ddb685de86754e598a77b8
Dorius <kornelydorius@gmail.com>
2022-05-29 14:54:42 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command):
+    sourse = command.split()[1]
+    target = command.split()[2]
+
+    if "/" in target:
+        target_split = target.split("/")
+        target_path = os.path.join(*target_split)
+
+        os.makedirs(target_path)
+
+        with open(target_path, "w") as f:
+            f.write(open(sourse, "r").read())
+
+        os.remove(sourse)
+    else:
+        os.rename(sourse, target)
