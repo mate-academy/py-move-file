33183e0466c0fc97b4c8eae921da75642cc97b9e
Flashmobber <ufinaid@gmail.com>
2022-05-29 18:01:10 +0300

    All done.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    parsed = command.split()
+
+    if parsed[0] == "mv" and parsed[1] != parsed[2]:
+        if os.path.isfile(parsed[2]):
+            return
+
+        if parsed[2][-1] == os.path.sep:
+            parsed[2] += parsed[1]
+
+        path = ""
+        for d in parsed[2].split(os.path.sep)[:-1]:
+            if not os.path.isdir(path + d):
+                os.mkdir(path + d)
+                path += d + os.path.sep
+
+        os.rename(parsed[1], parsed[2])
