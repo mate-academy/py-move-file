05973fb1535ffdaede44ed5a8b9811f2b4560413
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-05-30 15:03:33 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+from os import remove, mkdir
+
+
+def move_file(command: str):
+    command, old_file_name, new_position = command.split()
+
+    if command != "mv":
+        raise ValueError("We need mv command to move file")
+
+    *folders, new_file_name = new_position.split("/")
+    directory = ""
+
+    for folder in folders:
+        directory += folder + "/"
+        mkdir(directory)
+
+    with open(old_file_name, "r") as old_file:
+        with open(directory + new_file_name, "w") as new_file:
+            new_file.write(old_file.read())
+
+    remove(old_file_name)
