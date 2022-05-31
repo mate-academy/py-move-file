2c4b49c804dd3d5c76bea9e43a44ce81277dd51b
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-05-31 22:15:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    old = command.split()[1]
+    new = command.split()[2]
+
+    new_dir = "/".join(new.split("/")[:-1])
+    os.makedirs(new_dir)
+    os.rename(old, new)
