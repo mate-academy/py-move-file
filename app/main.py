c8a467c13928a188d330743c247bfc7eec36e0e3
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-05-31 22:24:37 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,6 +5,13 @@ def move_file(command: str):
     old = command.split()[1]
     new = command.split()[2]
 
+    if '/' not in new:
+        os.rename(old, new.split("/")[-1])
+        return
+
     new_dir = "/".join(new.split("/")[:-1])
     os.makedirs(new_dir)
     os.rename(old, new)
+
+
+move_file('mv file.txt files/siska/file2.txt')
