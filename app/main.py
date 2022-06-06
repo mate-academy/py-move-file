0675691ce5092cf95d875076577c1f57a1099f72
Serhii Leonenko <leonenko3149@gmail.com>
2022-06-06 19:18:54 +0300

    short implementation

--- app/main.py
+++ app/main.py
@@ -2,46 +2,25 @@ import os
 import shutil
 
 
-def move_file(command: str):
-    command_ls = command.replace("/", "\\").split()
-    name_of_command = command_ls[0]
-    name_of_file = command_ls[1]
-    name_and_way_new_file = command_ls[2]
+def create_file(file_name, new_file):
+    original = rf'{os.getcwd()}\{file_name}'
+    target = rf'{os.getcwd()}\{new_file}'
+    shutil.copyfile(original, target)
+    os.remove(file_name)
 
-    assert name_of_command != "mw", f"Command:{name_of_command} is not defined"
 
-    if "/" not in command:
-        original = rf'{os.getcwd()}\{name_of_file}'
-        target = rf'{os.getcwd()}\{name_and_way_new_file}'
-        shutil.copyfile(original, target)
-        os.remove(name_of_file)
-        print("Operation completed")
+def move_file(command_line: str):
+    command, file_name, new_file = command_line.replace("/", "\\").split()
+
+    assert command == "mv", f"Command:{command} is not defined"
+
+    if "/" not in command_line:
+        create_file(file_name, new_file)
     else:
-        # path to create new directories
-        path_to_directory = list(name_and_way_new_file)
-        path_to_directory.reverse()
-        path_to_directory_copy = path_to_directory[:]
-        for x in path_to_directory_copy:
-            if x != "\\":
-                path_to_directory.remove(x)
-            else:
-                path_to_directory.remove(x)
-                break
-        path_to_directory.reverse()
-        path_to_directory = "".join(path_to_directory)
-        path_to_create_directories = os.path.join(
-            os.getcwd(), path_to_directory
-        )
+        *path_new_file, new_file_name = new_file.split("\\")
+        path = os.getcwd()
+        for directory in path_new_file:
+            path = os.path.join(path, directory)
+        os.makedirs(path)
 
-        # create directories
-        os.makedirs(path_to_create_directories)
-
-        # copy file
-        original = rf'{os.getcwd()}\{name_of_file}'
-        target = rf'{os.getcwd()}\{name_and_way_new_file}'
-        shutil.copyfile(original, target)
-
-        # remove old file
-        os.remove(name_of_file)
-
-        print("Operation completed")
+        create_file(file_name, new_file)
