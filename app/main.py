73ad97c1098c66dfe473818b0529c84977dcc225
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-06-13 09:37:42 +0100

    Solution_fixed

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ import os
 
 
 def move_file(command):
+    ls = command.replace(" ", "/").split("/")
+    origin_file_name = ls[1]
+    moved_file_name = ls[-1]
+    path_name = ""
     if "/" in command:
-        ls = command.replace(" ", "/").split("/")
-        origin_file_name = ls[1]
-        moved_file_name = ls[-1]
         directories = ls[2:-1]
-        path_name = ""
         for directory in directories:
             os.mkdir(f"{path_name}{directory}")
             path_name += directory + "/"
-        with open(origin_file_name, "r") as origin_file,\
-             open(f"{path_name}{moved_file_name}", "w") as created_file:
-            text_to_copy = origin_file.read()
-            created_file.write(text_to_copy)
-        os.remove(origin_file_name)
+    with open(origin_file_name, "r") as origin_file,\
+         open(f"{path_name}{moved_file_name}", "w") as created_file:
+        text_to_copy = origin_file.read()
+        created_file.write(text_to_copy)
+    os.remove(origin_file_name)
