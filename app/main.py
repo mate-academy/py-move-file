12222777808540e74fe4c5ba166804989fc05480
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-20 13:13:24 +0300

    Refactored with used context manager

--- app/main.py
+++ app/main.py
@@ -4,19 +4,16 @@ from os import remove, makedirs
 def move_file(command):
     old_file_name = command.split()[1]
     path_to_move = command.split()[2]
+    with open(old_file_name, "r") as old_file:
 
-    old_file = open(old_file_name, "r")
-    dirs = path_to_move.split("/")[:-1]
-    path_without_file = "/".join(dirs)
+        dirs = path_to_move.split("/")[:-1]
+        path_without_file = "/".join(dirs)
 
-    if len(path_to_move.split("/")) > 1:
-        makedirs(path_without_file)
+        if len(path_to_move.split("/")) > 1:
+            makedirs(path_without_file)
 
-    new_file = open(path_to_move, "w")
+        with open(path_to_move, "w") as new_file:
+            for data in old_file.readlines():
+                new_file.write(data)
 
-    for data in old_file.readlines():
-        new_file.write(data)
-
-    old_file.close()
-    new_file.close()
     remove(old_file_name)
