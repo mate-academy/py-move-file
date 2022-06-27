5790983176b560da3ccabde97a5e2b7d4ca26354
SenchaPencha <SenchaPencha@mail.ru>
2022-06-27 16:42:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,17 +4,17 @@ import os
 def move_file(command: str):
     command = command.split()
     file = command[1]
-    direct = command[2]
-    with open(f"..\\{file}", "r") as f:
+    direct = "../" + command[2]
+    with open(f"../{file}", "r") as f:
         file_info = f.read()
-    direct_ls = direct.split("/")
-    direct = direct.replace("/", "//")
-    new_direct = ""
-    for i in direct_ls:
-        new_direct += f"{i}"
+    direct_ls = direct.split("/")[1:]
+    new_direct = "../"
+    for path_part in direct_ls:
+        new_direct += f"{path_part}"
         try:
-            with open(direct, "w") as f:
-                f.write(file_info)
+            print(new_direct)
+            with open(direct, "w") as file:
+                file.write(file_info)
         except FileNotFoundError:
             os.mkdir(new_direct)
-            new_direct += "\\"
+            new_direct += "/"
