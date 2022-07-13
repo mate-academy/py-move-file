ba77ada76f26a76492feba75de5f09c90d575a62
muhactive <muhactive@gmail.com>
2022-07-13 20:31:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    commands = command.split()
+    file_first = commands[1]
+    file_second = command[2]
+
+    with open(file_first, "r") as f:
+        file = f.read()
+
+    path_to_folder = ""
+    path = file_second.split("/")[:-1]
+    for name in path:
+        path_to_folder = f"{name}/"
+        os.mkdir(path_to_folder)
+
+    with open(file_second, "w") as file_2:
+        file_2.write(file)
+
+    os.remove(file_first)
