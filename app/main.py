5bb27fa9322a8916d5c133542bf004cd0c956ed1
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-07-18 21:43:59 +0300

    rename

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def move_file(command):
     if command.split()[0] == 'mv':
         if "/" in file_second:
             count_dir = file_second.count("/")
-            path_file_2 = file_second.split("/")
+            path_file_second = file_second.split("/")
             for i in range(count_dir):
-                os.mkdir("/".join(path_file_2[:i + 1]))
+                os.mkdir("/".join(path_file_second[:i + 1]))
         with open(file_first, "r") as file_in, \
                 open(file_second, "w") as file_out:
             file_out.write(file_in.read())
