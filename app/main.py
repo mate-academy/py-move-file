4bc98444e53b27996086cce496781648079913a3
Roman <coderc770@gmail.com>
2022-07-18 19:06:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    command_format = command.replace("/", ' ').split()[1:]
+    for i in range(len(command_format) - 2):
+        os.mkdir("/".join(command_format[1:i + 2]))
+    path = "/".join(command_format[1:])
+    with open(command_format[0], "r") as current_file, \
+            open(path, 'w') as new_file:
+        new_file.write(current_file.read())
+    os.remove(command_format[0])
