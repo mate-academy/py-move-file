6d862b99aac578f83028d3d14770faeeb4da33ca
Misha Hrynko <misha@mate.academy>
2022-07-20 14:49:46 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    ls_from_command = command.split()
+    path_and_name = ls_from_command[2].rsplit("/", 1)
+    file = open(ls_from_command[1], "r").readlines()
+    if len(path_and_name) == 1:
+        os.rename(ls_from_command[1], ls_from_command[2])
+    if len(path_and_name) > 1:
+        os.makedirs(path_and_name[0])
+        with os.popen(ls_from_command[2], "w") as new_f:
+            new_f.writelines(file)
+            os.remove(ls_from_command[1])
