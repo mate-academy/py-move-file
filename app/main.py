d092fd41891af2e7ec19bd919ba3d0e5f0298628
Inna Kushnir <innakushnir63@gmail.com>
2022-08-01 17:30:00 +0300

    Solutions

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    s = command.split(" ")
+    catalog = (s[2]).split("/")
+
+    if len(catalog) > 1:
+        catalog_ = catalog[:-1]
+        os.makedirs(catalog_)
+        with open("s[1]", "r") as f, open("s[2]", "w") as f_:
+            information = f.read()
+            f_.write(information)
+
+        os.remove("s[1]")
+
+
+if __name__ == "__main__":
+    move_file()
