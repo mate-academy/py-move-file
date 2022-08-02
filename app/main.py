db8a4902c88f4403293d5b58465cdbc6f48d6a8f
stoveprofi <stoveprofi@gmail.com>
2022-08-02 19:07:50 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command):
+    s_list = command.split(" ")
+    dao_str = s_list[2]
+    file_in = s_list[1]
+    file_out = dao_str[-dao_str.index('/'):]
+    dao = dao_str[:-dao_str.index('/')]
+
+    with open(file_in, 'r') as f1:
+        cont = f1.read()
+        os.makedirs(dao)
+
+    os.remove(file_in)
+    os.chdir(dao)
+
+    with open(file_out, 'w') as f2:
+        f2.write(cont)
