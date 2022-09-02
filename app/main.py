50306ea4add74ee5516b36e479d85203bbe7f592
Paul Starodub <amorallex@gmail.com>
2022-09-02 12:00:14 +0300

    Task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    command_list = command.split()
+    last_sl_ind = command_list[2].rfind("/")
+    path_first = command_list[1]
+    full_new_path = command_list[2]
+    path_to_file = command_list[2][:last_sl_ind]
+    os.makedirs(path_to_file)
+    os.replace(path_first, full_new_path)
