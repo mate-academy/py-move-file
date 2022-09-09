8d05f9a0446bfa9a9a4c082331915e037d87a69f
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-09 23:18:13 +0300

    Sol for check command

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from os import mkdir, remove
 
 def move_file(command: str):
     list_command = command.split()
+    if list_command[0] != "mv" and len(list_command) != 3:
+        return "Wrong command"
     if "/" in list_command[-1]:
         path_list = list_command[-1].split("/")
         for i in range(len(path_list) - 1):
