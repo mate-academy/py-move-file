6ae2d8a43bc6382c8e57cc82666d40b922d7ff55
Juliya Herasymniuk <juliya.gerasimnjyk@gmail.com>
2022-09-14 14:57:48 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-import os
+from os import mkdir, remove
 
 
-def move_file(command):
+def move_file(command: str):
     command_new = command.split()
     main_file = command_new[1]
     copy_way = command_new[2]
@@ -9,8 +9,8 @@ def move_file(command):
     copy_file = copy_way_split[-1]
     with open(main_file, "r") as file_in:
         text = file_in.read()
-        os.remove(main_file)
+        remove(main_file)
         for directory in copy_way_split[:-1]:
-            os.mkdir(directory)
+            mkdir(directory)
         with open(copy_file, "w") as file_out:
             file_out.write(text)
