89bae3989e0046765b7786ef051fab6be3adedce
Juliya Herasymniuk <juliya.gerasimnjyk@gmail.com>
2022-09-15 16:52:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from os import mkdir, remove
 def move_file(command: str):
     command_new = command.split()
     if command_new[0] != "mv":
-        print("Incorrect command!")
+        return "Incorrect command!"
     if "/" in command_new[-1]:
         path = command_new[-1].split("/")
         for i in range(len(path) - 1):
@@ -12,4 +12,4 @@ def move_file(command: str):
     with open(command_new[1], "r") as file_init, \
             open(command_new[2], "w") as file_out:
         file_out.write(file_init.read())
-    remove(command_new[1])
+    return remove(command_new[1])
