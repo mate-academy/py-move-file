b7d607878c6d78752a44f6e2e164ad40dc1c5d7f
Davyd Kryvko <davidkrivko@gmail.com>
2022-09-19 13:31:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def move_file(values: str):
+    values = values.split()
+    path = values[2].split("/")
+    with open(values[1], "r") as file_in:
+        content = file_in.read()
+    os.remove(values[1])
+    if len(path) == 2:
+        os.mkdir(path[0])
+    if len(path) == 3:
+        os.mkdir(path[0])
+        os.mkdir(path[0] + "/" + path[1])
+    if len(path) == 4:
+        os.mkdir(path[0])
+        os.mkdir(path[0] + "/" + path[1])
+        os.mkdir(path[0] + "/" + path[1] + "/" + path[2])
+    with open(values[2], "w") as file_out:
+        file_out.write(content)
