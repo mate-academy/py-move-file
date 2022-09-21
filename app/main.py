06f2f5ec13c391280b592ad1bbaec3cb114947e3
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-09-21 16:43:22 +0300

    py_move_file_Solution_last

--- app/main.py
+++ app/main.py
@@ -3,15 +3,18 @@ import os
 
 def move_file(command: str):
     command_list = command.split(" ")
-    path_out_list = command_list[2].split("/")
+    path_out_list = command_list[2].split("\\")
     path = ""
     if len(path_out_list) > 1:
         for i in range(len(path_out_list) - 1):
-            path += os.path.join(path_out_list[i])
+            path += f"{path_out_list[i]}\\"
             if not os.path.isdir(path):
                 os.mkdir(path)
     if path_out_list[-1] != "":
         with (open(command_list[1], "r") as file_in,
-              open(path + path_out_list[-1], "w") as file_out):
+              open(os.path.join(path, path_out_list[-1]), "w") as file_out):
             file_out.write(file_in.read())
     os.remove(command_list[1])
+
+
+move_file("mv file2.txt d:\\1\\2\\3\\file.txt")
