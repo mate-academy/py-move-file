4da49cda15937c1d6fc68ef0fb3bb6f5e1e8abd0
Ivan Antonishak <jus1stored@gmail.com>
2022-09-21 23:22:22 +0300

    Solution for task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split()[1:]
+    if "/" in command[1]:
+        directory = command[1][:command[-1].rindex("/")]
+        os.makedirs(directory)
+        with open(command[0], "r") as original_file,\
+                open(command[1], "w") as moved_file:
+            for text in original_file.read():
+                moved_file.write(text)
+        os.remove(command[0])
+    else:
+        os.rename(command[0], command[1])
