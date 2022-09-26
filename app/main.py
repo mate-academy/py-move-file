5cc0a2ed7b4737dbe99e6c8bc04dc655cfda96ae
Dmytro Kyrpan <d.kirpan@gmail.com>
2022-09-26 16:11:13 +0300

    solution v01

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str):
+    command_spl = command.replace(" ", "/")
+    elems = command_spl.split("/")
+
+    file = elems[1]
+    file_new = elems[-1]
+    destin_dir_str = "/".join(elems[2:-1])
+
+    os.makedirs(destin_dir_str)
+    shutil.move(file, os.path.join(destin_dir_str, file_new))
