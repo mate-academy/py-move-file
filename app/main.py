3719a337dacdcc901f3a23631201bcb4e97a85e4
Artem <Artemvyhovanec@gmail.com>
2022-09-26 09:46:45 +0300

    'renamed_arguments'

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ def move_file(command: str) -> None:
     command = command.split()
     command[2] = command[2].split("/")
     command.append(command[2][-1])
-    command[2] = "/".join(command[2][:-1])
+    current_file = command[1]
+    directory = "/".join(command[2][:-1])
+    new_file = command[-1]
 
-    if command[2] != '':
-        os.makedirs(command[2])
-        command[2] += '/'
-    with (open(command[1], "r") as old_file,
-          open(f"{command[2]}{command[3]}", "w") as new_file):
+    if directory != '':
+        os.makedirs(directory)
+        directory += '/'
+    with (open(current_file, "r") as old_file,
+          open(f"{directory}{new_file}", "w") as new_file):
         new_file.write(old_file.read())
-    os.remove(command[1])
+    os.remove(current_file)
