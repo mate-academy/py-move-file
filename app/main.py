3e59481e116ed0240fb5df6f1bead292391dea62
Stas Okhrym <s.okhrym@gmail.com>
2022-09-27 19:03:43 +0300

    Fixed solution with correct directories

--- app/main.py
+++ app/main.py
@@ -6,13 +6,13 @@ def move_file(command: str) -> None:
     file_name = command_parts[1]
     command_part_to_check = command_parts[2]
     if "/" in command_part_to_check:
-        file_path, file_name = command_part_to_check.rsplit("/", 1)
+        file_path, file_name_to_move = command_part_to_check.rsplit("/", 1)
         makedirs(file_path)
     else:
         new_file_name = command_parts[-1]
         rename(file_name, new_file_name)
         return
     with open(file_name, "r") as source, \
-            open(path.join(file_path, file_name), "w") as destination:
+            open(path.join(file_path, file_name_to_move), "w") as destination:
         destination.write(source.read())
         remove(file_name)
