b9867a51bac38c5ebee6749a56835266cf8054df
Stas Okhrym <s.okhrym@gmail.com>
2022-09-27 18:57:38 +0300

    Fixed slices in if/else conditions

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ from os import remove, rename, makedirs, path
 def move_file(command: str) -> None:
     command_parts = command.split()
     file_name = command_parts[1]
-    if "/" in command:
-        file_path, file_name = command.rsplit("/", 1)
+    command_part_to_check = command_parts[2]
+    if "/" in command_part_to_check:
+        file_path, file_name = command_part_to_check.rsplit("/", 1)
         makedirs(file_path)
     else:
         new_file_name = command_parts[-1]
