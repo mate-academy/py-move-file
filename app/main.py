3bda537b5f6058815c31ee0c29f3478affc38e9f
Serhii Kovalenko <27kovalenko@gmail.com>
2022-09-28 16:12:05 +0300

    Fixed error

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from os import remove, mkdir
 
 def move_file(command):
     command, file1, file2 = command.split()
-    if len(file2.split("/")) > 1:
+    if command == "mv" and len(file2.split("/")) > 1:
         for i in range(len(file2.split("/") - 1)):
             mkdir(file2.split("/")[i])
     with open(file1, "r") as f1, open(file2, "w") as f2:
