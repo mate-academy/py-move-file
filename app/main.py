4d611fb17254fded190e894c1074abb7917b14f2
shurushku <nastia.kuchina.741456963@gmail.com>
2022-09-28 18:05:21 +0300

    added path check

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ import os
 def move_file(command: str) -> None:
     mv, file_name_1, file_name_2 = command.split()
     if mv == "mv" and file_name_1 != file_name_2:
-        path = file_name_2[:file_name_2.rfind('/')]
-        os.makedirs(path)
+        if '/' in file_name_2:
+            path = file_name_2[:file_name_2.rfind('/')]
+            os.makedirs(path)
         with open(file_name_1, "r") as file_out, open(file_name_2,
                                                       "w") as file_in:
             file_in.write(file_out.read())
