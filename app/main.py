1594c281a92332cb253ac64e7a28f1e206b5eef8
Serhii Kovalenko <27kovalenko@gmail.com>
2022-09-28 14:19:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+from os import remove, mkdir
+
+
+def move_file(command):
+    command, file1, file2 = command.split()
+    if len(file2.split("/")) > 1:
+        for i in range(len(file2.split("/") - 1)):
+            mkdir(file2.split("/")[i])
+    with open(file1, "r") as f1, open(file2, "w") as f2:
+        f2.write(f1.read())
+
+    remove(file1)
