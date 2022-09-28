f39191ffac156356a7fec89bb56882f796ba6ce1
Eugene <buntyc8@gmail.com>
2022-09-28 18:45:35 +0300

    change solution

--- app/main.py
+++ app/main.py
@@ -5,8 +5,7 @@ def move_file(command):
     split_file = command.split(" ")
     if "/" in split_file[-1]:
         path = split_file[-1].split("/")
-        for i in range(len(path) - 1):
-            os.mkdir("/".join(path[:i]))
+        os.makedirs("/".join(path[:-1]))
     with open(split_file[1], "r") as file_out,\
             open(split_file[2], "w") as file_in:
         text = file_out.read()
