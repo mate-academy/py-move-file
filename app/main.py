50b206fe973742ce2b279e233213cfa67d1a799b
Taras Andruschenko <And999110@gmail.com>
2022-09-28 18:23:10 +0300

    Add variables for each argument and use os.mkdirs

--- app/main.py
+++ app/main.py
@@ -2,18 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    command = command.split()[1:]
-    if "/" not in command[1]:
-        os.rename(command[0], command[1])
+    old_path = command.split()[1]
+    new_path = command.split()[2]
+    if "/" not in new_path:
+        os.rename(old_path, new_path)
     else:
-        path = command[1].split('/')
-        true_path = ""
-        for i in range(len(path) - 1):
-            true_path += path[i]
-            os.mkdir(true_path)
-            true_path += "/"
-        with open(command[0], "r") as file_in, \
-                open(command[1], "w") as file_out:
+        path = new_path.split('/')
+        os.makedirs("/".join(path[:-1]))
+        with open(old_path, "r") as file_in, \
+                open(new_path, "w") as file_out:
             for line in file_in.read():
                 file_out.write(line)
-    os.remove(command[0])
+    os.remove(old_path)
