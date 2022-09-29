7add6185433d90a55a47b8e26120a2a80b9ccacc
igor kosianenko <funkcionalkos@gmail.com>
2022-09-29 17:03:01 +0300

    Solution)

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+
+    if len(os.path.dirname(command[2])) > 0:
+        os.makedirs(os.path.dirname(command[2]), exist_ok=True)
+        with open(command[2], "w") as f:
+            f.write("")
+        os.remove(os.path.dirname(command[2]) + "\\" + command[1])
+    else:
+        os.rename(command[1], command[2])
