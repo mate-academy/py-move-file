82de014a80ca90d1fbbddaf9627b1e95dc232993
Zakhar Podporinov <zp162162@gmail.com>
2022-10-01 21:48:36 +0300

    NewVersion

--- app/main.py
+++ app/main.py
@@ -2,21 +2,20 @@ import os
 
 
 def move_file(command: str) -> None:
-    operators = command.split()
-    if operators[0] != "mv":
-        print("Wrong command!")
+    command = command.split()
+    if command[0] != "mv":
         return None
-    filename = operators[1]
-    path = operators[2].split("/")
+
+    filename = command[1]
+    path = command[2].split("/")
     new_filename = path.pop()
-    started_dir = os.getcwd()
-    with open(filename, "r") as file, open(new_filename, "w") as new_file:
-        for directory in path:
-            try:
-                os.mkdir(directory)
-                os.chdir(directory)
-            except FileExistsError:
-                os.chdir(directory)
+    path = "/".join(path)
+
+    if path:
+        os.makedirs(path)
+
+    with open(filename, "r") as file, \
+            open(path + new_filename, "w") as new_file:
         new_file.write(file.read())
-    os.chdir(started_dir)
+
     os.remove(filename)
