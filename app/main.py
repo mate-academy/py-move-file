a1d227fe094cc07da2374f594250ce64d5d27d11
Andriy Makarenko <andriy@mate.academy>
2022-10-01 18:28:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from os import makedirs, remove
+
+
+def move_file(command: str):
+    command_parts = command.split()
+    source_file = command_parts[1]
+    second_file_path = command_parts[2]
+    destination_folder = second_file_path.rsplit("/", 1)[0]
+    makedirs(destination_folder)
+
+    with open(source_file, "r") as file:
+        source_data = file.read()
+
+    with open(second_file_path, "w") as copied_file:
+        copied_file.write(source_data)
+
+    remove(source_file)
