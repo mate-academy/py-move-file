22c850583d3fec6c9daeba1a7d6a58eb7104a926
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-10-01 15:30:00 +0300

    Fix condition

--- app/main.py
+++ app/main.py
@@ -3,16 +3,17 @@ import shutil
 
 
 def move_file(command: str) -> None:
-    if command.split()[0] == "mv" and not command.endswith("/"):
-        file_remove = command.split()[1]
-        new_file = command.split()[2].split("/")[-1]
-        file_path = command.split()[2].split("/")[:-1]
-        cur_dir = os.getcwd()
+    if not command.split()[0] == "mv" or command.endswith("/"):
+        return
+    file_remove = command.split()[1]
+    new_file = command.split()[2].split("/")[-1]
+    file_path = command.split()[2].split("/")[:-1]
+    cur_dir = os.getcwd()
 
-        for directory in file_path:
-            os.mkdir(directory)
-            os.chdir(directory)
-        new_dir = os.getcwd()
+    for directory in file_path:
+        os.mkdir(directory)
+        os.chdir(directory)
+    new_dir = os.getcwd()
 
-        shutil.move(src=f"{cur_dir}\\{file_remove}", dst=f"{new_dir}")
-        os.rename(f"{new_dir}\\{file_remove}", f"{new_dir}\\{new_file}")
+    shutil.move(src=f"{cur_dir}\\{file_remove}", dst=f"{new_dir}")
+    os.rename(f"{new_dir}\\{file_remove}", f"{new_dir}\\{new_file}")
