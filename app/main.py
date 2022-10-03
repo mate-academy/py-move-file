5486d98a8c4752deb15bdd8a63ae8234423b9eb4
Edik Marushchak <e.moscow138@gmail.com>
2022-10-03 14:06:31 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,19 +1,14 @@
-from os import mkdir, remove
+import os
 
 
 def move_file(command: str) -> None:
     command = command.split()
-    original_file_name = command[1]
-
-    with open(original_file_name, "r") as file:
-        data = file.read()
-
-    path = ''
-    for folder in command[2].split('/')[:-1]:
-        path += f"{folder}/"
-        mkdir(path)
-
-    with open(f"{command[-1]}", "w") as copied_file:
-        copied_file.write(data)
-
-    remove(original_file_name)
+    first_file = command[1]
+    second_file = command[2]
+    if len(os.path.dirname(second_file)) > 0:
+        os.makedirs(os.path.dirname(command[2]), exist_ok=True)
+        with open(second_file, "w") as f:
+            f.write("")
+        os.remove(os.path.dirname(second_file) + "\\" + first_file)
+    else:
+        os.rename(first_file, second_file)
