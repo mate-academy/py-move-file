b2548fc01d742862a2108e0579b6dcde4818c45a
graandda <graanddadays@gmail.com>
2022-10-03 14:20:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,18 +2,18 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_list = command.split()
-    cmd = command_list[0]
-    old_path = command_list[1]
-    new_path = "/".join(command_list[2].split("/")[:-1])
-    new_file_name = command_list[2].split("/")[-1]
+    command = command.split()
+    cmd = command[0]
+    old_path = command[1]
+    new_path = "/".join(command[2].split("/")[:-1])
+    new_file_name = command[2].split("/")[-1]
 
     if cmd != "mv" or old_path == new_path:
         return print("Wrong data")
-    else:
-        with open(old_path, "r") as file:
-            data = file.read()
-            os.makedirs(new_path)
-            with open(new_path + "/" + new_file_name, "w") as new_file:
-                new_file.write(data)
-        os.remove(old_path)
+
+    with open(old_path, "r") as source_file:
+        data = source_file.read()
+        os.makedirs(new_path)
+        with open(new_path + "/" + new_file_name, "w") as destination_file:
+            destination_file.write(data)
+    os.remove(old_path)
