d178f88783a81eabc29da7ecbead3305ac1be0db
Sergey <servizah@gmail.com>
2022-10-04 10:37:29 +0300

    quotes changed

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ import os
 
 def movie_file(command: str):
     command_list = command.split()
-    if command_list[0] != 'mv':
+    if command_list[0] != "mv":
         return
     file1 = command_list[1]
-    path = command_list[2].split('/')[:-1]
-    file2 = "".join(command_list[2].split('/')[-1:])
+    path = command_list[2].split("/")[:-1]
+    file2 = "".join(command_list[2].split("/")[-1:])
     if len(path) == 0:
         os.rename(file1, file2)
         return
@@ -16,6 +16,6 @@ def movie_file(command: str):
     else:
         os.makedirs("/".join(path))
     with open(file1, "r") as file_in:
-        with open(f"{'/'.join(path)}/{file2}", "w") as file_out:
+        with open(f"{"/".join(path)}/{file2}", "w") as file_out:
             file_out.writelines(file_in.readlines())
     os.remove(file1)
