fe190bd01c25b9af377de3bf3024ce79720429c0
Edik Marushchak <e.moscow138@gmail.com>
2022-10-04 21:13:39 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import os
 
 
-def move_file(command: str) -> None:
-    command = command.split()
-    first_file = command[1]
-    second_file = command[2]
-    if len(os.path.dirname(second_file)) > 0:
-        os.makedirs(os.path.dirname(command[2]), exist_ok=True)
-        with open(second_file, "w") as f:
-            f.write("")
-        os.remove(os.path.dirname(second_file) + "\\" + first_file)
+def move_file(text: str) -> None:
+    command, from_file, to_file = text.split()
+
+    if "/" in to_file:
+        directory, file_name = os.path.split(to_file)
+        directory += "/"
+        os.makedirs(directory)
     else:
-        os.rename(first_file, second_file)
+        os.rename(from_file, to_file)
+
+    if command == "mv" and from_file != to_file:
+        with open(from_file, "r") as file_in, open(to_file, "w") as to_file:
+            to_file.write(file_in.read())
+
+        os.remove(from_file)
