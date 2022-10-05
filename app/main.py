8519165ba6f11c2ef5c23cf139396894b5f6568a
Soroka Igor <copokaigor@gmail.com>
2022-10-05 09:56:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,25 @@
 import os
 
 
-def move_file(command):
-    old_file_name = command.split(" ")[1]
-    new_file_path = command.split(" ")[2]
-    if "/" not in new_file_path:
-        os.renames(old_file_name, new_file_path)
+def move_file(command: str):
+    cmd = command.split()
+    if cmd[0] != "mv":
+        return
+    old_file_name = cmd[1]
+    file_path = cmd[2].split("/")[:-1]
+    new_file_name = "".join(cmd[2].split("/")[-1:])
+    if len(file_path) == 0:
+        os.renames(old_file_name, new_file_name)
+        return
+    elif len(file_path) == 1:
+        os.mkdir(file_path[0])
     else:
-        dirc = new_file_path.split("/")
-        os.makedirs("/".join(dirc[:-1]))
-        with open(old_file_name, "r") as old_file,\
-                open(new_file_path, "w") as new_file:
-            for info in old_file.read():
-                new_file.write(info)
+        os.makedirs("/".join(file_path))
+        with open(old_file_name, "r") as old_file:
+            with open(f"{''.join(file_path)}"
+                      f"/{new_file_name}", "w") as new_file:
+                new_file.writelines(old_file.readlines())
     os.remove(old_file_name)
+
+
+print(move_file("mv file.txt first_dir/second_dir/new_file.txt"))
