ada443f1f997f2e74ad308ac8a6ca015df432d05
Oleksandr Mamchur <irex1988@gmail.com>
2022-10-05 16:08:27 +0300

    'fixed_mistakes'

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ import os
 
 def move_file(command):
     string = command.split(" ")
-    file1 = string[1]
-    list_dir = string[2].split("/")
-    path = ""
-    for i in range(len(list_dir) - 1):
-        path = os.path.join(path, list_dir[i])
-    if path:
-        os.makedirs(path)
     if string[0] == "mv":
-        with open(f"{file1}", "r") as file,\
+        file1 = string[1]
+        list_dir = string[2].split("/")
+        path = ""
+        for i in range(len(list_dir) - 1):
+            path = os.path.join(path, list_dir[i])
+        if path:
+            os.makedirs(path)
+        with open(f"{file1}", "r") as file, \
                 open(f"{string[2]}", "w") as file_copy:
             content = file.read()
             file_copy.write(content)
