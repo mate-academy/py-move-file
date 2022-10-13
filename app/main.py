07877d5c6b914a37d05c12a5e0f38a5d0cc4f024
Iurii Lobunets <iurii@lobunets.com.ua>
2022-10-13 14:57:13 +0300

    Solution of py-move-file (refactor)

--- app/main.py
+++ app/main.py
@@ -2,67 +2,67 @@ import os
 
 
 BUFFER_SIZE = 2048
+CHARACTER_SEPARATOR = "/"
+
+
+class FileInfo:
+    def __init__(self, file_name: str) -> None:
+        self.path = []
+        self.full_path = ""
+        self.path_only = False
+        self.filename = ""
+        self.full_filename = ""
+
+        if file_name[-1] == CHARACTER_SEPARATOR:
+            self.path = file_name[:-1].split(CHARACTER_SEPARATOR)
+            self.full_path = file_name.rstrip(CHARACTER_SEPARATOR)
+            self.path_only = True
+        else:
+            self.full_filename = file_name
+            temp = file_name.split(CHARACTER_SEPARATOR)
+            self.filename = temp[-1]
+            self.path = temp[:-1]
+            self.full_path = CHARACTER_SEPARATOR.join(self.path)
+
+    def path_add_filename(self, filename: str) -> str:
+        if self.path and self.path_only:
+            return CHARACTER_SEPARATOR.join((self.full_path, filename))
+        return self.full_filename
 
 
 def move_file(user_command: str) -> None:
-    character_separator = "/"
-
-    class FileInfo:
-        def __init__(self, file_name: str) -> None:
-            self.path = []
-            self.full_path = ""
-            self.path_only = False
-            self.filename = ""
-            self.full_filename = ""
-
-            if file_name[-1] == character_separator:
-                self.path = file_name[:-1].split(character_separator)
-                self.full_path = file_name.rstrip(character_separator)
-                self.path_only = True
-            else:
-                self.full_filename = file_name
-                temp = file_name.split(character_separator)
-                self.filename = temp[-1]
-                self.path = temp[:-1]
-                self.full_path = character_separator.join(self.path)
-
-        def path_add_filename(self, filename: str) -> str:
-            if self.path and self.path_only:
-                return character_separator.join((self.full_path, filename))
-            return self.full_filename
-
     command = user_command.split()
     if len(command) != 3 or \
             command[0].lower() != "mv" or \
             command[1] == command[2]:
         return
 
-    fileinfo1 = FileInfo(command[1])
-    if not os.path.isfile(fileinfo1.full_filename) \
-            or fileinfo1.path_only is True:
+    source = FileInfo(command[1])
+    if not os.path.isfile(source.full_filename) \
+            or source.path_only is True:
         return
 
-    fileinfo2 = FileInfo(command[2])
-    if fileinfo1.full_path == fileinfo2.full_path:
-        os.rename(fileinfo1.full_filename, fileinfo2.full_filename)
+    destination = FileInfo(command[2])
+    if source.full_path == destination.full_path:
+        os.rename(source.full_filename, destination.full_filename)
     else:
         path_traverse = ""
-        for path in fileinfo2.path:
+        for path in destination.path:
             path_traverse += \
-                (character_separator if path_traverse else "") + path
+                (CHARACTER_SEPARATOR if path_traverse else "") + path
             if not os.path.isdir(path_traverse):
                 os.mkdir(path_traverse)
 
-        full_filename2 = fileinfo2.path_add_filename(fileinfo1.filename)
-        with open(fileinfo1.full_filename, "rb") as src_obj, \
-                open(full_filename2, "wb") as dest_obj:
+        destination_filename = destination.path_add_filename(source.filename)
+        with open(source.full_filename, "rb") as src_obj, \
+                open(destination_filename, "wb") as dest_obj:
             buffer = src_obj.read(BUFFER_SIZE)
             while buffer:
                 dest_obj.write(buffer)
                 buffer = src_obj.read()
 
-        if os.path.isfile(full_filename2):
-            os.remove(fileinfo1.full_filename)
+        if os.path.isfile(destination_filename):
+            os.remove(source.full_filename)
 
 
 if __name__ == "__main__":
