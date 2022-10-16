977578215a77d29daf2134b3563b9c73c9c360d2
Nataliia <amirammina@gmail.com>
2022-10-16 21:53:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split(" ")
+
+    if command[0] != "mv":
+        raise NameError("Cannot recognize the command")
+
+    current_file = command[1]
+    path_to_file = command[2].split("/")
+    new_directory = "/".join(path_to_file[:-1])
+    os.makedirs(new_directory)
+
+    if path_to_file[-1] == "":
+        path_to_file.append(current_file)
+    path_to_file = "/".join(path_to_file)
+
+    with open(current_file, "r") as source, \
+            open(path_to_file, "a") as new_file:
+        info = source.read()
+        new_file.write(f"{info}")
+        os.remove(current_file)
