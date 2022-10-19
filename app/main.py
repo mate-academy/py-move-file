4759fb1bbbc4fd8ca3861246d1d4b047842de149
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-19 12:31:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    linux_mv = command.split()[0]
+    first_file_name = command.split()[1]
+    directory_file = command.split()[2]
+    if linux_mv == "mv":
+        if "/" in directory_file:
+            os.makedirs(directory_file)
+        else:
+            os.makedirs(first_file_name)
+            os.rename(first_file_name, directory_file)
