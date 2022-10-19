67debc518f23608c810906ab08997d8ecc893431
Taras Zelinskyi <taras.sv89@gmail.com>
2022-10-20 01:40:12 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    split_command = command.split()
+    path_elements = None
+    if "/" in split_command[2]:
+        path_elements = split_command[2].split("/")
+    path = ""
+    for element in path_elements:
+        if "." not in element:
+            path += element + "/"
+            os.mkdir(path)
+    with open(split_command[1], "r") as source,\
+            open(split_command[2], "w") as copy:
+        source_content = source.read()
+        copy.write(source_content)
+    os.remove(split_command[1])
