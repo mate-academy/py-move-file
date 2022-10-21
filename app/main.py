e6337b653013593004273e4b55e9c410ff32be2d
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-21 17:16:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import os
+import shutil
 
 
 def move_file(command: str) -> None:
     file_name = command.split()
     if file_name[0] == "mv":
         if "/" in file_name[2]:
-            os.makedirs(file_name[2])
+            second_file_name = "/".join(file_name[2].split("/")[-1:])
+            file_directory = file_name[2].replace(second_file_name, "")
+            os.makedirs(file_directory)
+            shutil.copy(file_name[1], file_name[2])
         else:
             os.makedirs(file_name[1])
             os.rename(file_name[1], file_name[2])
 
 
-move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt")
+move_file("mv file.txt file2.txt")
