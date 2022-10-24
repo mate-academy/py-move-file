ea78cb090c1f4470d122c75c2b6544b1c0a08b28
Anton Chumak <Chumak7377@gmail.com>
2022-10-25 00:34:29 +0300

    Solution Move file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import shutil
+import os
+
+
+def move_file(command: str) -> None:
+
+    command_list = command.split()
+
+    old_file = command_list[1]
+    new_file = command_list[2]
+    dest_new_file = "/".join(new_file.split("/")[:-1])
+
+    if not ("mv" in command_list[0]):
+        raise ValueError("Input command is not correct.")
+
+    if os.path.exists(old_file) and \
+            not ("/" in new_file):
+        os.rename(old_file, new_file)
+
+    os.mkdir(dest_new_file)
+    shutil.copy(old_file, new_file)
+    os.remove(old_file)
