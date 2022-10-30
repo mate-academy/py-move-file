8718a3f53f7361c34c5745df14230f4641f3f1e4
Denys Tereshchuk <terrya@ukr.net>
2022-10-30 19:15:16 +0400

    Solution

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def move_file(command: str) -> None:
     for i in range(len(path_list) - 1):
         path = os.path.join(path, path_list[i])
         os.mkdir(path)
-    with (open(command_list[1], "r") as source,
-          open(command_list[2], "w") as moved):
-        moved.write(source.read())
+    with (open(command_list[1], "r") as source_file,
+          open(command_list[2], "w") as moved_file):
+        moved_file.write(source_file.read())
     os.remove(command_list[1])
