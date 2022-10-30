cfd55fe951cd3dd2e71e30916d8c70ff322f1f30
Dede <specialized8393@gmail.com>
2022-10-30 22:27:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    # That will move a file from one location to another
+    # Ex: mv file.txt first_dir/second_dir/third_dir/file2.txt
+    if "mv" not in command:
+        print("Command mv not found")
+        return None
+    dir_path = ""
+    file_in = command.split(" ")[1]
+    file_out = command.split(" ")[2]
+    if "/" in file_out:
+        dir_ls = file_out.split("/")
+        for i in range(len(dir_ls) - 1):
+            dir_path += dir_ls[i] + "/"
+            os.mkdir(dir_path)
+    with open(file_in, "r") as f_in, open(file_out, "w") as f_out:
+        f_out.write(f_in.read())
+    if os.path.exists(file_in):
+        os.remove(file_in)
