0d49e3dca068e8046edd71b81c10b42e3a047821
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-01 15:02:02 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,24 +4,20 @@ import os
 def move_file(command: str) -> None:
     list_command = command.split(" ")
 
-    first_file = list_command[1]
+    first_filename = list_command[1]
 
     mv_file_path = list_command[2].split("/")
 
     second_filename = ""
+    for name in mv_file_path:
+        if ".txt" in name:
+            second_filename += name
 
-    for index in range(len(mv_file_path)):
-        if ".txt" in mv_file_path[index]:
-            second_filename += mv_file_path[index]
-            mv_file_path.pop(index)
+    new = "/".join(mv_file_path[:-1])
 
-    with open(first_file, "r"), open(second_filename, "w"):
-        path1 = ""
-        for path in mv_file_path:
-            path1 += path + "/"
-            os.mkdir(path1)
+    if not os.path.exists(new):
+        os.makedirs(new)
 
-        os.rename(first_file, path1 + second_filename)
+    new_file_path = f"{new}/{second_filename}"
 
-        if len(path1) != 0:
-            os.remove(second_filename)
+    os.rename(first_filename, new_file_path)
