08ff2bfd7cc653d710dac7d1814725e09c200632
Dede <specialized8393@gmail.com>
2022-11-01 22:34:11 +0200

    use docstring and added срÑcheck dir

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ import os
 
 
 def move_file(command: str) -> None:
-    # That will move a file from one location to another
+    """ That will move a file from one location to another """
     # Ex: mv file.txt first_dir/second_dir/third_dir/file2.txt
     if "mv" not in command:
         print("Command mv not found")
-        return None
+        return
     dir_path = ""
     file_in = command.split(" ")[1]
     file_out = command.split(" ")[2]
@@ -14,8 +14,12 @@ def move_file(command: str) -> None:
         dir_ls = file_out.split("/")
         for i in range(len(dir_ls) - 1):
             dir_path += dir_ls[i] + "/"
-            os.mkdir(dir_path)
+            if not os.path.exists(dir_path):
+                os.mkdir(dir_path)
     with open(file_in, "r") as f_in, open(file_out, "w") as f_out:
         f_out.write(f_in.read())
     if os.path.exists(file_in):
         os.remove(file_in)
+
+
+move_file("mv file.txt first_dir/second_dir/file2.txt")
