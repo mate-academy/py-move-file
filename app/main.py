2968ad4ba4f2b613f99aa44bea237fc17c2f3638
Your Name <y>
2022-11-10 14:49:36 +0200

    Solution added

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_list = command.split()
+
+    if command_list[0] != "mv":
+        raise SyntaxError("Unknown command!")
+
+    with open(command_list[1], "r") as old_file:
+        info = old_file.read()
+    os.remove(command_list[1])
+
+    path = command_list[2].split("/")
+
+    for folder in path[:-1]:
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        os.chdir(folder)
+
+    with open(path[-1], "w") as new_file:
+        new_file.write(info)
