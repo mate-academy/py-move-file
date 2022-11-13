68300fd6b697b3eecdc195ebdef2600ac593641b
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-11-13 15:42:36 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 import os
 
 
+def copy_file(filename: str, new_file: str) -> None:
+    with open(filename, "r") as file_in,\
+            open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
+
+
 def move_file(command: str) -> None:
-    split_command = command.split()
-    elements = None
-    if "/" in split_command[2]:
-        elements = split_command[2].split("/")
-    path = ""
-    for element in elements:
-        if "." not in element:
-            path += element + "/"
-            os.mkdir(path)
+    if "mv" in command:
+        filename = command.split()[1]
+        path = command.split()[2].split("/")[:-1]
+        new_filename = command.split()[2]
+        directory = ""
+        for i in range(len(path)):
+            directory = os.path.join(directory, path[i])
+            if not os.path.isfile(directory):
+                os.mkdir(directory)
+        copy_file(filename, new_filename)
+        os.remove(filename)
