9398de21dc4df7fe492ce323fd4c0066c91f196b
Igor <igornev83@gmail.com>
2022-11-13 18:11:22 +0200

    Need review

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from os import makedirs, remove, path, rename
+
+
+def move_file(command: str) -> None:
+    source_data = command.split()
+    paths = source_data[2].split("/")
+    new_path = "/".join(paths[:-1])
+    if "/" in source_data[2]:
+        makedirs(new_path)
+        with open(source_data[1], "w+") as file_in, \
+             open(path.join(new_path, paths[-1]), "w") as file_out:
+            save_data = file_in.read()
+            file_out.write(save_data)
+        remove(source_data[1])
+    else:
+        with open(source_data[1], "w+") as file_in:
+            pass
+        rename(source_data[1], paths[-1])
