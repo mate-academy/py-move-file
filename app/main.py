7aed3cf2a19112ea03f1cbf681b81bc5c9f14468
soobig666 <darkvader.ukr.net@gmail.com>
2022-11-26 15:00:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+from os import makedirs, remove
+
+
+def move_file(command: str) -> None:
+    list_with_info = command.split(" ")
+    file_in_name = list_with_info[1]
+    dirs = list_with_info[2].split("/")
+
+    if list_with_info[0] != "mv":
+        return
+    try:
+        makedirs(f"{dirs[0]}/{dirs[1]}/{dirs[2]}")
+    except OSError:
+        pass
+
+    with (
+        open(file_in_name, "r") as file_in,
+        open(list_with_info[-1], "w") as file_out
+    ):
+        file_out.write(file_in.read())
+        file_in.close()
+        file_out.close()
+        remove(file_in_name)
