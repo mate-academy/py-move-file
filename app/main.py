b0525cfe69cfa7fb1f788a129cfba7a31520da03
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-11-26 12:44:33 +0100

    Solution after changes

--- app/main.py
+++ app/main.py
@@ -2,21 +2,22 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_list = command.split()
-    command_name = command_list[0]
-    file_to_remove = command_list[1]
-    new_file = command_list[2]
+
+    command_name, file_to_remove, new_file = \
+        command.split()[0], \
+        command.split()[1], \
+        command.split()[2]
 
     if command_name == "mv":
 
-        if "/" not in command_list[2] and file_to_remove != new_file:
+        if "/" not in new_file and file_to_remove != new_file:
             os.rename(file_to_remove, new_file)
 
         else:
-            new_file_directories = command_list[2].split("/")[:-1]
+            new_file_directories = new_file.split("/")[:-1]
             creating_path = "/".join(new_file_directories)
             os.makedirs(creating_path)
-            new_file_path = "/".join(command_list[2].split("/"))
+            new_file_path = "/".join(new_file.split("/"))
 
             with open(file_to_remove, "r") as file_out,\
                     open(new_file_path, "w") as file_in:
