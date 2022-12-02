8f1f8ddac70a47a337cbcff0ff702e1ffe13f779
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-12-02 16:48:44 +0800

    Solution Maksym Maksimov

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    new_list = command.split()
+    file_name = new_list[1]
+    copy_name = new_list[2]
+
+    try:
+        copy_name.index("/")
+    except ValueError:
+        os.rename(file_name, copy_name)
+
+    path_keys = copy_name.split("/")
+    path = ""
+
+    for i in range(len(path_keys) - 1):
+        os.mkdir(path + path_keys[i])
+        path += path_keys[i] + "/"
+
+    file_path = path + path_keys[-1]
+
+    with open(file_name, "r") as file, open(path_keys[-1], "w") as copy:
+        copy.write(file.read())
+
+    os.replace(path_keys[-1], file_path)
+    os.remove(file_name)
