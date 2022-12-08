4be5547f80f4028e56a9f88e202c30ce051911f8
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2022-12-08 11:43:25 +0200

    'upd_solution'

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import shutil
 
 def move_file(command: str) -> None:
 
-    if "/" in command:
+    if "/" in command and command.split()[0] == "mv":
         list_command = command.split()
         list_path_to = command.split()[2].split("/")
 
@@ -14,7 +14,12 @@ def move_file(command: str) -> None:
             if upd_path_str[-4::] != "txt/":
                 os.mkdir(upd_path_str)
 
-        with open(list_command[1], "r"), open(list_command[2], "a"):
-            shutil.copyfile(list_command[1], list_command[-1])
+        with (open(list_command[1], "r") as file_in,
+              open(list_command[2], "a") as file_out):
+            shutil.copyfile(file_in.name, file_out.name)
 
-        os.remove(list_command[1])
+        os.remove(file_in.name)
+
+    if "/" not in command and len(command.split()) == 3\
+            and command.split()[0] == "mv":
+        os.rename(command.split()[1], command.split()[2])
