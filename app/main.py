9316162e7d1fb75f42f06f7853dfca99590f22d6
Artem Motrunich <motrunich2013@gmail.com>
2022-12-12 08:51:57 +0100

    Solution_3

--- app/main.py
+++ app/main.py
@@ -2,16 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    split_command = command.split()
-    old_filename = split_command[1]
-    directories = split_command[2].split("/")
-    new_filename = directories[-1]
-    with open(old_filename) as old_file:
-        reader = old_file.read()
-    os.remove(old_filename)
-    for direct_name in directories:
-        if direct_name.endswith("_dir"):
-            os.mkdir(direct_name)
-            os.chdir(f"{os.getcwd()}/{direct_name}")
-    with open(new_filename, "w") as new_file:
-        new_file.write(reader)
+    command_line_split = command.split()
+    if command_line_split[0] == "mv":
+        direction_path = command_line_split[2].split("/")[:-1]
+        new_path = ""
+        for directory in direction_path:
+            os.mkdir(f"{new_path}{str(directory)}")
+            new_path += str(directory) + "/"
+        with open(f"{command_line_split[1]}", "r") as file_in,\
+                open(f"{command_line_split[2]}", "w") as file_out:
+            file_out.write(file_in.read())
+        os.remove(command_line_split[1])
