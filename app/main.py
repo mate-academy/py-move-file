bc45664dc15618c850f15e34035f5a2bb05f3594
MIlan <milan.manoylov@gmail.com>
2022-12-19 13:51:35 -0600

    py-move-file

--- app/main.py
+++ app/main.py
@@ -3,17 +3,23 @@ import os
 
 def move_file(command: str) -> None:
 
-    sourse = command.split()[1]
+    source = command.split()[1]
     destination = command.split()[2]
 
-    if sourse.endswith("/") or destination.endswith("/"):
+    if source.endswith("/") or destination.endswith("/"):
         return
     files = destination.split("/")[:-1]
 
+    path_line = ""
     for direction in files:
-        os.mkdir("" + direction)
+        os.mkdir(path_line + direction)
+        path_line += direction + "/"
 
-    with open(sourse, "r") as file_in, open(destination, "w") as file_out:
+    with open(source, "r") as file_in, \
+            open(destination, "w") as file_out:
         file_out.write(file_in.read())
 
-    os.remove(sourse)
+    os.remove(source)
+
+
+move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt")
