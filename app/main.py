595835ed8c9440aa8566615a8b4f782311afb877
Oleksandr Panasiuk <provizor.307@gmail.com>
2022-12-20 14:10:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ with open("file.txt", "w") as f:
 
 
 def move_file(command: str) -> None:
-    file_split = command.split(" ")
-    if "mv" in file_split[0]:
-        directory = file_split[2].split("/")
+    f_split = command.split(" ")
+    if "mv" in f_split[0]:
+        directory = f_split[2].split("/")
         directories = ""
         if len(directory) > 1:
             for i in range(len(directory) - 1):
                 directories += directory[i] + "/"
                 os.mkdir(directories)
-        with open(file_split[1], "r") as file_in, open(file_split[2], "w") as file_out:
-            for line in file_in:
-                file_out.write(line)
-            os.remove(file_split[1])
+        with open(f_split[1], "r") as f_in, open(f_split[2], "w") as f_out:
+            for line in f_in:
+                f_out.write(line)
+            os.remove(f_split[1])
