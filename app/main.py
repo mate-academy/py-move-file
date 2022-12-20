06d46ca145808197077ffe7766fd654a1cdee314
Kostyantyn Chernyk <chernykko@gmail.com>
2022-12-20 13:09:10 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mv_command, from_file, destination_location = command.split()
+
+    if mv_command != "mv":
+        raise Exception('Command should start with "mv"!')
+
+    *directories, to_file = destination_location.split("/")
+    directory_path = ""
+
+    for directory in directories:
+        directory_path = os.path.join(directory_path, directory)
+        os.mkdir(directory_path)
+
+    with open(from_file, "r") as original, \
+            open(os.path.join(directory_path, to_file), "w") as destination:
+        destination.writelines(original)
+
+    os.remove(os.path.join(from_file))
