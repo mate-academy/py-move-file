f6ccc7f733b0a11acd6dbb8f7f7473e7461f9ef6
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2022-12-21 22:05:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str):
+    operation, filename, dir_file = command.split()
+    dir_file_split = dir_file.split("/")
+    if operation != "mv":
+        return
+    if filename == dir_file:
+        return
+    file_path = ""
+    for path in dir_file_split[:-1]:
+        file_path = os.path.join(file_path, path)
+    # file_path = os.path.relpath("\\".join(dir_file_split[:-1]))
+    os.mkdir(file_path)
+    with open(filename, "r") as source, open(dir_file_split[-1], "w") as outcome:
+        outcome.write(source.read())
+        os.remove(filename)
