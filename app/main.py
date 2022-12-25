6242a56176e994f29f3a4bb8af50b6a63a5c9e94
Vitalii Panochko <vpapplic@gmail.com>
2022-12-25 17:57:02 +0200

    redone move_file

--- app/main.py
+++ app/main.py
@@ -2,10 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_list = command.split()
-    os.mkdir(command_list[2])
+    command_list = command.split(" ")
     directory_list = command_list[2].split("/")
+    if len(directory_list) != 1:
+        if ".txt" in directory_list[-1]:
+            os.mkdir("/".join(directory_list[:-1]))
+        else:
+            os.mkdir(command_list[2])
+            command_list[2] += command_list[1]
     with open(command_list[1], "r") as file, \
-            open(directory_list[-1], "w") as new_file:
+            open(command_list[2], "w") as new_file:
         new_file.write(file.read())
     os.remove(command_list[1])
