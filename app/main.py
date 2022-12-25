ee0b38651e028630467ced069f4d5512a6a85f4f
Vitalii Panochko <vpapplic@gmail.com>
2022-12-25 17:26:07 +0200

    move_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_list = command.split()
+    os.mkdir(command_list[2])
+    directory_list = command_list[2].split("/")
+    with open(command_list[1], "r") as file, \
+            open(directory_list[-1], "w") as new_file:
+        new_file.write(file.read())
+    os.remove(command_list[1])
