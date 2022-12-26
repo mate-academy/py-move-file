88b687f2b3f3b5a4e49c3c5b6309bfb33268f59c
Oleksandr Panasiuk <provizor.307@gmail.com>
2022-12-26 16:55:44 +0200

    Slicing

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def move_file(command: str) -> None:
         directory = new_file.split("/")
         parent = ""
         if len(directory) > 1:
-            for i in range(len(directory) - 1):
-                parent = os.path.join(parent, directory[i])
+            for path in directory[:-1]:
+                parent = os.path.join(parent, path)
                 os.mkdir(parent)
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             for line in file_in:
