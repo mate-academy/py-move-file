7e9779d82453051fd574945f772613eca1486261
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-27 22:15:19 +0200

    fixed_unpaking

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    files = command.split()
-    with open(files[1], "r") as old_file:
-        if files[0] == "mv":
-            move_to = files[2].split("/")
+    command_type, source, destination = command.split()
+    with open(source, "r") as old_file:
+        if command_type == "mv":
+            move_to = destination.split("/")
             path = ""
             for folder in move_to:
                 if folder != move_to[-1]:
                     path = os.path.join(path, folder)
                     os.mkdir(path)
-            with open(files[2], "w") as new_file:
+            with open(destination, "w") as new_file:
                 new_file.write(old_file.read())
-                os.remove(files[1])
+                os.remove(source)
