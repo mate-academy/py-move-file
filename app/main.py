88474e629abcfd636e0b3cdcbb5faf6d2eee5533
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2022-12-29 19:59:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+import os
+
+
+def movie_file(command: str) -> None:
+    _, old_file, new_file = command.split()
+    os.mkdir(os.path.dirname(new_file))
+    with open(old_file, "r") as old_file, open(new_file, "w") as new_file:
+        new_file.write(old_file.read())
