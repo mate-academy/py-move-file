e463a8cfaedbd08812b484b16600b62c99121b49
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2022-12-30 23:30:01 +0200

    Solution_3

--- app/main.py
+++ app/main.py
@@ -5,4 +5,3 @@ def move_file(command: str) -> None:
     mv, old_file_path, new_file_path = command.split()
     if mv == "mv" and not os.path.exists(new_file_path):
         os.renames(old_file_path, new_file_path)
-        
