5ef8591b64b32b77000002e3b730a9a1928719af
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2022-12-30 14:08:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,14 +2,23 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_split = command.split()
-    if command_split[0] == "mv":
-        new_path = ""
-        directory = command_split[2].split("/")[:-1]
-        for direct in directory:
-            os.mkdir(f"{new_path}{str(direct)}")
-            new_path += str(direct) + "/"
-        with open(f"{command_split[1]}", "r") as file_in, \
-                open(f"{command_split[2]}", "w") as file_out:
-            file_out.write(file_in.read())
-        os.remove(command_split[1])
+    action, old_file, road_to_new_file = command.split()
+    if action == "mv":
+        raise Exception("Wrong command")
+
+    start_directory = os.getcwd()
+
+    to_file, new_file = road_to_new_file.split("/")
+    new_path = ""
+
+    for direction in to_file:
+        new_path = os.path.join(new_file, direction)
+        os.mkdir(new_path)
+    os.chdir(new_path)
+
+    with open(old_file, "r") as file_to_remove, \
+            open(new_file, "a") as file_to_add:
+        for line in file_to_remove:
+            file_to_add.write(line)
+        os.chdir(start_directory)
+        os.remove(old_file)
