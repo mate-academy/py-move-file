bdab66e106916ce2317e2dd22471992043aff43f
Oleksandr Tsvirko <oleksandr.tsvirko@gmail.com>
2023-01-01 17:04:33 +0100

    solution2

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def move_file(command: str) -> None:
     primary_path = os.getcwd()
     new_path = "/".join(split_path[:-1:])
 
-    with open(file_name, "r") as prim_file:
+    with open(file_name, "r") as file:
         os.makedirs(new_path)
         os.chdir(new_path)
         with open(new_file_name, "w") as new_file:
-            new_file.write(prim_file.read())
+            new_file.write(file.read())
 
     os.chdir(primary_path)
     os.remove(file_name)
