bc6bcabb82a3e3db17cd4145bb2f6553009d1a9d
Anna Kabatova <kabatova.anna@ukr.net>
2023-01-01 19:50:37 +0200

    2nd try solution (need help)

--- app/main.py
+++ app/main.py
@@ -2,23 +2,19 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_parts = command.split()
-    file_old = command_parts[1]
-    if "/" in command_parts[2]:
-        directories_list = command_parts[2].split("/")
-        file_new = directories_list[-1]
-        directories_list = directories_list[0:-1]
-        directories = "/".join(directories_list)
-        directories += "/"
-        os.makedirs(directories)
+    cmd, file_old, file_new = command.split()
+    if cmd == "mv":
+        if "/" in file_new:
+            directories_list = file_new.split("/")
+            file_new = directories_list[-1]
+            directories_list = directories_list[0:-1]
+            directories = "/".join(directories_list) + "/"
+            os.makedirs(directories)
         with open(file_old, "r") as file_in, open(file_new, "w") as file_out:
             file_out.write(file_in.read())
             file_in.close()
             os.remove(f"{file_in}")
-            os.replace(f"{file_out}", f"{command_parts[2]}")
+            if directories in locals():
+                os.replace(f"{file_out}", f"{directories}")
     else:
-        file_new = command_parts[2]
-        with open(file_old, "r") as file_in, open(file_new, "w") as file_out:
-            file_out.write(file_in.read())
-            file_in.close()
-            os.remove(f"{file_in}")
+        raise ValueError
