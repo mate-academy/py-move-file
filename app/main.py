bebb508be92eb4ab10dd464d1c0ea4e2c62ca31b
Tania Kharchuk <tkharchuk14@gmail.com>
2023-01-04 16:37:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_split = command.split()
+    if command_split[0] == "mv" and "/" in command_split[2]:
+        directory = command_split[2].split("/")
+        current_directory = ""
+        for subdirectory in directory:
+            current_directory = os.path.join(subdirectory, current_directory)
+            os.mkdir(current_directory)
+        with open(command_split[1], "r") as source_file, \
+                open(current_directory, "w") as new_file:
+            new_file.write(source_file.read())
+        os.remove(command_split[1])
+    elif command_split[0] == "mv" and len(command_split) == 3:
+        os.rename(command_split[1], command_split[2])
