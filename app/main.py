f13023c9c0ce62136f41d58fe7698b3cae0713db
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-07 11:39:31 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    path_list = command.replace("/", " ").split(" ")
+    path = ""
+
+    for num_dir in range(2, len(path_list) - 1):
+        path += f"{path_list[num_dir]}\\"
+        os.mkdir(path)
+    path += path_list[-1]
+    with open(path_list[1], "r") as file_read:
+        info_from_file = file_read.read()
+    with open(path, "w") as file_write:
+        file_write.write(info_from_file)
+    os.remove(path_list[1])
