a6e725f46d72edf84a0c4eea2c096c2ee72ea0b1
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-08 23:31:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,11 +6,16 @@ def move_file(command: str) -> None:
     path = ""
 
     for num_dir in range(2, len(path_list) - 1):
-        path += f"{path_list[num_dir]}\\"
-        os.mkdir(path)
-    path += path_list[-1]
-    with open(path_list[1], "r") as file_read:
+
+        path = os.path.join(path, path_list[num_dir])
+        try:
+            os.mkdir(path)
+        except Exception:
+            pass
+    path = os.path.join(path, path_list[-1])
+
+    with open(path_list[1], "r") as file_read, open(path, "w") as file_write:
         info_from_file = file_read.read()
-    with open(path, "w") as file_write:
         file_write.write(info_from_file)
+
     os.remove(path_list[1])
