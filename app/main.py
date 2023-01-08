df2df4c4b56c8e7b51813f6f283d4fd8d0d6d1ae
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-01-08 21:34:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+# mv file.txt some_dir/new_file.txt
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+
+    if command[0] != "mv":
+        raise NameError("The command does not exists")
+
+    our_file = command[1]
+    list_to_join = command[2].split("/")
+    new_dir = "/".join(list_to_join[:-1])
+    os.makedirs(new_dir)
+    path_to_file = "/".join(list_to_join)
+
+    with open(our_file, "r") as source_file, open(path_to_file, "a") as result_file:
+        result_file.writelines(source_file.readlines())
+    os.remove(our_file)
