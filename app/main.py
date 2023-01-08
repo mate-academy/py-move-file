aa00db4961a8f84785ed767e328d9bc502215da5
Artem Motrunich <motrunich2013@gmail.com>
2023-01-08 19:20:18 +0100

    Added case when dir already exists.

--- app/main.py
+++ app/main.py
@@ -2,14 +2,19 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_line_split = command.split()
-    if command_line_split[0] == "mv":
-        direction_path = command_line_split[2].split("/")[:-1]
-        new_path = ""
-        for directory in direction_path:
-            os.mkdir(f"{new_path}{str(directory)}")
-            new_path += str(directory) + "/"
-        with open(f"{command_line_split[1]}", "r") as file_in,\
-                open(f"{command_line_split[2]}", "w") as file_out:
-            file_out.write(file_in.read())
-        os.remove(command_line_split[1])
+    command, file, new_file = command.split()
+    if command == "mv":
+        full_directory = new_file.split("/")
+        new_filename = full_directory[-1]
+        with open(file, "r") as old_file:
+            reader = old_file.read()
+            os.remove(file)
+            if len(full_directory) != 1:
+                for directory in full_directory[:-1]:
+                    if os.path.isdir(directory):
+                        os.chdir(directory)
+                    else:
+                        os.mkdir(directory)
+                        os.chdir(directory)
+            with open(new_filename, "w") as new_file:
+                new_file.write(reader)
