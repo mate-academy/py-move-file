e0df30fd54d4cfd2d5c8d48bfc46466426ec65f1
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-09 11:33:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,19 +3,16 @@ import os
 
 def move_file(command: str) -> None:
     name_command, original_file, new_path_and_file = command.split()
-    path_list = new_path_and_file.replace("/", " ").split("")
+    path_list = new_path_and_file.split("/")
     path = ""
 
     for num_dir in range(len(path_list) - 1):
         path = os.path.join(path, path_list[num_dir])
-        try:
-            os.mkdir(path)
-        except Exception:
-            pass
+        os.makedirs(path)
+
     path = os.path.join(path, path_list[-1])
 
     with open(original_file, "r") as file_read, open(path, "w") as file_write:
-        info_from_file = file_read.read()
-        file_write.write(info_from_file)
+        file_write.write(file_read.read())
 
     os.remove(original_file)
