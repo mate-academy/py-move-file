8119a51f1a261f395601cc38d8fd0e928adaac29
6aPa6aH <6apa6ah666@gmail.com>
2023-01-09 09:36:33 +0200

    fix_code

--- app/main.py
+++ app/main.py
@@ -2,15 +2,12 @@ import os
 
 
 def move_file(command: str) -> None:
-    comm = command.split()
-    new_dir = comm[2][:comm[2].rfind("/")]
-    initial_file = comm[1]
-    new_file = comm[2][comm[2].rfind("/"):]
-    if comm[0] == "mv" and comm[1] != comm[2]:
-        os.makedirs(new_dir)
+    command, source_path, destination_path = command.split()
+    if command == "mv" and source_path != destination_path:
+        os.makedirs(os.path.dirname(destination_path))
         with (
-            open(f"{initial_file}", "r") as file_in,
-            open(f"{new_dir+new_file}", "w") as file_out
+            open(f"{source_path}", "r") as file_in,
+            open(f"{destination_path}", "w") as file_out
         ):
             file_out.write(file_in.read())
-            os.remove(initial_file)
+            os.remove(source_path)
