daf5a87b81550240061c746488c8cfd1b0063f8b
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-01-10 20:20:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,22 +1,20 @@
 import os
 
 
-# mv file.txt some_dir/new_file.txt
-
-
 def move_file(command: str) -> None:
-    command = command.split()
+    cmd, our_file, new_file = command.split()
 
-    if command[0] != "mv":
+    if cmd != "mv":
         raise NameError("The command does not exists")
 
-    our_file = command[1]
-    list_to_join = command[2].split("/")
-    new_dir = "/".join(list_to_join[:-1])
-    os.makedirs(new_dir)
-    path_to_file = "/".join(list_to_join)
+    if our_file != new_file:
+        try:
+            os.makedirs(os.path.dirname(new_file))
+        except FileNotFoundError:
+            print("File simply renamed")
+        with open(f"{our_file}", "r") as file_in, open(f"{new_file}", "w") as file_out:
+            file_out.writelines(file_in.readlines())
+        os.remove(our_file)
 
-    with open(our_file, "r") as source_file, \
-            open(path_to_file, "a") as result_file:
-        result_file.writelines(source_file.readlines())
-    os.remove(our_file)
+
+move_file("mv file.txt file2.txt")
