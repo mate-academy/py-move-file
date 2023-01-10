e3dcd844efe9ec64a1a05cf4006db79ce837a984
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-01-10 20:20:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -12,9 +12,7 @@ def move_file(command: str) -> None:
             os.makedirs(os.path.dirname(new_file))
         except FileNotFoundError:
             print("File simply renamed")
-        with open(f"{our_file}", "r") as file_in, open(f"{new_file}", "w") as file_out:
+        with open(f"{our_file}", "r") as file_in, \
+                open(f"{new_file}", "w") as file_out:
             file_out.writelines(file_in.readlines())
         os.remove(our_file)
-
-
-move_file("mv file.txt file2.txt")
