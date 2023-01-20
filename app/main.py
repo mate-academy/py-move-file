9ede5a7074da77d28f3bee0f528d50840f3a58a7
Alina Boichenko <ilub1108@gmail.com>
2023-01-20 22:49:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    if len(command.split()) == 3:
+        command_name, file_name, destination_path = command.split()
+
+        if "/" in destination_path:
+            os.makedirs(os.path.dirname(destination_path))
+
+        with (
+            open(f"{file_name}", "r") as file_in,
+            open(f"{destination_path}", "w") as file_out
+        ):
+            file_out.write(file_in.read())
+            os.remove(file_name)
