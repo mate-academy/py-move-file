c2b4ba29a50b4b4b57afe26d7eb5fcec7e7f29b1
Vitalii Khmura <vitalikkhmura13@gmail.com>
2023-01-20 21:09:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mv, file_in, file_out = command.split(" ")
+    if mv != "mv":
+        return
+    os.makedirs("/".join(file_out.split("/")[:-1]))
+    if file_out.split("/")[-1] == "":
+        raise FileNotFoundError
+    with open(file_in, "r") as source, \
+            open(file_out, "w") as new_file:
+        new_file.write(source.read())
+    os.remove(file_in)
