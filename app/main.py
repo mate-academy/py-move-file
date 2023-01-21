b40f903418e7771a1d1c643aa3f0f34c8a6b304b
Yehor Kornev <dmekkindom00@gmail.com>
2023-01-21 20:27:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,46 +1,37 @@
 from __future__ import annotations
-from typing import Any
 import os
 
 
-class ClearFile:
-    def __init__(self, filename: str) -> None:
-        self.filename = filename
-
-    def __enter__(self) -> ClearFile:
-        return self
-
-    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
-        os.remove(self.filename)
-
-
-def move_clear_file(command: str, file_path1: str, file_path2: str) -> None:
-    if command == "mv":
-        try:
-            with ClearFile(file_path1):
-                with (
-                    open(file_path1, "r") as file_in,
-                    open(file_path2, "w") as file_out
-                ):
-                    file_out.write(file_in.read())
-        except FileNotFoundError:
-            print(f"FileNotFoundError:"
-                  f" [WinError 2] Cannot find specified file: '{file_path1}'")
+def move_clear_file(file_path1: str, file_path2: str) -> None:
+    with (
+        open(file_path1, "r") as file_in,
+        open(file_path2, "w") as file_out
+    ):
+        file_out.write(file_in.read())
+        file_in.close()
+        os.remove(file_path1)
 
 
 def move_file(command: str) -> None:
+    if len(command.split()) != 3:
+        raise ValueError("This is not valid")
     command, file_path_name1, file_path_name2 = command.split()
+    if command != "mv":
+        raise ValueError(
+            f'"{command}" This is an invalid command!'
+            f' The correct one is: "mv"'
+        )
+
     path = os.path.join(file_path_name2)
-    path_catalog = path.split("/")
-    if path[-1] != "/":
-        creating_dir = []
-        for current_dir in path_catalog[:-1]:
-            creating_dir.append(current_dir)
-            if not os.path.isdir("/".join(creating_dir)):
-                os.mkdir("/".join(creating_dir))
+    head_path, tail_path = os.path.split(path)
 
-        if os.path.isdir("/".join(path_catalog[:-1])):
-            move_clear_file(command, file_path_name1, file_path_name2)
+    if not head_path and tail_path:
+        move_clear_file(file_path_name1, file_path_name2)
 
-    if len(path.split("/")) == 1:
-        move_clear_file(command, file_path_name1, file_path_name2)
+    if head_path and tail_path:
+        current_path = ""
+        for current_dir in head_path.split("/"):
+            current_path = os.path.join(current_path, current_dir)
+            if not os.path.isdir(current_path):
+                os.mkdir(current_path)
+        move_clear_file(file_path_name1, file_path_name2)
