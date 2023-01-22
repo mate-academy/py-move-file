6e29d99bcf46f22f9e027f8e35ba521b9336a268
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-22 16:45:16 +0100

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -2,16 +2,20 @@ import os
 
 
 def move_file(command: str) -> None:
-    files_info = command[3:].split(" ")
-    first_file = files_info[0]
-    second_file_info = files_info[1].split("/")
-    if len(second_file_info) == 1:
-        os.rename(first_file, second_file_info[0])
+    try:
+        mv, current_file, copied_file_path = command.split()
+    except ValueError:
+        raise ValueError("Command is incorrect")
+    if mv != "mv" or os.path.exists(current_file) is False:
+        raise ValueError("Command is incorrect")
+    copied_file_info = copied_file_path.split("/")
+    if len(copied_file_info) == 1:
+        os.rename(current_file, copied_file_info[0])
     else:
-        path = f"{second_file_info[0]}"
-        for i in range(1, len(second_file_info)):
+        path = f"{copied_file_info[0]}"
+        for index_of_folder in range(1, len(copied_file_info)):
             os.mkdir(path)
-            path += f"/{second_file_info[i]}"
-        with open(first_file, "r") as file_in, open(path, "w") as file_out:
+            path += f"/{copied_file_info[index_of_folder]}"
+        with open(current_file, "r") as file_in, open(path, "w") as file_out:
             file_out.write(file_in.read())
-        os.remove(first_file)
+        os.remove(current_file)
