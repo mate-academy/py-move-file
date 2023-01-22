954f8c65bd439a9c79b74c8ad408cb820967081b
Vitalii Khmura <vitalikkhmura13@gmail.com>
2023-01-22 22:30:35 +0200

    Changed Solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    mv, file_in, file_out = command.split(" ")
-    if mv != "mv":
-        return
-    os.makedirs("/".join(file_out.split("/")[:-1]))
-    if file_out.split("/")[-1] == "":
-        raise FileNotFoundError
-    with open(file_in, "r") as source, \
-            open(file_out, "w") as new_file:
-        new_file.write(source.read())
-    os.remove(file_in)
+    mv, source_file, current_path = command.split()
+    if mv == "mv":
+        if "/" in current_path:
+            last_slash = current_path.rfind("/")
+            new_file = current_path[last_slash + 1:]
+            os.makedirs(current_path[:last_slash])
+            current_path = os.path.join(current_path[:last_slash], new_file)
+        with open(source_file, "r") as file_read, \
+                open(current_path, "w") as file_write:
+            file_write.write(file_read.read())
+        os.remove(source_file)
