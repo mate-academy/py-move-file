0882523d00f766b1c90bfcee8e4e4ef5d73e6eef
Vitalii Khmura <vitalikkhmura13@gmail.com>
2023-01-23 20:45:07 +0200

    Solution with os method

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ def move_file(command: str) -> None:
     mv, source_file, current_path = command.split()
     if mv == "mv":
         if "/" in current_path:
-            last_slash = current_path.rfind("/")
-            new_file = current_path[last_slash + 1:]
-            os.makedirs(current_path[:last_slash])
-            current_path = os.path.join(current_path[:last_slash], new_file)
+            path = os.path.dirname(current_path)
+            new_file = os.path.basename(current_path)
+            os.makedirs(path)
+            current_path = os.path.join(path, new_file)
         with open(source_file, "r") as file_read, \
                 open(current_path, "w") as file_write:
             file_write.write(file_read.read())
