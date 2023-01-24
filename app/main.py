0052e31a0f6477b88274a7dd6d0ec4266ccc94c7
Yehor Kornev <dmekkindom00@gmail.com>
2023-01-24 18:12:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,31 +2,31 @@ from __future__ import annotations
 import os
 
 
-def move_clear_file(file_path1: str, file_path2: str) -> None:
+def move_clear_file(source_file: str, target_file: str) -> None:
     with (
-        open(file_path1, "r") as file_in,
-        open(file_path2, "w") as file_out
+        open(source_file, "r") as file_in,
+        open(target_file, "w") as file_out
     ):
         file_out.write(file_in.read())
         file_in.close()
-        os.remove(file_path1)
+        os.remove(source_file)
 
 
 def move_file(command: str) -> None:
     if len(command.split()) != 3:
         raise ValueError("This is not valid")
-    command, file_path_name1, file_path_name2 = command.split()
+    command, source_file, target_file = command.split()
     if command != "mv":
         raise ValueError(
             f'"{command}" This is an invalid command!'
             f' The correct one is: "mv"'
         )
 
-    path = os.path.join(file_path_name2)
+    path = os.path.join(target_file)
     head_path, tail_path = os.path.split(path)
 
     if not head_path and tail_path:
-        move_clear_file(file_path_name1, file_path_name2)
+        move_clear_file(source_file, target_file)
 
     if head_path and tail_path:
         current_path = ""
@@ -34,4 +34,4 @@ def move_file(command: str) -> None:
             current_path = os.path.join(current_path, current_dir)
             if not os.path.isdir(current_path):
                 os.mkdir(current_path)
-        move_clear_file(file_path_name1, file_path_name2)
+        move_clear_file(source_file, target_file)
