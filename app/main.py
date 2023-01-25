111888466c7e70be4e1951550feb61293a3a0a12
Tetiana Kozakova <kleotan901@gmail.com>
2023-01-25 18:18:00 +0100

    fixed

--- app/main.py
+++ app/main.py
@@ -3,15 +3,13 @@ import os
 
 def move_file(command: str) -> None:
     cmd, current_file_name, new_path = command.split()
-    new_file_name = os.path.split(new_path)[1]
-    new_dirs = os.path.split(new_path)[0]
+    new_dirs, new_file_name = os.path.split(new_path)
     if len(new_dirs) > 1:
         os.makedirs(os.path.join(new_dirs), exist_ok=True)
-
+    if len(new_dirs) == 1:
+        os.rename(current_file_name, new_file_name)
     with open(current_file_name, "r") as source,\
             open(os.path.join(new_dirs, new_file_name), "w+") as new_path:
         read_current_file = source.read()
         new_path.write(read_current_file)
-    if len(new_dirs) == 1:
-        os.rename(current_file_name, new_file_name)
     os.remove(current_file_name)
