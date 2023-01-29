b401fcfabc902591f96bd52d688dd22a46408e09
Andrii Skryl <a.skryl92@gmail.com>
2023-01-29 20:00:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,14 +2,17 @@ import os
 
 
 def move_file(command: str) -> None:
-    mv, original_file, new_file = command.split()
-    if mv == "mv":
-        if "/" in new_file:
-            dir_tuple = os.path.split(new_file)
-            dir_path = dir_tuple[1]
-            os.makedirs(dir_path)
-            new_file = os.path.join(dir_path, new_file)
-        with open(original_file, "r") as file_in, \
-                open(new_file, "w") as file_out:
-            file_out.write(file_in.read())
-            os.remove(original_file)
+    split_cmd = command.split()
+    if len(split_cmd) == 3:
+        input_command, original_file_path, destination_file_path = split_cmd
+        if input_command == "mv":
+            dir_path, file_name = os.path.split(destination_file_path)
+            if os.path.isdir(dir_path):
+                os.makedirs(dir_path)
+                new_file = os.path.join(dir_path, file_name)
+                with (
+                    open(original_file_path, "r") as file_in,
+                    open(new_file, "w") as file_out
+                ):
+                    file_out.write(file_in.read())
+                os.remove(original_file_path)
