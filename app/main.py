e53d631cdd0eef6bf6a9c451de9c8d789f34d219
Alina Boichenko <ilub1108@gmail.com>
2023-01-29 12:21:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,16 +2,17 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) == 3:
-        command_name, file_name, destination_path = command.split()
+    command = command.split()
+    if len(command) == 3:
+        command_name, file_name, destination_path = command
 
         if "/" in destination_path:
             destination_path = destination_path.split("/")
             new_file_name = destination_path[-1]
             destination_path = destination_path[:-1]
-            for path in destination_path:
-                os.makedirs(path)
-                new_file_name = os.path.join(path, new_file_name)
+
+            path = os.path.join(destination_path, new_file_name)
+            os.makedirs(path, exist_ok=True)
 
         with (
             open(f"{file_name}", "r") as file_in,
