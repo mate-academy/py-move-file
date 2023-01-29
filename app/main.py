9b3c6c6cd9864b49bd79431ec47df4e39724e8bf
Tetiana Kozakova <kleotan901@gmail.com>
2023-01-29 06:48:46 +0100

    fixed

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 import os
 
 
-def move_file(command: str) -> None:
+def move_file(command: str) -> str:
     cmd, current_file_name, new_path = command.split()
     new_dirs, new_file_name = os.path.split(new_path)
+    if cmd != "mv":
+        return "Input correct command"
     if len(new_dirs) > 1:
-        os.makedirs(os.path.join(new_dirs), exist_ok=True)
+        try:
+            os.makedirs(os.path.join(new_dirs))
+        except FileExistsError:
+            return f"Directory {new_dirs} exists"
     if len(new_dirs) == 1:
         os.rename(current_file_name, new_file_name)
     with open(current_file_name, "r") as source,\
