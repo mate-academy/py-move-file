c543482a5a501099bba61b714cca9de36327edb1
Paul Maslov <maslovp67@gmail.com>
2023-01-30 19:08:38 +0200

    'So;ution'

--- app/main.py
+++ app/main.py
@@ -2,16 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    cmd, file1, file2 = command.split()
-    if cmd == "mv" and file1 != file2:
-        path_to_file = file2.split("/")
-        if len(path_to_file) > 1:
-            name_dir = path_to_file[0]
-            os.mkdir(name_dir)
-            for i in range(1, len(path_to_file) - 1):
-                name_dir = name_dir + "/" + path_to_file[i]
-                os.mkdir(name_dir)
-        with open(file1, "r") as file_in, \
-             open(file2, "w") as file_out:
-            file_out.write(file_in.read())
-        os.remove(file1)
+    if len(command.split()) == 3:
+        input_command, source, destination = command.split()
+        if input_command == "mv" and source != destination:
+            path_to_file = os.path.split(destination)
+            os.makedirs(path_to_file[0], exist_ok=True)
+            with (
+                 open(source, "r") as f_input,
+                 open(destination, "w") as f_output
+                 ):
+                f_output.write(f_input.read())
+            os.remove(source)
