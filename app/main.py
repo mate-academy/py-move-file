c06fc24fc445231a70ba28ac87e78815e187ab40
Tetiana Kozakova <kleotan901@gmail.com>
2023-01-31 20:34:36 +0100

    fixed

--- app/main.py
+++ app/main.py
@@ -14,12 +14,11 @@ def move_file(command: str) -> str:
 
     if new_dirs:
         os.makedirs(new_dirs, exist_ok=True)
-        if os.path.exists(os.path.join(new_dirs, new_file_name)):
-            return f"File with name {new_file_name} already exist"
-        with open(input_file_path, "r") as source,\
-                open(os.path.join(new_dirs, new_file_name), "w")\
-                as destination_file:
-            destination_file.write(source.read())
+        with (
+            open(input_file_path, "r") as src_file,
+            open(os.path.join(new_dirs, new_file_name), "w") as dst_file
+        ):
+            dst_file.write(src_file.read())
             os.remove(input_file_path)
-    elif os.path.splitext(destination_file_path):
+    else:
         os.rename(input_file_path, new_file_name)
