2080e3a62cb7ba6db3c7ae5918b3ab4d56604453
IhorPokr <pokroveckijigor@gmail.com>
2023-02-01 11:41:53 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,15 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    split_command = command.split()
-    if split_command[0] == "mv":
-        source_file = split_command[1]
-        new_file = split_command[2]
-        if new_file.count("/") > 0:
-            slice_ = split_command[2].rfind("/")
-            new_file = split_command[2][slice_ + 1:]
-            current_path = split_command[2][:slice_]
-            os.makedirs(current_path)
+    cmd, src, dst = command.split()
+    if cmd == "mv":
+        source_file = src
+        new_file = dst
+        if "/" in dst:
+            current_path = os.path.dirname(dst)
+            os.makedirs(current_path, exist_ok=True)
+            new_file = os.path.basename(dst)
             new_file = os.path.join(current_path, new_file)
         with open(source_file, "r") as file_read, \
                 open(new_file, "w") as file_write:
