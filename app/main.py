4c29fc3873a624bf5bbec1bbf6cdb745d74bf4d9
IhorPokr <pokroveckijigor@gmail.com>
2023-02-02 18:34:00 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,12 +5,13 @@ def move_file(command: str) -> None:
     cmd, src, dst = command.split()
     if cmd == "mv":
         source_file = src
-        new_file = dst
-        if "/" in dst:
-            current_path = os.path.dirname(dst)
-            os.makedirs(current_path, exist_ok=True)
-            new_file = os.path.basename(dst)
-            new_file = os.path.join(current_path, new_file)
+        source_folder, source_filename = os.path.split(src)
+        dst_folder, dst_filename = os.path.split(dst)
+
+        if dst_folder:
+            os.makedirs(dst_folder, exist_ok=True)
+
+        new_file = os.path.join(dst_folder, dst_filename)
         with open(source_file, "r") as file_read, \
                 open(new_file, "w") as file_write:
             text = file_read.read()
