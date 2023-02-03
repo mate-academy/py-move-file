70735868cbf3dca946c1a61952ccb188ceea86a7
Illia Smyrnov <ilushasm@gmail.com>
2023-02-03 18:35:44 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -2,27 +2,26 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) == 3:
-        command, old_file, new_file = command.split()
+    if len(command.split()) != 3:
+        raise
 
-        if command == "mv":
-            new_file_path, new_file_name = os.path.split(new_file)
+    command, old_file, new_file = command.split()
 
-            if len(new_file_path) > 0:
-                if not os.path.isdir(new_file_path):
-                    os.makedirs(new_file_path)
+    if command == "mv":
+        new_file_path, new_file_name = os.path.split(new_file)
 
-            if len(new_file_name) == 0:
-                new_file_name = old_file
+        if len(new_file_path) > 0:
+            os.makedirs(new_file_path, exist_ok=True)
 
-            print(new_file_path + new_file_name)
+        if len(new_file_name) == 0:
+            new_file_name = old_file
 
-            with open(old_file, "r") as file_old, open(
-                    os.path.join(new_file_path, new_file_name),
-                    "w"
-            ) as file_copy:
-                file_copy.write(file_old.read())
+        destination_path = os.path.join(new_file_path, new_file_name)
 
-            os.remove(old_file)
+        with (
+            open(old_file, "r") as file_old,
+            open(destination_path, "w") as file_copy
+        ):
+            file_copy.write(file_old.read())
 
-    return
+        os.remove(old_file)
