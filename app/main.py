6afbeb74fb8d01d8032afbf61b68dd343df4ce4f
IhorPokr <pokroveckijigor@gmail.com>
2023-02-05 12:52:48 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,15 +2,26 @@ import os
 
 
 def move_file(command: str) -> None:
-    _, src, dst = command.split()
+    if not command.startswith("mv"):
+        print("Error: invalid command")
+        return
+
+    src, dst = command.split()[1:]
     dst_folder, dst_filename = os.path.split(dst)
 
-    if dst_folder:
+    try:
         os.makedirs(dst_folder, exist_ok=True)
+    except FileExistsError:
+        pass
 
-    new_file = dst
-    with open(src, "r") as file_read, \
-            open(new_file, "w") as file_write:
-        text = file_read.read()
-        file_write.write(text)
-    os.remove(src)
+    with (
+        open(src, "r") as file_input,
+        open(dst, "w") as file_output
+    ):
+        text = file_input.read()
+        file_output.write(text)
+
+    try:
+        os.remove(src)
+    except FileNotFoundError:
+        print(f"Error: source file '{src}' does not exist")
