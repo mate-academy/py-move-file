4cd3e71ab0bad68dc513fb2f09ad2bf0ea6056f9
Andriy <andriykrav@gmail.com>
2023-02-07 21:35:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+from pathlib import Path
+
+
+def move_file(command: str) -> None:
+    splitted_all = command.split("/")
+    splitted_first_str = str(splitted_all[0]).split()
+    if len(splitted_all) == 1:
+        with open(splitted_first_str[1], "w") as source_f, \
+                open(splitted_first_str[2], "w") as target_f:
+            for line in source_f:
+                target_f.write(line)
+            os.remove(splitted_first_str[1])
+    if len(splitted_all) >= 2:
+        filename_2 = splitted_all[-1]
+        path = "/".join([splitted_first_str[2]] + splitted_all[1:-1])
+        dir_path = Path(path)
+        target_file = dir_path / filename_2
+        os.makedirs(path)
+        with open(target_file, "w") as target_f,\
+                open(splitted_first_str[1], "r") as source_f:
+            for line in source_f:
+                target_f.write(line)
+            os.remove(splitted_first_str[1])
