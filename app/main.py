2ac18884f0ea464f66e4c690614f4b406a7c34c4
Nataliia Podoroha <ingenuous999@gmail.com>
2023-02-09 13:31:53 +0200

    Solution_fixed

--- app/main.py
+++ app/main.py
@@ -2,22 +2,19 @@ import os
 
 
 def move_file(command: str) -> None:
-    copy_command, name, path = command.split()
-    if "/" not in path:
+    splitted = command.split()
+
+    if splitted[0] == "mv" and len(splitted) == 3:
+        copy_command, name, path = splitted
+
+        if "/" in path:
+            dirs_names = os.path.split(path)[:-1]
+            dirs_path = os.path.join(*dirs_names)
+            os.makedirs(dirs_path, exist_ok=True)
+
+        if path.endswith("/"):
+            path = os.path.join(path, name)
+
         with open(name, "r") as file_in, open(path, "w") as file_out:
             file_out.write(file_in.read())
             os.remove(name)
-
-    dir_names = ""
-    for word in path.split("/")[:-1]:
-        dir_names += word + "/"
-        os.mkdir(dir_names)
-
-    new_path = dir_names + path.split("/")[-1]
-
-    if path.endswith("/"):
-        new_path = dir_names + name
-
-    with open(name, "r") as file_in, open(new_path, "w") as file_out:
-        file_out.write(file_in.read())
-        os.remove(name)
