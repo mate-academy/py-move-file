00510f752ce9b212b1e47c48baa599d79e6f2eef
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-02-10 19:49:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,23 +2,25 @@ import os
 
 
 def move_file(command: str) -> None:
-    command = command.split()
+    if len(command.split()) != 3:
+        raise ValueError("Input should consist of 3 parts: "
+                         "command, source file, target file")
+    else:
+        command, source_file, target_file = command.split()
 
-    if command[0] != "mv":
+    if source_file == target_file:
+        raise ValueError("Source file and target file must be difference")
+
+    if command != "mv":
         raise ValueError("Command must be 'mv'")
 
-    if "/" not in command[-1]:
-        with (
-            open(command[1], "r") as file_in,
-            open(command[2], "w") as file_out
-        ):
-            file_out.write(file_in.read())
-    else:
-        folders, file = os.path.split(command[-1])
+    folders, file = os.path.split(target_file)
+    if len(folders) != 0:
         os.makedirs(folders, exist_ok=True)
-        with (
-            open(command[1], "r") as file_in,
-            open(command[-1], "w") as file_out
-        ):
-            file_out.write(file_in.read())
-            os.remove(command[1])
+
+    with (
+        open(source_file, "r") as file_in,
+        open(target_file, "w") as file_out
+    ):
+        file_out.write(file_in.read())
+    os.remove(source_file)
