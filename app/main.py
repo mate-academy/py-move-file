cc395f329f7529ec7c7e8514a6bfa4fc29931e4b
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-02-10 18:12:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+
+    if command[0] != "mv":
+        raise ValueError("Command must be 'mv'")
+
+    if "/" not in command[-1]:
+        with (
+            open(command[1], "r") as file_in,
+            open(command[2], "w") as file_out
+        ):
+            file_out.write(file_in.read())
+    else:
+        folders, file = os.path.split(command[-1])
+        os.makedirs(folders, exist_ok=True)
+        with (
+            open(command[1], "r") as file_in,
+            open(command[-1], "w") as file_out
+        ):
+            file_out.write(file_in.read())
+            os.remove(command[1])
