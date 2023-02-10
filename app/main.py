1e6d045c14713d3c5a4b83e15b6e025a59a87099
Zoriana Dvulit <zdvulit@ukr.net>
2023-02-10 19:24:51 +0200

    New Solution

--- app/main.py
+++ app/main.py
@@ -2,15 +2,16 @@ import os
 
 
 def move_file(command: str) -> None:
-    com = command.split()
-    directory = com[2].split("/")
+    _, copied_file, file_to_copy = command.split()
+    directory = file_to_copy.split("/")
     path = directory[0]
     os.mkdir(path)
     for index in range(1, len(directory) - 1):
         path += "/" + directory[index]
         os.mkdir(path)
 
-    with open(com[1], "r") as file_in, open(com[2], "w") as file_out:
+    with open(copied_file, "r") as file_in, \
+            open(file_to_copy, "w") as file_out:
         file_out.write(file_in.read())
 
-    os.remove(com[1])
+    os.remove(copied_file)
