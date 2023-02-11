bbd8fd92e5548c4531c46b7b4f279362b3a6f977
Andriy <andriykrav@gmail.com>
2023-02-11 20:22:04 +0200

    Solution4

--- app/main.py
+++ app/main.py
@@ -1,28 +1,17 @@
 import os
-from pathlib import Path
 
 
 def move_file(command: str) -> None:
-    splitted_all = command.split("/")
-    splitted_first_str = str(splitted_all[0]).split()
-    if len(splitted_all) == 1:
-        if len(splitted_first_str) <= 2 or len(splitted_first_str) > 3:
-            raise ValueError("no 'cp' command present or command syntax error")
-        with open(splitted_first_str[1], "r") as source_f, \
-                open(splitted_first_str[2], "w") as target_f:
-            for line in source_f:
-                target_f.write(line)
-            os.remove(splitted_first_str[1])
-    if len(splitted_all) >= 2:
-        filename_2 = splitted_all[-1]
-        path = "/".join([splitted_first_str[2]] + splitted_all[1:-1])
-        dir_path = Path(path)
-        target_file = dir_path / filename_2
-        os.makedirs(path)
+    if len(command.split()) != 3:
+        raise ValueError("no 'cp' command present or command syntax error")
+    command, source_path, destination_path = command.split()
+    if "/" not in destination_path:
+        os.rename(source_path, destination_path)
+    elif command == "mv" and source_path != destination_path:
+        os.makedirs(os.path.dirname(destination_path))
         with (
-            open(target_file, "w") as target_f,
-            open(splitted_first_str[1], "r") as source_f
+            open(source_path, "r") as source_f,
+            open(destination_path, "w") as target_f
         ):
-            for line in source_f:
-                target_f.write(line)
-            os.remove(splitted_first_str[1])
+            target_f.write(source_f.read())
+            os.remove(source_path)
