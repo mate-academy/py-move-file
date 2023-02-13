cd33fffcb4eb56d0ce0d7ee4f283cdf8a60a2d0d
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-13 16:24:20 +0200

    1 commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    command_mv, file_to_opening, directory_to_writing = command.split(" ")
+    if command_mv == "mv":
+        if directory_to_writing.count("/"):
+
+            directory_to_writing_in_list = directory_to_writing.split("/")
+            file_to_writing = directory_to_writing_in_list[-1]
+            folders = ""
+            for ind in range(len(directory_to_writing_in_list) - 1):
+                folders += directory_to_writing_in_list[ind]
+                os.mkdir(folders)
+                folders += "/"
+            shutil.copyfile(file_to_opening, folders + file_to_writing)
+            os.remove(file_to_opening)
+        else:
+
+            shutil.copyfile(file_to_opening, directory_to_writing)
+            os.remove(file_to_opening)
