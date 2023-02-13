5d011696cb187ac133d15de323c82f4cd7b1f7c3
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-13 20:31:47 +0200

    add check to exist directory

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ import os
 
 
 def move_file(command_line: str) -> None:
-    command, first_file, second_file = command_line.split(" ")
+    command, first_file, second_file = command_line.split()
     last_slash_index = second_file.rfind("/")
     second_file_path = second_file[:last_slash_index]
     content = ""
-    os.makedirs(second_file_path)
+
+    if not os.path.exists(second_file_path):
+        os.makedirs(second_file_path)
 
     with open(first_file, "r") as file_content:
         content += file_content.read()
