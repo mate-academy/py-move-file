bc1b0b13dbfcf0d69d3ebfa8e04a0edac73a3620
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-13 23:50:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    func, file, directory = command.split()
+    if "/" in command:
+        command_ls = directory.split("/")
+        for direc in command_ls[:-1]:
+            os.mkdir(f"{directory[: directory.index(direc)]}{direc}")
+
+        with open(file, "r") as source, open(directory, "w") as copy:
+            copy.write(source.read())
+
+        os.remove(file)
+    else:
+        os.rename(file, directory)
