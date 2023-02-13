24c86eb6597cc182000a8f09d13ba6d964731f5d
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-13 19:09:28 +0200

    fixed some errors

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command_line: str) -> None:
+    command, first_file, second_file = command_line.split(" ")
+    last_slash_index = second_file.rfind("/")
+    second_file_path = second_file[:last_slash_index]
+    content = ""
+    os.makedirs(second_file_path)
+
+    with open(first_file, "r") as file_content:
+        content += file_content.read()
+
+    with open(second_file, "w") as file_out:
+        file_out.write(content)
+        os.remove(first_file)
