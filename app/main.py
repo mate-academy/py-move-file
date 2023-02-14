cc0da04607b4c93f948abd75970b9504e6d634bd
Ruslan Yanyshyn <ruslan.yanyshyn@yahoo.com>
2023-02-14 16:54:45 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    name, source, destination = command.split()
+
+    if destination.endswith("/") or name != "mv":
+        raise ValueError("Invalid command")
+
+    separator = destination.rfind("/")
+    if separator == -1:
+        os.rename(source, destination)
+        return
+
+    des_path = destination[: separator]
+    os.makedirs(des_path, exist_ok=True)
+    with (
+        open(source, "r") as file_in,
+        open(destination, "w") as file_out
+    ):
+        file_out.write(file_in.read())
+
+    os.remove(source)
