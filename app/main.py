71ed39c5557a6ff7994a40c77692865c0cf7bfe9
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-16 08:33:25 +0200

    crossplatfrom fix

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ class UnrecognizedException(BaseException):
     pass
 
 
+def crossplatfrom_speculation(file_path: str) -> str:
+    return os.path.join("/".join(file_path))
+
+
 def move_file(exec_string: str) -> None:
     commands = ["mv"]
     command, old_file, new_file = exec_string.split()
@@ -12,6 +16,10 @@ def move_file(exec_string: str) -> None:
         raise AttributeError(f"Allowed commands: {commands},"
                              f" {command} is not allowed!")
     path1, path2 = old_file.split("/")[:-1], new_file.split("/")[:-1]
+
+    path1 = crossplatfrom_speculation(path1)
+    path2 = crossplatfrom_speculation(path2)
+
     if path1 == path2:
         if old_file == new_file:
             return
