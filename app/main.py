486e1f4c52b685219673d9b1c2266e6fab0e652e
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-02-16 20:27:16 +0100

    removed return Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ def move_file(command: str) -> None:
     command, file_name, directory = command.split()
     if command != "mv":
         raise ValueError("Wrong command!")
-        return
     directory = directory.split("/")
     new_file = directory.pop(-1)
     path = ""
