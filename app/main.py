67fd1a544ca4c824a0e4e31eb01e32622761b97a
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-02-16 20:25:02 +0100

    removed try-except Solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,10 @@ import os
 
 
 def move_file(command: str) -> None:
-    try:
-        command, file_name, directory = command.split()
-        if command != "mv":
-            raise ValueError
-    except ValueError:
-        print("Wrong command!")
-        return None
+    command, file_name, directory = command.split()
+    if command != "mv":
+        raise ValueError("Wrong command!")
+        return
     directory = directory.split("/")
     new_file = directory.pop(-1)
     path = ""
