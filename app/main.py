2ee04a48f9de0b202a386dd9fad4966ee88e5e47
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-16 16:12:56 +0200

    Cross_platform with different path scenarios added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,39 @@
 import os
+from sys import platform
+
+
+class DirectoryExistError(Exception):
+
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def __str__(self) -> str:
+        return f"Directory with path {self.path} is already exist!"
+
+
+def cross_platform_solution(
+    path_file_in: str, path_file_out: str
+) -> tuple[str, str, str]:
+    absolute_file_in = os.path.join(os.getcwd(), path_file_in)
+    path_file = ""
+    path_dir_out = ""
+    if platform == "win32":
+        if "/" in path_file_out:
+            path_file = "\\".join(path_file_out.split("/"))
+        elif "\\" in path_file_out:
+            path_file = "\\".join(path_file_out.split("\\"))
+        path_dir_out = "\\".join(path_file.split("\\")[:-1])
+    elif platform == "linux" or platform == "linux2":
+        if "/" in path_file_out:
+            path_file = path_file_out
+        elif "\\" in path_file_out:
+            path_file = "/".join(path_file_out.split("\\"))
+        path_dir_out = "/".join(path_file.split("/")[:-1])
+
+    directory_path = os.path.join(os.getcwd(), path_dir_out)
+    output_file_path = os.path.join(os.getcwd(), path_file)
+
+    return absolute_file_in, directory_path, output_file_path
 
 
 def move_file(command: str) -> None:
@@ -7,27 +42,28 @@ def move_file(command: str) -> None:
     if input_command != "mv":
         return
 
-    if "/" not in path_file_out:
+    if ("/" or "\\") not in path_file_out:
         os.rename(path_file_in, path_file_out)
         return
 
-    path_file_out = path_file_out.replace("/", "\\")
-    path = "C:\\Programming Study\\Mate\\py-move-file"
-    path_file_out_list = path_file_out.split("\\")
+    (
+        absolute_file_in,
+        output_directory_path,
+        output_directory_file_path,
+    ) = cross_platform_solution(path_file_in, path_file_out)
+    try:
+        if os.path.exists(output_directory_path):
+            raise DirectoryExistError(output_directory_path)
+    except DirectoryExistError as e:
+        print(e)
+    else:
+        os.makedirs(output_directory_path)
 
-    directories_path_file_out = "\\".join(path_file_out_list[:-1])
-    absolute_path_file_out = os.path.join(path, directories_path_file_out)
-
-    if not os.path.exists(absolute_path_file_out):
-        os.makedirs(absolute_path_file_out)
-
-    absolute_path_file_input = path + "\\" + path_file_in
-    absolute_path_file_output = (absolute_path_file_out
-                                 + "\\"
-                                 + path_file_out_list[-1])
-
-    with (open(absolute_path_file_input, "r") as file_in,
-          open(absolute_path_file_output, "w") as file_out):
+    with (
+        open(absolute_file_in, "r") as file_in,
+        open(output_directory_file_path, "w") as file_out,
+    ):
         read_content = file_in.read()
         file_out.write(read_content)
-    os.remove(absolute_path_file_input)
+
+    os.remove(absolute_file_in)
