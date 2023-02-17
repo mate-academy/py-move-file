f34cff28e03d8d0424c86679fdd64cc7cf5838e9
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-17 18:32:14 +0200

    renames checks if old == new

--- app/main.py
+++ app/main.py
@@ -9,18 +9,15 @@ def move_file(exec_string: str) -> None:
     commands = {"mv"}
     command, old_file, new_file = exec_string.split()
     if command not in commands:
-        raise AttributeError(f"Allowed commands: {commands},"
-                             f" {command} is not allowed!")
-    path1, path2 = os.path.split(old_file)[0], os.path.split(new_file)[0]
-    path1, path2 = os.path.join(path1), os.path.join(path2)
+        raise ValueError(f"Allowed commands: {commands},"
+                         f" {command} is not allowed!")
 
-    if path1 == path2:
-        if old_file == new_file:
-            return
-        if old_file != new_file:
-            if os.path.isfile(new_file):
-                raise FileExistsError(f"{new_file} already exists, "
-                                      f"process terminated.")
+    old_file, new_file = os.path.join(old_file), os.path.join(new_file)
+
+    if old_file != new_file:
+        if os.path.isfile(new_file):
+            raise FileExistsError(f"{new_file} already exists, "
+                                  f"process terminated.")
     try:
         os.renames(old_file, new_file)
     except BaseException as unknown:
