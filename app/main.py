aec6e6acb00a1cf3de2c2412c251566215a9948a
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-17 21:10:07 +0200

    refactor

--- app/main.py
+++ app/main.py
@@ -3,17 +3,14 @@ import os
 
 def move_file(command: str) -> None:
     command, first_file, second_file = command.split()
-    second_file_part = os.path.split(second_file)
-    directories_list = second_file_part[0].split("/")
-    directories_path = os.path.join(*directories_list)
-    path_with_file = os.path.join(directories_path, second_file_part[1])
+    directories = os.path.split(second_file)[0]
 
     if command == "mv":
-        if not os.path.exists(directories_path):
-            os.makedirs(directories_path)
+        if not os.path.exists(directories):
+            os.makedirs(directories)
 
         with (open(first_file, "r") as file_content,
-              open(path_with_file, "w") as file_out):
+              open(second_file, "w") as file_out):
             file_out.write(file_content.read())
 
         os.remove(first_file)
