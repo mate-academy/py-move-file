5ba387f741e416b7debb09859df69a5725e39739
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-17 17:59:58 +0200

    fixed some bugs

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 import os
 
 
-def move_file(command_line: str) -> None:
-    command, first_file, second_file = command_line.split()
-    last_slash_index = second_file.rfind("/")
-    second_file_path = second_file[:last_slash_index]
-    content = ""
+def move_file(command: str) -> None:
+    command, first_file, second_file = command.split()
+    second_file_path = second_file[:second_file.rfind("/")]
 
-    if command == "mv" and last_slash_index != "-1":
+    if command == "mv":
         if not os.path.exists(second_file_path):
             os.makedirs(second_file_path)
 
-        with open(first_file, "r") as file_content:
-            content += file_content.read()
+        with (open(first_file, "r") as file_content,
+              open(second_file, "w") as file_out):
+            file_out.write(file_content.read())
 
         os.remove(first_file)
 
-        with open(second_file, "w") as file_out:
-            file_out.write(content)
+
+move_file("mv text.txt dir1/dir2/dir3/text.txt")
