74d27570af3aedf61af3ff6469b9ef119710095b
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-17 02:39:10 +0200

    YAGNI, DRY

--- app/main.py
+++ app/main.py
@@ -5,10 +5,6 @@ class UnrecognizedException(BaseException):
     pass
 
 
-def crossplatfrom_speculation(file_path: str) -> str:
-    return os.path.join("/".join(file_path))
-
-
 def move_file(exec_string: str) -> None:
     commands = {"mv"}
     command, old_file, new_file = exec_string.split()
@@ -17,8 +13,8 @@ def move_file(exec_string: str) -> None:
                              f" {command} is not allowed!")
     path1, path2 = os.path.split(old_file)[0], os.path.split(new_file)[0]
 
-    path1 = crossplatfrom_speculation(path1)
-    path2 = crossplatfrom_speculation(path2)
+    path1 = os.path.join(path1)
+    path2 = os.path.join(path2)
 
     if path1 == path2:
         if old_file == new_file:
@@ -26,9 +22,9 @@ def move_file(exec_string: str) -> None:
         if old_file != new_file:
             if os.path.isfile(new_file):
                 raise FileExistsError(f"{new_file} already exists, "
-                                      f"process terminated")
+                                      f"process terminated.")
     try:
         os.renames(old_file, new_file)
     except BaseException as unknown:
         raise UnrecognizedException(f"All should be fine, but huge error "
-                                    f"named {unknown} occurred")
+                                    f"named {unknown} occurred.")
