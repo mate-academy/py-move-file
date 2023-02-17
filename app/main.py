1ff31777df433b860584e516d5a9cb155fefccd0
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-17 21:35:48 +0200

    6 commit

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ def move_file(input_command: str) -> None:
     command, file_to_opening, path_to_file = input_command.split()
 
     if command == "mv":
+
         if path_to_file.count("/"):
             os.makedirs(os.path.join(os.path.split(path_to_file)[0]))
             shutil.copyfile(file_to_opening, os.path.join(path_to_file))
