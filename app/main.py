2b6bbd9657b622c9547c0c7964bec99c3bb702ee
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-17 20:20:01 +0200

    less complicated, used os.split

--- app/main.py
+++ app/main.py
@@ -1,43 +1,28 @@
 import os
 
 
-class DirectoryExistError(Exception):
-
-    def __init__(self, path: str) -> None:
-        self.path = path
-
-    def __str__(self) -> str:
-        return f"Directory with path {self.path} is already exist!"
-
-
 def move_file(command: str) -> None:
     input_command, path_file_in, path_file_out = command.split()
 
     if input_command != "mv":
-        exit()
-
+        return
     if ("/" or "\\") not in path_file_out:
         os.rename(path_file_in, path_file_out)
-        return None
+        return
 
-    absolute_file_in = os.path.join(os.getcwd(), path_file_in)
-    output_directory_path = os.path.join(
-        os.getcwd(), "\\".join(path_file_out.split("/")[:-1])
-    )
     output_directory_file_path = os.path.join(os.getcwd(), path_file_out)
-
+    output_directory_path = os.path.split(output_directory_file_path)[0]
     try:
         if os.path.exists(output_directory_path):
-            raise DirectoryExistError(output_directory_path)
-    except DirectoryExistError as e:
+            raise FileExistsError("File already exists!")
+    except FileExistsError as e:
         print(e)
     else:
         os.makedirs(output_directory_path)
 
     with (
-        open(absolute_file_in, "r") as file_in,
+        open(path_file_in, "r") as file_in,
         open(output_directory_file_path, "w") as file_out,
     ):
         file_out.write(file_in.read())
-
-    os.remove(absolute_file_in)
+    os.remove(path_file_in)
