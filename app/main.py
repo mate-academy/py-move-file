db6af6f6d61826f7e80b54cb683572bc5c4ea6bb
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-02-18 19:39:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, file_before, direction = command.split()
+    direction = direction.split("/")
+    new_file = direction.pop(-1)
+    path = ""
+
+    for directory in direction:
+        path += directory + "/"
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+    with open(file_before) as old_file, open(path + new_file, "w") as file:
+        file.write(old_file.read())
+    os.remove(file_before)
