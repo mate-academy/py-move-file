49c5e0052cf37b3d75f55c219da4219a6e5bf738
kuznetsb <kuznetsbog@gmail.com>
2023-02-18 16:46:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import os
+
+
+class IncorrectCommand(Exception):
+    """Error if command is not move"""
+
+
+def move_file(command: str) -> None:
+    command, copied_file, new_file = command.split()
+    if command != "mv":
+        raise IncorrectCommand("Function supports only mv command")
+
+    if "/" in new_file:
+        folders = new_file.split("/")[:-1]
+        file_path = [folders[0]]
+        for i in range(1, len(folders)):
+            file_path.append(f"{file_path[i - 1]}/{folders[i]}")
+        for directory in file_path:
+            if not os.path.exists(directory):
+                os.mkdir(directory)
+
+    with open(copied_file, "r") as old, open(new_file, "w") as new:
+        new.write(old.read())
+
+    os.remove(copied_file)
