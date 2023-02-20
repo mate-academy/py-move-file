ad09eba53bb87a36684dee7d5fae08582c52f3a8
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-20 19:18:04 +0200

    without return 0, without additional check, less split

--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) != 3 or command[:3] != "mv ":
-        return 0
-    name_f_in = command.split()[1]
-    name_f_out_with_rout = command.split()[2]
-    rout_list = name_f_out_with_rout.split("/")
-    name_f_out_with_rout_cp = os.path.join(*rout_list)
-    os.renames(name_f_in, name_f_out_with_rout_cp)
+    command, file_in, file_out_with_rout = command.split()
+    if command == "mv":
+        rout_list = file_out_with_rout.split("/")
+        file_out_with_rout_cp = os.path.join(*rout_list)
+        os.renames(file_in, file_out_with_rout_cp)
