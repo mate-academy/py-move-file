1dfb6987d7da0463518d2acf589fd2baffe998a8
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-20 15:49:37 +0200

    Changes

--- app/main.py
+++ app/main.py
@@ -4,15 +4,12 @@ import os
 def move_file(command: str) -> None:
     command, file, directory = command.split()
     if command == "mv":
-        if "/" in command:
-            command_ls = directory.split("/")
-            for direction in command_ls[:-1]:
-                file_directory = directory[: directory.index(direction)]
-                os.mkdir(f"{file_directory}{direction}")
+        command_ls = directory.split("/")
+        for direction in command_ls[:-1]:
+            file_directory = directory[: directory.index(direction)]
+            os.mkdir(f"{file_directory}{direction}")
 
-            with open(file, "r") as first_file, open(directory, "w") as copy:
-                copy.write(first_file.read())
+        with open(file, "r") as first_file, open(directory, "w") as copy:
+            copy.write(first_file.read())
 
-            os.remove(file)
-        else:
-            os.rename(file, directory)
+        os.remove(file)
