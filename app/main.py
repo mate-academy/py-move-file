ce1f981b3e09dcc8311265b5fd2e63a9cc71ce79
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-20 16:49:42 +0200

    Changes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ import os
 def move_file(command: str) -> None:
     command, file, directory = command.split()
     if command == "mv":
-        command_ls = directory.split("/")
+        if "/" in directory:
+            command_ls = directory.split("/")
+        else:
+            command_ls = directory
         for direction in command_ls[:-1]:
             file_directory = directory[: directory.index(direction)]
             os.mkdir(f"{file_directory}{direction}")
