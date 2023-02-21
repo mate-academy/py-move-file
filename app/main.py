9b6758773a65b14f1425d76a8527dd9c3eb24c19
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-02-21 18:24:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, file_name, destination = command.split()
+    destination_file = os.path.basename(destination)
+    path_for_func = destination.split("/")
+    path_for_func.pop(-1)
+    temp = ""
+    for directory in path_for_func:
+        temp = os.path.join(temp, directory)
+        if not os.path.exists(temp):
+            os.mkdir(temp)
+
+    with open(file_name, "r") as file:
+        data = file.read()
+
+    with open(os.path.join(temp, destination_file), "w") as dest_f:
+        dest_f.write(data)
+
+    os.remove(file_name)
