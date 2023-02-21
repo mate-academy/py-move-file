f521f755dee6b3f92c85adfb7a7fc916e019fe63
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-21 18:50:18 +0200

    Without os.path.join

--- app/main.py
+++ app/main.py
@@ -4,6 +4,4 @@ import os
 def move_file(command: str) -> None:
     command, file_in, file_out_with_rout = command.split()
     if command == "mv":
-        rout_list = file_out_with_rout.split("/")
-        file_out_with_rout_cp = os.path.join(*rout_list)
-        os.renames(file_in, file_out_with_rout_cp)
+        os.renames(file_in, file_out_with_rout)
