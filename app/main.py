a5c2035c8e48ec6c8ed74ef17cf6935aead84c07
usrofgh <mailforgimail@gmail.com>
2023-02-21 21:10:42 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,27 +1,20 @@
-from os import mkdir, remove, path, rename
+from os import makedirs, remove, rename
 
 
 def move_file(command: str) -> None:
-    try:
-        mv, file, new_location = command.split()
-        if mv != "mv":
-            raise ValueError
-    except ValueError:
+    mv_command, file, new_location = command.split()
+    if mv_command != "mv":
         raise ValueError(
             "Incorrect command. Example: mv file1.txt file2.txt "
             "or mv file1.txt new_folder/file1.txt"
         )
 
-    if "/" not in new_location and file != new_location:
+    if "/" not in new_location:
         rename(file, new_location)
-
-    elif "/" in new_location:
+    else:
         directories, new_file = new_location.rsplit("/", 1)
-        full_path = ""
-        for directory in directories.split("/"):
-            full_path = path.join(full_path, directory)
-            if not path.exists(full_path):
-                mkdir(full_path)
+        
+        makedirs(directories)
 
         with open(file) as read_file, open(new_location, "w") as write_file:
             write_file.write(read_file.read())
