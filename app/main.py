25fb2738bd7698b2e18c4ff76bb13f76a0e634ca
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-02-22 12:42:30 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,7 @@
 import os
 
 
-def move_file(commands: str) -> object:
-    command, file, directory = commands.split()
+def move_file(command: str) -> None:
+    command, file, directory = command.split()
     if command == "mv":
-        if "/" not in directory:
-            os.rename(file, directory)
-            return
-
-        *path, file1 = directory.split("/")
-        direct = "/".join(path)
-        if not os.path.exists(direct):
-            os.makedirs(direct)
-        moved_direct = os.path.join(direct, file1)
-        with open(moved_direct, "w") as filename, open(file, "r") as source:
-            filename.write(source.read())
-
-        os.remove(file)
+        os.renames(file, directory)
