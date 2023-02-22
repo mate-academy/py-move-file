eddc008ef0fae650e2b7914ee060db81bd06c4e4
Sergii Machulin <machulinsv@gmail.com>
2023-02-22 16:57:46 +0200

    Update solution

--- app/main.py
+++ app/main.py
@@ -3,23 +3,29 @@ import shutil
 
 
 def move_file(command: str) -> None:
-    if len(command.split(" ")) != 3 or command.split(" ")[0] != "mv":
-        return
-    command_parts = command.split(" ")
-
-    if os.path.exists(command_parts[1]):
-        file_in_name = command_parts[1]
-    else:
-        raise FileNotFoundError("No such file you try to move")
-
-    if command_parts[2].count("/") == 0:
-        file_out_name = command_parts[2]
-        os.rename(file_in_name, file_out_name)
-    else:
-        directory_path, file_out_name = os.path.split(command_parts[2])
-        if not os.path.exists(directory_path):
-            os.makedirs(directory_path)
-        shutil.move(
-            file_in_name,
-            os.path.join(directory_path, file_out_name)
+    try:
+        mv_command, file_in, file_out = command.split()
+    except ValueError:
+        print(
+            "Argument must consist of: "
+            "mv command, file name to move and destination"
         )
+    else:
+        if mv_command != "mv":
+            return
+
+        if not os.path.exists(file_in):
+            raise FileNotFoundError("No such file you try to move")
+        file_in_name = file_in
+
+        if "/" not in file_out:
+            file_out_name = file_out
+            os.rename(file_in_name, file_out_name)
+        else:
+            directory_path, file_out_name = os.path.split(file_out)
+            if not os.path.exists(directory_path):
+                os.makedirs(directory_path)
+            shutil.move(
+                file_in_name,
+                os.path.join(directory_path, file_out_name)
+            )
