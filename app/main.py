6ae59f0eec84fe922d077f136274bf84a569967e
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-02-23 17:49:50 +0200

    Refactor solution

--- app/main.py
+++ app/main.py
@@ -16,13 +16,8 @@ def move_file(command: str) -> None:
         os.rename(source, destination)
         return
 
-    directories = destination.split("/")[:-1]
-    path = ""
-    for directory in directories:
-        path = os.path.join(path, directory)
-        if not os.path.exists(path):
-            os.mkdir(path)
-
+    directories = os.path.split(destination)[0]
+    os.makedirs(directories, exist_ok=True)
     with open(source) as file_in, open(destination, "w") as file_out:
         file_out.write(file_in.read())
     os.remove(source)
