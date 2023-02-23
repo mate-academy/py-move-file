46d225ff0b332a946293a47f8cacc5b7dd81b5d5
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2023-02-23 18:47:33 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,23 +3,22 @@ import os
 
 def move_file(command: str) -> None:
     cp_command, current_file, future_file = command.split()
-    if cp_command != "mv":
+    if cp_command != "mv" or len(command.split()) != 3:
         return
-    path_to_file = os.path.split(future_file)
+    directories, file_name = os.path.split(future_file)
     new_path = ""
 
-    for new_dir in path_to_file[0].split("/"):
+    for new_dir in directories.split("/"):
         new_path += new_dir + "/"
-        if not os.path.exists(new_path):
-            os.mkdir(new_path)
+        os.makedirs(new_path, exist_ok=True)
 
-    if path_to_file[0] != "":
+    if directories != "":
         with open(
             current_file, "r"
         ) as file, open(
-            f"{path_to_file[0]}/{path_to_file[1]}", "w"
+            f"{directories}/{file_name}", "w"
         ) as new_file:
             new_file.write(file.read())
         os.remove(current_file)
     else:
-        os.rename(current_file, path_to_file[1])
+        os.rename(current_file, file_name)
