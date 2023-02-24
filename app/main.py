4c6fa0625c91b338a95c2c7f72f63a976012ede2
WalDragnir <akgk530@gmail.com>
2023-02-25 01:22:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+from pathlib import Path
+from shutil import move
+
+
+def move_file(command: str) -> None:
+    input_command, source_path, move_path = command.split()
+    if input_command != "mv":
+        raise ValueError("The command should be 'mv'")
+    elif "/" in move_path:
+        path = move_path[:move_path.rfind("/")]
+        Path(path).mkdir(parents=True, exist_ok=True)
+    move(source_path, move_path)
