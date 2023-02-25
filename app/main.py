628d3f82ab72776cae4e9d0870c570c8d498ed9c
sdobzhynetskyi <sergii.dobzhynetskyi@gmail.com>
2023-02-25 16:04:56 +0200

    solved task 1 - edited

--- app/main.py
+++ app/main.py
@@ -2,20 +2,27 @@ import os
 
 
 def move_file(command: str) -> None:
-    operation, file_in, file_to_move = command.split()
-    if (
-            len(command.split()) != 3
-            or operation != "mv"
+    try:
+        operation, file_in, file_to_move = command.split()
+    except ValueError as error:
+        print(error)
+        return
+    try:
+        if (
+            operation != "mv"
             or not os.path.isfile(file_in)
             or file_to_move.endswith("/")
-    ):
-        raise ValueError("The input command has wrong structure")
+        ):
+            raise ValueError("The input command has wrong structure")
+    except ValueError as error:
+        print(error)
+        return
     if not os.path.exists(file_to_move):
         try:
             os.rename(file_in, file_to_move)
         except FileNotFoundError:
-            path, new_file = os.path.split(file_to_move)
-            os.makedirs(path, exist_ok=True)
+            directories, new_file_name = os.path.split(file_to_move)
+            os.makedirs(directories, exist_ok=True)
             with (
                 open(file_to_move, "w") as created_file,
                 open(file_in, "r") as context
