619d7c83d5b65faf6f7ecfac7f43665ef134ab41
Oleksandr Leha <legus96@gmail.com>
2023-02-25 17:21:08 +0200

    'Solution_with_mv_error'

--- app/main.py
+++ app/main.py
@@ -3,16 +3,17 @@ import os
 
 def move_file(command: str) -> None:
     move_command, from_file, to_file = command.split()
-    with open(from_file) as fr_f:
-        content = fr_f.read()
+    if move_command != "mv":
+        raise SyntaxError("No such command")
     dirs = to_file.split("/")
     final_name = dirs.pop(-1)
     path = ""
-    for directory in dirs:
-        path = os.path.join(path, directory)
+    for i in dirs:
+        path = os.path.join(path, i)
         if not os.path.exists(path):
             os.mkdir(path)
     final_name_path = os.path.join(path, final_name)
-    with open(final_name_path, "w") as to_f:
+    with open(from_file) as fr_f, open(final_name_path, "w") as to_f:
+        content = fr_f.read()
         to_f.write(content)
     os.remove(from_file)
