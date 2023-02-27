dedc71649319b11f6a5ff25ba646edb92ff551f1
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-02-28 00:01:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    source_path, dest_path = command.split()[1:]
+    if "/" not in dest_path:
+        os.rename(source_path, dest_path)
+    elif "/" in dest_path:
+        dir_names = []
+        for name in dest_path.split("/")[:-1]:
+            dir_names.append(name)
+            if not os.path.exists("/".join(dir_names)):
+                os.mkdir("/".join(dir_names))
+        os.rename(source_path, dest_path)
