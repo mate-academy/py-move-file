68bfaef77a1c967b68487032bb1e732cc74e350a
WalDragnir <akgk530@gmail.com>
2023-02-28 13:16:40 +0200

    Fixed exists_ok

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ def move_file(command: str) -> None:
     if input_command != "mv":
         raise ValueError("The command should be 'mv'")
     head_tail = os.path.split(move_path)
-    if head_tail[0] != "" and not os.path.exists(head_tail[0]):
-        os.makedirs(head_tail[0])
+    if head_tail[0] != "":
+        os.makedirs(head_tail[0], exist_ok=True)
     os.replace(source_path, move_path)
