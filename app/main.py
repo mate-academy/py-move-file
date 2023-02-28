37b5699802bd2dacc27a00ef506ca8b46dca985d
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-02-28 23:57:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,24 @@ import os
 
 
 def move_file(command: str) -> None:
-    source_path, dest_path = command.split()[1:]
-    if "/" not in dest_path:
-        os.rename(source_path, dest_path)
-    elif "/" in dest_path:
-        dir_names = []
-        for name in dest_path.split("/")[:-1]:
-            dir_names.append(name)
-            if not os.path.exists("/".join(dir_names)):
-                os.mkdir("/".join(dir_names))
-        os.rename(source_path, dest_path)
+    if len(command.split()) != 3:
+        raise ValueError("Invalid command")
+    input_command, old_file_name, new_file_name = command.split()
+
+    if new_file_name.endswith("/") or input_command != "mv":
+        raise ValueError("Invalid command")
+
+    separator = new_file_name.rfind("/")
+    if separator == -1:
+        os.rename(old_file_name, new_file_name)
+        return
+
+    destination_folder = new_file_name[:separator]
+    os.makedirs(destination_folder, exist_ok=True)
+    with (
+        open(old_file_name, "r") as file_in,
+        open(new_file_name, "w") as file_out
+    ):
+        file_out.write(file_in.read())
+
+    os.remove(old_file_name)
