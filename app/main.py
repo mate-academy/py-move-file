c57ecfb88aedec8c123051ea810c9bf9bba48e7b
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-02-28 17:37:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,12 +3,15 @@ import os
 
 def move_file(command: str) -> None:
     _, old_name, directory = command.split()
+
     if _ == "mv":
         path, new_file = os.path.split(directory)
 
         if not os.path.exists(path) and path != "":
             os.makedirs(name=path)
+
         with open(old_name, "r") as old:
             with open(directory, "w") as new:
                 new.write(old.read())
+
         os.remove(old_name)
