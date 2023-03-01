440fbf8eb92ab710554e6524db38f96d1fff921e
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-02-28 18:55:06 -0600

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_arr = command.split()
+
+    os.makedirs(command_arr[2][:-1])
+    with (
+        open(command_arr[1], "r") as f_file,
+        open(command_arr[2], "w") as s_file
+    ):
+        try:
+            s_file.write(f_file.read())
+        except FileExistsError:
+            pass
+        os.remove(command_arr[1])
