1756a6c5ead21e023a77cfdaa21ef3f72176d419
Yulia <Mala>
2023-03-01 18:36:06 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from os import makedirs, path
+from shutil import move
+
+
+def move_file(command: str) -> None:
+    args = command.split()
+    source = args[1]
+    destination = args[2].split("/")
+    new_filename = destination[-1]
+    dirs = []
+    if len(destination) > 1:
+        dirs = destination[:-1]
+        if not path.exists("/".join(dirs)):
+            makedirs("/".join(dirs))
+    dirs.append(new_filename)
+    move(source, "/".join(dirs))
+
