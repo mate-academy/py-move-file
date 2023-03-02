788e8989f036b06cd63f354e5f45dc9bc429aa92
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-03-02 15:49:31 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -5,14 +5,13 @@ def move_file(command: str) -> None:
     command_split_arr = command.split()
 
     if command_split_arr[0] != "mv":
-        raise Exception(f"There is not '{command_split_arr[0]}' command!")
+        raise ValueError(f"There is not '{command_split_arr[0]}' command!")
     if len(command_split_arr) != 3:
-        raise Exception(f"Expected 3 arguments, got: {len(command_split_arr)}")
+        raise ValueError(
+            f"Expected 3 arguments, got: {len(command_split_arr)}"
+        )
 
-    try:
-        os.makedirs(command_split_arr[2][:-1])
-    except FileExistsError:
-        pass
+    os.makedirs(command_split_arr[2][:-1], exist_ok=True)
 
     with (
         open(command_split_arr[1], "r") as file_with_content,
