80f94cf141a281859a8b36c1a90b425aca774993
Roman <parom95@ukr.net>
2023-03-04 19:17:44 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    cm, first_file, second_file = command.split()
-    if cm == "mv" and first_file != second_file:
-        if "/" not in second_file:
-            create_file(first_file, second_file)
-        else:
-            directory = second_file.split("/")[0:-1]
-            os.makedirs(os.path.join(*directory), exist_ok=True)
-            create_file(first_file, second_file)
+    if len(command.split()) == 3:
+        cm, first_file, second_file = command.split()
+        if cm == "mv" and first_file != second_file:
+            if "/" not in second_file:
+                create_file(first_file, second_file)
+            else:
+                directory = second_file.split("/")[0:-1]
+                os.makedirs(os.path.join(*directory), exist_ok=True)
+                create_file(first_file, second_file)
 
 
 def create_file(
