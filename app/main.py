472af87d580a2e679552a87acb4e1f94a4453b86
Arsen Makovei <makoveyarsen@gmail.com>
2023-03-07 16:47:17 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import os
 
 
 def move_file(command: str) -> None:
-    items = command.split(" ")
+    items = command.split()
     if (
             len(items) == 3 and items[0] == "mv"
             and items[1] != items[2]
     ):
-        old_file = command.split(" ")[1]
-        new_file = command.split(" ")[2]
+        old_file = command.split()[1]
+        new_file = command.split()[2]
 
         new_path, file_name = os.path.split(new_file)
         if len(new_path) != 0:
