ff25ae28658a5d0bb84b5112339d2480b8422e93
Yura <yuraua5@gmail.com>
2023-03-08 16:35:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_split = command.split()
+    if command_split[0] == "mv" and "/" in command_split[2]:
+        os.makedirs(os.path.dirname(command_split[2]), exist_ok=True)
+    with (open(command_split[1], "r") as file_one,
+          open(command_split[2], "w") as file_two):
+        file_two.write(file_one.read())
+    os.remove(command_split[1])
