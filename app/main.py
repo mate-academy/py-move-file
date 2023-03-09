1786d8e9f34b6aff0651fa5d7e268d90e061dedf
Yura <yuraua5@gmail.com>
2023-03-09 14:30:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ def move_file(command: str) -> None:
 
     if len(command.split()) == 3:
         command, source_file_path, destination_file_path = command.split()
-        print(destination_file_path)
         if command == "mv" and os.path.split(destination_file_path)[0] != "":
             os.makedirs(os.path.dirname(destination_file_path), exist_ok=True)
         with (open(source_file_path, "r") as file_on,
