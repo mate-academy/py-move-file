a02ad292115ebe7621f0d332262e65f5b6f57be5
Dmytro Prokofiev <forbuffa@gmail.com>
2023-03-10 16:34:56 +0100

    try2

--- app/main.py
+++ app/main.py
@@ -2,18 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    if command.split()[0] == "mv" and command.split() == 3:
+    if command.split()[0] == "mv" and len(command.split()) == 3:
         command, source_file_name, new_file_info = command.split()
-        if "/" not in new_file_info:
-            new_file_name = new_file_info
-        else:
-            directories, file_name = os.path.split(new_file_info)
-            if directories:
-                os.makedirs(directories, exist_ok=True)
-            new_file_name = os.path.join(directories, file_name)
+        directories, file_name = os.path.split(new_file_info)
+        if directories:
+            os.makedirs(directories, exist_ok=True)
+        new_file_name = os.path.join(directories, file_name)
         with open(f"{source_file_name}", "r") as old_file:
             temp_text = old_file.read()
         os.remove(source_file_name)
         with open(f"{new_file_name}", "w") as new_file:
             new_file.write(temp_text)
-
