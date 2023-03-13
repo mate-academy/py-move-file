0e6e5967fc06e21b62dbb1aef5c6456426d1df6c
Svitlana Savikhina <savihina111@gmail.com>
2023-03-13 20:46:38 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from typing import Any
+import os
+
+
+def move_file(command: str) -> Any:
+    args = command.split()
+    file_start = args[1]
+    new_file = args[2]
+
+    if new_file.endswith("/"):
+        new_file_dir = new_file
+        destination_file = os.path.join(new_file_dir,
+                                        os.path.basename(file_start))
+    else:
+        new_file_dir, destination_file = os.path.split(new_file)
+
+    if new_file_dir and not os.path.exists(new_file_dir):
+        os.makedirs(new_file_dir)
+
+    os.rename(file_start, os.path.join(new_file_dir, destination_file))
