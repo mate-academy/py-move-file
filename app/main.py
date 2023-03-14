e821267d40307e46193e18ab8b56cbe0c6f1ef5e
Svitlana Savikhina <savihina111@gmail.com>
2023-03-14 20:00:32 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,9 +4,7 @@ import os
 def move_file(command: str) -> None:
     if not command.startswith("mv"):
         return
-    args = command.split()
-    file_start = args[1]
-    new_file = args[2]
+    method, file_start, new_file = command.split()
 
     if new_file.endswith("/"):
         new_file_dir = new_file
@@ -15,7 +13,7 @@ def move_file(command: str) -> None:
     else:
         new_file_dir, destination_file = os.path.split(new_file)
 
-    if new_file_dir and not os.path.exists(new_file_dir):
-        os.makedirs(new_file_dir)
+    if new_file_dir:
+        os.makedirs(new_file_dir, exist_ok=True)
 
     os.rename(file_start, os.path.join(new_file_dir, destination_file))
