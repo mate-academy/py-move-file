1c6e7a9d7ac0d8935d5f9cde8f1f43a59c8a68e0
Oleksandr <@gmail.com>
2023-03-17 07:18:29 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,30 +2,27 @@ import os
 
 
 def move_file(command: str) -> None:
+    if len(command.split()) != 3:
+        return
+
     cmd, source_file, final_file = command.split()
 
-    if (
-            len(command.split()) == 3
-            and cmd == "mv"
-            and final_file[-1] != "/"
-    ):
+    if cmd == "mv":
 
         with open(source_file) as source:
             data = source.read()
 
-        path, final_file_name = os.path.split(final_file)
+        final_path, fin_file_name = os.path.split(final_file)
 
-        if len(path) > 1:
-            directory = os.path.join(path)
+        if len(final_path) > 1:
+            directory = final_path
         else:
             directory = "."
 
         if not os.path.exists(directory):
-            os.makedirs(directory)
+            os.makedirs(directory, exist_ok=True)
 
-        new_file = os.path.join(directory, final_file_name)
-
-        with open(new_file, "w") as copy:
-            copy.write(data)
+        with open(os.path.join(directory, fin_file_name), "w") as target_file:
+            target_file.write(data)
 
         os.remove(source_file)
