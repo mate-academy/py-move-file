87e0d75bc376b1f6f7280ca37bf4a1f5b5ef87d4
Alex Isaev <lesisv@gmail.com>
2023-03-18 17:07:12 +0200

    Solution v5

--- app/main.py
+++ app/main.py
@@ -2,10 +2,7 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_list = command.split(" ")
-    instruction = command_list[0]
-    in_file_name = command_list[1]
-    new_file_destination = command_list[2]
+    instruction, in_file_name, new_file_destination = command.split()
     new_file_path = os.path.dirname(new_file_destination)
     new_file_name = os.path.basename(new_file_destination)
     current_dir = os.getcwd()
@@ -13,16 +10,10 @@ def move_file(command: str) -> None:
         with open(in_file_name, "r") as in_file:
             file_in = in_file.read()
             if "/" in new_file_destination:
-                path_list = new_file_path.split("/")
-                for folder in path_list:
-                    if not os.path.isdir(folder):
-                        os.mkdir(folder)
-                    os.chdir(folder)
-                with open(new_file_name, "w") as new_file:
-                    new_file.write(file_in)
-            else:
-                with open(new_file_name, "w") as new_file:
-                    new_file.write(file_in)
+                os.makedirs(new_file_path, exist_ok=True)
+                os.chdir(new_file_path)
+            with open(new_file_name, "w") as new_file:
+                new_file.write(file_in)
 
             os.chdir(current_dir)
             in_file.close()
