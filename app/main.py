788569e68e99799664090162b312e0133c88ce2b
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-03-18 12:40:41 +0200

    Trying to fix this condition: 'directory must be created if it does not exist'

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def move_file(command: str) -> None:
     parts = command.split()
     source_path = parts[1]
     destination_path = parts[2]
+    if not os.path.isfile(source_path):
+        raise FileNotFoundError(f"Source file {source_path} not found")
     if destination_path.endswith("/"):
         os.makedirs(destination_path, exist_ok=True)
         dst_file = os.path.join(
@@ -12,12 +14,4 @@ def move_file(command: str) -> None:
         )
     else:
         dst_file = destination_path
-    os.replace(source_path, dst_file)
-
-
-with open("file.txt", "w") as source:
-    source.write("Some\nText\n")
-move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt")
-with open("first_dir/second_dir/third_dir/file2.txt", "r") as new_files:
-    print(new_files.read())
-os.remove("first_dir/second_dir/third_dir/file2.txt")
+    os.rename(source_path, dst_file)
