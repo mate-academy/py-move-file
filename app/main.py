5363465c9f88415cf63ddb361036bf7534f8305c
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-03-18 15:55:13 +0200

    Checked for 3 arguments and set the condition when there is no file

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ import os
 def move_file(command: str) -> None:
     parts = command.split()
     source_path = parts[1]
+    if len(parts) != 3:
+        raise ValueError("Invalid format")
     destination_path = parts[2]
-    if not os.path.isfile(source_path):
-        raise FileNotFoundError(f"Source file {source_path} not found")
-    if destination_path.endswith("/"):
+    if not os.path.split(source_path) and destination_path.endswith("/"):
         os.makedirs(destination_path, exist_ok=True)
         dst_file = os.path.join(
             destination_path, os.path.basename(source_path)
