46936add590be800d7d2df0bce60c1a44a3763f9
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-03-20 19:19:47 +0200

    Solution with corrections

--- app/main.py
+++ app/main.py
@@ -2,18 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    parts = command.split()
-    if len(parts) != 3:
+    if len(command.split()) != 3:
         raise ValueError("Invalid format")
-    source_path = parts[1]
-    destination_path = parts[2]
-    if not os.path.isdir(destination_path):
-        raise FileNotFoundError(f"Source file {destination_path} not found")
-    if destination_path.endswith("/"):
-        os.makedirs(destination_path, exist_ok=True)
-        dst_file = os.path.join(
-            destination_path, os.path.basename(source_path)
-        )
-    else:
-        dst_file = destination_path
-    os.rename(source_path, dst_file)
+    md, source_path, destination_path = command.split()
+    if md not in command or source_path == destination_path:
+        raise FileNotFoundError(f"Source file {source_path} not found")
+    if not os.path.isfile(source_path):
+        raise FileNotFoundError(f"Source file {source_path} not found")
+    directories_path, file_name = os.path.split(source_path)
+    if not len(directories_path) != 0:
+        os.makedirs(directories_path, exist_ok=True)
+        os.path.join(directories_path, os.path.basename(source_path))
+    os.rename(source_path, destination_path)
