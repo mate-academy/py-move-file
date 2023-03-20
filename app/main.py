1ee0a9e318920403f5129bcd2da1a1bd013063c3
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-03-20 11:44:16 +0200

    Checked for directory and if it ends with '/'

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ import os
 
 def move_file(command: str) -> None:
     parts = command.split()
-    source_path = parts[1]
     if len(parts) != 3:
         raise ValueError("Invalid format")
+    source_path = parts[1]
     destination_path = parts[2]
-    if not os.path.split(source_path) and destination_path.endswith("/"):
+    if not os.path.isdir(destination_path):
+        raise FileNotFoundError(f"Source file {destination_path} not found")
+    if destination_path.endswith("/"):
         os.makedirs(destination_path, exist_ok=True)
         dst_file = os.path.join(
             destination_path, os.path.basename(source_path)
