1d3c31e562f8e28785d27b17c861ebef0091f3ae
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-03-21 22:53:30 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+import os
+
+
+def move_file(mv: str) -> None:
+    if len(mv.split()) != 3:
+        return
+    mv, source_file, new_file = mv.split()
+    if "/" not in new_file:
+        with open(new_file, "w") as n_file:
+            with open(source_file) as s_file:
+                n_file.write(s_file.read())
+        os.remove(source_file)
+    elif new_file[-1] == "/":
+        oll_dir = str(new_file[:-1])
+        with_first_dir = ""
+        for current_dir in oll_dir.split("/"):
+            with_first_dir = os.path.join(with_first_dir, current_dir)
+            if os.path.exists(with_first_dir):
+                os.mkdir(with_first_dir)
+        with open(os.path.join(with_first_dir, new_file), "w") as n_file:
+            with open(source_file) as s_file:
+                n_file.write(s_file.read())
+        os.remove(source_file)
+    else:
+        oll_dir = str(new_file)
+        new_file = oll_dir.split("/")[-1]
+        with_first_dir = ""
+        for current_dir in oll_dir.split("/")[:-1]:
+            with_first_dir = os.path.join(with_first_dir, current_dir)
+            if not os.path.isdir(with_first_dir):
+                os.mkdir(with_first_dir)
+        with open(os.path.join(with_first_dir, new_file), "w") as n_file:
+            with open(source_file) as s_file:
+                n_file.write(s_file.read())
+        os.remove(source_file)
