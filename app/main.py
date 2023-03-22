77be9c906cdafeb25b160d84722bf9eee2ec5d2a
Bohdan Salamakha <86322722+Bohdan-Salamakha@users.noreply.github.com>
2023-03-22 10:37:55 +0200

    Update app/main.py

--- app/main.py
+++ app/main.py
@@ -6,8 +6,13 @@ def move_file(command: str) -> None:
     if len(parts) != 3:
         raise ValueError("Invalid format")
     md, source_path, destination_path = parts
-    if md not in command or source_path == destination_path or not os.path.isfile(source_path):
-        raise FileNotFoundError(f"Source file {source_path} not found")
+    if (
+        md not in command or
+        source_path == destination_path or
+        not os.path.isfile(source_path)
+):
+    raise FileNotFoundError(f"Source file {source_path} not found")
+
     directories_path, file_name = os.path.split(source_path)
     if len(directories_path):
         os.makedirs(directories_path, exist_ok=True)
