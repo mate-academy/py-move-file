15a04d9bea10631e184973254ebda347ca4d12d0
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-03-22 16:22:36 +0200

    'Solution4'

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ import os
 def move_file(command: str) -> None:
     if len(command.split()) != 3:
         return
-    mv_, source_path, destination_path = command.split()
+    cmv, source_path, destination_path = command.split()
     some_dir, new_file = os.path.split(destination_path)
     if some_dir:
         os.makedirs(name=some_dir, exist_ok=True)
-    with open(source_path) as s_file, \
-            open(os.path.join(destination_path), "w") as n_file:
-        n_file.write(s_file.read())
+    with (
+        open(source_path) as source,
+        open(destination_path, "w") as destination
+    ):
+        destination.write(source.read())
     os.remove(source_path)
