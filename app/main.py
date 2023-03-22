2ca5790c1ca0aa58d2f5a635086c168f7231130c
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-03-22 10:41:21 +0200

    Updated app/main.py

--- app/main.py
+++ app/main.py
@@ -11,6 +11,6 @@ def move_file(command: str) -> None:
     if not os.path.isfile(source_path):
         raise FileNotFoundError(f"Source file {source_path} not found")
     directories_path, file_name = os.path.split(source_path)
-    if len(directories_path):
+    if len(directories_path) != 0:
         os.makedirs(directories_path, exist_ok=True)
     os.rename(source_path, destination_path)
