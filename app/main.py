6488675cae7368fb9f605e673084efc8356336b8
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-03-22 11:17:09 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -6,10 +6,8 @@ def move_file(mv: str) -> None:
         return
     mv, source_file, new_file = mv.split()
     if "/" not in new_file:
-        with open(new_file, "w") as n_file:
-            with open(source_file) as s_file:
-                n_file.write(s_file.read())
-        os.remove(source_file)
+        with open(new_file, "w") as n_file, open(source_file) as s_file:
+            n_file.write(s_file.read())
     elif new_file[-1] == "/":
         oll_dir = str(new_file[:-1])
         with_first_dir = ""
@@ -17,10 +15,9 @@ def move_file(mv: str) -> None:
             with_first_dir = os.path.join(with_first_dir, current_dir)
             if os.path.exists(with_first_dir):
                 os.mkdir(with_first_dir)
-        with open(os.path.join(with_first_dir, new_file), "w") as n_file:
-            with open(source_file) as s_file:
-                n_file.write(s_file.read())
-        os.remove(source_file)
+        with open(os.path.join(with_first_dir, new_file), "w") as n_file, \
+                open(source_file) as s_file:
+            n_file.write(s_file.read())
     else:
         oll_dir = str(new_file)
         new_file = oll_dir.split("/")[-1]
@@ -29,7 +26,7 @@ def move_file(mv: str) -> None:
             with_first_dir = os.path.join(with_first_dir, current_dir)
             if not os.path.isdir(with_first_dir):
                 os.mkdir(with_first_dir)
-        with open(os.path.join(with_first_dir, new_file), "w") as n_file:
-            with open(source_file) as s_file:
-                n_file.write(s_file.read())
-        os.remove(source_file)
+        with open(os.path.join(with_first_dir, new_file), "w") as n_file,\
+                open(source_file) as s_file:
+            n_file.write(s_file.read())
+    os.remove(source_file)
