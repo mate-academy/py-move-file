6d859f1ea55fe2e2286183252eb5bd29cc15e65d
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-03-29 23:35:10 +0300

    TRY

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    if len(command.split()) == 3:
+        command_id, file_name, file_path = command.split()
+        if command_id != "mv":
+            raise ValueError("Command should be 'mv'")
+
+        if "/" not in command:
+            os.rename(file_name, file_path)
+        else:
+            os.makedirs(file_path, exist_ok=True)
+            os.replace(file_name, file_path)
+
+            with (open(f"{file_name}", "r") as file_in,
+                  open(f"{file_path}", "w") as file_out):
+                file_out.write(file_in.read())
+            os.remove(file_name)
