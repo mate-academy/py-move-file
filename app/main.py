551a261473b2a3025fdeae09bd0d8d163717c150
Halyna Petrova <1208petrova@gmail.com>
2023-04-01 19:37:37 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -4,14 +4,10 @@ import os
 def move_file(command: str) -> None:
     if len(command.split()) == 3:
         command, current_file, new_file = command.split()
-        dir_name = new_file.split("/")
-        if len(dir_name) == 2:
-            os.mkdir("first_dir")
-        if len(dir_name) == 3:
-            os.mkdir("first_dir/second_dir")
-        if len(dir_name) == 4:
-            os.mkdir("first_dir/second_dir/third_dir")
+        dir_name, file_to_copy = os.path.split(new_file)
+        if len(dir_name) > 0:
+            os.makedirs(dir_name, exist_ok=True)
         with (open(current_file, "r") as file_in,
-              open(new_file, "w") as file_out):
+              open(os.path.join(dir_name, file_to_copy), "w") as file_out):
             file_out.write(file_in.read())
         os.remove(current_file)
