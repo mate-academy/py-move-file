161432d71568270076a266cdace5eab24a8f42d4
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-04-03 23:45:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,18 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) == 3:
-        command_id, file_name, file_path = command.split()
+    command_parts = command.split()
+    if len(command_parts) == 3:
+        command_id, file_name, file_path = command_parts
         if command_id != "mv":
             raise ValueError("Command should be 'mv'")
 
-        if "/" not in command:
-            os.rename(file_name, file_path)
-        else:
-            os.makedirs(file_path, exist_ok=True)
-            os.replace(file_name, file_path)
+        head, tail = os.path.split(file_path)
+        if head and not os.path.exists(head):
+            os.makedirs(head)
 
-            with (open(f"{file_name}", "r") as file_in,
-                  open(f"{file_path}", "w") as file_out):
-                file_out.write(file_in.read())
-            os.remove(file_name)
+        os.replace(file_name, file_path)
