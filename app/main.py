bf4c55721d7400e678978ee186363343eb0431ad
Mykola <nickkozlov90@gmail.com>
2023-04-04 22:58:34 +0300

    optimized

--- app/main.py
+++ app/main.py
@@ -2,25 +2,13 @@ import os
 
 
 def move_file(command: str) -> None:
-    arguments = command.split()
-    if len(arguments) != 3:
-        return
-    if arguments[0] == "mv":
-        file_to_move = arguments[1]
-        new_file_path = None
-        if "/" in arguments[2]:
-            new_file_path = arguments[2].rpartition("/")[0]
-            new_file = arguments[2].rpartition("/")[2]
-        else:
-            new_file = arguments[2]
-        with open(file_to_move, "r") as file_in:
-            content = file_in.read()
-        os.remove(file_to_move)
-        if new_file_path is not None:
+    command, file_to_move, new_file = command.split()
+    if command == "mv":
+        new_file_path = os.path.split(new_file)[0]
+        if new_file_path:
             os.makedirs(new_file_path, exist_ok=True)
-            new_file_path = os.path.join(new_file_path, new_file)
-            with open(new_file_path, "w") as file_out:
-                file_out.write(content)
-        else:
-            with open(new_file, "w") as file_out:
-                file_out.write(content)
+        with (open(file_to_move, "r") as file_in,
+              open(new_file, "w") as file_out):
+            content = file_in.read()
+            file_out.write(content)
+        os.remove(file_to_move)
