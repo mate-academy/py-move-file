1ce0020e46cfd46bd9a97ea299ddc79efbb7eec4
Dmytro Shvets <dirolius@gmail.com>
2023-04-04 15:24:55 +0300

    Done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    splitted_command = command.split()
+    if "/" not in splitted_command[-1]:
+        comm, old_name, new_name = splitted_command
+        os.rename(old_name, new_name)
+    else:
+        name_dir, new_file_name = splitted_command[-1].rsplit("/", maxsplit=1)
+        old_file_name = splitted_command[1]
+        os.makedirs(name_dir, exist_ok=True)
+
+        new_dir = name_dir + "/" + new_file_name
+        with (open(old_file_name, "r") as old_file,
+              open(new_dir, "w") as new_file):
+            new_file.write(old_file.read())
+        os.remove(old_file_name)
