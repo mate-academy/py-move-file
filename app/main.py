8901632ccb577a510ecd90cb7ab74d4c53c858fc
Mykola <nickkozlov90@gmail.com>
2023-04-05 11:49:01 +0300

    Solution4

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def move_file(command: str) -> None:
     command, file_to_move, new_file = command.split()
     if command == "mv":
         new_file_path = os.path.split(new_file)[0]
-        if new_file_path:
-            os.makedirs(new_file_path, exist_ok=True)
+        if new2_file_path:
+            os.makedirs(new2_file_path, exist_ok=True)
         with (open(file_to_move, "r") as file_in,
               open(new_file, "w") as file_out):
             content = file_in.read()
