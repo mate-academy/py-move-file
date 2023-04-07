99abc3b6d4996681e71c518a72641d467e1fffe6
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-07 21:32:29 +0300

    shorted code with os.makedirs function

--- app/main.py
+++ app/main.py
@@ -6,13 +6,8 @@ def move_file(command: str) -> None:
         command_name, source_file, directories_and_file = command.split()
 
         # prepare all child directories
-        file_destination = directories_and_file.split("/")
-        directories_number = 1
-        while directories_number < len(file_destination):
-            directory = "/".join(file_destination[:directories_number])
-            if not os.path.exists(directory):
-                os.mkdir(directory)
-            directories_number += 1
+        directories = directories_and_file[:directories_and_file.rfind("/")]
+        os.makedirs(directories, exist_ok=True)
 
         # move file
         if command_name == "mv" and source_file != directories_and_file:
