b87d8bd4d2ad11d448721dae73ffc9c0b819cfda
“Ruslan <callogan217@gmail.com>
2023-04-08 18:35:33 +0300

    Second commit

--- app/main.py
+++ app/main.py
@@ -2,20 +2,24 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_set = command.split()
-    if command_set[0] != "mv":
-        raise Exception(
-            f"You are allowed to use only 'mv' command! "
-            f"Meanwhile you have inputted {command_set[0]}!"
-        )
+    comparison_list = command.split()
+    if len(comparison_list) == 3:
+        command_set = command.split()
+        if command_set[0] != "mv":
+            raise Exception(
+                f"You are allowed to use only 'mv' command! "
+                f"Meanwhile you have inputted {command_set[0]}!"
+            )
     file_in_path = os.path.abspath(command_set[1])
     file_out_path = os.path.abspath(command_set[2])
     file_out_dir_path = os.path.split(file_out_path)[0]
     if not os.path.exists(file_out_dir_path):
-        os.makedirs(file_out_dir_path)
+        os.makedirs(file_out_dir_path, exist_ok=True)
 
-    with open(file_in_path, "r") as file_in, \
-         open(file_out_path, "w") as file_out:
+    with (
+        open(file_in_path, "r") as file_in,
+        open(file_out_path, "w") as file_out
+    ):
         file_out.write(file_in.read())
 
     os.remove(file_in_path)
