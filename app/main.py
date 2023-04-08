839d79d55b0dfc4bc319c55a3760a2052e85f2d4
“Ruslan <callogan217@gmail.com>
2023-04-08 19:06:11 +0300

    Third commit

--- app/main.py
+++ app/main.py
@@ -2,24 +2,25 @@ import os
 
 
 def move_file(command: str) -> None:
-    comparison_list = command.split()
-    if len(comparison_list) == 3:
-        command_set = command.split()
-        if command_set[0] != "mv":
-            raise Exception(
-                f"You are allowed to use only 'mv' command! "
-                f"Meanwhile you have inputted {command_set[0]}!"
-            )
-    file_in_path = os.path.abspath(command_set[1])
-    file_out_path = os.path.abspath(command_set[2])
-    file_out_dir_path = os.path.split(file_out_path)[0]
-    if not os.path.exists(file_out_dir_path):
-        os.makedirs(file_out_dir_path, exist_ok=True)
+    command_set = command.split()
+    command_type = command_set[0]
+    if command_type != "mv":
+        raise Exception(
+            f"You are allowed to use only 'mv' command! "
+            f"Meanwhile you have inputted {command_type}!"
+        )
 
-    with (
-        open(file_in_path, "r") as file_in,
-        open(file_out_path, "w") as file_out
-    ):
-        file_out.write(file_in.read())
+    if len(command_set) == 3:
+        file_in_path = os.path.abspath(command_set[1])
+        file_out_path = os.path.abspath(command_set[2])
+        file_out_dir_path = os.path.split(file_out_path)[0]
+        if not os.path.exists(file_out_dir_path):
+            os.makedirs(file_out_dir_path, exist_ok=True)
 
-    os.remove(file_in_path)
+        with (
+            open(file_in_path, "r") as file_in,
+            open(file_out_path, "w") as file_out
+        ):
+            file_out.write(file_in.read())
+
+        os.remove(file_in_path)
