1333adb204f9721c2734f707aa42ef6f4c511ca1
Volodymyr <elohimwz@gmail.com>
2023-04-12 14:34:38 +0300

    'Solution#50on50'

--- app/main.py
+++ app/main.py
@@ -3,19 +3,19 @@ import os.path
 
 def move_file(command: str) -> None:
     command_in_parts = command.split(" ")
-    need_derectory = command_in_parts[2].split("/")
-    need_derectory.pop(-1)
+    target_directory_parts = command_in_parts[2].split("/")
+    target_directory_parts.pop(-1)
     if (
         len(command_in_parts) == 3
         and command_in_parts[0] == "mv"
         and os.path.exists(command_in_parts[1])
     ):
-        ex_dir = ""
-        for directory in need_derectory:
-            ex_dir += directory
-            if not os.path.exists(ex_dir):
-                os.mkdir(ex_dir)
-            ex_dir += "/"
+        existing_directory = ""
+        for directory in target_directory_parts:
+            existing_directory += directory
+            if not os.path.exists(existing_directory):
+                os.mkdir(existing_directory)
+            existing_directory += "/"
         with (
             open(command_in_parts[2], "w") as new_file,
             open(command_in_parts[1], "r") as old_file
