3c5af0304e7ba98f0b1ff0a7329c377ec660fda2
Dmytry Bondariev <bondarievmd@gmail.com>
2023-04-12 13:00:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,20 +5,17 @@ def move_file(command: str) -> None:
     command_parts = command.split()
     if len(command_parts) != 3:
         return
-    action, file, destination = command_parts
-    destination_parts = destination.split("/")
-    if destination_parts[-1] == "":
+    action, source_file, destination = command_parts
+    destination_dir, destination_file = os.path.split(destination)
+    if destination_file == "":
         destination_is_dir = True
-        destination_parts = destination_parts[:-1]
     else:
         destination_is_dir = False
-    destination_dir = "/".join(destination_parts[:-1])
-    destination_file = destination_parts[-1]
     if destination_dir != "":
         os.makedirs(destination_dir, exist_ok=True)
     destination_path = os.path.join(destination_dir, destination_file)
     if os.path.exists(destination_path):
         os.remove(destination_path)
-    os.rename(file, destination_path)
+    os.rename(source_file, destination_path)
     if destination_is_dir:
         os.remove(destination_path)
