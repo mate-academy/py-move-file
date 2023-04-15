4eb1234f61daf537b2a9a3406ee6d23073efa9f7
Maksym Bezhevets <begik4@gmail.com>
2023-04-15 21:17:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    list_command = command.split()
+    if len(list_command) != 3 or "mv" not in list_command[0]:
+        return
+    action, file_old, path_file = list_command
+    os.renames(file_old, path_file)
