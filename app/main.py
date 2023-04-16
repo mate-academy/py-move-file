3644079810168b54c5224fe9356cf01f7ca88da2
Maksym Bezhevets <begik4@gmail.com>
2023-04-16 13:32:42 +0300

    New solution

--- app/main.py
+++ app/main.py
@@ -6,4 +6,9 @@ def move_file(command: str) -> None:
     if len(list_command) != 3 or "mv" not in list_command[0]:
         return
     action, file_old, path_file = list_command
-    os.renames(file_old, path_file)
+    if "/" in path_file:
+        path, file_name = os.path.split(path_file)
+        os.makedirs(path, exist_ok=True)
+    with open(file_old, "r") as file_first, open(path_file, "w") as file_new:
+        file_new.write(file_first.read())
+    os.remove(file_old)
