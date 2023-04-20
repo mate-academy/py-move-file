cd6ad5f61a89f6f222a6d4ac2766eb7593f8cec5
Bohdan2001007 <bogdan1991007@gmail.com>
2023-04-20 17:09:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command):
+    cmd_parts = command.split()
+
+    if len(cmd_parts) != 3 or cmd_parts[0] != "mv":
+        raise ValueError("Invalid command format. Use: mv source destination")
+
+    source, destination = cmd_parts[1], cmd_parts[2]
+
+    if destination.endswith("/"):
+        destination = os.path.join(destination, os.path.basename(source))
+
+    destination_dir = os.path.dirname(destination)
+    if destination_dir:
+        os.makedirs(destination_dir, exist_ok=True)
+
+    shutil.move(source, destination)
