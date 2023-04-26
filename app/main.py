9871610ccbb4353804fcbdd74108ecc78fc62c04
Volodymyr <elohimwz@gmail.com>
2023-04-26 16:03:56 +0300

    'Solution#please'

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import os.path
 
 
 def move_file(command: str) -> None:
-    command_in_parts = command.split(" ")
+    command_in_parts = command.split()
     if len(command_in_parts) != 3:
         return
     if (
         command_in_parts[0] == "mv"
+        and command_in_parts[1] != command_in_parts[2]
     ):
         target_directory, file_name = os.path.split(command_in_parts[2])
         if target_directory:
