04f7478bba6597d83540e0d6e787a7c96c95e4cb
Volodymyr <elohimwz@gmail.com>
2023-04-26 11:40:07 +0300

    'Solution#'

--- app/main.py
+++ app/main.py
@@ -5,17 +5,12 @@ def move_file(command: str) -> None:
     command_in_parts = command.split(" ")
     if len(command_in_parts) != 3:
         return
-    target_directory = os.path.dirname(command_in_parts[2])
     if (
         command_in_parts[0] == "mv"
-        and os.path.exists(command_in_parts[1])
     ):
-        existing_directory = ""
-        target_directory_parts = target_directory.split("/")
-        for directory in target_directory_parts:
-            existing_directory = os.path.join(existing_directory, directory)
-            if not os.path.exists(existing_directory):
-                os.makedirs(existing_directory, exist_ok=True)
+        target_directory, file_name = os.path.split(command_in_parts[2])
+        if target_directory:
+            os.makedirs(target_directory, exist_ok=True)
         with (
             open(command_in_parts[2], "w") as new_file,
             open(command_in_parts[1], "r") as old_file
