36fbb7a219209e9ef47aa2af05a0f2ed087b0f8d
Oleksandr Panasiuk <provizor.307@gmail.com>
2023-05-01 11:49:30 -0400

    Changes

--- app/main.py
+++ app/main.py
@@ -3,13 +3,10 @@ import os
 
 def move_file(command: str) -> None:
     command, old_file, new_file = command.split()
-    if "mv" in command:
-        directory = os.path.split(new_file)
-        parent = ""
-        if len(directory) > 1:
-            for path in directory[:-1]:
-                parent = os.path.join(parent, path)
-                os.makedirs(parent)
+    if "mv" == command:
+        directory, file_name = os.path.split(new_file)
+        if directory:
+            os.makedirs(directory, exist_ok=True)
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             for line in file_in:
                 file_out.write(line)
