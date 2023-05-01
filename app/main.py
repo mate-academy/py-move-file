dd43c785854a2527bc96fe2ea53a832f7e62a923
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-05-01 14:48:52 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mv_command, first_file, second_file_with_path = command.split()
+    if mv_command == "mv" and len(command.split()) == 3:
+        directories, second_file = os.path.split(second_file_with_path)
+        if directories:
+            os.makedirs(directories, exist_ok=True)
+        new_file_directory = os.path.join(directories, second_file)
+        with (
+            open(first_file, "r") as old_file,
+            open(new_file_directory, "w") as new_file
+        ):
+            new_file.write(old_file.read())
+        os.remove(first_file)
