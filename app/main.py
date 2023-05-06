5439b44d73b534def2ac57984d6b1503d092a7f8
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-05-06 20:12:44 +0300

    Using os functions

--- app/main.py
+++ app/main.py
@@ -3,24 +3,19 @@ import os
 
 def move_file(command: str) -> None:
     content = command.split()
-    mv, move_from, file_path = content
-    *directory, move_to = file_path.split("/")
-
-    if "mv" != mv or len(content) != 3:
+    if "mv" != content[0] or len(content) != 3:
         print("Invalid command. Usage: mv {from_file} {directory}/{to_file}")
         return
+
+    _, move_from, file_path = content
+    dirs_to_create, move_to = os.path.split(file_path)
+
     if move_from == move_to or move_from == file_path:
         print("Cannot create a file when that file already exists")
         return
+    elif "/" in dirs_to_create:
+        os.makedirs(dirs_to_create, exist_ok=True)
 
-    dirs_path = ""
-    for dr in directory:
-        dirs_path = os.path.join(dirs_path, dr)
-        try:
-            os.mkdir(dirs_path)
-        except FileExistsError:
-            pass
-
-    with open(move_from, "r") as _from, open(file_path, "w") as _to:
-        _to.write(_from.read())
+    with open(move_from, "r") as source, open(file_path, "w") as destination:
+        destination.write(source.read())
     os.remove(move_from)
