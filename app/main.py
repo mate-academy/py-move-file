a4c68e9b2cff87f317fe50f2839e379a57dc4041
Vlad Tetruk <vladteteruk01@gmail.com>
2023-05-06 13:23:31 +0300

    fix/create function due to avoid duplication  fix/use os.path and os.makedirs

--- app/main.py
+++ app/main.py
@@ -1,24 +1,28 @@
-from os import remove, mkdir
+from os import remove, makedirs, path
+
+
+def moving_file(input_file: str, output_file: str) -> None:
+    with (open(input_file) as file,
+          open(output_file, "w") as result_file):
+        result_file.write(file.read())
+    remove(input_file)
 
 
 def move_file(command: str) -> None:
-    mv, input_file, output_file = command.split()
-    if "/" not in output_file:
+    if len(command.split()) == 3:
+        mv, input_file, output_file = command.split()
+    else:
+        raise ValueError("check your command. There are not 3 values")
+    if not path.split(output_file):
         if input_file == output_file:
             return
-        with (open(input_file) as file,
-              open(output_file, "w") as result_file):
-            result_file.write(file.read())
-        remove(input_file)
+        moving_file(input_file=input_file, output_file=output_file)
     else:
         directory = list()
         for folder in output_file.split("/")[:-1]:
             directory.append(folder)
             try:
-                mkdir("/".join(directory))
+                makedirs(path.join(*directory), exist_ok=True)
             except OSError:
                 continue
-        with (open(input_file) as file,
-              open(output_file, "w") as result_file):
-            result_file.write(file.read())
-        remove(input_file)
+        moving_file(input_file=input_file, output_file=output_file)
