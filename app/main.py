2efced5cea9e076669f1577f5f59d09ee41656b9
Tatasha <NatashaLeonidovna319@gmail.com>
2023-05-08 23:36:08 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    cmd, origin_file, path = command.split()
+    if cmd == "mv":
+        if "/" in path:
+            part_of_path = path.split("/")
+            part_of_path.pop()
+            new_path = "/".join(part_of_path)
+            os.makedirs(new_path, exist_ok=True)
+            with open(origin_file, "r") as f:
+                date = f.read()
+            with open(path, "w") as b:
+                b.write(date)
+            os.remove(origin_file)
+        else:
+            os.rename(origin_file, path)
