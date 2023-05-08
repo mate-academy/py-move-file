11304833c6e2b0155fc2619863be6bec6e5c2fb4
Dmitry <dimior1992@gmail.com>
2023-05-08 14:47:08 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) != 3 or command.split()[0] != "mv":
+    command_parts = command.split()
+    if len(command_parts) != 3 or command_parts[0] != "mv":
         return
 
-    _, source, destination = command.split()
+    _, source, destination = command_parts
     directory = os.path.dirname(destination)
     if directory:
         os.makedirs(directory, exist_ok=True)
