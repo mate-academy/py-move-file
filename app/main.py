e81eb74adef41f56083da2d2aa407dba28309db6
Tatasha <NatashaLeonidovna319@gmail.com>
2023-05-10 01:35:01 +0300

    'Changes1'

--- app/main.py
+++ app/main.py
@@ -2,17 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    cmd, origin_file, path = command.split()
-    if cmd == "mv":
+    if "mv" in command:
+        cmd, origin_file, path = command.split()
         if "/" in path:
-            part_of_path = path.split("/")
-            part_of_path.pop()
-            new_path = "/".join(part_of_path)
-            os.makedirs(new_path, exist_ok=True)
+            folders, file = os.path.split(path)
+            os.makedirs(folders, exist_ok=True)
             with open(origin_file, "r") as f:
-                date = f.read()
+                data = f.read()
             with open(path, "w") as b:
-                b.write(date)
+                b.write(data)
             os.remove(origin_file)
         else:
             os.rename(origin_file, path)
