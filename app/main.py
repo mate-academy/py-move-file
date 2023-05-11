301c57b15223fca7a9bb49c915f568739e058d5a
Vladimir Byshevets <texnadzor.info@gmail.com>
2023-05-11 23:38:11 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 import os
-import shutil
 
 
 def move_file(command: str) -> None:
-    lst = command.split(' ')
-    if os.path.isfile(lst[1]):
-        if lst[2].find("/") > -1 and os.path.exists(lst[2]) is False:
-            new = os.path.dirname(lst[2])
-            new_f = os.path.basename(lst[2])
-            os.makedirs(new, exist_ok=True)
-            shutil.copy2(lst[1], new + '/' + new_f)
-            open(new + '/' + lst[1], "w+")
-            os.remove(lst[1])
+    com_spl = command.split()
+    cmd, file_1, path_file = com_spl
+    if len(com_spl) != 3:
+        return
+    if path_file.find("/") > -1 and os.path.exists(path_file) is False:
+        new = os.path.dirname(path_file)
+        os.makedirs(new, exist_ok=True)
+        with (
+            open(file_1, "r") as file_one,
+            open(path_file, "w") as file_new
+        ):
+            file_new.write(file_one.read())
+        os.remove(file_1)
 
-        elif os.path.isfile(lst[1]) and lst[2].find("/") < 0:
-            os.rename(lst[1], lst[2])
+    elif os.path.isfile(file_1) and path_file.find("/") < 0:
+        os.rename(file_1, path_file)
