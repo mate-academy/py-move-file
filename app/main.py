8dc36f9f1c9db1acdea84f06026f805196efd9ce
Vladyslav Kaminskyi <119516716+VladyslavKaminskyi@users.noreply.github.com>
2023-05-11 20:34:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+from os import makedirs, path, remove
+
+
+def move_file(command: str) -> None:
+    command_list = command.split()
+    if len(command.split()) != 3 or command_list[0] != "mv":
+        raise Exception("Wrong command!")
+    mv, incoming_file, new_file = command_list
+    if "/" in new_file:
+        makedirs(path.dirname(new_file), exist_ok=True)
+    with (open(incoming_file, "r") as incoming,
+          open(new_file, "w") as new):
+        new.write(incoming.read())
+        remove(incoming_file)
