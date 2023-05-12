1c8fe7a68ff7bb9560990ec115f95d4b6f572a03
Anton Piddubnyi <antonpiddubnyii@gmail.com>
2023-05-12 14:46:36 -0600

    removed redundant code

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ import os
 
 def move_file(command: str) -> None:
     command_split = command.split()
-    if len(command_split) != 3 or "mv" not in command_split[0]:
+    if len(command_split) != 3 or "mv" != command_split[0]:
         return
     cmd, file_old, path_file = command_split
+    path, file_name = os.path.split(path_file)
 
-    if "/" in path_file:
-        path, file_name = os.path.split(path_file)
+    if path:
         os.makedirs(path, exist_ok=True)
 
     with (
