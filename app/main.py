41811a0315c1b3714f6f271f0c0ce23d5f1e1422
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-15 20:15:24 +0300

    solution_2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import os
 
 
+class PlannedError(Exception):
+    """To create os.mkdir("first/second"), first directory needs to exist."""
+
+
 def move_file(command: str) -> None:
     rename_case_flow = command.split(" ")
     move_case_flow = command.split("/")
@@ -17,17 +21,19 @@ def move_file(command: str) -> None:
         path = [f"{move_case_flow[0].split(' ')[2]}"]
         mkdir_path = ""
 
-        for i in move_case_flow[1:-1]:
-            path.append(i)
-        for i in range(len(path)):
+        for path_component in move_case_flow[1:-1]:
+            path.append(path_component)
+        for new_folder in range(len(path)):
             try:
-                mkdir_path += f"{path[i]}/"
-                os.mkdir(mkdir_path)
-            except Exception as e:
-                print(f"{e}")
+
+                mkdir_path += f"{path[new_folder]}/"
+                os.makedirs(mkdir_path)
+            except PlannedError:
+                print("The path is already created. "
+                      "os.makedirs failed as the root directory "
+                      "already exists.")
 
         with (open(f"{source_name}", "r") as source_file,
               open(f"{mkdir_path + new_name}", "w") as file_copy):
-            content = source_file.read()
-            file_copy.write(content)
+            file_copy.write(source_file.read())
         os.remove(f"{source_name}")
