921b6d4997ee906937531f7a4d74d46d37fc35db
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-16 15:37:45 +0300

    Py move file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    part = command.split()
+    if len(part) != 3 or part[0] != "mv":
+        raise ValueError("Invalid command")
+    source = part[1]
+    path = part[2]
+    path_dir = os.path.dirname(path)
+    if path_dir:
+        os.makedirs(path_dir, exist_ok=True)
+    os.replace(source, path)
