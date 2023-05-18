7b8fca40ebaf13244c8c691f1ee71f716bd49fcf
Tatasha <NatashaLeonidovna319@gmail.com>
2023-05-18 17:31:36 +0300

    'Changes5'

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ import os
 def move_file(command: str) -> None:
     cmd, origin_file, path = command.split()
     if cmd == "mv":
-        if "/" in path:
-            folders, file = os.path.split(path)
+        folders, file = os.path.split(path)
+        if len(folders) > 0:
             os.makedirs(folders, exist_ok=True)
             with open(origin_file, "r") as f, open(path, "w") as e:
                 data = f.read()
