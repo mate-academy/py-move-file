2bc783c8d95e147a9b66900503286dc43c5f4014
Tatasha <NatashaLeonidovna319@gmail.com>
2023-05-20 00:26:46 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import os
 def move_file(command: str) -> None:
     command_list = command.split()
     if len(command_list) == 3:
-        cmd, origin_file, path = command_list[0], command_list[1], command_list[2]
+        cmd, origin_file, path = \
+            command_list[0], command_list[1], command_list[2]
         if cmd == "mv":
             folders, file = os.path.split(path)
             if len(folders) > 0:
