0f1415fa50426d986d3dfa093518e44f3fdcfd89
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-05-20 17:13:32 +0300

    Implement move file task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_name, source, destination = command.split()
+
+    if command_name != "mv" or len(command.split()) != 3:
+        raise Exception("You entered the wrong command!")
+
+    destination_path = os.path.dirname(destination)
+
+    if destination_path:
+        os.makedirs(destination_path, exist_ok=True)
+
+    os.replace(source, destination)
