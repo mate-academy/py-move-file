8e4fd0bb54e3fdf882d9ceb0520adce4a2042f4f
laskarj <koks.adekvat@gmail.com>
2023-05-23 20:11:51 +0200

    Task solution.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_file(commands: str) -> None:
+    commands: list = commands.split()
+    if commands[0] == "mv" and len(commands) == 3:
+        source, destination = commands[1], commands[2]
+        path_file = os.path.dirname(destination)
+        if path_file:
+            os.makedirs(path_file, exist_ok=True)
+        os.replace(source, destination)
