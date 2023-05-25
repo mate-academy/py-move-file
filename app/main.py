ed9827677c1b727420c13637a55fe30e1401bc27
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-05-25 17:28:25 +0300

    add usage of os.path.split

--- app/main.py
+++ app/main.py
@@ -3,25 +3,22 @@ import os
 
 def move_file(command: str) -> None:
     commands = command.split()
+    head = os.path.split(commands[2])[0]
+    tail = os.path.split(commands[2])[1]
     if len(commands) != 3:
         raise ValueError("The command should contain the name of the command, "
                          "source file name, and directory to move")
     elif commands[0] != "mv":
         raise ValueError("The command should begin from 'mv'")
-    elif commands[2].find("/") == -1:
+    elif head == "":
         os.rename(commands[1], commands[2])
     else:
-        if commands[2][-1] == "/":
-            if not os.path.exists(commands[2]):
-                os.makedirs(commands[2])
-            destination_path = os.path.join(commands[2], commands[1])
+        if not os.path.exists(head):
+            os.makedirs(head)
+        if tail == "":
+            destination_path = os.path.join(head, commands[1])
         else:
-            index = commands[2].rfind("/")
-            destination = commands[2][:index + 1]
-            if not os.path.exists(destination):
-                os.makedirs(destination)
             destination_path = commands[2]
-
         source_path = commands[1]
         with (
             open(source_path, "r") as source_file,
