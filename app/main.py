6c9c1c091fe60ca553be44c279dea4120f21c594
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-05-26 22:00:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,6 @@ def move_file(command: str) -> None:
             path = os.path.dirname(file_path)
             if len(path) > 1:
                 os.makedirs(path, exist_ok=True)
-            with open(file_in, "r") as f_in, open(file_path, "w") as f_out:
-                f_out.write(f_in.read())
+            with open(file_in, "r") as file_entry, open(file_path, "w") as file_out:
+                file_out.write(file_entry.read())
             os.remove(file_in)
