778d33f4d59bab4256361931ad1d56e00f12d201
Andrii Kuzmenkov <kuzmenkov.andr@gmail.com>
2023-05-29 17:36:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    try:
+        cmd, file, path = command.split()
+    except ValueError:
+        print("Command should have 2 arguments")
+        return
+
+    if cmd != "mv":
+        return
+
+    dirs = "/".join(path.split("/")[:-1])
+
+    if dirs:
+        os.makedirs(dirs)
+
+    with open(file, "r") as source, open(path, "w") as target:
+        target.write(source.read())
+
+    os.remove(file)
+
+
+move_file("mv file.txt file2.txt")
