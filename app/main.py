dbecf182a36a6042dfb5240dba15373ae82d7ab7
Andrii Kuzmenkov <kuzmenkov.andr@gmail.com>
2023-05-29 18:12:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,18 +3,21 @@ import os
 
 def move_file(command: str) -> None:
     try:
-        cmd, file, path = command.split()
+        command, file, path = command.split()
     except ValueError:
         print("Command should have 2 arguments")
         return
 
-    if cmd != "mv":
+    if command != "mv":
         return
 
-    dirs = "/".join(path.split("/")[:-1])
+    dirs, target_file = os.path.split(path)
 
     if dirs:
-        os.makedirs(dirs)
+        try:
+            os.makedirs(dirs)
+        except FileExistsError:
+            pass
 
     with open(file, "r") as source, open(path, "w") as target:
         target.write(source.read())
