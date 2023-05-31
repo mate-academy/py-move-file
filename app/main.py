467e24439ae51fc43210ac36a428c9b13ebe2eff
Glieb <gleblitvinenko3007@gmail.com>
2023-05-31 21:56:29 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 import os
 
 
-def move_file(commnad: str) -> None:
-    try:
-        command, current_file_name, new_file_path = commnad.split()
-    except ValueError:
-        print("Command must have 3 arguments")
-        return
-    if current_file_name == new_file_path or command != "mv":
-        return
-    destination_path = os.path.dirname(new_file_path)
-    if destination_path:
-        os.makedirs(destination_path, exist_ok=True)
-    os.replace(current_file_name, new_file_path)
+def move_file(command: str) -> None:
+    if len(command.split()) != 3:
+        raise Exception("Invalid command")
+    command, current_file_name, new_file_path = command.split()
+    if command != "mv":
+        raise Exception("Command not found")
+    destination_dir, new_file_name = os.path.split(new_file_path)
+    if destination_dir:
+        os.makedirs(destination_dir, exist_ok=True)
+    os.replace(current_file_name, os.path.join(destination_dir, new_file_name))
 
 
 if __name__ == "__main__":
