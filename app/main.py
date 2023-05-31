200f66cfe21a8c53ee25ca3e033288015b57d334
Glieb <gleblitvinenko3007@gmail.com>
2023-05-31 18:23:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(commnad: str) -> None:
+    try:
+        command, current_file_name, new_file_path = commnad.split()
+    except ValueError:
+        print("Command must have 3 arguments")
+        return
+    if current_file_name == new_file_path or command != "mv":
+        return
+    destination_path = os.path.dirname(new_file_path)
+    if destination_path:
+        os.makedirs(destination_path, exist_ok=True)
+    os.replace(current_file_name, new_file_path)
+
+
+if __name__ == "__main__":
+    move_file("mv file.txt f/p/file1.txt")
