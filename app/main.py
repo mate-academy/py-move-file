7f692d83dd7175ff556673892f62f1557147c506
yuro <yuro740@gmail.com>
2023-06-02 16:57:12 +0300

    Solution deuxieme

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
 from os import rename, path, makedirs, replace
 
 
-def move_file(line: str) -> None:
-    line_lst = line.split()
-    if len(line_lst) != 3 or line_lst[0] != "mv":
+def move_file(command: str) -> None:
+    if len(command.split()) != 3 or command.split()[0] != "mv":
         raise ValueError("must be: keyword *mv* and two arguments")
-    line_src = line_lst[1]
-    line_dst = line_lst[2]
-    if path.isfile(line_src) is False or not path.split(line_dst)[1]:
+    source_line, destination_line = command.split()[1:]
+    destination_location, destination_file = path.split(destination_line)
+    source_location, source_file = path.split(source_line)
+    if path.isfile(source_line) is False or not destination_file:
         raise ValueError("source file is not exist or error in 2nd argument")
-    if path.split(line_src)[0] == path.split(line_dst)[0]:
-        rename(line_src, line_dst)
+    if source_location == destination_location:
+        rename(source_line, destination_line)
     else:
-        head_dst = path.split(line_dst)[0]
-        makedirs(head_dst, exist_ok=True)
-        if path.isdir(head_dst) is False:
+        makedirs(destination_location, exist_ok=True)
+        if path.isdir(destination_location) is False:
             raise ValueError("can't create directory destination")
-        replace(line_src, line_dst)
+        replace(source_line, destination_line)
