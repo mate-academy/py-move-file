1ee9afa68c1e50b97261e0be83f0bf4c5716f9c7
Munkoor <eee1lvlboss@gmail.com>
2023-06-02 18:09:54 +0300

    Simplify a code

--- app/main.py
+++ app/main.py
@@ -9,11 +9,7 @@ def move_file(command: str) -> None:
 
         if comm == "mv":
             path, _ = os.path.split(file_path)
-            if not path:
-                path = "./"
-            if file_path.endswith("/"):
-                path = file_path
-            elif len(path) > 1 and not os.path.exists(path):
+            if path:
                 os.makedirs(path, exist_ok=True)
 
             with open(file_in, "r") as entry_file, open(file_path,
