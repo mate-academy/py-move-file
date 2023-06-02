be5cb274604543e0f45ab38a602a2fc45ed4dd95
yuro <yuro740@gmail.com>
2023-06-02 17:38:14 +0300

    Solution troisieme

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ from os import rename, path, makedirs, replace
 def move_file(command: str) -> None:
     if len(command.split()) != 3 or command.split()[0] != "mv":
         raise ValueError("must be: keyword *mv* and two arguments")
-    source_line, destination_line = command.split()[1:]
-    destination_location, destination_file = path.split(destination_line)
-    source_location, source_file = path.split(source_line)
-    if path.isfile(source_line) is False or not destination_file:
+    source_path, destination_path = command.split()[1:]
+    destination_dirs, destination_filename = path.split(destination_path)
+    source_dirs, source_filename = path.split(source_path)
+    if path.isfile(source_path) is False or not destination_filename:
         raise ValueError("source file is not exist or error in 2nd argument")
-    if source_location == destination_location:
-        rename(source_line, destination_line)
+    if source_dirs == destination_dirs:
+        rename(source_path, destination_path)
     else:
-        makedirs(destination_location, exist_ok=True)
-        if path.isdir(destination_location) is False:
+        makedirs(destination_dirs, exist_ok=True)
+        if path.isdir(destination_dirs) is False:
             raise ValueError("can't create directory destination")
-        replace(source_line, destination_line)
+        replace(source_path, destination_path)
