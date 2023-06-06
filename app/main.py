2af754dfa3754342de2334b54b71acabe47bd9ca
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-06 17:36:47 +0300

    mv implemented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+from shutil import move
+from os import makedirs
+from os.path import dirname
+
+
+def move_file(command: str) -> None:
+    params = command.split()
+    if len(params) == 3 and params[0] == "mv":
+        dest_dir = dirname(params[2])
+        if dest_dir != "":
+            makedirs(dest_dir, exist_ok=True)
+        move(params[1], params[2])
