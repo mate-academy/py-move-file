a19cb84f913edef986dfb56850fad115387a9b74
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-06-07 19:14:35 +0200

    Refactor

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 import os
 
-from contextlib import contextmanager
 from pathlib import Path
 
 
-@contextmanager
 def move_file(command: str) -> None:
     try:
         command, source, target = command.split(maxsplit=3)
@@ -20,12 +18,7 @@ def move_file(command: str) -> None:
         if not os.path.exists(target_directory):
             target_directory.mkdir(parents=True, exist_ok=True)
 
-    source_file = open(source, "r")
-    target_file = open(target, "w")
-
-    try:
+    with open(source, "r") as source_file, open(target, "w") as target_file:
         target_file.write(source_file.read())
-    finally:
-        source_file.close()
-        target_file.close()
-        os.remove(source)
+
+    os.remove(source)
