e982e65e1db064082c490e6703a441287dd024d3
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-07 17:44:08 +0300

    Completely rewritten using exceptions handling

--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
 from shutil import move
 from os import makedirs
-from os.path import dirname
+from os.path import dirname, exists
+
+
+class ArgumentException(Exception):
+    pass
 
 
 def move_file(command: str) -> None:
-    params = command.split()
-    if len(params) == 3 and params[0] == "mv":
-        dest_dir = dirname(params[2])
-        if dest_dir != "":
+    try:
+        cmd, source, destination = command.split()
+        if cmd != "mv":
+            raise ArgumentException("Unknown command")
+        if not exists(source):
+            raise FileNotFoundError(f"Unable to find the "
+                                    f"source file at '{source}'")
+    except ValueError:
+        print("You have to provide 3 parameters in the command string")
+    except ArgumentException as er:
+        print(f"Invalid arguments: {er}")
+    except FileNotFoundError as er:
+        print(er)
+    except Exception as er:
+        print(f"Unexpected error: {er}")
+    else:
+        dest_dir = dirname(destination)
+        if dest_dir:
             makedirs(dest_dir, exist_ok=True)
-        move(params[1], params[2])
+        move(source, destination)
