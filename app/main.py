4307c58618d5f185049c471a770655bb69b9943f
Nikita <melniknikita2002@icloud.com>
2023-06-08 21:45:43 +0300

    task

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ import os
 
 def move_file(command: str) -> None:
     command_parts = command.split(" ")
+    if command_parts[0] != "mv" or len(command_parts) != 3:
+        raise ValueError("Command is incorrect")
     file_name = command_parts[1]
     new_file_path = command_parts[2]
     dir_list = command_parts[2].split("/")[:-1]
-    if command_parts[0] != "mv":
-        raise ValueError('There is not "mv" command')
     path = ""
-    for elem in dir_list:
-        path = os.path.join(path, elem)
+    for directory in dir_list:
+        path = os.path.join(path, directory)
         if not os.path.exists(path):
             os.mkdir(path)
     with (open(file_name, "r") as file_in,
