a97a9e63c9c32169bb21d79deca115cd2b68f3d6
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-08 13:26:20 +0300

    Doing things pythonic way (i think)

--- app/main.py
+++ app/main.py
@@ -3,28 +3,19 @@ from os import makedirs
 from os.path import dirname, exists
 
 
-class ArgumentException(Exception):
-    pass
-
-
 def move_file(command: str) -> None:
     try:
         cmd, source, destination = command.split()
         if cmd != "mv":
-            raise ArgumentException("Unknown command")
+            raise ValueError("Unknown command")
         if not exists(source):
             raise FileNotFoundError(f"Unable to find the "
                                     f"source file at '{source}'")
-    except ValueError:
-        print("You have to provide 3 parameters in the command string")
-    except ArgumentException as er:
-        print(f"Invalid arguments: {er}")
-    except FileNotFoundError as er:
-        print(er)
-    except Exception as er:
-        print(f"Unexpected error: {er}")
-    else:
         dest_dir = dirname(destination)
         if dest_dir:
             makedirs(dest_dir, exist_ok=True)
         move(source, destination)
+    except (ValueError, FileNotFoundError) as er:
+        print(f"Error: {er}")
+    except Exception as er:
+        print(f"Unexpected error: {er}")
