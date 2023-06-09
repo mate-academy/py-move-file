f2f0a58bfc233c37e9e9833d9cf9f5c86bd6ba0a
Nikita <melniknikita2002@icloud.com>
2023-06-09 12:50:35 +0300

    task

--- app/main.py
+++ app/main.py
@@ -3,18 +3,14 @@ import os
 
 def move_file(command: str) -> None:
     command_parts = command.split(" ")
-    if command_parts[0] != "mv" or len(command_parts) != 3:
+    if len(command_parts) != 3 or command_parts[0] != "mv":
         raise ValueError("Command is incorrect")
-    file_name = command_parts[1]
-    new_file_path = command_parts[2]
-    dir_list = command_parts[2].split("/")[:-1]
-    path = ""
-    for directory in dir_list:
-        path = os.path.join(path, directory)
-        if not os.path.exists(path):
-            os.mkdir(path)
+    file_name, new_file_with_path = command_parts[1:]
+    path_to_file = os.path.split(new_file_with_path)[0]
+    if len(path_to_file) != 0:
+        os.makedirs(path_to_file, exist_ok=True)
     with (open(file_name, "r") as file_in,
-          open(new_file_path, "w") as file_out):
+          open(new_file_with_path, "w") as file_out):
         content = file_in.read()
         file_out.write(content)
     os.remove(file_name)
