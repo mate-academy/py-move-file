8f0a0d532fea59ca2fdae77294cbed5a491161cf
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-09 20:27:37 +0300

    Fixed the split lines

--- app/main.py
+++ app/main.py
@@ -8,8 +8,11 @@ def move_file(command: str) -> None:
         if root[0] != "":
             os.makedirs(root[0], exist_ok=True)
 
-        with open(source_file) as first_file, \
-                open(destination_file, "w") as result_file:
+        with (
+            open(source_file) as first_file,
+            open(destination_file, "w") as result_file
+        ):
+
             data = first_file.read()
             result_file.write(data)
         os.remove(source_file)
