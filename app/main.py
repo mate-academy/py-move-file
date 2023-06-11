61ef04390875643ce93e26a20fc966c85f6aced1
Oleksii Kolii <igravkompik@gmail.com>
2023-06-12 00:03:50 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import os
+
+
+class CommandError(Exception):
+    pass
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+    if len(command) != 3:
+        raise CommandError(f"Wrong format of command: {command}")
+
+    if command[0] != "mv":
+        raise CommandError(f"Wrong command:"
+                           f"'{command[0]}', did you mean 'mv'?")
+
+    file_name, full_path = command[1], command[2]
+    only_path = os.path.split(full_path)[0]
+    if only_path:
+        os.makedirs(only_path, exist_ok=True)
+    with (open(file_name, "r") as source, open(full_path, "w") as write_file):
+        write_file.write(source.read())
+    os.remove(file_name)
