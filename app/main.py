7800e4f8a105cca896fb5e6744a907336c8b7ef8
Anton Shpakovych <shpakovych18@gmail.com>
2023-06-11 21:04:19 +0300

    refactor3

--- app/main.py
+++ app/main.py
@@ -5,17 +5,18 @@ import shutil
 def move_file(command: str) -> bool:
     try:
         command, file, new_file = command.split(" ")
+        if command != "mv" or file == new_file:
+            raise ValueError
+
         consist_directory = "/" in new_file
 
-        if command != "mv":
-            raise ValueError
-        elif consist_directory:
+        if consist_directory:
             dir_path = os.path.dirname(new_file)
 
             if not os.path.exists(dir_path):
                 os.makedirs(dir_path)
             shutil.move(file, new_file)
-        elif file != new_file:
+        else:
             os.rename(file, new_file)
     except (ValueError, FileNotFoundError, PermissionError):
         return False
