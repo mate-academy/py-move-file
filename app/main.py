dbadf490ed91ffe0570914bb4dce83233b969819
Diana Shyrokikh <diashiroks@gmail.com>
2023-06-11 14:09:22 +0300

    Added validation

--- app/main.py
+++ app/main.py
@@ -5,9 +5,29 @@ from os import (
 )
 
 
+class MoveCommandSyntaxError(Exception):
+    pass
+
+
 def move_file(command: str) -> None:
-    command = command.split(" ")
+    command = command.split()
+
+    if command[0] != "mv":
+        raise MoveCommandSyntaxError("Move command not found")
+
+    if len(command) != 3:
+        raise MoveCommandSyntaxError(
+            "Invalid move command syntax.\n"
+            "For a successful moving of file "
+            "the command should look like this: "
+            "mv file.txt some_dir/new_file.txt"
+        )
+
     file_to_move = command[1]
+
+    if not path.exists(file_to_move):
+        raise FileNotFoundError(f"File {file_to_move} does not exist")
+
     path_of_new_file = command[-1].split("/")
     new_file = path_of_new_file[-1]
     new_file_path = ""
@@ -23,7 +43,6 @@ def move_file(command: str) -> None:
     new_file = path.join(new_file_path, new_file)
 
     with open(file_to_move, "r") as source, open(new_file, "w") as moved:
-        for line in source:
-            moved.write(line)
+        moved.write(source.read())
 
     remove(f"{file_to_move}")
