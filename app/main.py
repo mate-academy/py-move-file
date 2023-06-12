aeb8d11f15475821547eb0931b8fb180d681c329
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-06-12 22:31:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    try:
+        cmd, source, destination = command.split()
+        if cmd != "mv":
+            raise ValueError("Invalid command!")
+        if not os.path.exists(source):
+            raise FileNotFoundError(f"{source} doesn't exists!")
+        if destination.endswith("/"):
+            raise ValueError("There's no name for new file!")
+
+        dest_dir = os.path.dirname(destination)
+
+        if dest_dir:
+            os.makedirs(dest_dir, exist_ok=True)
+        shutil.move(source, destination)
+
+    except (ValueError, FileNotFoundError, PermissionError) as error:
+        print(f"There was an error: {error}")
+    except Exception:
+        print("There was an unexpected error")
