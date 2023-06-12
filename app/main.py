1dd3cf3cab40bf136fc21a7c01d599187eb7354a
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-12 19:01:30 +0300

    Made changes

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def move_file(command: str) -> None:
     current_command, source_file, destination_file = command.split()
     if current_command == "mv":
         root, _ = os.path.split(destination_file)
-        if root != "":
+        if root:
             os.makedirs(root, exist_ok=True)
 
         with (
