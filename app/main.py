56dcc824ca77d72d76fe04bbded5f71235ad981b
Vitalii Shevchuk <shevch.nl@gmail.com>
2023-06-12 16:45:25 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -7,15 +7,14 @@ def move_file(command: str) -> None:
     if len(parts) != 3:
         raise ValueError("Invalid command")
 
-    if parts[0] != "mv":
+    cmd, source_path, destination_path = parts
+
+    if cmd != "mv":
         raise ValueError("Command not found")
 
-    current_file_path = parts[1]
-    new_file_path = parts[2]
-
-    destination_dir, new_file_name = os.path.split(new_file_path)
+    destination_dir, new_file_name = os.path.split(destination_path)
 
     if destination_dir:
         os.makedirs(destination_dir, exist_ok=True)
 
-    os.replace(current_file_path, os.path.join(destination_dir, new_file_name))
+    os.replace(source_path, os.path.join(destination_dir, new_file_name))
