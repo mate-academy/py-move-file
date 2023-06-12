27a3f3d4b8aed3b9620dd37ebb7e8415f49d25e7
Tarasidze <dtar84@ukr.net>
2023-06-12 21:29:26 +0300

    Solution 1.02

--- app/main.py
+++ app/main.py
@@ -2,22 +2,21 @@ import shutil
 import os
 
 
-def move_file(command: str) -> any:
-    commands = command.split(" ")
+def move_file(command: str) -> None:
+    commands = command.split()
 
     if len(commands) == 3 and command.startswith("mv "):
         _, first_file_name, path_to_second_file = commands
-        bare_path_to_second_file = os.path.dirname(path_to_second_file)
-        second_file_name = path_to_second_file.split("/")[-1]
+        (path_to_second_file,
+            second_file_name) = os.path.split(path_to_second_file)
 
-        try:
-            if "/" not in path_to_second_file:
-                os.rename(first_file_name, second_file_name)
-                return
-        except FileNotFoundError as e:
-            return str(e)
+        if path_to_second_file == "":
+            os.rename(first_file_name, second_file_name)
+            return
 
-        if not os.path.exists(bare_path_to_second_file):
-            os.makedirs(bare_path_to_second_file)
+        if not os.path.exists(path_to_second_file):
+            os.makedirs(path_to_second_file)
 
+        path_to_second_file = os.path.join(path_to_second_file,
+                                           second_file_name)
         shutil.move(first_file_name, path_to_second_file)
