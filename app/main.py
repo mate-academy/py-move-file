af8d3632048c29d0b87ddd987757ce662615c614
Soloveyk1n <swain.volt@gmail.com>
2023-06-12 09:41:26 +0300

    fixing and changing

--- app/main.py
+++ app/main.py
@@ -4,25 +4,18 @@ import os
 def move_file(command: str) -> None:
     command_list = command.split()
     mv, incoming_file, new_file = command_list
-    new_file_list = new_file.split("/")
     dir_path, file_name = os.path.split(new_file)
     if len(command_list) != 3:
-        raise IndexError("list index out of range")
+        raise IndexError("command is wrong")
 
     if mv == "mv":
         directory = os.path.join(dir_path)
         if dir_path:
             os.makedirs(directory, exist_ok=True)
         with (
-            open(incoming_file, "r") as incoming,
-            open(new_file, "w") as new
+            open(incoming_file, "r") as source_file,
+            open(new_file, "w") as destination_file
         ):
 
-            new.write(incoming.read())
-        os.replace(incoming_file, new_file_list[-1])
+            destination_file.write(source_file.read())
         os.remove(incoming_file)
-
-
-print(open("file.txt").read())
-move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt")
-print(open("first_dir/second_dir/third_dir/file2.txt").read())
