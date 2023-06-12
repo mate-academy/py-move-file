1f42ef34a1cc3541192a907a66f0490b328971c0
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-12 19:00:37 +0300

    Used the unpacking

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import os
 def move_file(command: str) -> None:
     current_command, source_file, destination_file = command.split()
     if current_command == "mv":
-        root = os.path.split(destination_file)
-        if root[0] != "":
-            os.makedirs(root[0], exist_ok=True)
+        root, _ = os.path.split(destination_file)
+        if root != "":
+            os.makedirs(root, exist_ok=True)
 
         with (
             open(source_file) as first_file,
