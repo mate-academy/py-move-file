8673d116a141ae483eb091307bac856310c916d3
Yaryk Konoplyov <kannabis252@gmail.com>
2023-06-13 14:11:37 +0300

    unpacking used for creating variables:command, file_orig and path

--- app/main.py
+++ app/main.py
@@ -2,14 +2,13 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_list = command.split()
+    command, file_orig, path = command.split()
 
-    if command_list[0] != "mv":
+    if command != "mv":
         return
 
-    file_orig = command_list[1]
-    file_copy = os.path.split(command_list[2])[-1]
-    path = command_list[2].replace(file_copy, "")
+    file_copy = os.path.split(path)[-1]
+    path = path.replace(file_copy, "")
 
     if path and not os.path.exists(path):
         os.makedirs(path)
