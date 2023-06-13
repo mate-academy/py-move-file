6df8cb515ab0eeb1653f1dafb3315606393424be
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-13 18:33:17 +0300

    Tried to fix all)

--- app/main.py
+++ app/main.py
@@ -3,14 +3,9 @@ import os
 
 def move_file(command: str) -> None:
     command, origin, copy = command.split()
-    if command != "mv":
-        print("Wrong command")
-        return
-    try:
+    if command == "mv" and os.path.isfile(origin):
         destination_path = os.path.dirname(copy)
         if destination_path:
             if not os.path.exists(destination_path):
                 os.makedirs(destination_path)
         os.rename(origin, copy)
-    except FileNotFoundError:
-        print("There is no file with this name")
