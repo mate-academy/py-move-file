24ad68574f4d2439df767e6f8675e929c008b193
mark.shapka <mark.shapka1@gmail.com>
2023-06-14 19:51:51 +0300

    changed if-condition and file will delete after close the file

--- app/main.py
+++ app/main.py
@@ -4,15 +4,17 @@ import os
 
 def move_file(command: str) -> None:
     words = re.split(r"[ ]", command)
-    if words[0] == "mv":
-        path_words = re.split(r"[/]", words[2])
-        current_path = ""
-        for folder in path_words[: -1]:
-            current_path += folder
-            if not os.path.exists(current_path):
-                os.mkdir(current_path)
-            current_path += "/"
+    if words[0] != "mv":
+        return
 
-        with open(words[1], "r") as file_in, open(words[2], "w") as file_out:
-            file_out.write(file_in.read())
-            os.remove(words[1])
+    path_words = re.split(r"[/]", words[2])
+    current_path = ""
+    for folder in path_words[: -1]:
+        current_path += folder
+        if not os.path.exists(current_path):
+            os.mkdir(current_path)
+        current_path += "/"
+
+    with open(words[1], "r") as file_in, open(words[2], "w") as file_out:
+        file_out.write(file_in.read())
+    os.remove(words[1])
