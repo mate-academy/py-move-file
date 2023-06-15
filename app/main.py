c823eb6c72709b911ca7cd61d3022725d6a3abdd
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-15 18:37:36 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -3,23 +3,21 @@ import os
 
 def move_file(command: str) -> None:
 
-    mv, old_file_name, new_path_file_name = command.split(" ")
+    mv, old_file_name, new_path_file_name = command.split()
 
-    if new_path_file_name[-1] == "/":
-        directories = new_path_file_name
+    path = os.path.dirname(new_path_file_name)
+
+    new_file_name = new_path_file_name.split("/")[-1]
+
+    if not new_file_name:
         new_file_name = old_file_name
-    else:
-        new_file_name = new_path_file_name.split("/")[-1]
-        directories = new_path_file_name.replace(new_file_name, "")
 
-    try:
-        if directories != "":
-            os.makedirs(directories)
-    except FileExistsError:
-        pass
+    if path:
+        os.makedirs(path, exist_ok=True)
+        path += "/"
 
     with (open(old_file_name, "r") as old_file,
-          open(directories + new_file_name, "w") as new_file):
+          open(path + new_file_name, "w") as new_file):
         new_file.write(old_file.read())
 
     os.remove(old_file_name)
