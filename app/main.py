95aba7ff02c9f9e1e356a827b8fc71f8c0d45f0d
Vlad Larin <lordgadyka71@gmail.com>
2023-06-16 18:19:53 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from os import mkdir, path, remove
+from shutil import copy
+
+
+def move_file(command: str) -> None:
+    command_list = command.split()
+    if len(command_list) == 3:
+        mv_command, file_in_name, file_out_path = command_list
+        if mv_command == "mv" and file_in_name != file_out_path:
+            if "/" in file_out_path:
+                dir_path_list = file_out_path.split("/")[:-1]
+                current_dir_path = ""
+                for directory in dir_path_list:
+                    current_dir_path += directory + "/"
+                    if not path.exists(current_dir_path):
+                        mkdir(current_dir_path)
+            copy(file_in_name, file_out_path)
+            remove(file_in_name)
