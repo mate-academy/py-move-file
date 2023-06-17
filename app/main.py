777632b39b2b8b14f716174117b98f5b0068b1ca
Denys Gordiichuk <dd111320@gmail.com>
2023-06-17 20:27:57 +0300

    finish

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, origin, path = command.split()
+
+    if command == "mv":
+
+        destination = os.path.dirname(path)
+        if destination:
+            if not os.path.exists(destination):
+                os.makedirs(destination)
+
+        with open(origin, "r") as f_out, open(path, "w") as f_in:
+            f_in.write(f_out.read())
+
+        os.remove(origin)
