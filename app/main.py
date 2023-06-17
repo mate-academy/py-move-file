32f677a96187417525e97983ff46046ebb830c3a
Oleg Fadeev <drivemax26@gmail.com>
2023-06-17 18:07:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 import os
 
 
-def move_file(command):
-    command = command.split()
-    if "/" not in command[2]:
-        os.rename(command[1], command[2])
+def move_file(command: str) -> None:
+    command_list = command.split()
+    if len(command_list) < 3 or command_list[0] != "mv":
+        print("command wrong")
+    if "/" not in command_list[2]:
+        os.rename(command_list[1], command_list[2])
 
-    patch = "/".join(command[2].split('/')[0:-1])
-    name = command[2].split('/')[-1]
+    patch, name = os.path.split(command_list[2])
     os.rename(command[1], name)
     os.makedirs(patch)
-
-    os.replace(name, f"{patch}/{name}")
+    os.replace(name, command_list[2])
