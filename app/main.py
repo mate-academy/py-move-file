0669f0320de70bab87672ecd342ec1afc65b3128
Oleg Fadeev <drivemax26@gmail.com>
2023-06-17 19:09:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,10 +3,14 @@ import os
 
 def move_file(command: str) -> None:
     command_list = command.split()
+
     if len(command_list) < 3 or command_list[0] != "mv":
         print("command wrong")
+        return
+
     if "/" not in command_list[2]:
         os.rename(command_list[1], command_list[2])
+        return
 
     patch, name = os.path.split(command_list[2])
     os.rename(command[1], name)
