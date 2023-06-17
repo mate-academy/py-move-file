b97eb54f1aa97c6dd7458ec3a9be03b7dac87188
Vlad Larin <lordgadyka71@gmail.com>
2023-06-17 16:18:50 +0300

    solution3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from os import mkdir, path, remove
-from shutil import copy
+from os import makedirs, remove, path
 
 
 def move_file(command: str) -> None:
@@ -7,12 +6,11 @@ def move_file(command: str) -> None:
     if len(command_list) == 3:
         mv_command, file_in_name, file_out_path = command_list
         if mv_command == "mv" and file_in_name != file_out_path:
-            if "/" in file_out_path:
-                file_out_path_tuple = tuple(file_out_path.split("/"))
-                for i in range(1, len(file_out_path_tuple)):
-                    new_dir_path = path.join(*file_out_path_tuple[:i])
-                    if not path.exists(new_dir_path):
-                        mkdir(new_dir_path)
-                file_out_path = path.join(*file_out_path_tuple)
-            copy(file_in_name, file_out_path)
+            file_out_path = path.normpath(file_out_path)
+            file_out_dir = path.dirname(file_out_path)
+            if file_out_dir:
+                makedirs(file_out_dir, exist_ok=True)
+            with (open(file_in_name, "r") as file_in,
+                  open(file_out_path, "w") as file_out):
+                file_out.write(file_in.read())
             remove(file_in_name)
