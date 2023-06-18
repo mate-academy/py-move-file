845fa5fe130f5e307317be5b868e7955252e1078
Denys Gordiichuk <dd111320@gmail.com>
2023-06-18 20:23:52 +0300

    finish

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    mv, origin, path = command.split()
 
-    if len(command.split()) == 3 and mv == "mv":
+    if len(command.split()) == 3:
+        mv, origin, path = command.split()
 
-        destination = os.path.dirname(path)
-        if destination:
-            os.makedirs(destination, exist_ok=True)
+        if mv == "mv":
+            destination = os.path.dirname(path)
+            if destination:
+                os.makedirs(destination, exist_ok=True)
 
         with open(origin, "r") as read_file, open(path, "w") as write_file:
             write_file.write(read_file.read())
