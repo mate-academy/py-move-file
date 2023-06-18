9a1e9affa0d6364a381b98d057a178e659c9e7c9
Denys Gordiichuk <dd111320@gmail.com>
2023-06-18 18:40:15 +0300

    finish

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import os
 
 
 def move_file(command: str) -> None:
-    command, origin, path = command.split()
+    mv, origin, path = command.split()
 
-    if command == "mv":
+    if len(command.split()) == 3 and mv == "mv":
 
         destination = os.path.dirname(path)
         if destination:
