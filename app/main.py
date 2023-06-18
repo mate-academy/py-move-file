6ebf655462cfc4d3caf8ac47518ff697f416d3ac
Denys Gordiichuk <dd111320@gmail.com>
2023-06-18 20:55:01 +0300

    finish

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def move_file(command: str) -> None:
             if destination:
                 os.makedirs(destination, exist_ok=True)
 
-        with open(origin, "r") as read_file, open(path, "w") as write_file:
-            write_file.write(read_file.read())
+            with open(origin, "r") as read_file, open(path, "w") as write_file:
+                write_file.write(read_file.read())
 
-        os.remove(origin)
+            os.remove(origin)
