1f439b255fd52cd2a3dfc6695f737fda3d0af3b0
arturiermolenko <arturiermolenko@gmail.com>
2023-06-22 09:24:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_name, file_name, path_to_new_file = command.split()
+    if command_name == "mv" and os.path.isfile(file_name):
+        new_file = os.path.dirname(path_to_new_file)
+        if new_file:
+            if not os.path.exists(new_file):
+                os.makedirs(new_file)
+        os.rename(file_name, path_to_new_file)
