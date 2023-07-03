e82eda0a50e6c4de683b370ecd388a4093b75cda
Andrew Sulym <a.sulymm@gmail.com>
2023-07-03 16:46:39 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    split_command = command.split()
+    if len(split_command) != 3:
+        raise ValueError("Invalid command")
+    source_command, source_path, destination_path = split_command
+    destination_directory = os.path.dirname(destination_path)
+
+    if destination_directory:
+        os.makedirs(destination_directory, exist_ok=True)
+    os.replace(source_path, destination_path)
