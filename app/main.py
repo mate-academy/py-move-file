9705b60f78b7980154a67d7078b9877e5b0eee80
Andrew Sulym <a.sulymm@gmail.com>
2023-07-04 11:24:56 +0100

    Fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import os
 
 def move_file(command: str) -> None:
     split_command = command.split()
-    if len(split_command) != 3:
+    if len(split_command) != 3 and split_command[0] == "mv":
         raise ValueError("Invalid command")
     source_command, source_path, destination_path = split_command
     destination_directory = os.path.dirname(destination_path)
