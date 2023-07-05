c3f4f91999a8638b04a17dded1fd639664896519
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-05 20:45:55 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+from os import mkdir, remove, path
+
+
+def move_file(command: str) -> None:
+    sep_command = command.split()
+
+    file_in = sep_command[1]
+    file_out = sep_command[2]
+    dir_list = sep_command[2].split("/")
+    dir_list.pop()
+
+    if "/" in command:
+        pth = ""
+        for drc in dir_list:
+            pth += drc
+            if path.exists(pth):
+                pth += "/"
+                continue
+            mkdir(pth)
+            pth += "/"
+
+    with open(file_in, "r") as f_in, open(file_out, "w") as f_out:
+        f_out.write(f_in.read())
+
+    remove(file_in)
