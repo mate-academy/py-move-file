db3f75a633d5e7088e643a20e13acd97807172e2
Illya Zhmurskiy <thebrawler6@gmail.com>
2023-07-05 19:26:35 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def move_file(command):
+    parts = command.split(' ')
+    source_path = os.path.join(os.getcwd(), parts[1])
+    destination_path = os.path.join(os.getcwd(), parts[2])
+
+    if destination_path.endswith('/'):
+        destination_path = os.path.join(destination_path, os.path.basename(source_path))
+
+    os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+
+    os.replace(source_path, destination_path)
