a345747d4b8f2d753070f3039f73dea814e8f34e
Kateryna Semesenko <semesenkoekaterina@gmail.com>
2023-07-05 14:47:36 +0300

    Modified using makedirs

--- app/main.py
+++ app/main.py
@@ -12,8 +12,7 @@ def move_file(command: str) -> None:
 
         for new_dir in dest_list[:-1]:
             path_dir = os.path.join(path_dir, new_dir)
-            if not os.path.exists(path_dir):
-                os.mkdir(path_dir)
+            os.makedirs(path_dir, exist_ok=True)
 
         with (open(src_file, "r") as file_in,
               open(dest_file, "w") as file_out):
