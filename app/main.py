c2d7fa9d9dd6c096a9d15d76d09deb2e30a16aef
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-06 11:21:40 +0300

    'path_split_makedirs'

--- app/main.py
+++ app/main.py
@@ -4,25 +4,15 @@ from os import makedirs, remove, path
 def move_file(command: str) -> None:
     sep_command = command.split()
 
-    if len(sep_command) != 3:
-        print("Incorrect command!")
+    if sep_command[0] != "mv":
+        return
 
-    file_in = sep_command[1]
-    file_out = sep_command[2]
-    dir_list = sep_command[2].split("/")
-    dir_list.pop()
+    split_path = path.split(sep_command[2])
+    if split_path[0]:
+        makedirs(split_path[0], exist_ok=True)
 
-    if "/" in command:
-        path_file_out = ""
-        for drc in dir_list:
-            path_file_out += drc
-            if path.exists(path_file_out):
-                path_file_out += "/"
-                continue
-            makedirs(path_file_out)
-            path_file_out += "/"
+    with (open(sep_command[1], "r") as file_in,
+          open(sep_command[2], "w") as file_out):
+        file_out.write(file_in.read())
 
-    with open(file_in, "r") as f_in, open(file_out, "w") as f_out:
-        f_out.write(f_in.read())
-
-    remove(file_in)
+    remove(sep_command[1])
