605d4335320f9f8a87ce2556ef580c0a3393a4b8
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-06 17:26:19 +0300

    'len_separate_command'

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ from os import makedirs, remove, path
 
 
 def move_file(command: str) -> None:
-    sep_command = command.split()
+    separate_command = command.split()
 
-    if sep_command[0] != "mv":
+    if separate_command[0] != "mv" and len(separate_command) != 3:
         return
 
-    split_path = path.split(sep_command[2])
+    split_path = path.split(separate_command[2])
     if split_path[0]:
         makedirs(split_path[0], exist_ok=True)
 
-    with (open(sep_command[1], "r") as file_in,
-          open(sep_command[2], "w") as file_out):
+    with (open(separate_command[1], "r") as file_in,
+          open(separate_command[2], "w") as file_out):
         file_out.write(file_in.read())
 
-    remove(sep_command[1])
+    remove(separate_command[1])
