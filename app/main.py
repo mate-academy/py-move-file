ecb9bd169436992e593eaa69105558c816378889
Michael <manchester6913@gmail.com>
2023-07-06 07:32:40 -0400

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+    if len(command) != 3 or command[0] != "mv":
+        raise Exception("Wrong command")
+
+    file_path = os.path.dirname(command[2])
+
+    if file_path:
+        os.makedirs(file_path, exist_ok=True)
+    os.replace(command[1], command[2])
