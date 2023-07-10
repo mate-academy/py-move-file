cecae45f882a1c75defb5f889a6d37a6768c558c
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-07-10 13:14:49 +0300

    Implemented Move-file task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    elements = command.split()
+    if len(elements) == 3:
+        mv_command, initial_file, path_and_copied_filename = elements
+        if mv_command == "mv":
+            path_to_copied_file = os.path.dirname(path_and_copied_filename)
+            if path_to_copied_file:
+                os.makedirs(path_to_copied_file, exist_ok=True)
+            if os.path.exists(initial_file):
+                os.replace(initial_file, path_and_copied_filename)
