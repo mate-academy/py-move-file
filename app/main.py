6a9bbd80ee196035f4b7971b92191a9a0f971195
Max Lapchuk <makslapchuk@gmail.com>
2023-07-13 20:52:08 +0300

    Create 'move_file' function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from os import makedirs, remove
+
+
+def move_file(command: str) -> None:
+    command, filename, new_file_path = command.split()
+
+    if command == "mv":
+        if len(new_file_path.split("/")) > 1:
+            makedirs("/".join(new_file_path.split())[:-1])
+
+        with (
+            open(filename, "r") as file_out,
+            open(new_file_path, "w") as file_in
+        ):
+            file_in.write(file_out.read())
+
+        remove(filename)
+
+    else:
+        print("Command is invalid!")
