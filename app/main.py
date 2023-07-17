8606e94cbddb285fde4a9bc3751f947da555a3ca
Max Lapchuk <124440598+maxlapchuk@users.noreply.github.com>
2023-07-17 17:55:59 +0300

    Update app/main.py
    Co-authored-by: ArsenPidhoretskyi <65166203+ArsenPidhoretskyi@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ def move_file(command: str) -> None:
     if len(command.split()) == 3 and command.split()[0] == "mv":
         command, filename, new_file_path = command.split()
 
-        if path.split(new_file_path)[0]:
-            makedirs(path.split(new_file_path)[0], exist_ok=True)
+        directories, _ = path.split(new_file_path)
+        if directories:
+            makedirs(directories, exist_ok=True)
 
         with (
             open(filename, "r") as file_out,
