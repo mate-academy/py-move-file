18444b1f80a5e24df4df40a4a0b44399cc08a76e
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-07-20 00:20:02 +0300

    solution_v2

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) != 3:
+    splitted_cmd = command.split()
+    if len(splitted_cmd) != 3:
         return
-    if command.split()[0] != "mv":
+    if splitted_cmd[0] != "mv":
         return
-    cmd, file_from, file_to = command.split()
-    if "/" in file_to:
-        first_part, second_part = os.path.split(file_to)
-        os.makedirs(first_part, exist_ok=True)
+    cmd, file_from, file_to = splitted_cmd
+    if "/" in file_to or r"\\" in file_to:
+        directory = os.path.dirname(file_to)
+        os.makedirs(directory, exist_ok=True)
         with open(file_from, "r") as file_from, open(file_to, "w") as file_to:
             file_to.write(file_from.read())
-
         os.remove(file_from.name)
     else:
         os.rename(file_from, file_to)
