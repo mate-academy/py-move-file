6fcfc80013928c9a4fe8961b846b20d57e7898ca
Roman Muraviov <info@roman-muraviov.com>
2023-07-19 20:38:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    cmd, source_path, destination_path = command.split()
+    new_file_path = "/".join(destination_path.split("/")[:-1])
+
+    if cmd != "mv":
+        raise ValueError('Invalid command. Only "mv" command is supported.')
+
+    if source_path == destination_path:
+        print("Source file and destination file have the same name. "
+              "No action needed.")
+        return
+
+    if new_file_path and not os.path.exists(new_file_path):
+        os.makedirs(new_file_path)
+
+    os.rename(source_path, destination_path)
