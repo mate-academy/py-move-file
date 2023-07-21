60d7884e659a3a665428da9a848155371b3525cb
Alexander Izmashkin <alexandr.izmashkin@gmail.com>
2023-07-21 18:18:49 +0300

    Solution/Alex/2

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ def move_file(command: str) -> None:
         if cmd == "mv" and "/" in path_file2:
             path, file2 = os.path.split(path_file2)
             os.makedirs(path, exist_ok=True)
-            with open(file1, "r") as file_in, open(path_file2, "w") as file_out:
+            with open(file1, "r") as file_in, \
+                    open(path_file2, "w") as file_out:
                 file_out.write(file_in.read())
             os.remove(file1)
         else:
