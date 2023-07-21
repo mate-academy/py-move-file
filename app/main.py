db8f495bf5451dd3ee41f31a363d75d6b7261675
Alexander Izmashkin <alexandr.izmashkin@gmail.com>
2023-07-21 18:30:22 +0300

    Solution/Alex/3

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) == 3:
-        cmd, file1, path_file2 = command.split()
+    command_split = command.split()
+    if len(command_split) == 3:
+        cmd, file1, path_file2 = command_split
         if cmd == "mv" and "/" in path_file2:
             path, file2 = os.path.split(path_file2)
             os.makedirs(path, exist_ok=True)
-            with open(file1, "r") as file_in, \
-                    open(path_file2, "w") as file_out:
+            with (open(file1, "r") as file_in,
+                  open(path_file2, "w") as file_out):
                 file_out.write(file_in.read())
             os.remove(file1)
         else:
