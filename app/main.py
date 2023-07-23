6e63031297b149e2d5cca8e0ad95777c1fd798d5
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-23 13:32:44 +0300

    added separate value for split and added slash fix for Windows

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import os
 def check_command_params(command: str,
                          old_file_name: str, path: str
                          ) -> bool:
-    if (command == "mv" and old_file_name.count(".") == 1
+    if (command == "mv"
+            and old_file_name.count(".") == 1
             and path.count(".") == 1
             and old_file_name != path):
         return True
@@ -12,16 +13,25 @@ def check_command_params(command: str,
 
 
 def make_dir_on_path(path: str) -> None:
-    if path.count("/") > 0:
-        dir_path = "/".join(path.split("/")[:-1])
-        os.makedirs(dir_path, exist_ok=True)
+    slashes = ["/", "\\"]
+    for index, slash in enumerate(slashes):
+        if path.count(slash) > 0 and slashes[index-1] not in path:
+            dir_path = slash.join(path.split(slash)[:-1])
+            os.makedirs(dir_path, exist_ok=True)
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) == 3:
-        command_name, file, path = command.split()
+    split = command.split()
+    if len(split) == 3:
+        command_name, file, path = split
         if check_command_params(command_name, file, path):
             make_dir_on_path(path)
             with open(file) as content, open(path, "w") as copy:
                 copy.write(content.read())
             os.remove(file)
+
+
+if __name__ == "__main__":
+    move_file("mv resp.txt first_dir/second_dir/third_dir/file2.txt".replace("/", "\\"))
+    #move_file("mv resp.txt first_dir/second_dir/third_dir/file2.txt")
+
