09943fb3208f4656099cdbb9f8f59b2328e4ac8e
Illya Zhmurskiy <thebrawler6@gmail.com>
2023-07-28 12:08:25 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,13 +2,22 @@ import os
 
 
 def move_file(command: str) -> None:
-    parts = command.split(" ")
+    parts = command.split()
+
+    if len(parts) != 3:
+        print("Invalid command. Please provide source and destination paths.")
+        return
+
+    if parts[0] != "mv":
+        print("Invalid command. The first parameter must be 'mv'.")
+        return
+
     source_path = os.path.join(os.getcwd(), parts[1])
     destination_path = os.path.join(os.getcwd(), parts[2])
 
     if destination_path.endswith("/"):
-        destination_path = (os.path.join(destination_path,
-                                         os.path.basename(source_path)))
+        destination_path = os.path.join(destination_path,
+                                        os.path.basename(source_path))
 
     os.makedirs(os.path.dirname(destination_path), exist_ok=True)
 
