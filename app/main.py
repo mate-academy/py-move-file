0c758a2b5885334dd722cfb6784bc0336d2f87f6
jinjo <vitaly.tyagunov@gmail.com>
2023-08-03 16:40:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    if len(command.split()) == 3:
+        command, old_file, new_file = command.split()
+        with open(old_file, "r") as o_file:
+            content = o_file.read()
+        os.remove(old_file)
+        new_file_path = os.path.dirname(new_file)
+        if new_file_path != "":
+            new_file_name = os.path.basename(new_file)
+            try:
+                os.makedirs(new_file_path)
+                with open(str(new_file_path + "/" + new_file_name), "w")\
+                        as n_file:
+                    n_file.write(content)
+            except FileExistsError:
+                with open(str(new_file_path + "/" + new_file_name), "w")\
+                        as n_file:
+                    n_file.write(content)
+        else:
+            with open(new_file, "w") as new_file:
+                new_file.write(content)
