901c632a3f52c770d9f4f8afeb42d54298918751
jinjo <vitaly.tyagunov@gmail.com>
2023-08-04 19:33:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -10,11 +10,11 @@ def move_file(command: str) -> None:
         path_to_new_file, name_of_new_file = os.path.split(new_file)
         if path_to_new_file != "":
             os.makedirs(path_to_new_file, exist_ok=True)
-            join_path = os.path.normcase(os.path.join(path_to_new_file, name_of_new_file))
+            join_path = os.path.normcase(os.path.join(path_to_new_file,
+                                                      name_of_new_file))
             # join_path = path_to_new_file + "/" + name_of_new_file
             with open(join_path, "w") as n_file:
                 n_file.write(content)
         else:
             with open(new_file, "w") as n_file:
                 n_file.write(content)
-
