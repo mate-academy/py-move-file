16fb4ee46d8ab19fb80212666a518f84178a0dab
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-08-04 08:18:42 +0300

    add annotation remove redundant code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 import os
-from typing import Any
 
 
 class MoveFile:
@@ -21,22 +20,19 @@ class MoveFile:
         self.new_filename.write(content_to_copy)
         return self.new_filename
 
-    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+    def __exit__(self, exc_type: BaseException, exc_val: BaseException, exc_tb: None) -> None:
         os.remove(self.old_filename)
         self.new_filename.close()
 
 
 def move_file(command: str) -> None:
-    content = command.split()
-    if len(content) == 3:
-        command, old_file, new_file_and_direction = content
-        if command == "mv":
-            direction, new_file_name = os.path.split(new_file_and_direction)
-            if direction:
-                if not os.path.exists(direction):
-                    os.makedirs(direction, exist_ok=True)
-            else:
-                direction = os.getcwd()
+    command, old_file, new_file_and_direction = command.split()
+    direction, new_file_name = os.path.split(new_file_and_direction)
+    if direction:
+        if not os.path.exists(direction):
+            os.makedirs(direction, exist_ok=True)
+    else:
+        direction = os.getcwd()
 
-            with MoveFile(old_file, new_file_name, direction):
-                pass
+    with MoveFile(old_file, new_file_name, direction):
+        pass
