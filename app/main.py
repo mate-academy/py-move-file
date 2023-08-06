8aa3186f2e90bca6718dfa064ff359451c331b84
Dima Khrystenko <hrystanko.dmytro@outlook.com>
2023-08-06 11:48:07 +0200

    Correction 2

--- app/main.py
+++ app/main.py
@@ -9,21 +9,17 @@ def move_file(command: str) -> None:
     cmd, source_file, result_file = split_command
     flag = cmd == "mv" and Path(source_file).exists()
 
-    if "/" in result_file:
-        result_dir, result_filename = os.path.split(result_file)
+    result_dir, result_filename = os.path.split(result_file)
 
-        if not result_filename:
-            result_filename = Path(source_file).name
+    if not result_filename:
+        result_filename = Path(source_file).name
 
+    if result_dir:
         os.makedirs(result_dir, exist_ok=True)
-
-        path_to_result = os.path.join(result_dir, result_filename)
-    else:
-        path_to_result = Path(result_file).resolve()
 
     if flag:
         with open(source_file, "r") as source, open(
-            path_to_result, "w"
+            result_file, "w"
         ) as res:
             source_file_data = source.read()
             res.write(source_file_data)
