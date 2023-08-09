267a1e87fb1675bafd875d55f3f0aa014fc14b86
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-08-09 13:29:57 +0200

    Move File

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    value = command.split()
+    if len(value) != 3:
+        raise ValueError("Incorrect command line.")
+    if value[0] == "mv":
+        current = value[1]
+        destination = value[2]
+        separated = destination.split("/")[:-1]
+        directories = "/".join(separated)
+        if directories:
+            os.makedirs(directories)
+        with (
+            open(current, "r") as current_file,
+            open(destination, "w") as destination_file
+        ):
+            for each_line in current_file:
+                destination_file.write(each_line)
+        os.remove(current)
