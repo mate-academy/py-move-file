8d63bd74155f9f21c35f77769bfd6f066d8a8e6e
Andrii Ishchenko <iscenkoa@gmail.com>
2023-08-10 17:56:22 +0300

    Another variant

--- app/main.py
+++ app/main.py
@@ -6,22 +6,18 @@ def move_file(command: str) -> None:
     if len(command.split()) == 3:
         action, file_name, path = command.split()
         if action == "mv":
+            head, tail = os.path.split(path)
             with open(file_name) as file_to_read:
-                if "/" in path:
-                    list_of_directories = path.split("/")[:-1]
-                    file_name_to_move = path.split("/")[-1]
-                    current_path = os.getcwd()
-                    for folder in list_of_directories:
-                        current_path = os.path.join(current_path, folder)
-                        if not os.path.isdir(current_path):
-                            os.mkdir(current_path)
+                current_path = os.getcwd()
+                if head:
+                    try:
+                        current_path = os.path.join(current_path, head)
+                        os.makedirs(current_path)
+                    except OSError:
+                        pass
 
-                    with (open(os.path.join(current_path,
-                                            file_name_to_move), "w")
-                          as file_to_write):
-                        file_to_write.write(file_to_read.read())
+                with (open(os.path.join(current_path, tail), "w")
+                      as file_to_write):
+                    file_to_write.write(file_to_read.read())
 
-                else:
-                    with open(path, "w") as file_to_write:
-                        file_to_write.write(file_to_read.read())
             os.remove(file_name)
