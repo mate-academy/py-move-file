bb722555866499792076f0d3a3b5af1804fecd95
Andrii Ishchenko <iscenkoa@gmail.com>
2023-08-10 21:35:36 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -6,18 +6,15 @@ def move_file(command: str) -> None:
     if len(command.split()) == 3:
         action, file_name, path = command.split()
         if action == "mv":
-            head, tail = os.path.split(path)
-            with open(file_name) as file_to_read:
-                current_path = os.getcwd()
-                if head:
-                    try:
-                        current_path = os.path.join(current_path, head)
-                        os.makedirs(current_path)
-                    except OSError:
-                        pass
+            head = os.path.split(path)[0]
+            if head:
+                try:
+                    os.makedirs(os.path.join(os.getcwd(), head))
+                except OSError:
+                    pass
 
-                with (open(os.path.join(current_path, tail), "w")
-                      as file_to_write):
-                    file_to_write.write(file_to_read.read())
+            with (open(path, "w") as file_to_write,
+                  open(file_name) as file_to_read):
+                file_to_write.write(file_to_read.read())
 
             os.remove(file_name)
