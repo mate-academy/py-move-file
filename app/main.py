c951bf8e9448fb081cdcd3a0469346b49b52a8ea
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-08-11 19:01:23 +0300

    add os.path.split

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ import os
 def move_file(command: str) -> None:
     if len(command.split()) == 3:
         command_name, file_name, mv_file = command.split()
-        path_folder_file = mv_file[0:mv_file.rfind("/") + 1]
+        path_folder_file = os.path.split(mv_file)
 
         if command_name == "mv":
 
             if mv_file.count("/") > 0:
 
-                os.makedirs(path_folder_file, exist_ok=True)
+                os.makedirs(path_folder_file[0], exist_ok=True)
 
             os.rename(file_name, mv_file)
