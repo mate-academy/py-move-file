33ff58a6e2d78e6b85386aee934cc69df527ffa1
neostyle88 <makssimm1@gmail.com>
2023-08-11 17:24:21 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -5,24 +5,24 @@ def move_file(command: str) -> None | str:
     if len(command.split()) != 3:
         return "Incorrect statement printed"
 
-    cmd, curent_file, destination = command.split()
+    cmd, current_file, destination = command.split()
 
     if cmd != "mv":
         return "Command is incorrect"
 
-    dir_list = destination.split("/")
-    new_file_name = dir_list[-1]
-    dir_list.remove(new_file_name)
+    dir_path, new_file_name = os.path.split(destination)
 
     try:
-        os.makedirs("/".join(dir_list))
-    except FileExistsError:
-        print("Folder is already exists")
+        os.makedirs(dir_path, exist_ok=True)
     except FileNotFoundError:
-        print("Not need to create folders")
+        print("Not needed to create folders")
 
-    with open(curent_file, "r") as source_file, open(
-        destination, "w"
-    ) as moved_file:
-        moved_file.write(source_file.read())
-    os.remove(curent_file)
+    try:
+        with open(current_file, "r") as source_file, open(
+            destination, "w"
+        ) as moved_file:
+            moved_file.write(source_file.read())
+    except FileNotFoundError:
+        return f"Source file '{current_file}' not found"
+
+    os.remove(current_file)
