441dbd02b16aeee48eebfc701e6a2c70a4634a8e
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-08-11 17:28:48 +0300

    add exist_ok

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_name, file_name, mv_file = command.split()
-    path_folder_file = mv_file[0:mv_file.rfind("/") + 1]
+    if len(command.split()) == 3:
+        command_name, file_name, mv_file = command.split()
+        path_folder_file = mv_file[0:mv_file.rfind("/") + 1]
 
-    if len(command.split()) == 3 and command_name == "mv":
+        if command_name == "mv":
 
-        if mv_file.count("/") > 0:
+            if mv_file.count("/") > 0:
 
-            if not os.path.exists(path_folder_file):
-                os.makedirs(path_folder_file)
+                os.makedirs(path_folder_file, exist_ok=True)
 
-        if not os.path.exists(mv_file):
-            os.rename(file_name, mv_file)
+            if not os.path.exists(mv_file):
+                os.rename(file_name, mv_file)
