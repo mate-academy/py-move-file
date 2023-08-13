419dcfb7a50248e48a294a8be5b7cc5559a1e15b
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-08-13 17:01:05 +0300

    'Solution3'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import os
 
 
 def move_file(command: str) -> None:
-    args = command.split(" ")
+    args = command.split()
 
     if len(args) == 3:
         cmd, file1, file2 = args
@@ -19,13 +19,17 @@ def move_file(command: str) -> None:
                 path_file2 = os.path.join(*item)
 
                 try:
-                    os.mkdir(path_file2)
+                    os.makedirs(path_file2, exist_ok=True)
                 except WindowsError:
                     continue
 
             if name_file2 != "":
-                with (open(file1, "r") as file_in,
-                      open(file2, "w") as file_out):
-                    file_out.write(file_in.read())
+                path_file2 = os.path.join(path_file2, name_file2)
+            else:
+                path_file2 = os.path.join(path_file2, file1)
+
+            with (open(file1, "r") as file_in,
+                  open(path_file2, "w") as file_out):
+                file_out.write(file_in.read())
 
             os.remove(file1)
