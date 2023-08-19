21ed7e05447578e8e3dd0449455cf06eb6c2882f
Roman Podolets <romanpyu@gmail.com>
2023-08-19 13:26:39 +0300

    Implemented solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    try:
+        curr_command, source_file, output_file = command.split()
+    except ValueError:
+        raise ValueError("Not enough arguments")
+
+    if curr_command != "mv":
+        return
+
+    dir_path = os.path.dirname(output_file)
+    if not os.path.exists(output_file) and dir_path:
+        os.makedirs(dir_path, exist_ok=True)
+
+    with open(source_file, "r") as source:
+        file_content = source.read()
+        with open(output_file, "w") as new_file:
+            new_file.write(file_content)
+
+    os.remove(source_file)
