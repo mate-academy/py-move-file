0482c86c443b969a1356d040be8a99eba41613f8
Bohdan Kolomiiets <kb907406@gmail.com>
2023-08-21 15:27:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,12 +2,9 @@ import os
 
 
 def move_file(command: str) -> None | str:
-    # Split the command by 3 aspects
     command, source_file, target = command.split()
-    # Check that command is "mv"
     if command != "mv":
         return f"'{command}' is incorrect"
-    # Check if target has path or it's just file
     if "/" in target:
 
         path, moved_file = os.path.split(target)
@@ -17,6 +14,5 @@ def move_file(command: str) -> None | str:
             nf.write(sf.read())
 
         os.remove(source_file)
-    # if it's file, rename it
     else:
         os.rename(source_file, target)
