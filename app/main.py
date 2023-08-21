3863ff0e82983438fadecf84b713d9b1a257850c
Denys <deibukdenyscloud@gmail.com>
2023-08-21 22:02:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    keyword, source, target = command.strip().split()
+    if keyword != "mv":
+        return
+    if (target_dirname := os.path.dirname(target)) != "":
+        os.makedirs(target_dirname, exist_ok=True)
+    with open(source, "r") as source_fobj, open(target, "w") as target_fobj:
+        target_fobj.write(source_fobj.read())
+    os.remove(source)
