2d991c9e7d05e2a7e084a4c7556f9e3d87b339f9
Vlad <hanovskyiv@gmail.com>
2023-08-21 16:52:29 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None | str:
+    command, source_file, location = command.split()
+    path, moved_file = os.path.split(location)
+    if command != "mv":
+        return "Command is incorrect"
+    if path:
+        os.makedirs(path, exist_ok=True)
+    os.rename(source_file, location)
