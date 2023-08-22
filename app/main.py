7089a472e0a35088245bba592e8984c37b0a12b8
Kristian Yendrek <kristianendreck4@gmail.com>
2023-08-22 12:50:39 +0300

    Implemented move_file function.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    try:
+        mv_command, old_file, new_file = command.split()
+    except ValueError:
+        raise Exception("Enter correct command!")
+
+    if old_file != new_file and mv_command == "mv":
+        dir_path = os.path.dirname(new_file)
+        if dir_path and not os.path.exists(dir_path):
+            dirs = dir_path.split("/")
+            dir_ = ""
+            for temp in dirs:
+                dir_ += temp + "/"
+                if not os.path.exists(dir_):
+                    os.mkdir(dir_)
+
+        with open(old_file) as file_in, open(new_file, "w") as file_out:
+            file_content = file_in.read()
+            file_out.write(file_content)
+
+        os.remove(old_file)
