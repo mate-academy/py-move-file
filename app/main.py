8fede37328def7d388c348193b02405af717b4a6
Mikhail <tiunovmixs@gmail.com>
2023-08-22 16:33:11 +0300

    Implemented move file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    args = command.split()[1:]
+
+    source = args[0]
+    destination = args[1]
+
+    if not os.path.isfile(source):
+        return
+
+    dest_directory = os.path.dirname(destination)
+    if dest_directory and not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+
+    os.rename(source, destination)
