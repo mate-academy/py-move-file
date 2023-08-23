fda52bb5e815258adb6c575f3353c097ebcdd485
Olexander Bryl <edlrian814@gmail.com>
2023-08-23 13:31:35 +0300

    corrected?

--- app/main.py
+++ app/main.py
@@ -1,49 +1,19 @@
 import os
-from typing import Any
-
-
-class CleanUpFile:
-    def __init__(
-            self,
-            filename: str
-    ) -> None:
-        self.filename = filename
-        self.file = None
-
-    def __enter__(self) -> Any:
-        self.file = open(self.filename, "+r")
-        return self.file
-
-    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
-        self.file.close()
-        try:
-            os.remove(self.filename)
-        except FileNotFoundError:
-            pass
-
-
-def copy_file(command: str) -> None:
-    command_ls = command.split()
-    if command_ls[1] == command_ls[2]:
-        return
-    with CleanUpFile(command_ls[1]) as source,\
-         open(command_ls[2], "w") as receiver:
-        source_contents = source.read()
-        receiver.write(source_contents)
 
 
 def move_file(command: str) -> None:
     destination = command.split()[2]
-    if destination.count("/") == 1:
-        directory = destination.split("/")[0]
+    if "/" in destination:
+        directories = destination.split("/")[:-1]
+        concurrent_dir = directories[0]
+        for directory in directories[1:]:
+            concurrent_dir = os.path.join(concurrent_dir, directory)
         try:
-            os.mkdir(directory)
+            os.makedirs(concurrent_dir)
         except FileExistsError:
             pass
-    elif destination.count("/") > 1:
-        directories = destination[:destination.rfind("/")]
-        try:
-            os.makedirs(directories)
-        except FileExistsError:
-            pass
-    copy_file(command)
+    with (open(command.split()[1], "r") as old,
+          open(destination, "w") as new):
+        copied = old.read()
+        new.write(copied)
+    os.remove(command.split()[1])
