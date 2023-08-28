c95380c7a02facde093944d908bf36ffb5cfdafd
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-28 14:00:24 +0200

    fixed unpacking

--- app/main.py
+++ app/main.py
@@ -2,10 +2,7 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_arr = command.split()
-    action = command_arr[0]
-    source = command_arr[1]
-    destination = command_arr[2]
+    action, source, destination = command.split()
 
     if source == destination and action == "mv":
         return
@@ -18,8 +15,8 @@ def move_file(command: str) -> None:
         destination = destination[:-1]
         new_file_name = source
     else:
-        destination = "/".join(command_arr[2].split("/")[:-1])
-        new_file_name = "".join(command_arr[2].split("/")[-1])
+        new_file_name = "".join(destination.split("/")[-1])
+        destination = "/".join(destination.split("/")[:-1])
 
     if not os.path.exists(destination):
         os.makedirs(destination)
