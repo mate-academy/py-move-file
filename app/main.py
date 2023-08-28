e90eea474624f1967cb35dceafc1c431069e1aff
ivan <ivan@isba.local>
2023-08-28 14:17:00 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -10,11 +10,8 @@ def move_file(command: str) -> None:
     if len(command_list) != 3 or command_list[0] != "mv":
         raise InvalidCommandError("Unknown command.")
 
-    path = ""
-    for folder in command_list[2].split("/")[:-1]:
-        path = os.path.join(path, folder)
-        if not os.path.isdir(path):
-            os.mkdir(path)
+    dirs, _ = os.path.split(command_list[2])
+    os.makedirs(dirs)
 
     with (
         open(command_list[1], "r") as source_file,
