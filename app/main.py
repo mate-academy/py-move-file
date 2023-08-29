79a489bea39d002bdbe814a55d9141bb746597c3
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-29 21:00:22 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ def move_file(command: str) -> None:
     if len(command) == 3:
         keyword, moving_file, path_to_file = command.split()
         if keyword == "mv" and path_to_file != moving_file:
-    
+
             directories = os.path.split(path_to_file)[0]
-    
+
             if directories:
                 os.makedirs(directories, exist_ok=True)
-    
+
             with (
                 open(moving_file, "r") as file_in,
                 open(path_to_file, "w") as file_out
