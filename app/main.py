f3c77dea48c8353eca80ba1c0cbe74c1f6f0ede3
Max Katkalov <makskatkalov@gmail.com>
2023-09-05 00:55:01 +0300

    another solution

--- app/main.py
+++ app/main.py
@@ -2,40 +2,23 @@ import os
 
 
 def move_file(command: str) -> None:
-    """this IF-block just will rename file without moving somewhere"""
     parts = command.split()
 
-    if len(parts) == 3 and parts[0] == "mv":
-        file_to_move, destination = parts[1], parts[2]
-        destination_parts = destination.split("/")
-        if len(destination_parts) == 1:
-            os.rename(file_to_move, destination)
-            return
+    if len(parts) != 3 or parts[0] != "mv":
+        print("Invalid command format")
+        return
 
-        """this block just will open the existing dir and create new file"""
-        *dirs, name_file_2_create = destination_parts
-        if os.path.exists(f"{os.path.join('/'.join(dirs))}"):
-            with (
-                open(file_to_move, "r") as moving_file,
-                open(
-                    f"{os.path.join('/'.join(dirs), name_file_2_create)}", "w"
-                ) as moved_file,
-            ):
-                moved_file.write(moving_file.read())
-                os.remove(file_to_move)
-                return
+    source_path, destination_path = parts[1], parts[2]
 
-        """last possible way is to create dirs"""
-        dir_path = ""
-        for folder in dirs:
-            dir_path = os.path.join(dir_path, folder)
-            os.makedirs(dir_path, exist_ok=True)
+    if destination_path.endswith("/"):
+        dest_dir = destination_path
+        dest_file = os.path.basename(source_path)
+    else:
+        dest_dir, dest_file = os.path.split(destination_path)
+        if dest_dir:
+            os.makedirs(dest_dir, exist_ok=True)
 
-        with (
-            open(file_to_move, "r") as moving_file,
-            open(
-                f"{os.path.join('/'.join(dirs), name_file_2_create)}", "w"
-            ) as moved_file,
-        ):
-            moved_file.write(moving_file.read())
-            os.remove(file_to_move)
+    try:
+        os.rename(source_path, os.path.join(dest_dir, dest_file))
+    except Exception as e:
+        print(f"Error: {e}")
