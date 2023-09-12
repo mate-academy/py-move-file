9fa4d35969ae6fd6847754bcb8fd040e0252920a
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-09-12 21:40:32 +0200

    in progress

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) == 3 and parts[0] == "mv":
+        old_file, full_path_to_new_file = parts[1:]
+
+        if "/" not in full_path_to_new_file:
+            os.rename(old_file, full_path_to_new_file)
+        else:
+            path_new_file = os.path.split(full_path_to_new_file)[0]
+            os.makedirs(path_new_file, exist_ok=True)
+
+            with (
+                open(old_file, "r") as file_in,
+                open(full_path_to_new_file, "w") as file_out
+            ):
+                file_out.write(file_in.read())
+
+            os.remove(old_file)
