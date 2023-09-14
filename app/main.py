29ad06ffd8e0ffbea23cdd21e60f546eca29f999
Valeriia Tymchenko <maksimova.tymchenko@gmail.com>
2023-09-14 12:19:43 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os.path
+
+
+def move_file(command: str) -> None:
+    cmd, file, new_file = command.split()
+
+    if len(command.split()) != 3 or cmd != "mv":
+        raise SyntaxError("Incorrect command")
+
+    destination, file_name = os.path.split(new_file)
+
+    if "/" in new_file:
+        os.makedirs(destination, exist_ok=True)
+
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
+        os.remove(file)
+    else:
+        os.rename(file, new_file)
