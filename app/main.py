81c1c1d212f4330313f142a5f72e8a98eedef4d5
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-09-14 20:22:14 +0200

    not a solution

--- app/main.py
+++ app/main.py
@@ -6,12 +6,12 @@ def move_file(command: str) -> None:
 
     if len(parts) == 3 and parts[0] == "mv":
         old_file, full_path_to_new_file = parts[1:]
+        dirs, new_file = os.path.split(full_path_to_new_file)
 
-        if "/" not in full_path_to_new_file:
-            os.rename(old_file, full_path_to_new_file)
+        if dirs:
+            os.makedirs(dirs, exist_ok=True)
         else:
-            path_new_file = os.path.split(full_path_to_new_file)[0]
-            os.makedirs(path_new_file, exist_ok=True)
+            os.rename(old_file, new_file)
 
             with (
                 open(old_file, "r") as file_in,
