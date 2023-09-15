d77b22dd02dca0b7b96bd533f8d2fb1c0ab0aa6e
Olgierrd <oolgierrd@gmail.com>
2023-09-15 19:08:27 +0200

    Solution after changes: added checks for the command format

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ import os
 
 
 def move_file(command: str) -> None | str:
-    if command.startswith("mv "):
-        pass
-    else:
-        return "write correct command"
+    if not command.startswith("mv "):
+        print("write correct command: it should start with 'mv '")
+        exit()
+    if not len(command.split(" ")) == 3:
+        print("write correct command: mv source_file destination_file")
+        exit()
     source_name, dest_path = command[3:].split(" ")
     if os.path.sep in dest_path:
         dest_file = dest_path.rstrip("/").split("/")[-1]
