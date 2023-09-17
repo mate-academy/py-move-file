e86655645262240769ddffbd971051d93145c2e8
Ostap <ostapbojko04@gmail.com>
2023-09-17 17:07:53 +0300

    pu-move(fixed v1)

--- app/main.py
+++ app/main.py
@@ -1,19 +1,6 @@
 import os
 
 
-# def move_file(command: str) -> None:
-#     if len(command.split()) == 3:
-#         cmd, original, file_path = command.split()
-#         if "/" in file_path:
-#             head, tail = os.path.split(file_path)
-#             os.makedirs(head, exist_ok=True)
-#             with open(original, "r") as file1, open(file_path, "w") as file2:
-#                 file2.write(file1.read())
-#             os.remove(original)
-#         else:
-#             os.rename(original, file_path)
-import os
-
 def move_file(command: str) -> None:
     command_parts = command.split()
     if len(command_parts) == 3:
@@ -22,7 +9,8 @@ def move_file(command: str) -> None:
             if "/" in file_path:
                 head, tail = os.path.split(file_path)
                 os.makedirs(head, exist_ok=True)
-                with open(original, "r") as file1, open(file_path, "w") as file2:
+                with open(original, "r") as file1,\
+                        open(file_path, "w") as file2:
                     file2.write(file1.read())
                 os.remove(original)
             else:
