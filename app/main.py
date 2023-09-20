ba68ed93eb2a8ee779d77d698b758f377c462a9f
Anton <anton380969658858@gmail.com>
2023-09-20 16:26:47 +0300

    changed concatenation for variable current

--- app/main.py
+++ app/main.py
@@ -2,21 +2,23 @@ import os
 
 
 def move_file(command: str) -> None:
-    file_names = command.split(" ")
+    mv, input_name, output_name = command.split(" ")
 
-    if file_names[0] != "mv" or len(file_names) != 3:
+    if mv != "mv":
         return
-    with open(file_names[1], "r") as file:
+    with open(input_name, "r") as file:
         text = file.read()
-    os.remove(file_names[1])
+    os.remove(input_name)
 
-    file_path = file_names[2].split("/")
+    file_path = output_name.split("/")
+    print(file_path)
     current = ""
 
-    for directory in file_path[:-1]:
-        current += directory + "/"
-        if not os.path.exists(current):
-            os.mkdir(current)
+    if file_path[0] != "":
+        for directory in file_path[:-1]:
+            current = os.path.join(current, directory)
+            if not os.path.exists(current):
+                os.mkdir(current)
 
-    with open(file_names[2], "w") as file:
+    with open(output_name, "w") as file:
         file.write(text)
