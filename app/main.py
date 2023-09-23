1111fac906cf23781a565eadd27649559d288dd0
Manynik <s.onufriyenko@gmail.com>
2023-09-23 12:41:25 +0300

    Solution-2

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ import os
 
 def move_file(command: str) -> None:
     parts = command.split()
-    _, source, target = parts
+    command, source, target = parts
     if len(parts) != 3 or parts[0] != "mv" or source == target:
         return
 
-    if os.path.dirname(target) != "":
+    if os.path.dirname(target) != "" or target.endswith("/"):
         os.makedirs(os.path.dirname(target), exist_ok=True)
     with open(source, "r") as file1, open(target, "w") as file2:
         file2.write(file1.read())
