5a865fda6968fb71b3be730040051b8889746d1d
Philip Plokhikh <filllick13@gmail.com>
2023-09-28 17:44:07 +0300

    Move_file_v2

--- app/main.py
+++ app/main.py
@@ -3,11 +3,12 @@ import os
 
 def move_file(command: str) -> None:
     command_data = command.split()
-    original_file = command_data[1]
-    file_path = command_data[2]
 
     if command_data[0] != "mv" or len(command_data) != 3:
         return
+
+    original_file = command_data[1]
+    file_path = command_data[2]
 
     if "/" in file_path:
         path = ""
