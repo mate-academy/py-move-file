900f9b8e854577c473999152bda0a4463d5bf1c3
Vadym Tiron <tironvadim1@gmail.com>
2023-09-29 23:58:22 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,37 +2,22 @@ import os
 import shutil
 
 
-def make_directories(destination_path: str) -> None:
-
-    directories = destination_path.split(os.path.sep)
-    current_path = ""
-
-    for directory in directories:
-        current_path = os.path.join(current_path, directory)
-
-        if not os.path.exists(current_path):
-            os.makedirs(current_path, exist_ok=True)
-
-
 def move_file(command: str) -> None:
     parts = command.split()
-    mv, source_path, destination_path = parts
-    if len(parts) != 3 or mv != "mv":
+    if len(parts) != 3 or parts[0] != "mv":
         return
+
+    mv, source_path, destination_path = parts
 
     if not os.path.exists(source_path):
         return
 
-    is_directory = False
-    if destination_path.endswith(os.path.sep):
-        is_directory = True
-        destination_path = destination_path.rstrip(os.path.sep)
+    if os.path.isdir(destination_path):
+        os.makedirs(destination_path, exist_ok=True)
 
-    if is_directory:
-        make_directories(destination_path)
-
-    if is_directory:
-        destination_file = os.path.join(destination_path, os.path.basename(source_path))
+    if os.path.isdir(destination_path):
+        destination_file = os.path.join(destination_path,
+                                        os.path.basename(source_path))
     else:
         destination_file = destination_path
 
