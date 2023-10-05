ef5a96f863e350853d5820e046c194b0431b7713
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-05 19:34:46 +0300

    434

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ import os
 def move_file(command: str) -> None:
     comm, file_to_move, path_to_move = command.split()
     place_to_move = path_to_move.split("/")[:-1]
-    for dir in place_to_move:
-        os.mkdir(dir)
-        os.chdir(f"{os.getcwd()}\\{dir}")
+    for direction in place_to_move:
+        os.mkdir(direction)
+        os.chdir(f"{os.getcwd()}\\{direction}")
     os.chdir(f"{'../' * len(place_to_move)}/")
-    with (open(file_to_move, "r") as file_in, open(os.path.join(path_to_move), "w") as file2):
+    with (open(file_to_move, "r") as file_in,
+          open(os.path.join(path_to_move), "w") as file2):
         file2.write(file_in.read())
     os.remove(file_to_move)
-
