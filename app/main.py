f03817ad3012521c6e5b6a56cc3573c4081db6fe
Andii Rashevskyi <phoenixerazer@gmail.com>
2023-10-07 20:59:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import os
 
 def move_file(command: str) -> None:
     action, source_file, destination = command.split()
-    if action == "mv" and "/" in destination:
+    if action == "mv":
         destination_dir, destination_filename = os.path.split(destination)
-        os.makedirs(destination_dir, exist_ok=True)
+        if destination_dir:
+            os.makedirs(destination_dir, exist_ok=True)
         with open(source_file, "r") as source, open(destination, "w") as dest:
             dest.write(source.read())
         os.remove(source_file)
