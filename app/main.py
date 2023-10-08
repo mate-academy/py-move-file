711a3d07f38d775dd678eb5ded6b35b8df52749f
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-08 21:02:44 +0300

    Solution3

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def move_file(command: str) -> None:
         command_, file_to_move, path_to_move = command.split()
         path, final_file = os.path.split(path_to_move)
         if command_ == "mv" and file_to_move != final_file:
-            os.makedirs(path)
+            os.makedirs(path, exist_ok=True)
             with (open(file_to_move, "r") as file_in,
                   open(os.path.join(path_to_move), "w") as file_out):
                 file_out.write(file_in.read())
