817e5974449ee299c18a0e12aefb392910782509
Vyacheslav Vasylenko <vvg04021968@gmail.com>
2023-10-12 21:53:03 +0300

    'Solution__move-file'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+
+    parts = command.split()
+
+    if len(parts) == 3 and parts[0] == "mv":
+        _, file_source, file_destination = parts
+
+        if "/" in file_destination or "\\" in file_destination:
+            way, name_file = os.path.split(file_destination)
+            os.makedirs(way, exist_ok=True)
+
+            with (
+                open(file_source, "r") as file_in,
+                open(file_destination, "w") as file_out
+            ):
+                file_out.write(file_in.read())
+            os.remove(file_source)
+        else:
+            os.rename(file_source, file_destination)
