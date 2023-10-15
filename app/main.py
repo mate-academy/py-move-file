911b0d8d57ef43ed5417b2b06981e30be66654f8
TROT <seotrotpol@gmail.com>
2023-10-15 13:18:55 +0300

    'Solution 4'

--- app/main.py
+++ app/main.py
@@ -9,9 +9,9 @@ def move_file(command: str) -> None:
     if len(command_list) == 3:
         mv, file_name, new_file_name = command_list
         if mv == "mv":
-            if "/" in new_file_name:
-                path_file = new_file_name.split("/")
-                os.makedirs("/".join(path_file[:-1]), exist_ok=True)
+            path_file, name_file = os.path.split(new_file_name)
+            if path_file:
+                os.makedirs(path_file, exist_ok=True)
 
             with (open(file_name, "r") as file_in,
                   open(new_file_name, "w") as file_out):
