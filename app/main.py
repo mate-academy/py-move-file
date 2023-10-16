21171151cbcc2d457d2dad656d429c6cb4f92d65
Kostia <kostia3507@gmail.com>
2023-10-17 01:02:06 +0300

    solutio

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+import shutil
+from shutil import move
+
+
+def move_file(command: str) -> None:
+    command_list = command.split(" ")
+
+    flag = command_list[0]
+    file_name = command_list[1]
+    move_to = command_list[2]
+
+    if flag == "mv":
+        dirs = os.path.dirname(move_to)
+        if dirs:
+            os.makedirs(dirs, exist_ok=True)
+        os.replace(file_name, move_to)
+
