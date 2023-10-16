63f0667741546e9429928390c4741621ee48eca9
Evgen Milokumov <evgen.milok@gmail.com>
2023-10-16 22:39:27 +0300

    fix/logic

--- app/main.py
+++ app/main.py
@@ -1,26 +1,23 @@
 import os
-import shutil
 
 
 def move_file(command: str) -> None:
-    splitted_command = command.split()
 
-    if len(splitted_command) < 3:
-        print(f"Incorrect command {command}")
-        return
+    if "mv" not in command or len(command.split()) != 3:
+        raise ValueError("Incorrect command")
+    else:
+        command, file, new_file = command.split()
 
-    init_file = splitted_command[1]
-    new_file_directory_with_name = splitted_command[2]
-    new_file_directory = os.path.dirname(new_file_directory_with_name)
+    if not os.path.exists(file):
+        raise ValueError("File doesn't exist")
 
-    if not os.path.exists(init_file):
-        print(f"File: '{init_file}' isn't exist.")
-        return
+    destination_path, new_file_name = os.path.split(new_file)
 
-    try:
-        os.makedirs(new_file_directory)
-        shutil.move(init_file, new_file_directory_with_name)
-        print(f"File was moved to directory: {new_file_directory_with_name}")
-    except OSError as error:
-        shutil.move(init_file, new_file_directory_with_name)
-        print(f"Error: {error}")
+    if destination_path:
+        os.makedirs(destination_path, exist_ok=True)
+
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
+        os.remove(file)
+    else:
+        os.rename(file, new_file)
