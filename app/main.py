f00c9c7fbe16583bdede0c95ed3e7a31634231f9
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-17 18:30:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, first_file, second_file = command.split()
+    if "/" in second_file:
+        way = os.path.dirname(second_file)
+        try:
+            os.makedirs(way)
+        except FileExistsError:
+            pass
+        with (open(first_file, "r") as old_file,
+             open(second_file, "w") as new_file):
+            content = old_file.read()
+            new_file.write(content)
+        os.remove(first_file)
+    else:
+        os.rename(first_file, second_file)
