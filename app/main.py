ff672127f1cc08e4cd4b91da184334b7730edcd2
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-17 18:36:34 +0300

    Changes

--- app/main.py
+++ app/main.py
@@ -4,11 +4,7 @@ import os
 def move_file(command: str) -> None:
     command, first_file, second_file = command.split()
     if "/" in second_file:
-        way = os.path.dirname(second_file)
-        try:
-            os.makedirs(way)
-        except FileExistsError:
-            pass
+        os.makedirs(os.path.dirname(second_file), exist_ok=True)
         with (open(first_file, "r") as old_file,
              open(second_file, "w") as new_file):
             content = old_file.read()
