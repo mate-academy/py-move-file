0ea99941b6548056c8979a8467b264f08f58cce1
Kostia <kostia3507@gmail.com>
2023-10-21 01:21:26 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -2,11 +2,7 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_list = command.split(" ")
-
-    flag = command_list[0]
-    file_name = command_list[1]
-    move_to = command_list[2]
+    flag, file_name, move_to = command.split()
 
     if flag == "mv":
         dirs = os.path.dirname(move_to)
