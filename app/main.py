f00c90f87ec3cf72e9eb06d67c339e61fa03ae42
naiv <nazarivankiv1@gmail.com>
2023-10-21 13:00:51 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
-from os import path, makedirs, replace
+from os import path, makedirs, remove
 
 
 def move_file(command: str) -> None:
-    mv_command = command.split()
-    if mv_command[0] != "mv" or len(mv_command) != 3:
-        return None
-
-    path_file = path.dirname(mv_command[2])
-    if path_file:
-        makedirs(path_file, exist_ok=True)
-    replace(mv_command[1], mv_command[2])
+    mv_command, initial_file, new_file_dir = command.split()
+    if mv_command == "mv":
+        path_file = path.dirname(new_file_dir)
+        if path_file:
+            makedirs(path_file, exist_ok=True)
+        with (open(initial_file, "r") as old_file,
+             open(new_file_dir, "w") as new_file):
+            new_file.write(old_file.read())
+        remove(initial_file)
