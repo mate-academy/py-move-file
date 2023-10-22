2707bfc37f1b57b4ee7c8467d75d8c44bc3e7359
Ihor Prochnickiy <2prochigor@gmail.com>
2023-10-22 13:53:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,17 +4,14 @@ import os
 def move_file(command: str) -> None:
     command_name, file, path = command.split()
 
-    if command_name != "mv":
-        print("This command not exist")
+    if not command_name == "mv" or file == path:
         return
 
+    path_to_file, file_from_path = os.path.split(path)
     if len(path.split("/")) > 1:
-        os.makedirs("/".join(path.split("/")[:-1]), exist_ok=True)
+        os.makedirs(os.path.join(path_to_file), exist_ok=True)
 
-    try:
-        with (open(file, "r") as current_file,
-              open(path, "x") as new_file):
-            new_file.write(current_file.read())
-        os.remove(file)
-    except FileExistsError:
-        return
+    with (open(file, "r") as current_file,
+            open(path, "w") as new_file):
+        new_file.write(current_file.read())
+    os.remove(file)
