cc8649b0b7c0a91ee0e732740ec2f121e21b7ea9
Ruslan Misko <miskoruslan6@gmail.com>
2023-10-22 03:39:52 +0300

    Fix by using replace()

--- app/main.py
+++ app/main.py
@@ -2,10 +2,9 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) == 3:
-        command_name, file_name, mv_file = command.split()
-        directories, file_name_out = os.path.split(mv_file)
-        if command_name == "mv" and os.path.isfile(file_name):
-            if directories:
-                os.makedirs(directories, exist_ok=True)
-            os.rename(file_name, mv_file)
+    mv, source_file, destination_path = command.split()
+    if mv == "mv":
+        path = os.path.dirname(destination_path)
+        if path:
+            os.makedirs(path, exist_ok=True)
+        os.replace(source_file, destination_path)
