5cb383024c1067d44788c16960d9e6628b471dfe
Eugene Rabenko <qwer09846@gmail.com>
2023-10-22 12:22:58 +0300

    Corrected

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import os
 def move_file(command: str) -> None:
     command_list = command.split()
     if len(command_list) == 3:
-        mv, file_name, destination = command_list
-        dirs, file_name_out = os.path.split(destination)
-        if mv == "mv":
+        command_name, file_name, destination = command_list
+        dirs, _ = os.path.split(destination)
+        if command_name == "mv":
             if dirs:
                 os.makedirs(dirs, exist_ok=True)
-            os.rename(file_name, destination)
+            os.replace(file_name, destination)
