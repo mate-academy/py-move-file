4c1c900b3fb662bf06911b3802a4bf900d5cfad4
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-23 14:21:21 +0300

    Added replace

--- app/main.py
+++ app/main.py
@@ -6,10 +6,4 @@ def move_file(command: str) -> None:
     path = os.path.dirname(second_file)
     if path:
         os.makedirs(path, exist_ok=True)
-        with (open(first_file, "r") as old_file,
-             open(second_file, "w") as new_file):
-            content = old_file.read()
-            new_file.write(content)
-        os.remove(first_file)
-    else:
-        os.rename(first_file, second_file)
+    os.replace(first_file, second_file)
