05e1917b8996d7beb9b25e092f1e77ec7d4a4565
Alex Ilchenko <alec.ilchenko@gmail.com>
2023-10-23 20:19:31 +0400

    Refactor hardcode

--- app/main.py
+++ app/main.py
@@ -7,13 +7,10 @@ def move_file(command: str) -> None:
     if len(command_split) != 3:
         return
     tag, source, destination = command_split
-    if len(destination.split("/")) == 1:
+    directory, filename = os.path.split(destination)
+    if not directory:
         os.rename(source, destination)
         return
-    split_path = destination.split("/")
-    folder_path = split_path[:-1]
-    new_name = split_path[-1]
-    path = "/".join(folder_path)
-    os.makedirs(path, exist_ok=True)
-    os.rename(source, new_name)
-    shutil.move(new_name, path)
+    os.makedirs(directory, exist_ok=True)
+    os.rename(source, filename)
+    shutil.move(filename, directory)
