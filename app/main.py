a96ae1d858bb016a1ad7ea774ce33981f3dc6d99
Alex Ilchenko <alec.ilchenko@gmail.com>
2023-10-23 04:35:31 +0400

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    tag, source, destination = command.split()
+    if len(destination.split("/")) == 1:
+        os.rename(source, destination)
+        return
+    split_path = destination.split("/")
+    folder_path = split_path[:-1]
+    new_name = split_path[-1]
+    path = "/".join(folder_path)
+    os.makedirs(path, exist_ok=True)
+    os.rename(source, new_name)
+    shutil.move(new_name, path)
