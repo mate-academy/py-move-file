c1ca3d8ef88dfde6bc9a80fad22a15b6ed99995c
Hryhoriy <hryhoriy.bilous@gmail.com>
2023-11-02 18:25:27 +0200

    Vyrvit' meni ochi

--- app/main.py
+++ app/main.py
@@ -3,19 +3,14 @@ import os
 
 def move_file(command: str) -> None:
     mv, source_file, path_file = command.split()
-    if mv == "mv" and len(command.split()) == 3:
+    if len(command.split()) == 3 and mv == "mv":
         if "/" not in path_file:
             os.rename(source_file, path_file)
         elif "/" in path_file:
             full_path_file = path_file
-            path_file = path_file.split("/")
-            new_file_name = path_file[-1]
-            path_file.pop(-1)
-            path_file = "/".join(path_file)
+            path_file, new_file_name = os.path.split(path_file)
             if not os.path.exists(path_file):
                 os.makedirs(path_file)
-            if os.path.exists(new_file_name):
-                os.remove(new_file_name)
             with (
                 open(source_file, "r") as in_file,
                 open(full_path_file, "w") as out_file
