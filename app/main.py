b189bf740cb2343c3f6622cb6ba2ce5902a6de85
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-11-03 15:41:06 +0200

    check if command == mv

--- app/main.py
+++ app/main.py
@@ -2,25 +2,18 @@ import os
 
 
 def move_file(command: str) -> None:
-    parts = command.split()
-    if len(parts) < 3:
-        raise ValueError("Invalid command format.")
+    comand, source_file, destination = command.split()
 
-    source_file = parts[1]
-    destination = parts[2]
+    if comand == "mv" and source_file != destination:
+        if destination.endswith("/"):
+            destination_dir = destination
+            file_name = os.path.basename(source_file)
+            destination_path = os.path.join(destination_dir, file_name)
+        else:
+            destination_path = destination
 
-    if destination.endswith("/"):
-        # Consider it as a directory
-        destination_dir = destination
-        file_name = os.path.basename(source_file)
-        destination_path = os.path.join(destination_dir, file_name)
-    else:
-        destination_path = destination
+        destination_directory = os.path.dirname(destination_path)
+        if destination_directory:
+            os.makedirs(destination_directory, exist_ok=True)
 
-    # Create directories if they don't exist
-    destination_directory = os.path.dirname(destination_path)
-    if destination_directory:
-        os.makedirs(destination_directory, exist_ok=True)
-
-    # Attempt to move the file
-    os.rename(source_file, destination_path)
+        os.rename(source_file, destination_path)
