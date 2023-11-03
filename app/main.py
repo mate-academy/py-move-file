f341b318dce7bb279dcf094d3b0bf01659563c8a
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-11-03 20:30:09 +0200

    add platform-independenence

--- app/main.py
+++ app/main.py
@@ -2,9 +2,16 @@ import os
 
 
 def move_file(command: str) -> None:
-    comand, source_file, destination = command.split()
+    parts = command.split()
+    if len(parts) != 3:
+        print("Invalid command format. Please provide a valid command.")
+        return
+
+    comand, source_file, destination = parts
 
     if comand == "mv" and source_file != destination:
+        source_file = os.path.normpath(source_file)
+        destination = os.path.normpath(destination)
         if destination.endswith("/"):
             destination_dir = destination
             file_name = os.path.basename(source_file)
