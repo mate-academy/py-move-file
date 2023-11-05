358f2468d510c13bd03a6fccad945f4322b0f01e
Alexandr K. <alex329657@gmail.com>
2023-11-05 11:40:22 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+from os import path, mkdir, chdir, curdir, rename
+
+
+def move_file(command: str) -> None:
+    args = command.split()
+    root_dir = path.abspath(curdir)
+    if (len(args) == 3 and args[0] == "mv" and args[2] != args[1]):
+        dest_path = args[2].split("/")
+        if len(dest_path) > 1:
+            for i in range(len(dest_path) - 1):
+                if not path.exists(dest_path[i]):
+                    mkdir(dest_path[i])
+                chdir("./" + dest_path[i])
+        chdir(root_dir)
+        rename(args[1], "./" + args[2])
