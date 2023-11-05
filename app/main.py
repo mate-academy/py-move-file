4b10cc3902820d91442287ed40e781c147a4597d
Alexandr K. <alex329657@gmail.com>
2023-11-05 13:33:36 +0200

    'Fix1'

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def move_file(command: str) -> None:
     if (len(args) == 3 and args[0] == "mv" and args[2] != args[1]):
         dest_path = args[2].split("/")
         if len(dest_path) > 1:
-            for i in range(len(dest_path) - 1):
-                if not path.exists(dest_path[i]):
-                    mkdir(dest_path[i])
-                chdir("./" + dest_path[i])
+            for tree_item in range(len(dest_path) - 1):
+                if not path.exists(dest_path[tree_item]):
+                    mkdir(dest_path[tree_item])
+                chdir("./" + dest_path[tree_item])
         chdir(root_dir)
         rename(args[1], "./" + args[2])
