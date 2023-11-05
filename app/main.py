c2c7d994a54eb8db2d93821ecaeb488230706671
Hryhoriy <hryhoriy.bilous@gmail.com>
2023-11-05 23:13:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,19 +2,13 @@ import os
 
 
 def move_file(command: str) -> None:
-    mv, source_file, path_file = command.split()
-    if len(command.split()) == 3 and mv == "mv":
-        if "/" not in path_file:
-            os.rename(source_file, path_file)
-        elif "/" in path_file:
-            full_path_file = path_file
-            path_file, new_file_name = os.path.split(path_file)
-            if not os.path.exists(path_file):
-                os.makedirs(path_file)
-            with (
-                open(source_file, "r") as in_file,
-                open(full_path_file, "w") as out_file
-            ):
-                out_file.write(in_file.read())
+    if len(command.split()) != 3:
+        return
+    cmd, filename, path_to_file = command.split()
 
-            os.remove(source_file)
+    if cmd == "mv":
+        directories = os.path.dirname(path_to_file)
+        if directories:
+            os.makedirs(directories, exist_ok=True)
+
+        os.replace(filename, path_to_file)
