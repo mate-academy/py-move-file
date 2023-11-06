f86a042f84734a0d4defc0cff15914e38b87c43c
Sergii Pryima <spryima@gmail.com>
2023-11-06 10:44:50 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) == 3 and parts[0] == "mv" and parts[1] != parts[2]:
+        source = parts[1]
+        dest = parts[2]
+
+        if os.path.isdir(dest) or dest.endswith('/'):
+            dest = os.path.join(dest, source)
+        elif not os.path.dirname(dest):
+            pass
+        else:
+            os.makedirs(os.path.dirname(dest), exist_ok=True)
+
+        os.rename(source, dest)
