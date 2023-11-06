5addec3d57a3f7b5909b9f8865cfa86f89cfa280
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2023-11-06 18:54:09 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ def move_file(command: str) -> None:
     command_parts = command.split()
 
     if len(command_parts) == 3 and command_parts[0] == "mv":
-        _, source, destination = command_parts
+        mv, source_file, destination_file = command_parts
 
-        if os.path.exists(source):
-            destination_dir = os.path.dirname(destination)
+        if os.path.exists(source_file):
+            destination_dir = os.path.dirname(destination_file)
             if destination_dir:
                 os.makedirs(destination_dir, exist_ok=True)
 
-            os.rename(source, destination)
+            os.rename(source_file, destination_file)
