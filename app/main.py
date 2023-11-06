09678caf1a88c6f51be631718dd08ae600b4426c
Andrii Malovanyi <malovaniy.an@gmail.com>
2023-11-06 19:10:26 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ import os
 
 
 def move_file(command: str) -> None:
-    mv, file, directory = command.split()
-    if mv == "mv":
-        path = os.path.dirname(directory)
-        if path:
-            os.makedirs(path, exist_ok=True)
-        os.replace(file, directory)
+    if len(command.split()) == 3:
+        mv, file, directory = command.split()
+        if mv == "mv":
+            path = os.path.dirname(directory)
+            if path:
+                os.makedirs(path, exist_ok=True)
+            os.replace(file, directory)
