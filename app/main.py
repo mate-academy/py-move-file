5c2a5b6e034b6a8d6099f75fe72eda992de19733
Alexandr K. <alex329657@gmail.com>
2023-11-09 04:42:13 +0200

    Fix2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
-from os import path, mkdir, chdir, curdir, rename
+from os import path, rename, makedirs
 
 
 def move_file(command: str) -> None:
     args = command.split()
-    root_dir = path.abspath(curdir)
+
     if (len(args) == 3 and args[0] == "mv" and args[2] != args[1]):
-        dest_path = args[2].split("/")
-        if len(dest_path) > 1:
-            for tree_item in range(len(dest_path) - 1):
-                if not path.exists(dest_path[tree_item]):
-                    mkdir(dest_path[tree_item])
-                chdir("./" + dest_path[tree_item])
-        chdir(root_dir)
+        directory_path = path.dirname(args[2])
+        if directory_path:
+            makedirs(directory_path, exist_ok=True)
         rename(args[1], "./" + args[2])
