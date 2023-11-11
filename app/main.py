a1cb5a90a9c28e961ebbb0cd9102a73e6ada3275
Yuliia Dudych <juliadudych@gmail.com>
2023-11-11 22:36:23 +0200

    Add logic to identify correct slash based on OS

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    # Get correct slash based on OS, fix command if needed
+    correct_slash, wrong_slash = get_slash()
+    if wrong_slash in command:
+        command = command.replace(wrong_slash, correct_slash)
+
+
+def get_slash() -> str:
+    for slash in ["/", "\\"]:
+        if slash in os.getcwd():
+            correct = slash
+        else:
+            wrong = slash
+    return correct, wrong
