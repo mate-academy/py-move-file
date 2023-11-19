487e75a0be62b341a9aa424e3d8d0d819530aa51
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2023-11-19 23:40:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_files(command: str) -> None:
+    list_files = command.split()
+    if len(list_files) == 3 and list_files[0] == "mv":
+        print(list_files[2])
+        path = (list_files[2][0:list_files[2].rindex("/")])
+        os.mkdir(f"./{path}")
+
+move_files("mv file.txt first_dir/file2.txt")
