532c631ba75900ab394286263bdf8ced331c3eb1
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2023-11-22 12:48:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    source_file = parts[1]
+    destination = parts[2]
+    if parts[1] == parts[2]:
+        return
+    directory, file_name = os.path.split(destination)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+    with open(source_file, "r") as src, open(destination, "w") as dst:
+        dst.write(src.read())
+    os.remove(source_file)
