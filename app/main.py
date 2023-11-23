b0826f257543c66535b932c195ee42780f9496b9
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2023-11-23 13:03:52 +0200

    New solution

--- app/main.py
+++ app/main.py
@@ -3,14 +3,16 @@ import os
 
 def move_file(command: str) -> None:
     list_files = command.split()
-    if len(list_files) == 3 and list_files[0] == "mv":
+    if (len(list_files) == 3
+            and list_files[0] == "mv"
+            and list_files[1] != list_files[2]):
         depth = list_files[2].split("/")
         path = ""
-        for n in range(len(depth) - 1):
-            path += f"{depth[n]}{os.sep}"
+        for num in range(len(depth) - 1):
+            path += f"{depth[num]}{os.sep}"
             try:
                 os.mkdir(f".{os.sep}{path}")
-            except:
+            except FileExistsError:
                 pass
         with (open(list_files[1], "r") as file_in,
               open(list_files[2], "w") as file_out):
