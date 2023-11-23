982a932569dc2ff40147b70f4de439be3d98dee0
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2023-11-23 12:27:12 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 import os
 
 
-def move_files(command: str) -> None:
+def move_file(command: str) -> None:
     list_files = command.split()
     if len(list_files) == 3 and list_files[0] == "mv":
-        print(list_files[2])
-        path = (list_files[2][0:list_files[2].rindex("/")])
-        os.mkdir(f"./{path}")
-
-move_files("mv file.txt first_dir/file2.txt")
+        depth = list_files[2].split("/")
+        path = ""
+        for n in range(len(depth) - 1):
+            path += f"{depth[n]}{os.sep}"
+            try:
+                os.mkdir(f".{os.sep}{path}")
+            except:
+                pass
+        with (open(list_files[1], "r") as file_in,
+              open(list_files[2], "w") as file_out):
+            lines = file_in.readlines()
+            for line in lines:
+                file_out.write(line)
+        os.remove(list_files[1])
