9f92883cc91df48d7fd25c0e2bc8bffd97c8bb0c
Serhii <serhii.domik@gmail.com>
2023-11-23 13:21:58 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -2,16 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    list_commands = command.split()
+    commands = command.split()
 
-    if len(list_commands) == 3 and list_commands[0] == "mv":
-        source_file = list_commands[1]
-        destination_file = list_commands[2]
+    if len(commands) == 3 and commands[0] == "mv":
 
-        destination_dir = os.path.dirname(destination_file)
-        file_name = os.path.basename(destination_file)
+        source_file_path, destination_file_path = commands[1:]
+
+        destination_dir, file_name = os.path.split(destination_file_path)
 
         if destination_dir and not os.path.exists(destination_dir):
             os.makedirs(destination_dir, exist_ok=True)
 
-        os.rename(source_file, os.path.join(destination_dir, file_name))
+        os.rename(source_file_path, destination_file_path)
