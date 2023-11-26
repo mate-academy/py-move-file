db0740a30c3de3c1bd1005ea49909a8a52bfb437
MykolaMazhula <mykm3ua@gmail.com>
2023-11-26 11:14:26 +0100

    Move-File-Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import shutil
+import os
+
+
+def move_file(command: str) -> None:
+    command_elements = command.split()
+    if len(command_elements) != 3 or command_elements[0] != "mv":
+        raise ValueError('"command" must be a string with command "mv", '
+                         "file name to move and new file name(path), "
+                         "separated by spaces.")
+
+    file_to_move_path, new_file_path = command_elements[1:]
+    assert os.path.exists(file_to_move_path),\
+        f"There is no file {file_to_move_path}"
+    new_path, new_file_name = os.path.split(new_file_path)
+    if new_path and not os.path.exists(new_path):
+        os.makedirs(new_path)
+    shutil.move(file_to_move_path, new_file_path)
