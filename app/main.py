604068d2d8af598098a214234e7536deaa358a33
xboris <boris.tikhonov.21@gmail.com>
2023-11-29 22:28:47 +0200

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mode, source, destination = command.split()
+
+    if "/" in destination:
+        os.makedirs(os.path.dirname(destination), exist_ok=True)
+
+        with open(source, "r") as file_in, open(destination, "w") as file_out:
+            content = file_in.read()
+            file_out.write(content)
+        os.remove(source)
+    else:
+        os.rename(source, destination)
