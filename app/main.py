48fe0db71f2929d5844c93ac73b98635ae0ed02b
Anton Konyk <antonkonyk.com>
2023-11-30 12:07:11 +0100

    Solution_30112023

--- app/main.py
+++ app/main.py
@@ -6,23 +6,11 @@ def move_file(command: str) -> None:
 
     if (parts[0] == "mv"
        and parts[1] != parts[2]
-       and "/" not in parts[2]
-       and len(parts) == 3):
-        with (open(parts[1], "r") as file_in,
-              open(parts[2], "w") as new_file):
-            new_file.write(file_in.read())
-            file_in.close()
-            os.remove(parts[1])
-
-    if (parts[0] == "mv"
-       and parts[1] != parts[2]
-       and "/" in parts[2]
        and len(parts) == 3):
 
         current_dir = os.getcwd()
 
         with open(parts[1], "r") as file_in:
-
             dirs = parts[2].split("/")
             new_file = dirs.pop(len(dirs) - 1)
 
@@ -34,6 +22,7 @@ def move_file(command: str) -> None:
                     os.chdir(directory)
 
             with open(new_file, "w") as new_file:
+
                 new_file.write(file_in.read())
                 file_in.close()
                 os.chdir(current_dir)
