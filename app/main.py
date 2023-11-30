d75ae966d642ef1cc9618b1dd4d0be948acc10e4
Anton Konyk <antonkonyk.com>
2023-11-30 18:17:41 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,26 +4,16 @@ import os
 def move_file(command: str) -> None:
     parts = command.split()
 
-    if (parts[0] == "mv"
-       and parts[1] != parts[2]
-       and len(parts) == 3):
-
-        current_dir = os.getcwd()
+    if (len(parts) == 3
+            and parts[0] == "mv"
+            and parts[1] != parts[2]):
 
         with open(parts[1], "r") as file_in:
-            dirs = parts[2].split("/")
-            new_file = dirs.pop(len(dirs) - 1)
+            head_tail = os.path.split(parts[2])
+            if head_tail[0] != "":
+                os.makedirs(head_tail[0], exist_ok=True)
 
-            for directory in dirs:
-                if os.path.isdir(directory):
-                    os.chdir(directory)
-                else:
-                    os.mkdir(directory)
-                    os.chdir(directory)
-
-            with open(new_file, "w") as new_file:
-
+            with open(parts[2], "w") as new_file:
                 new_file.write(file_in.read())
-                file_in.close()
-                os.chdir(current_dir)
-                os.remove(parts[1])
+
+        os.remove(parts[1])
