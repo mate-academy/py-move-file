69d7320f6362150c3d1d5d9d265296af2c61ddb1
Yuliia Valko <yuliia.valko1@gmail.com>
2023-12-01 15:00:07 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,28 +3,18 @@ import os
 
 def move_file(command: str) -> None:
     user_input = command.split()
-    if len(user_input) == 3:
+    if len(user_input) == 3 and user_input[0] == "mv":
         filename, directories = user_input[1], user_input[2]
 
-        if "/" in directories:
-            new_filename = directories[directories.rfind("/"):]
+        if os.path.sep in directories:
+            new_filename = directories[directories.rfind(os.path.sep):]
+
             without_name_directories = directories.replace(new_filename, "")
 
             if not os.path.exists(without_name_directories):
                 os.makedirs(without_name_directories)
 
-            with open(filename, "r") as source_file:
-                user_input = source_file.read()
-
-            with open(directories, "w") as dest_file:
-                dest_file.write(user_input)
-
+            os.replace(filename, directories)
         else:
             new_filename = directories
-            with open(filename, "r") as source_file:
-                user_input = source_file.read()
-
-            with open(new_filename, "w") as dest_file:
-                dest_file.write(user_input)
-
-        os.remove(filename)
+            os.replace(filename, new_filename)
