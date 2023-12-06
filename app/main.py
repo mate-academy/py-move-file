8ae4fcb761dc8cb2b6a3769e8ffc413973630bfc
Alex Moon <moon.ra.lex@gmail.com>
2023-12-06 22:17:07 +0100

    Done

--- app/main.py
+++ app/main.py
@@ -1,29 +1,20 @@
 import os
 
 
+def create_valid_path(path: str) -> str | None:
+    file_name = os.path.basename(path)
+    directory = os.path.dirname(path)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+    return os.path.join(directory, file_name)
+
+
 def move_file(command: str) -> None:
     command_elements = command.split()
     if len(command_elements) == 3:
-        command, file_name, new_path = command_elements
+        command, source_file, destination_path = command_elements
         if command == "mv":
-            if new_path.endswith("/"):
-                destination_directory = new_path
-                destination_file_path = (
-                    os.path.join(destination_directory, file_name)
-                )
-                try:
-                    if not os.path.exists(destination_directory):
-                        os.makedirs(destination_directory)
-                    os.rename(file_name, destination_file_path)
-                except FileNotFoundError:
-                    raise FileNotFoundError(f"File '{file_name}' not found.")
-            else:
-                try:
-                    new_path_dirs = new_path.split("/")
-                    for i in range(1, len(new_path_dirs)):
-                        directory_to_check = "/".join(new_path_dirs[:i])
-                        if not os.path.exists(directory_to_check):
-                            os.makedirs(directory_to_check)
-                    os.rename(file_name, new_path)
-                except FileNotFoundError:
-                    raise FileNotFoundError(f"File '{file_name}' not found.")
+            new_file = create_valid_path(destination_path)
+            os.rename(source_file, new_file)
+        if os.path.exists(source_file):
+            os.remove(source_file)
