448686b913da52c1012d2bb0007fff4d4be6bc4c
Nataliia Povashevych <interchanal@ukr.net>
2023-12-06 07:48:45 +0200

    'Solution_rename_variables'

--- app/main.py
+++ app/main.py
@@ -2,19 +2,13 @@ from os import path, makedirs, rename
 
 
 def move_file(command: str) -> None:
-    command_list = command.split()
-    if len(command_list) != 3:
+    commands = command.split()
+    if len(commands) != 3:
         raise ValueError("Invalid command format")
-
-    user_command, user_file, new_user_file = command_list
-    dir_path, file_name = path.split(new_user_file)
-
+    cmd, source_path, destination_path = commands
+    dir_path, file_name = path.split(destination_path)
     if dir_path:
         makedirs(dir_path, exist_ok=True)
-
-    full_new_user_file = path.join(dir_path, file_name)
-
-    if path.exists(full_new_user_file):
+    if path.exists(destination_path):
         raise FileExistsError("The file already exists.")
-
-    rename(user_file, full_new_user_file)
+    rename(source_path, destination_path)
