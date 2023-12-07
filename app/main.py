fe73636ef2ded5b8a37b670721001e1e00f4e1c1
eLQeR <131271125+eLQeR@users.noreply.github.com>
2023-12-07 22:38:17 +0200

    Solution  full

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
-from os import remove, makedirs
+from os import remove, makedirs, path
 
 
 def move_file(command: str) -> None:
     if command.startswith("mv "):
         commands, first_file_name, second_file_name = command.split()
         if "/" in second_file_name:
-            paths = "".join([
-                directory + "/"
-                for directory in second_file_name.split("/")
-                if "." not in directory
-            ])
+            paths = path.split(second_file_name)[0]
             makedirs(paths, exist_ok=True)
         with (open(first_file_name, "r") as file_to_copy,
               open(second_file_name, "w") as file_copy):
