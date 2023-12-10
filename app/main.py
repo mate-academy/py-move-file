0c04f199a12abf9a9efcb5768b58a042cb2daf8f
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-12-10 20:01:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+
+    if len(command) == 3 and command[0] == "mv":
+        with open(command[1], "r") as file_to_delete:
+            context = file_to_delete.read()
+
+            os.remove(command[1])
+
+            if "/" in command[2]:
+
+                dirs_to_create = "/".join(command[2].split("/")[:-1])
+                file_to_create = command[2].split("/")[-1]
+
+                os.makedirs(dirs_to_create, exist_ok=True)
+
+                with open(
+                        dirs_to_create + "/" + file_to_create, "w"
+                ) as file_to_create:
+                    file_to_create.write(context)
+
+            else:
+                with open(command[2], "w") as file_to_create:
+                    file_to_create.write(context)
