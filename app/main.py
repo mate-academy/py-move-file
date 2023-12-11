74e41efa5d406fc03126181b1317198d73943af8
Alex <oleksa.shcherbakov@gmail.com>
2023-12-11 09:20:02 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import shutil
 from pathlib import Path
 
 
-def move_file(command) -> None | str:
-    command, source_file_name, path_second_file  = command.split()
+def move_file(command:str) -> None | str:
+    command, source_file_name, path_second_file = command.split()
 
     if command != "mv":
         return "command error"
@@ -13,9 +13,9 @@ def move_file(command) -> None | str:
     if len(path_arr) == 1:
         os.rename("./" + source_file_name, "./" + path_arr[0])
     else:
-        aim_file_name =    path_arr.pop()
+        aim_file_name = path_arr.pop()
         path_arr = path_second_file.split("/")[:-1]
-        path = "./" +"/".join(path_arr)
+        path = "./" + "/".join(path_arr)
         if Path(path).exists():
             shutil.move(fr"./{source_file_name}", fr"{path}/{aim_file_name}")
         else:
