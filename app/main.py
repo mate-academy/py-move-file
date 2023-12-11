5f9141b39152ef41383779305c1644b2d3088891
P-is-here <pasha.nasonovhtc@gmail.com>
2023-12-11 22:18:22 +0200

    deleting old after creating new one

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def move_file(command: str) -> None:
         with open(command[1], "r") as file_to_delete:
             context = file_to_delete.read()
 
-        os.remove(command[1])
-
         destination, new_file_name = os.path.split(command[2])
         if destination:
             os.makedirs(destination, exist_ok=True)
 
         with open(command[2], "w") as file:
             file.write(context)
+
+        os.remove(command[1])
