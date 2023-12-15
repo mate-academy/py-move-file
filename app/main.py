0c48a044c225f288db6527fdc2a60ed4e054b94e
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-15 21:38:10 +0200

    the checking was added

--- app/main.py
+++ app/main.py
@@ -2,17 +2,21 @@ import os
 
 
 def move_file(command: str) -> None:
-    _, source_file, destination_path = command.split()
-    if not os.path.dirname(destination_path):
-        destination_file = destination_path
-        os.rename(source_file, destination_file)
-    elif destination_path.endswith("/"):
-        os.makedirs(destination_path, exist_ok=True)
-        destination_file = os.path.join(destination_path,
-                                        os.path.basename(source_file))
-        os.rename(source_file, destination_file)
+    if len(command.split()) == 3:
+        operation, source_file, destination_path = command.split()
+        if operation == "mv":
+            if not os.path.dirname(destination_path):
+                destination_file = destination_path
+            elif destination_path.endswith("/"):
+                os.makedirs(destination_path, exist_ok=True)
+                destination_file = os.path.join(destination_path,
+                                                os.path.basename(source_file))
+            else:
+                destination_file = destination_path
+                destination_path = os.path.dirname(destination_path)
+                os.makedirs(destination_path, exist_ok=True)
+        else:
+            print("Error: operation should be 'mv'")
     else:
-        destination_file = destination_path
-        destination_path = os.path.dirname(destination_path)
-        os.makedirs(destination_path, exist_ok=True)
-        os.rename(source_file, destination_file)
+        print("Error: wrong format")
+    os.rename(source_file, destination_file)
