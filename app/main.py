965cfdc35c0fbd16cbb960333541014a21e6cd8d
Borys Kirichenko <127791726+bkirichenko@users.noreply.github.com>
2023-12-15 16:29:29 +0100

    Update app/main.py
    Co-authored-by: ArsenPidhoretskyi <65166203+ArsenPidhoretskyi@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -3,9 +3,12 @@ import os
 
 def move_file(command: str) -> None:
     parts = command.split()
-    command, source, target = parts
-    if any([len(parts) != 3, command != "mv", source == target]):
+    if len(parts) != 3:
         return
+
+    command, source, target = parts
+    if any((command != "mv", source == target)):
+         return
     if os.path.dirname(target) != "" or target.endswith("/"):
         os.makedirs(os.path.dirname(target), exist_ok=True)
     with open(source, "r") as file1, open(target, "w") as file2:
