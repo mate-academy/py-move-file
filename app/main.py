179c93cc7231b7af2573418b8377145cfddc5659
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-12-15 09:54:30 +0100

    all tests passed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    command, source, target = parts
+    if any([len(parts) != 3, command != "mv", source == target]):
+        return
+    if os.path.dirname(target) != "" or target.endswith("/"):
+        os.makedirs(os.path.dirname(target), exist_ok=True)
+    with open(source, "r") as file1, open(target, "w") as file2:
+        file2.write(file1.read())
+    os.remove(source)
