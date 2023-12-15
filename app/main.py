fc64904fa4917ed3ae5a14e9e6c3be718b7007a8
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-15 19:54:08 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, source_file, destination_path = command.split()
+    if not os.path.dirname(destination_path):
+        destination_file = destination_path
+        os.rename(source_file, destination_file)
+    elif destination_path.endswith("/"):
+        os.makedirs(destination_path, exist_ok=True)
+        destination_file = os.path.join(destination_path,
+                                        os.path.basename(source_file))
+        os.rename(source_file, destination_file)
+    else:
+        destination_file = destination_path
+        destination_path = os.path.dirname(destination_path)
+        os.makedirs(destination_path, exist_ok=True)
+        os.rename(source_file, destination_file)
