c17c1dad82f813c3d373c058bfbb15ea7b9cf579
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2023-12-24 20:27:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mode, copy, paste = command.split()
+
+    if mode == "mv":
+        if "/" in paste and not os.path.exists(paste):
+            os.makedirs(f"{os.getcwd()}/{paste.rsplit("/", 1)[0]}",
+                        exist_ok=True)
+
+        with (open(copy) as from_file,
+              open(paste, "w") as to_file):
+            to_file.write(from_file.read())
+        os.remove(copy)
