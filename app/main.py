2b634d66e278ca986a9504fa9295da8f24ae97c2
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2023-12-29 00:09:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    cd, source_name, target_name, *_ = command.split()
+    if cd != "mv":
+        return
+
+    if "/" in target_name:
+        *target_path_parts, file_name = target_name.split("/")
+        current_path = ""
+        for directory in target_path_parts:
+            current_path = f"{current_path}/{directory}"\
+                if current_path else directory
+            try:
+                os.mkdir(current_path)
+            except FileExistsError:
+                pass
+
+        target_name = f"{current_path}/{file_name}"
+    try:
+        with open(source_name, "rb") as source_file:
+            with open(target_name, "wb") as target_file:
+                target_file.write(source_file.read())
+
+        os.remove(source_name)
+
+    except FileNotFoundError:
+        print(f"File not found: {source_name}")
