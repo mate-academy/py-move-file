f35447b5a40a9cee3e9ad2cc962ce12e4b9d2106
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2023-12-28 11:44:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,19 +4,31 @@ import os
 def move_file(command: str) -> None:
     command, in_file, out_file = command.split()
     if command == "mv":
-        if "/" in out_file:
-            path_list = out_file.split("/")
+        path = out_file
+        path_list = []
+        while path != "":
+            path_list.append(os.path.split(path)[1])
+            path = os.path.split(path)[0]
+
+        if len(path_list) == 1:
+            with (open(in_file, "r")as start_file,
+                  open(out_file, "w") as end_file):
+                end_file.write(start_file.read())
+
+        else:
+            path_list = path_list[3::-1]
+
             path = ""
             for i in range(len(path_list) - 1):
                 if os.path.exists(os.path.join(path, path_list[i])):
                     path = os.path.join(path, path_list[i])
+                    print(path)
                 else:
                     os.mkdir(os.path.join(path, path_list[i]))
                     path = os.path.join(path, path_list[i])
-            with (open(in_file, "r")as in_f,
-                  open(os.path.join(path, path_list[-1]), "w") as out_f):
-                out_f.write(in_f.read())
-        else:
-            with open(in_file, "r")as in_f, open(out_file, "w") as out_f:
-                out_f.write(in_f.read())
+                    print(path)
+            with (open(in_file, "r")as start_file,
+                  open(os.path.join(path, path_list[-1]), "w") as end_file):
+                end_file.write(start_file.read())
+
     os.remove(in_file)
