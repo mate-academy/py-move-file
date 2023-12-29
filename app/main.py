65c33b17f8abf7acde6a7a651f8b078b7b214114
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2023-12-29 13:06:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,33 +2,32 @@ import os
 
 
 def move_file(command: str) -> None:
-    command, in_file, out_file = command.split()
+    command, in_file, out_file, *_ = command.split()
     if command == "mv":
-        path = out_file
-        path_list = []
-        while path != "":
-            path_list.append(os.path.split(path)[1])
-            path = os.path.split(path)[0]
+        # path = out_file
+        path = os.path.normpath(out_file)
+        print(path)
 
-        if len(path_list) == 1:
+        path = path.split(os.sep)
+        print(path)
+        *file_path, file_name = path
+        print(file_path, file_name)
+
+        if len(file_path) == 0:
             with (open(in_file, "r")as start_file,
                   open(out_file, "w") as end_file):
                 end_file.write(start_file.read())
 
         else:
-            path_list = path_list[3::-1]
-
             path = ""
-            for i in range(len(path_list) - 1):
-                if os.path.exists(os.path.join(path, path_list[i])):
-                    path = os.path.join(path, path_list[i])
-                    print(path)
+            for part_of_path in file_path:
+                if os.path.exists(os.path.join(path, part_of_path)):
+                    path = os.path.join(path, part_of_path)
                 else:
-                    os.mkdir(os.path.join(path, path_list[i]))
-                    path = os.path.join(path, path_list[i])
-                    print(path)
-            with (open(in_file, "r")as start_file,
-                  open(os.path.join(path, path_list[-1]), "w") as end_file):
+                    os.mkdir(os.path.join(path, part_of_path))
+                    path = os.path.join(path, part_of_path)
+            with (open(in_file, "r") as start_file,
+                  open(os.path.join(path, file_name), "w") as end_file):
                 end_file.write(start_file.read())
 
     os.remove(in_file)
