b06ca31c37f6b58199d4804ca6a244a9daa679cd
Yura Push <youdeimos1@gmail.com>
2024-01-08 22:41:41 +0200

    -added check for command name -used os.makedirs -opened 2 context managers at once

--- app/main.py
+++ app/main.py
@@ -1,23 +1,6 @@
 import os
 
 
-# def move_file(command: str) -> None:
-#     command_name, old_file, path = command.split()
-#     path_dirs = path.split("/")
-#     new_file = path_dirs.pop(-1)
-#
-#     with open(old_file, "r") as file_out:
-#         content = file_out.read()
-#         new_path = ""
-#         for directory in path_dirs:
-#             new_path += directory + "/"
-#             if not os.path.exists(new_path):
-#                 os.mkdir(new_path)
-#         with open(new_path + new_file, "w") as file_in:
-#             file_in.write(content)
-#     os.remove(old_file)
-
-
 def move_file(command: str) -> None:
     command_name, old_file, path, *_ = command.split()
 
