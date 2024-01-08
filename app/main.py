cf64de9c01cf011adba466c7f9a8283d8660f973
ladmael <don.sosiska339@gmail.com>
2024-01-08 22:18:14 +0200

    res

--- app/main.py
+++ app/main.py
@@ -4,13 +4,10 @@ import shutil
 
 def move_file(command: str) -> None:
     _, source, destination = command.split()
-    if destination.endswith('/'):
-        # Ensure the directory exists
+    if destination.endswith("/"):
         os.makedirs(destination, exist_ok=True)
-        # Set the destination file path
         destination = os.path.join(destination, os.path.basename(source))
     else:
-        # Check if the destination's parent directory exists, and if not, create it
         parent_dir = os.path.dirname(destination)
         if parent_dir and not os.path.exists(parent_dir):
             os.makedirs(parent_dir)
