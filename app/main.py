bf197d3f4d1026b98de15fdca070df1d88065413
Vladyslav Pliashechnyk <vlad.pliashechnyk@gmail.com>
2024-01-09 16:44:36 +0200

    Simplified version

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
-from os import remove, rename, makedirs, path
+from os import makedirs, path, replace
 
 
 def move_file(command: str) -> None:
     command_name, old_file_name, new_file_name = command.split()
-    if command_name == "mv" and "/" in new_file_name:
-        directory_path = path.dirname(new_file_name)
-        makedirs(directory_path, exist_ok=True)
-        with (
-            open(old_file_name, "r") as old_file,
-            open(new_file_name, "w") as new_file
-        ):
-            new_file.write(old_file.read())
-        remove(old_file_name)
-    else:
-        rename(old_file_name, new_file_name)
+    if command_name == "mv":
+        if "/" in new_file_name:
+            directory_path = path.dirname(new_file_name)
+            makedirs(directory_path, exist_ok=True)
+            replace(old_file_name, new_file_name)
+        else:
+            replace(old_file_name, new_file_name)
