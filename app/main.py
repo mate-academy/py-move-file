460fe909ad0134d682d5e8f10d18e86fce1eecb1
Vladyslav Pliashechnyk <vlad.pliashechnyk@gmail.com>
2024-01-10 12:16:24 +0200

    Added additional validation and replaced check condition

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from os import makedirs, path, replace
 
 
 def move_file(command: str) -> None:
-    command_name, old_file_name, new_file_name = command.split()
-    if command_name == "mv":
-        if "/" in new_file_name:
+    if len(command.split()) == 3:
+        command_name, old_file_name, new_file_name = command.split()
+        if command_name == "mv":
             directory_path = path.dirname(new_file_name)
-            makedirs(directory_path, exist_ok=True)
-            replace(old_file_name, new_file_name)
-        else:
-            replace(old_file_name, new_file_name)
+            if directory_path:
+                makedirs(directory_path, exist_ok=True)
+                replace(old_file_name, new_file_name)
+            else:
+                replace(old_file_name, new_file_name)
