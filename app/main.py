587854caf7f5bcb1f85fec06cc77dbf98973748e
Dariia Shostak <davorobii@gmail.com>
2024-01-11 15:44:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) == 3 and parts[0] == "mv":
+        mv, file_name, new_file_name = parts
+        if "/" in new_file_name:
+            way, name_file = os.path.split(new_file_name)
+            os.makedirs(way)
+            with (open(file_name, "r") as source,
+                  open(new_file_name, "w") as new_file):
+                new_file.write(source.read())
+            os.remove(file_name)
+        else:
+            os.rename(file_name, new_file_name)
