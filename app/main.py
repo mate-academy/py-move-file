318c0d4cc8ecdf73fc617cd6188e91ac6cd479af
Oleksandr Asauliak <varmen67@gmail.com>
2024-01-15 06:11:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mv, file_name, destination = command.split()
+
+    if mv == "mv":
+        dirs = os.path.dirname(destination)
+        if dirs:
+            os.makedirs(dirs, exist_ok=True)
+
+        if os.path.exists(destination):
+            os.remove(destination)
+
+        os.replace(file_name, destination)
