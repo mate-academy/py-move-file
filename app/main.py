5e0e02c46b33afd74b4d437b970ea44da6727409
Valeriia Danyliuk <bladeryz@gmail.com>
2024-01-15 18:23:40 +0200

    Implemented the move_file function!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, source_file, destination = command.split()
+
+    if not os.path.dirname(destination):
+        os.replace(source_file, destination)
+    else:
+        target_directory = os.path.dirname(destination)
+
+        os.makedirs(target_directory, exist_ok=True)
+        os.replace(source_file, destination)
