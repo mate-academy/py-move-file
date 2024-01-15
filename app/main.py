ccbefba6764161ce020c4a801970a8c6e798aadc
Valeriia Danyliuk <bladeryz@gmail.com>
2024-01-15 18:56:15 +0200

    Removed unnecessary lines of code!

--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ import os
 def move_file(command: str) -> None:
     command, source_file, destination = command.split()
 
-    if not os.path.dirname(destination):
-        os.replace(source_file, destination)
-    else:
+    if os.path.dirname(destination):
         target_directory = os.path.dirname(destination)
 
         os.makedirs(target_directory, exist_ok=True)
-        os.replace(source_file, destination)
+
+    os.replace(source_file, destination)
