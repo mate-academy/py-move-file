20b0c7d1f982a456e97b3d1306e3e0cd2e619920
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-01-16 21:03:30 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    new_command = command.split()
+    if len(new_command) == 3 and new_command[0] == "mv":
+        if "/" in new_command[2]:
+            new_dir = new_command[2].split("/")
+            new_dir.pop(len(new_dir) - 1)  # Delete file name from path of directory.
+            path_of_dir = ""
+            for folder in new_dir:  # Create new directory.
+                try:
+                    path_of_dir += folder + "/"
+                    os.mkdir(path_of_dir)
+                except FileExistsError:
+                    pass
+        with (open(new_command[1], "r") as file_in,
+              open(new_command[2], "w") as file_out):
+            file_out.write(file_in.read())
+        os.remove(new_command[1])
