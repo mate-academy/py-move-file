66cbf4dc565c9642608210fa79872e741bbf33b1
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-01-18 14:51:47 +0200

    remove returns from function

--- app/main.py
+++ app/main.py
@@ -2,17 +2,13 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) != 3:
-        return
     operation, source_file, new_file = command.split()
-    if operation != "mv":
-        return
-    dirs = os.path.dirname(new_file)
-    if dirs:
-        os.makedirs(dirs, exist_ok=True)
+    if operation == "mv":
+        dirs = os.path.dirname(new_file)
+        if dirs:
+            os.makedirs(dirs, exist_ok=True)
 
-    with open(source_file, "r") as source:
-        with open(new_file, "w") as target:
+        with (open(source_file, "r") as source,
+              open(new_file, "w") as target):
             target.writelines(source.read())
-
-    os.remove(source_file)
+        os.remove(source_file)
