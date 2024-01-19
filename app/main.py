1ed6444c46dbd30543beb0b67938e423b01ce386
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-01-19 21:49:05 +0100

    fix task

--- app/main.py
+++ app/main.py
@@ -4,16 +4,9 @@ import os
 def move_file(command: str) -> None:
     new_command = command.split()
     if len(new_command) == 3 and new_command[0] == "mv":
-        if "/" in new_command[2]:
-            new_dir = new_command[2].split("/")
-            new_dir.pop(len(new_dir) - 1)  # Delete file name.
-            path_of_dir = ""
-            for folder in new_dir:  # Create new directory.
-                try:
-                    path_of_dir += folder + "/"
-                    os.mkdir(path_of_dir)
-                except FileExistsError:
-                    pass
+        new_dir = os.path.split(new_command[2])
+        if len(new_dir[0]) != 0:
+            os.makedirs(new_dir[0], exist_ok=True)
         with (open(new_command[1], "r") as file_in,
               open(new_command[2], "w") as file_out):
             file_out.write(file_in.read())
