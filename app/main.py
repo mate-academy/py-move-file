0c92d18dba34be3589dc8a8f80c35a0171155ae9
Eliza Petrova <elizapetrova8@gmail.com>
2024-01-19 16:39:18 +0200

    py-move-file

--- app/main.py
+++ app/main.py
@@ -2,23 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_split = command.split()
+    mv, file_name, destination_path = command.split()
+    if mv == "mv":
+        dirs = os.path.dirname(destination_path)
 
-    if len(command_split) == 3:
-        action, file_in, file_out = command_split
+        if dirs:
+            os.makedirs(dirs, exist_ok=True)
 
-        if action == "mv":
-            file_out_path = file_out.split("/")[:-1]
-            file_in_content = open(file_in, "r").read()
-            os.remove(file_in)
+        if os.path.exists(destination_path):
+            os.remove(destination_path)
 
-            if len(file_out_path) > 0:
-
-                for i in range(1, len(file_out_path) + 1):
-                    try:
-                        os.mkdir(os.path.join(file_out_path[:i]))
-                    except FileExistsError:
-                        continue
-
-            with open(file_out, "w") as file_out:
-                file_out.write(file_in_content)
+        os.replace(file_name, destination_path)
