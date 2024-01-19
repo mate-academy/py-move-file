f007cd8caf1d9279534a10f24e0632a8a81c58d2
Yuriy P <xxxx@yy.com>
2024-01-19 03:55:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, source_path, destination_path = command.split()
+    directory, filename = os.path.split(destination_path)
+    if not directory:
+        os.rename(source_path, destination_path)
+        return
+    os.makedirs(directory, exist_ok=True)
+    os.rename(source_path, destination_path)
