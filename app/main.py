3b1a14e7f3d7d881a9a26a854a0c2944cec6234f
IvanGuculyak <ivan.guculyak@gmail.com>
2024-01-20 18:03:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,19 +5,19 @@ def move_file(command: str) -> None:
     if len(command.split()) != 3:
         raise ValueError("Command must to have 3 parts.")
 
-    operation, file1, path_and_file2 = command.split()
+    operation, source_file, path_and_file2 = command.split()
+
+    if operation != "mv":
+        raise ValueError("Operation must be mv.")
 
     path = os.path.dirname(path_and_file2)
     if path:
         os.makedirs(path, exist_ok=True)
-        file_name = path + "/" + path_and_file2.split("/")[-1]
-    else:
-        file_name = path_and_file2
 
     with (
-        open(file1, "r") as source,
-        open(file_name, "w") as new_file
+        open(source_file, "r") as source,
+        open(path_and_file2, "w") as new_file
     ):
         new_file.write(source.read())
 
-    os.remove(file1)
+    os.remove(source_file)
