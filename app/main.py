84e09803cc16f975d7eb5be40e64a1af3bebdf50
Dmytry-S <sviridoffd@gmail.com>
2024-01-20 17:27:56 +0200

    update move-file task

--- app/main.py
+++ app/main.py
@@ -2,14 +2,12 @@ import os
 
 
 def move_file(command: str) -> None:
-    com_d = command.split()
-    if com_d[0] != "mv":
+    comm, source_path, dest_path = command.split()
+    if comm != "mv":
         return None
-    n_dir = com_d[2].split("/")
-    if len(n_dir) == 1:
-        os.rename(com_d[1], com_d[2])
+    directory, filename = os.path.split(dest_path)
+    if directory == "":
+        os.rename(source_path, filename)
     else:
-        dir_path = "/".join(n_dir[:-1]) + "/"
-        dir_path = os.path.join(dir_path, os.path.basename(n_dir[-1]))
-        os.makedirs(os.path.dirname(dir_path), exist_ok=True)
-        os.rename(com_d[1], dir_path)
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        os.rename(source_path, dest_path)
