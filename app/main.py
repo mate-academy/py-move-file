50a4f3dcccc5f36a75d4e1c6d6faa203c085f597
Yevhen <evgeniy.playid@icloud.com>
2024-01-24 16:11:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    operation, source_file, new_file = command.split()
+
+    path = os.path.dirname(new_file)
+    if path:
+        os.makedirs(path, exist_ok=True)
+
+    with (
+        open(source_file, "r") as source,
+        open(new_file, "w") as new
+    ):
+        new.writelines(source.read())
+
+    os.remove(source_file)
