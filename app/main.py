e77f2098274820571dd44df7ca5a202eede12408
andriy-py <andriy.omelya@gmail.com>
2024-01-24 15:00:19 +0200

    Done

--- app/main.py
+++ app/main.py
@@ -4,14 +4,16 @@ import os
 def move_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3:
-        raise ValueError("Command must contain exactly three parts: 'mv', 'file_name', and 'directory'")
+        raise ValueError("Command must contain exactly three parts: 'mv',"
+                         " 'file_name', and 'directory'")
 
     cm, name_file, directory = parts
 
     if cm != "mv":
         raise ValueError("The first part of the command must be 'mv'")
     if name_file == directory or directory == "":
-        raise ValueError("The file name and directory cannot be the same or empty")
+        raise ValueError("The file name and directory"
+                         " cannot be the same or empty")
 
     new_dir = os.path.dirname(directory)
     if new_dir:
