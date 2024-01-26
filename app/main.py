376e080823a5d490d383bcf2be518958e68cbfb6
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-01-26 11:55:53 +0200

    split to os.path.split

--- app/main.py
+++ app/main.py
@@ -3,19 +3,17 @@ import os
 
 def move_file(command: str) -> None:
     file, path_to_move = command[3:].split(" ")
-    directories = path_to_move.split("/")
+    directories, filename = os.path.split(path_to_move)
 
     with open(file, "r") as r:
         content = r.read()
 
-        path_without_file = os.path.join(directories[:-1])
-        if os.path.isdir(path_without_file):
-            with open(path_to_move, "w") as a:
+        if os.path.isdir(directories):
+            with open(os.path.join(directories, filename), "w") as a:
                 a.write(content)
-
         else:
-            if directories[:-1]:
-                os.makedirs(path_without_file, exist_ok=True)
+            if directories:
+                os.makedirs(directories, exist_ok=True)
 
             with open(path_to_move, "a") as a:
                 a.write(content)
