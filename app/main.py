32e6e3e93c4ee7e32398b2f974fd5ad3a13015db
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-01-27 13:02:32 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,19 +3,23 @@ import os
 
 def move_file(command: str) -> None:
     split_command = command.split()
-    source_path = split_command[1]
-    destination_path = split_command[2]
-    if (len(split_command) == 3
-            and source_path != destination_path
-            and split_command[0] == "mv"):
 
-        destination_directory, file_name = os.path.split(destination_path)
+    if len(split_command) == 3 and split_command[0] == "mv":
+        source_path, destination_path = split_command[1], split_command[2]
 
-        if destination_directory:
-            os.makedirs(destination_directory, exist_ok=True)
+        if source_path != destination_path:
+            destination_directory, _ = os.path.split(destination_path)
 
-        with (open(source_path, "r") as source_file,
-              open(destination_path, "w") as destination_file):
-            destination_file.write(source_file.read())
+            if destination_directory:
+                os.makedirs(destination_directory, exist_ok=True)
 
-        os.remove(source_path)
+            with (open(source_path, "r") as source_file,
+                  open(destination_path, "w") as destination_file):
+                destination_file.write(source_file.read())
+
+            os.remove(source_path)
+        else:
+            print("Source and destination paths are the same.")
+    else:
+        print("Invalid command format. "
+              "Usage: move_file mv <source_path> <destination_path>")
