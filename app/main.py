32cc193cfce1bd7ffee5bc1339f2bc8aa9e339ce
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-29 12:41:56 +0200

    Update main.py
    1) added walrus operator
    2) deleted condition

--- app/main.py
+++ app/main.py
@@ -5,9 +5,7 @@ def move_file(command: str) -> None:
     cmd, file_name, file_path = command.split()
 
     if cmd == "mv":
-        if os.path.dirname(file_path):
-            destination = os.path.dirname(file_path)
-            if not os.path.exists(destination):
-                os.makedirs(destination, exist_ok=True)
+        if destination := os.path.dirname(file_path):
+            os.makedirs(destination, exist_ok=True)
 
         os.replace(file_name, file_path)
