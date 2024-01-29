65087053f2358b23564bb2e75e1eb0e60879ff73
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-01-29 20:40:45 +0200

    use 1 context manager and deleted function calling

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 
 def move_file(command: str) -> None:
@@ -9,19 +8,12 @@ def move_file(command: str) -> None:
         _, file, path_to_move = commands
         directories, filename = os.path.split(path_to_move)
 
-        with open(file, "r") as r:
+        if not os.path.isdir(directories) and directories != "":
+            os.makedirs(directories, exist_ok=True)
+
+        with open(file, "r") as r, open(path_to_move, "a") as a:
             content = r.read()
 
-            if os.path.isdir(directories) or directories == "":
-                with open(os.path.join(directories, filename), "a") as a:
-                    a.write(content)
-            else:
-                os.makedirs(directories, exist_ok=True)
-                with open(path_to_move, "a") as a:
-                    a.write(content)
+            a.write(content)
 
         os.remove(file)
-
-
-if __name__ == "__main__":
-    move_file(str(sys.argv))
