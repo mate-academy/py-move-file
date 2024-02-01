63c09051d926f298f6b8d82fc426707b2670ef12
volry <v.v.ryzhuk@gmail.com>
2024-02-01 07:50:26 +0200

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+import os
+
+
+def move_file(command) -> None:
+    
+    parts = command.split(" ")
+    if len(parts) != 3  or parts[0] != "mv":
+        raise ValueError("Invalid command")
+    
+    sourse_file = part[1]
+
