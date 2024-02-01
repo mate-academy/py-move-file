4ce55fab5ba7739b47279c2a2d87be165e5cf462
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2024-02-01 21:49:41 +0200

    Solution5

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    list_files = command.split()
+    if (len(list_files) == 3
+            and list_files[0] == "mv"
+            and list_files[1] != list_files[2]):
+        head_tail = os.path.split(list_files[2])
+        if head_tail[0] != "":
+            os.makedirs(head_tail[0], exist_ok=True)
+        with (open(list_files[1], "r") as file_in,
+              open(list_files[2], "w") as file_out):
+            lines = file_in.readlines()
+            for line in lines:
+                file_out.write(line)
+        os.remove(list_files[1])
