250e115e7aca82127457616c655dc0fc699f9af4
sergey <38cerg12021979@jmal.com>
2024-02-02 08:05:45 +0200

    02_02_24

--- app/main.py
+++ app/main.py
@@ -3,18 +3,18 @@ import os
 
 
 def move_file(command: str) -> None:
-    linc = command.split()
-    pathway = os.path.split(linc[2])
-    linc_file = os.path.join(linc[2])
-    if os.path.exists(linc[1]):
-        if len(linc) == 3 and linc[0] == "mv" and linc[2].count("/") == 0:
-            os.rename(linc[1], linc[2])
-        if len(linc) == 3 and linc[0] == "mv" and not linc[2].count("/") == 0:
-            if not os.path.isdir(pathway[0]):
-                os.makedirs(pathway[0])
-            else:
-                pass
-            with (open(linc[1], "r") as file_in,
-                  open(f"{linc_file}", "w") as file_out):
+    file = command.split()
+    pathway_file = os.path.split(file[2])
+    if os.path.exists(file[1]):
+        if file[2].count("/") == 0:
+            os.rename(file[1], file[2])
+        else:
+            if not os.path.isdir(pathway_file[0]):
+                os.makedirs(pathway_file[0])
+            with (open(file[1], "r") as file_in,
+                  open(f"{os.path.join(file[2])}", "w") as file_out):
                 file_out.write(file_in.read())
-                os.remove(linc[1])
+                os.remove(file[1])
+
+
+#move_file(f"mv file.txt first_dir/second_dir/file2.txt")
