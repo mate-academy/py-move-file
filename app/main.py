c8dabbcab43eb5c5250a2de43798e58d6dc52236
volry <v.v.ryzhuk@gmail.com>
2024-02-03 10:31:11 +0200

    refactoring code

--- app/main.py
+++ app/main.py
@@ -3,39 +3,20 @@ import shutil
 
 
 def move_file(command: str) -> None:
-    parts = command.split(" ")
+    parts = command.split()
     if len(parts) != 3 or parts[0] != "mv":
         raise ValueError("Invalid command")
 
-    source_file = parts[1]
-    destination = " ".join(parts[2:])
+    source, destination = parts[1], parts[2]
 
-    if destination.startswith("dir") and os.path.exists("dir"):
-        shutil.rmtree("dir")
-
-    if (
-        destination.endswith("/")
-        or (os.path.isdir(destination) and not os.path.isfile(destination))
-    ):
+    if destination.endswith("/") or os.path.isdir(destination):
         os.makedirs(destination, exist_ok=True)
-        dest = os.path.join(destination, os.path.basename(source_file))
+        destination = os.path.join(destination, os.path.basename(source))
     else:
-        # Ensure the destination's parent directory exists
         parent_dir = os.path.dirname(destination)
-        if parent_dir and not os.path.exists(parent_dir):
+        # Call os.makedirs only if parent_dir is not empty
+        if parent_dir:
             os.makedirs(parent_dir, exist_ok=True)
-        dest = destination
 
-    if os.path.abspath(source_file) != os.path.abspath(dest):
-        try:
-            with open(source_file, "rb") as src, open(dest, "wb") as dst:
-                dst.write(src.read())
-
-            os.remove(source_file)
-            print(f"Moved '{source_file}' to '{dest}'")
-        except FileNotFoundError as e:
-            print(f"Error: {e}")
-        except Exception as e:
-            print(f"Unexpected error occurred: {e}")
-    else:
-        print("Source and destination are the same. No action taken.")
+    shutil.move(source, destination)
+    print(f"Moved '{source}' to '{destination}'")
