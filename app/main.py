377c2b7010a1ca9bccc4a53a5e0c73bc4a4dcdb0
voronvasia <voronvasia@gmail.com>
2024-02-04 11:59:44 +0200

    task completed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_mv, file, file_mv_to = command.split()
+
+    if command_mv != "mv":
+        return
+
+    head, tile = os.path.split(file_mv_to)
+
+    if not head:
+        os.rename(file, file_mv_to)
+        return
+
+    os.makedirs(head, exist_ok=True)
+    os.rename(file, file_mv_to)
