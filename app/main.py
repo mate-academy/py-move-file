3e8e47784a21ee339dcfa5bd281dda7757ae43fc
voronvasia <voronvasia@gmail.com>
2024-02-04 14:36:10 +0200

    bugs fixed

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_mv, file, file_mv_to = command.split()
+    parts = command.split()
+    if len(parts) != 3:
+        return
 
-    if command_mv != "mv":
+    command_mv, file, file_mv_to = parts
+
+    if command_mv != "mv" or file == file_mv_to:
         return
 
     head, tile = os.path.split(file_mv_to)
