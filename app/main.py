b9754c39e06cb145ac4fa1ca02aacea80d69751e
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-02-05 20:25:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 import os
 
 
+class CommandLenError(Exception):
+    pass
+
+
+class CommandNameError(Exception):
+    pass
+
+
 def move_file(command: str) -> None:
-    command, original_file, full_path = command.split()
+    command_split = command.split()
+    if len(command_split) != 3:
+        raise CommandLenError("Command should have 3 arguments.")
 
-    if command == "mv":
-        directory, file = os.path.split(full_path)
+    command, original_file, full_path = command_split
+    if command != "mv":
+        raise CommandNameError("Command must begin with 'mv'.")
 
-        if directory == "":
-            os.rename(original_file, file)
-        else:
-            directory_path = os.path.dirname(full_path)
-            os.makedirs(directory_path, exist_ok=True)
-            os.rename(original_file, full_path)
+    directory, file = os.path.split(full_path)
+    if not directory:
+        os.rename(original_file, file)
+    else:
+        directory_path = os.path.dirname(full_path)
+        os.makedirs(directory_path, exist_ok=True)
+        os.rename(original_file, full_path)
