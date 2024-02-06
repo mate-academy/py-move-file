0815f60bae1f2708f252ef81b66580a7f53fece8
Damodar <alaso.se@gmail.com>
2024-02-06 16:34:27 +0200

    changed split to os.path.split

--- app/main.py
+++ app/main.py
@@ -9,12 +9,9 @@ def move_file(command: str) -> None:
         if words_of_command[2].count("/") == 0:
             os.rename(words_of_command[1], words_of_command[2])
         else:
-            create_directory = words_of_command[2].split("/")
-            new_directory = create_directory[0]
-            for i in range(len(create_directory) - 1):
-                if os.path.exists(new_directory) is False:
-                    os.mkdir(new_directory)
-                new_directory += f"/{create_directory[i + 1]}"
+            create_directory = os.path.split(words_of_command[2])
+            if os.path.exists(create_directory[0]) is False:
+                os.makedirs(create_directory[0])
             with (open(words_of_command[1], "r") as data_file,
                   open(words_of_command[2], "w") as new_data_file):
                 new_data_file.write(data_file.read())
