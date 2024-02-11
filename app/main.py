a824b8993addf450a6389f6af3ad95f388328317
Molchanov Ihor <=>
2024-02-11 23:41:40 +0200

    solved task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(parameter: str) -> None:
+    if parameter.startswith("mv"):
+        filtered_param = parameter.replace("\\", "/")
+        command, source_filename, destiny_file = filtered_param.split()
+        destiny_folder = f"{os.sep}".join(destiny_file.split("/")[:-1:])
+        output_filename = destiny_file.split("/")[-1]
+        if destiny_folder:
+            os.makedirs(os.path.join(destiny_folder), exist_ok=True)
+        data = None
+        with open(source_filename, "r") as source_file:
+            data = source_file.read()
+
+        with open(
+                file=os.path.join(destiny_folder, output_filename),
+                mode="w"
+        ) as output_file:
+            output_file.write(data)
+        os.remove(source_filename)
