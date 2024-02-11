e419b1c686bb53c1fb585bf435159f3a50386cd6
RomanHlodann <roman.hlodann@gmail.com>
2024-02-11 23:20:11 +0200

    some fixes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from os import makedirs, remove
-from os.path import join
+from os.path import dirname
 
 
 def move_file(command: str) -> None:
@@ -8,13 +8,11 @@ def move_file(command: str) -> None:
     if len(arguments) != 3:
         return
     com, original_filename, copy_filename = arguments
-    if com != "mv" or original_filename == copy_filename:
-        return
+    if com == "mv" and original_filename != copy_filename:
+        if directories := dirname(copy_filename):
+            makedirs(directories, exist_ok=True)
 
-    new_path = join("", *arguments[2].split("/")[:-1])
-    makedirs(new_path, exist_ok=True)
-
-    with (open(original_filename, "r") as original_file,
-          open(copy_filename, "w") as copy_file):
-        copy_file.write(original_file.read())
-    remove(original_filename)
+        with (open(original_filename, "r") as original_file,
+              open(copy_filename, "w") as copy_file):
+            copy_file.write(original_file.read())
+        remove(original_filename)
