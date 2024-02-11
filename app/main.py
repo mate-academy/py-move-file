97a5d952431927bbb86b20650457726531c49542
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-02-11 11:59:34 +0200

    Case without еÑthe target file name was handled. The test for the case was added

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ def move_file(command: str) -> None:
     new_file_path = command_parts[2]
 
     new_path_parts = os.path.split(new_file_path)
+    old_path_parts = os.path.split(old_file_path)
 
     if not new_path_parts[0]:
         os.rename(old_file_path, new_file_path)
@@ -18,12 +19,16 @@ def move_file(command: str) -> None:
 
     os.makedirs(new_path_parts[0], exist_ok=True)
 
-    with open(old_file_path, "r") as file:
+    new_file_name = new_path_parts[-1]
+
+    path_to_move = new_file_path if new_file_name else (new_file_path
+                                                        + old_path_parts[-1])
+
+    with (
+        open(old_file_path, "r") as file,
+        open(path_to_move, "w") as new_file
+    ):
         content = file.read()
-
-    path_to_move = new_file_path
-
-    with open(path_to_move, "w") as new_file:
         new_file.write(content)
 
     os.remove(old_file_path)
