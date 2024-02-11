f70b1f5c604938e90c2884e4690d3a012b9e70a6
RomanHlodann <roman.hlodann@gmail.com>
2024-02-11 18:10:52 +0200

    created func move_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from os import mkdir, remove
+from os.path import exists
+
+
+def move_file(command: str) -> None:
+    arguments = command.split(" ")
+    if arguments[0] != "mv" or arguments[1] == arguments[2]:
+        return
+
+    directories_to_create = arguments[2].split("/")[:-1]
+    created = ""
+    for directory in directories_to_create:
+        created += directory + "/"
+        if not exists(created):
+            mkdir(created)
+
+    with (open(arguments[1], "r") as original_file,
+          open(arguments[2], "w") as copy_file):
+        copy_file.write(original_file.read())
+    remove(arguments[1])
