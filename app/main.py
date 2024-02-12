86e54dc4bf5bdca6c25635be4a3103d2916e4258
Molchanov Ihor <=>
2024-02-12 13:23:23 +0200

    add check for parameter

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ import os
 
 
 def move_file(parameter: str) -> None:
-    if parameter.startswith("mv"):
-        filtered_param = parameter.replace("\\", "/")
-        command, source_filename, destiny_file = filtered_param.split()
+    filtered_param = parameter.replace("\\", "/")
+    command, source_filename, destiny_file = filtered_param.split()
+    if command == "mv":
         destiny_folder = f"{os.sep}".join(destiny_file.split("/")[:-1:])
         output_filename = destiny_file.split("/")[-1]
         if destiny_folder:
             os.makedirs(os.path.join(destiny_folder), exist_ok=True)
-        data = None
+
         with open(source_filename, "r") as source_file:
             data = source_file.read()
 
