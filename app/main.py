de2ecada62048804cc7c3c8ba6bcae945dd46091
Molchanov Ihor <=>
2024-02-12 17:24:11 +0200

    delete hardcode, add os.path.split()

--- app/main.py
+++ app/main.py
@@ -2,11 +2,9 @@ import os
 
 
 def move_file(command: str) -> None:
-    filtered_command = command.replace("\\", "/")
-    param, source_filename, destiny_file = filtered_command.split()
+    param, source_filename, destiny_file = command.split()
     if param == "mv":
-        destiny_folder = f"{os.sep}".join(destiny_file.split("/")[:-1:])
-        output_filename = destiny_file.split("/")[-1]
+        destiny_folder, output_filename = os.path.split(destiny_file)
         if destiny_folder:
             os.makedirs(os.path.join(destiny_folder), exist_ok=True)
 
