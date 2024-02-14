5b76e3a86f5f310a5b661157c4723c45f7aa4084
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-02-14 13:18:14 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split(" ")
+    if len(parts) < 3:
+        print("Invalid command format. Usage: mv source_file destination_file")
+        return
+
+    source = parts[1]
+    file_path = parts[2]
+
+    directory_path = os.path.dirname(file_path)
+    if directory_path:
+        os.makedirs(directory_path, exist_ok=True)
+
+    try:
+        with open(source, "r") as source_file:
+            with open(file_path, "w") as destination_file:
+                destination_file.write(source_file.read())
+        os.remove(source)
+        print(f"File '{source}' moved to '{file_path}' successfully.")
+    except Exception as e:
+        print(f"Error moving file: {e}")
