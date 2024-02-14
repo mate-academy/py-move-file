e3619be2101767f28d63b50842db436ef7189aa2
Halyna Baklanova <novabalka@gmail.com>
2024-02-14 09:34:37 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or "mv" not in command:
+        raise ValueError(
+            "Error: Invalid command format."
+        )
+
+    mv, source_path, destination_path = parts
+
+    if destination_path.endswith(os.path.sep):
+        destination_path = os.path.join(
+            destination_path, os.path.basename(source_path)
+        )
+
+    destination_dir = os.path.dirname(destination_path)
+
+    if destination_dir:
+        os.makedirs(destination_dir, exist_ok=True)
+
+    os.rename(source_path, destination_path)
