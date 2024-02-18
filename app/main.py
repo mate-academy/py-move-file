c866e9be7aa0d5be4444b8a0a8c1e0a22fdf9717
Eugen <zond342@gmail.com>
2024-02-18 15:27:54 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    commands = command.split(" ")
+    destination = commands[2].split("/")
+    new_file = destination.pop()
+    target_directory = ""
+    if len(commands) == 3:
+        if commands[0] == "mv":
+            for flooder in destination:
+                target_directory += flooder + "/"
+                if os.path.isdir(target_directory):
+                    continue
+                os.mkdir(target_directory)
+    with (open(commands[1], "r") as file,
+          open(target_directory + new_file, "w") as moved):
+        moved.write(file.read())
+    os.remove(commands[1])
