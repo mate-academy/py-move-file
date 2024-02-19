517475bcc3555792b7bac86643840d0c9d06aa38
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-19 12:35:17 +0200

    Implement py-move-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    mv, source, destination = command.split()
+    directory = os.path.dirname(destination)
+    new_destination = os.path.join(directory, os.path.basename(destination))
+    if mv == "mv":
+        if not destination.count("/"):
+            if os.path.exists(destination):
+                os.remove(destination)
+            os.rename(source, destination)
+        else:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            shutil.move(source, new_destination)
+
