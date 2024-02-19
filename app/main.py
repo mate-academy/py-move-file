ae4b2ac6268ba873ed0e955b9da8ac39b2f9cfce
Eugen <zond342@gmail.com>
2024-02-19 15:07:54 +0200

    used os.path.join, os.makedirs with exist_ok=True

--- app/main.py
+++ app/main.py
@@ -3,17 +3,14 @@ import os
 
 def move_file(command: str) -> None:
     commands = command.split(" ")
-    destination = commands[2].split("/")
-    new_file = destination.pop()
-    target_directory = ""
-    if len(commands) == 3:
-        if commands[0] == "mv":
-            for folder in destination:
-                target_directory += folder + "/"
-                if os.path.isdir(target_directory):
-                    continue
-                os.mkdir(target_directory)
-    with (open(commands[1], "r") as file,
-          open(target_directory + new_file, "w") as moved):
-        moved.write(file.read())
-    os.remove(commands[1])
+    if len(commands) == 3 and commands[0] == "mv" and os.path.exists(commands[1]):
+        destination = commands[2]
+        new_file = destination.split("/")[len(destination.split("/")) - 1]
+        target_directory = os.path.join(
+            destination.replace(f"{new_file}", ""))
+        if len(target_directory) != 0:
+            os.makedirs(target_directory, exist_ok=True)
+        with (open(commands[1], "r") as file,
+              open(destination, "w") as moved):
+            moved.write(file.read())
+        os.remove(commands[1])
