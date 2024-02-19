b7d943e338dfeee088a32a618ceace754aa6d955
Julia Okhrymenko <Ohrimenko15@gmail.com>
2024-02-19 19:38:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+def move_file(command):
+    parts = command.split()
+
+    if len(parts) == 3 and parts[0] == "mv":
+        source_file = parts[1]
+        destination = parts[2]
+
+        if not os.path.exists(source_file):
+            raise FileNotFoundError(f"The file '{source_file}' does not exist.")
+
+        destination_directory = os.path.dirname(destination)
+        if destination_directory:
+            os.makedirs(destination_directory, exist_ok=True)
+
+        os.replace(source_file, destination)
+
+
+
+
