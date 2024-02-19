1d5ffd1374737d0c5abb5685c683f10434f42450
OleksandrLitvin <sanek.litvin@gmail.com>
2024-02-19 16:19:31 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import shutil
+import os
+
+
+def move_file(command: str) -> None:
+    if len(command.split(" ")) != 3:
+        raise ValueError("Please, input correct request")
+    cmnd, file1, file2 = command.split()
+    if cmnd == "mv" and file1 != file2:
+        file2 = file2.split("/")
+        temp = ""
+        while len(file2) > 1:
+            temp += file2[0]
+            try:
+                os.makedirs(temp)
+            except FileExistsError:
+                pass
+            temp += "/"
+            file2.pop(0)
+        file2 = temp + str(file2[0])
+        shutil.move(file1, file2)
