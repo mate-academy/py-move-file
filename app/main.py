b9e28deecf8e14c1b12f77f72bf17d3b30463989
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-19 14:41:15 +0200

    Simplify the code

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ import shutil
 
 
 def move_file(command: str) -> None:
-    mv, source, destination = command.split()
-    directory = os.path.dirname(destination)
-    new_destination = os.path.join(directory, os.path.basename(destination))
-    if mv == "mv":
-        if not destination.count("/"):
-            if os.path.exists(destination):
-                os.remove(destination)
-            os.rename(source, destination)
-        else:
-            os.makedirs(directory, exist_ok=True)
+    if len(command.split()) == 3:
+        mv, source, destination = command.split()
+        directory = os.path.dirname(destination)
+        new_destination = os.path.join(
+            directory, os.path.basename(destination)
+        )
+        if mv == "mv":
+            if directory:
+                os.makedirs(directory, exist_ok=True)
+
             shutil.move(source, new_destination)
