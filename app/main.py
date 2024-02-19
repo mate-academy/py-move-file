d5fd1d393846fb54ea62b66ab899002a394c1a55
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-19 15:25:52 +0200

    Improve logic

--- app/main.py
+++ app/main.py
@@ -3,14 +3,13 @@ import shutil
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) == 3:
-        mv, source, destination = command.split()
-        directory = os.path.dirname(destination)
-        new_destination = os.path.join(
-            directory, os.path.basename(destination)
+    command = command.split()
+    if len(command) != 3 or command[0] != "mv":
+        raise Exception(
+            "Invalid command; must be format: <mv> <source_file> <destination>"
         )
-        if mv == "mv":
-            if directory:
-                os.makedirs(directory, exist_ok=True)
-
-            shutil.move(source, new_destination)
+    _, source, destination = command
+    directory = os.path.dirname(destination)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+    shutil.move(source, destination)
