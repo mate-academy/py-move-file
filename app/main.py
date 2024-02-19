b32438104b08feb969556445cfac6171550b1d38
Julia Okhrymenko <Ohrimenko15@gmail.com>
2024-02-19 20:03:47 +0200

    'Solution3'

--- app/main.py
+++ app/main.py
@@ -9,15 +9,11 @@ def move_file(command: str) -> None:
         destination = parts[2]
 
         if not os.path.exists(source_file):
-            raise FileNotFoundError(f"The file '{source_file}' does not exist.")
+            raise FileNotFoundError\
+                (f"The file '{source_file}' does not exist.")
 
         destination_directory = os.path.dirname(destination)
         if destination_directory:
             os.makedirs(destination_directory, exist_ok=True)
 
         os.replace(source_file, destination)
-
-
-
-
-
