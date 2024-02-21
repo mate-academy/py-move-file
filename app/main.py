831d868236b77e46e2dd608399fa2f3ba7f17af9
Mykola Kokoiev <veokoknik@gmail.com>
2024-02-21 23:00:21 +0100

    SOL

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import shutil
+import os
+
+
+def move_file(command: str) -> None:
+    command_part = command.split()
+
+    if command_part[0] == "mv":
+        directories = command_part[2].split("/")[:-1]
+        previous_directory = ""
+
+        for directory in directories:
+
+            if previous_directory == "":
+                if not os.path.exists(directory):
+                    os.mkdir(directory)
+            else:
+                if not os.path.exists(previous_directory + directory):
+                    os.mkdir(previous_directory + directory)
+            previous_directory += f"{directory}/"
+
+        shutil.move(command_part[1], command_part[2])
