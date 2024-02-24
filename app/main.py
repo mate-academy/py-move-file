66559f4a24aa106d2500d78c1ddee0ebe58eb803
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-02-24 17:04:03 +0100

    Fixed some mistakes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import os
 def move_file(command: str) -> None:
     command, file_name, file_copy = command.split(" ")
 
-    if command == "mv" and file_copy[-1] != "/":
+    if command == "mv" and file_copy[-1] != "\\":
         with open(file_name, "r") as file:
             file_content = file.read()
         os.remove(file_name)
@@ -15,8 +15,7 @@ def move_file(command: str) -> None:
         path = ""
         for source in file_copy_source:
             path = os.path.join(path, source)
-            if not os.path.exists(path):
-                os.makedirs(path)
+            os.makedirs(path, exist_ok=True)
 
         with open(os.path.join(path, file_copy_name), "w") as file_copy:
             file_copy.write(file_content)
