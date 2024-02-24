2a940d8fd3fe669a37052d59c588fdc1c5a30504
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-02-24 12:16:50 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, file_name, file_copy = command.split(" ")
+
+    if command == "mv" and file_copy[-1] != "/":
+        with open(file_name, "r") as file:
+            file_content = file.read()
+        os.remove(file_name)
+
+        file_copy_source = file_copy.split("/")
+        file_copy_name = file_copy_source.pop(-1)
+
+        path = ""
+        for source in file_copy_source:
+            path = os.path.join(path, source)
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+        with open(os.path.join(path, file_copy_name), "w") as file_copy:
+            file_copy.write(file_content)
