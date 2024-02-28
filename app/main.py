81d5329dd616d97e2f2dc199e762ba481195dcb5
rtsadm <98526243+rtsadm@users.noreply.github.com>
2024-02-28 15:57:20 +0200

    code-fix

--- app/main.py
+++ app/main.py
@@ -1,22 +1,22 @@
 import os
 
 
+def copy_file_and_remove_original(file_in: str, file_out: str) -> None:
+    with open(file_in, "r") as f1, open(file_out, "w") as f2:
+        f2.write(f1.read())
+        f1.close()
+        f2.close()
+        os.remove(os.path.abspath(file_in))
+
+
 def move_file(command: str) -> None:
     if len(command.split()) == 3:
         command, file, new_file = command.split()
         if command == "mv":
             if "/" not in new_file:
-                with open(file, "r") as f1, open(new_file, "w") as f2:
-                    f2.write(f1.read())
-                    f1.close()
-                    f2.close()
-                os.remove(os.path.abspath(file))
+                copy_file_and_remove_original(file, new_file)
             else:
                 dirs = new_file.split("/")
                 dirs.pop()
                 os.makedirs("/".join(dirs), exist_ok=True)
-                with open(file, "r") as f1, open(new_file, "w") as f2:
-                    f2.write(f1.read())
-                    f1.close()
-                    f2.close()
-                os.remove(os.path.abspath(file))
+                copy_file_and_remove_original(file, new_file)
