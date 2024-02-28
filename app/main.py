64554694a71be1cd8fe64fb7c29abd8b02093de2
Roma Nesterenko <rom.puh@gmail.com>
2024-02-28 09:17:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mv, file, path_to = command.split()
+
+    if mv == "mv" and len(command.split()) == 3 and file != path_to:
+        part_of_path = os.path.split(path_to)
+        if part_of_path[0]:
+            os.makedirs(part_of_path[0], exist_ok=True)
+        with (
+            open(file, "r") as file_in,
+            open(path_to, "w") as file_out
+        ):
+            file_out.write(file_in.read())
+
+        os.remove(file)
