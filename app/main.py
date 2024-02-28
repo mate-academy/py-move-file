71e7f56f08985abc630c9435bec6525b6dc3a303
rtsadm <98526243+rtsadm@users.noreply.github.com>
2024-02-28 17:24:52 +0200

    final-i-guess

--- app/main.py
+++ app/main.py
@@ -4,9 +4,6 @@ import os
 def copy_file_and_remove_original(file_in: str, file_out: str) -> None:
     with open(file_in, "r") as f1, open(file_out, "w") as f2:
         f2.write(f1.read())
-        f1.close()
-        f2.close()
-        os.remove(os.path.abspath(file_in))
 
 
 def move_file(command: str) -> None:
@@ -17,3 +14,4 @@ def move_file(command: str) -> None:
                 dirs = new_file.split("/")[:-1]
                 os.makedirs("/".join(dirs), exist_ok=True)
             copy_file_and_remove_original(file, new_file)
+            os.remove(os.path.abspath(file))
