3e1fda28977aea639eb28089ad7af31edc8648b2
rtsadm <98526243+rtsadm@users.noreply.github.com>
2024-02-28 14:39:58 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    if len(command.split()) == 3:
+        command, file, new_file = command.split()
+        if command == "mv":
+            if "/" not in new_file:
+                with open(file, "r") as f1, open(new_file, "w") as f2:
+                    f2.write(f1.read())
+                    f1.close()
+                    f2.close()
+                os.remove(os.path.abspath(file))
+            else:
+                dirs = new_file.split("/")
+                dirs.pop()
+                os.makedirs("/".join(dirs), exist_ok=True)
+                with open(file, "r") as f1, open(new_file, "w") as f2:
+                    f2.write(f1.read())
+                    f1.close()
+                    f2.close()
+                os.remove(os.path.abspath(file))
