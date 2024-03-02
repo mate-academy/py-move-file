ec9e6c59789a1d43bb2812b7fc71fad279c4b6dc
Roman Sokolov <gnonasis@gmail.com>
2024-03-02 14:47:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,11 +3,10 @@ import os
 
 def move_file(command: str) -> None:
     split_command = command.split()
-    if len(split_command) != 3 or split_command[0] != "mv":
-        return
-    mod, original_file, moved_file = split_command
+    if len(split_command) == 3 or split_command[0] == "mv":
+        mod, original_file, moved_file = split_command
 
-    directory = os.path.dirname(moved_file)
-    if directory:
-        os.makedirs(directory, exist_ok=True)
-    os.rename(original_file, moved_file)
+        directory = os.path.dirname(moved_file)
+        if directory:
+            os.makedirs(directory, exist_ok=True)
+        os.rename(original_file, moved_file)
