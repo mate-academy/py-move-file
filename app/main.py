d57f007f40b90f9b177b33941127514b7ad3ed9f
Mykola Kokoiev <veokoknik@gmail.com>
2024-03-05 15:21:20 +0100

    SOL

--- app/main.py
+++ app/main.py
@@ -5,10 +5,9 @@ def move_file(command: str) -> None:
     mode, old_filename, fullpath_to_file = command.split()
     path_to_file = os.path.split(fullpath_to_file)[0]
 
-    if mode == "mv":
-        if old_filename != fullpath_to_file and path_to_file != "":
-            if not os.path.exists(path_to_file):
-                os.makedirs(path_to_file)
+    if mode == "mv" and old_filename != fullpath_to_file:
+        if path_to_file != "" and not os.path.exists(path_to_file):
+            os.makedirs(path_to_file)
 
         with open(
                 old_filename, "r"
