696b64aa16ed56debfafdc063fecec627abd3232
meier <Meyertal2@gmail.com>
2024-03-09 21:29:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+from os import remove, path, makedirs
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+    if len(command) != 3:
+        return
+    if command[0] != "mv":
+        return
+    move_to, name = path.split(command[2])
+    if move_to:
+        makedirs(move_to, exist_ok=True)
+    with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
+        file_out.write(file_in.read())
+    remove(command[1])
