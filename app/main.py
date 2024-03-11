a8c0f0d8d8b87a7817a2b46ee6c6de53b0054894
Serhii Dannyk <serhii.dannyk@gmail.com>
2024-03-11 12:49:56 +0200

    Removed unused part of code

--- app/main.py
+++ app/main.py
@@ -10,15 +10,9 @@ def move_file(command: str) -> None:
 
     _, source_file, destination_file = tokens
 
-    try:
-        if not os.path.dirname(destination_file):
-            os.replace(source_file, destination_file)
-        else:
-            destination_path = os.path.dirname(destination_file)
-            os.makedirs(destination_path, exist_ok=True)
-            os.replace(source_file, destination_file)
-
-    except FileNotFoundError:
-        raise
-    except FileExistsError:
-        raise
+    if not os.path.dirname(destination_file):
+        os.replace(source_file, destination_file)
+    else:
+        destination_path = os.path.dirname(destination_file)
+        os.makedirs(destination_path, exist_ok=True)
+        os.replace(source_file, destination_file)
