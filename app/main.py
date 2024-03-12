c4651c3b1bbcad48a121f4e04f24f02494fe2113
skeletronys <mykytcey@gmail.com>
2024-03-12 17:08:05 +0100

    change if strcuture, use unpacking

--- app/main.py
+++ app/main.py
@@ -2,12 +2,11 @@ from os import remove, path, makedirs
 
 
 def move_file(command: str) -> None:
-    commands = command.split()
-    if len(commands) != 3 or commands[0] != "mv":
-        return
-    move_to, name = path.split(commands[2])
-    if move_to:
-        makedirs(move_to, exist_ok=True)
-    with open(commands[1], "r") as file_in, open(commands[2], "w") as file_out:
-        file_out.write(file_in.read())
-    remove(commands[1])
+    command_input, path_info, file_name = command.split()
+    if len(command.split()) == 3 or command_input != "mv":
+        move_to, name = path.split(file_name)
+        if move_to:
+            makedirs(move_to, exist_ok=True)
+        with open(path_info, "r") as file_in, open(file_name, "w") as file_out:
+            file_out.write(file_in.read())
+        remove(path_info)
