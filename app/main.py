81bdda3dbd3def89156d881ac1cf3cbee9ee8ed1
skeletronys <mykytcey@gmail.com>
2024-03-13 01:04:03 +0100

    fix

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from os import remove, path, makedirs
 
 
 def move_file(command: str) -> None:
-    command_input, path_info, file_name = command.split()
-    if len(command.split()) == 3 or command_input != "mv":
-        move_to, name = path.split(file_name)
-        if move_to:
-            makedirs(move_to, exist_ok=True)
-        with open(path_info, "r") as file_in, open(file_name, "w") as file_out:
-            file_out.write(file_in.read())
-        remove(path_info)
+    if len(command.split()) == 3:
+        command_input, path_info, file_name = command.split()
+        if command_input == "mv":
+            move_to, name = path.split(file_name)
+            if move_to:
+                makedirs(move_to, exist_ok=True)
+            with open(path_info, "r") as file_in, open(file_name, "w") as file_out:
+                file_out.write(file_in.read())
+            remove(path_info)
