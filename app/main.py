e85c7471d6eeb1947e1016e01a2caf02fd586105
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-03-14 14:41:56 -0500

    new solution

--- app/main.py
+++ app/main.py
@@ -2,22 +2,23 @@ import os
 
 
 def move_file(command: str) -> None:
-    new_one = command.split()
-    if len(new_one) != 3 and new_one[0] != "mv":
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "mv":
         raise ValueError("Invalid command format")
 
-    move, first, second = new_one
-    destination = os.path.dirname(second)
+    move, source, destination = parts
 
-    if destination:
-        os.makedirs(destination, exist_ok=True)
+    if not os.path.exists(source):
+        raise FileNotFoundError(f"Source file '{source}' not found")
 
-    new_file = os.path.join(destination, os.path.basename(second))
+    destination_dir = os.path.dirname(destination)
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+
     with (
-        open(first, "r") as file_in,
-        open(new_file, "w") as file_out
+        open(source, "r") as file_in,
+        open(destination, "w") as file_out
     ):
         file_out.write(file_in.read())
-
-    if first != new_file:
-        os.remove(first)
+    os.remove(source)
