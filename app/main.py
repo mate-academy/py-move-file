4c1ddb4a1daae840e0dc84ad4020f56fe0123159
Viesich <v.s.vesich@gmail.com>
2024-03-15 15:07:50 +0200

    remove double code

--- app/main.py
+++ app/main.py
@@ -7,24 +7,18 @@ def move_file(command: str) -> None:
         if len(parts) == 3:
             com, first_file, second_file = parts
             if com == "mv":
-                if "/" not in second_file:
-                    with (
-                        open(first_file, "r") as file_in,
-                        open(second_file, "w") as file_out
-                    ):
-                        file_out.write(file_in.read())
-                    os.remove(first_file)
-                else:
+                parts_dir = second_file
+                if "/" in parts_dir:
                     parts_dir = second_file.split("/")
-                    for directory in range(1, len(parts_dir)):
-                        path_to_file = "/".join(parts_dir[:directory])
-                        if not os.path.exists(path_to_file):
-                            os.mkdir(path_to_file)
-                    with (
-                        open(first_file, "r") as file_in,
-                        open(second_file, "w") as file_out
-                    ):
-                        file_out.write(file_in.read())
-                    os.remove(first_file)
+                for directory in range(1, len(parts_dir)):
+                    path_to_file = "/".join(parts_dir[:directory])
+                    if not os.path.exists(path_to_file):
+                        os.mkdir(path_to_file)
+                with (
+                    open(first_file, "r") as file_in,
+                    open(second_file, "w") as file_out
+                ):
+                    file_out.write(file_in.read())
+                os.remove(first_file)
     except FileNotFoundError:
         pass
