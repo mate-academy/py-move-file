6af69ce7eed9833739b97a619faba736c043414f
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-17 15:41:20 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,31 +1,14 @@
-import os
-import shutil
+from os import remove, path, makedirs
 
 
 def move_file(command: str) -> None:
-    parts = command.split()
-    if len(parts) != 3 or parts[0] != "mv":
-        raise ValueError("Command should be in the format:"
-                         " 'mv source_file destination_path'")
-
-    source, destination = parts[1], parts[2]
-
-    if not os.path.exists(source):
-        raise FileNotFoundError(f'The source file "{source}" does not exist.')
-
-    if destination.endswith("/") or (os.path.isdir(destination)
-                                     and not os.path.isfile(destination)):
-        os.makedirs(destination, exist_ok=True)
-        destination = os.path.join(destination, os.path.basename(source))
-    else:
-        destination_dir = os.path.dirname(destination)
-        if destination_dir and not os.path.exists(destination_dir):
-            os.makedirs(destination_dir, exist_ok=True)
-        custom_move(source, destination)
-
-
-def custom_move(source: str, destination: str) -> None:
-    with open(source, "rb") as fsrc:
-        with open(destination, "wb") as fdst:
-            shutil.copyfileobj(fsrc, fdst)
-    os.remove(source)
+    if len(command.split()) == 3:
+        command_input, path_info, file_name = command.split()
+        if command_input == "mv":
+            move_to, name = path.split(file_name)
+            if move_to:
+                makedirs(move_to, exist_ok=True)
+            with (open(path_info, "r") as file_in,
+                  open(file_name, "w") as file_out):
+                file_out.write(file_in.read())
+            remove(path_info)
