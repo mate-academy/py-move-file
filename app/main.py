1adad5ee07c413b5a193c24e87bbaf60f03993ae
pochtalon <pochtalon1010@gmail.com>
2024-03-21 10:11:25 +0200

    Solved

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mv, old_file, new_file = command.split(" ")
+    if old_file == new_file:
+        return
+    path = new_file.split("/")
+    new_dir = "/".join(path[:-1])
+    if new_dir != '':
+        os.makedirs(new_dir, exist_ok=True)
+    os.replace(old_file, new_file)
