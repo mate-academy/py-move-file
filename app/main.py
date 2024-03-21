cf9508f3c6ce83379125f7aac5ab3bb11be948ac
Maksim Li <sma4no.7@gmail.com>
2024-03-21 16:16:46 +0100

    Solution.

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ import os
 
 def move_file(command: str) -> None:
     commands = command.split()
+    if len(commands) != 3:
+        raise Exception("Invalid command. Please provide input file and output file.")
+
     input_file = commands[1]
     output_file = commands[2]
     with open(input_file, "r") as input_data:
