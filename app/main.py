195fe488bf8dd523b432ae29f111fd94fb04dceb
Maksim Li <sma4no.7@gmail.com>
2024-03-21 16:14:30 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -5,13 +5,13 @@ def move_file(command: str) -> None:
     commands = command.split()
     input_file = commands[1]
     output_file = commands[2]
-    with open(input_file, "r") as i_file:
-        input_content = i_file.read()
+    with open(input_file, "r") as input_data:
+        input_content = input_data.read()
     if "/" in output_file:
         output_directory = os.path.dirname(output_file)
         os.makedirs(output_directory, exist_ok=True)
-        with open(output_file, "w") as o_file:
-            o_file.write(input_content)
+        with open(output_file, "w") as output_data:
+            output_data.write(input_content)
         os.remove(input_file)
     else:
         os.rename(input_file, output_file)
