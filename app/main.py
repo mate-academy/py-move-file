ad61c816eaa5f6cb6ef41d1a860eb2409a150a5b
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-22 13:13:38 +0200

    'fixed_name'

--- app/main.py
+++ app/main.py
@@ -9,13 +9,17 @@ def move_file(command: str) -> None:
                          "Please provide the command in the format:"
                          " mv <source_file> <destination_path>")
 
-    mv_value, source_file, test_path = parts
-    if mv_value == "mv":
-        directory = path.dirname(test_path)
-        if directory and not path.exists(directory):
-            makedirs(path.dirname(test_path), exist_ok=True)
-    else:
+    command_type, source_file, destination_path = parts
+    if command_type != "mv":
         raise ValueError("Invalid command format. "
                          "Please provide the command in the format:"
                          " mv <source_file> <destination_path>")
-    replace(source_file, test_path)
+
+    if source_file == destination_path:
+        raise ValueError("Source and destination paths cannot be the same.")
+
+    destination_directory = path.dirname(destination_path)
+    if destination_directory and not path.exists(destination_directory):
+        makedirs(destination_directory, exist_ok=True)
+
+    replace(source_file, destination_path)
