3cd53063fc32dbadc225ea25f3394dd9b6b7f6df
tiway <stilon670@gmail.com>
2024-03-22 14:41:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, old_file, path_and_new_file = command.split()
+    path_and_new_file = path_and_new_file.split("/")
+    new_file = path_and_new_file.pop(-1)
+    file_path = ""
+
+    for path in path_and_new_file:
+        file_path += f"{path}/"
+        if not os.path.exists(file_path):
+            os.mkdir(file_path)
+
+    with (open(old_file, "r") as from_file,
+          open(file_path + new_file, "w") as in_file):
+        in_file.write(from_file.read())
+
+    os.remove(old_file)
