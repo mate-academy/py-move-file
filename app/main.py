ef9feb647679389775eaa6062d51e27c30223eff
pochtalon <pochtalon1010@gmail.com>
2024-03-22 15:11:59 +0200

    Solved

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ def move_file(command: str) -> None:
     if old_file == new_file:
         return
     new_dir = os.path.dirname(new_file)
-    if new_dir != "":
+    if new_dir:
         os.makedirs(new_dir, exist_ok=True)
     os.replace(old_file, new_file)
