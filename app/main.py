90b1d37247ff112e5deae81a14aacf46efe25c35
Olha <olgazinevych@gmail.com>
2024-03-24 12:49:34 +0200

    Used path split to check for directories

--- app/main.py
+++ app/main.py
@@ -4,12 +4,9 @@ import os
 def move_file(command: str) -> None:
     command = command.split()
     if len(command) == 3 and command[0] == "mv":
-        if "/" not in command[2]:
-            os.rename(command[1], command[2])
-        else:
-            directories = command[2].split("/")
-            parent_directory = os.path.join(*directories[:-1])
-            os.makedirs(parent_directory, exist_ok=True)
+        directories, file_name = os.path.split(command[2])
+        if directories:
+            os.makedirs(directories, exist_ok=True)
             with (
                 open(command[1], "r") as src_file,
                 open(command[2], "w") as dst_file
@@ -17,3 +14,5 @@ def move_file(command: str) -> None:
                 data = src_file.read()
                 dst_file.write(data)
             os.remove(command[1])
+        else:
+            os.rename(command[1], command[2])
