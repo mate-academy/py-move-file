d5cef53f18c8369c2ff600e465ffc5915bf3cb0e
Vitalii Haponiuk <doninpotato@gmail.com>
2024-03-25 19:46:25 +0200

    'correted'

--- app/main.py
+++ app/main.py
@@ -3,11 +3,10 @@ import os
 
 def move_file(command: str) -> None:
     tokens = command.split()
-    if len(tokens) != 3 or tokens[0] != "mv":
-        return
-    cp, source, destination = tokens
-    name_dirs = os.path.dirname(destination)
+    if len(tokens) == 3 and tokens[0] == "mv":
+        cp, source, destination = tokens
+        name_dirs = os.path.dirname(destination)
 
-    if name_dirs:
-        os.makedirs(name_dirs, exist_ok=True)
-    os.replace(source, destination)
+        if name_dirs:
+            os.makedirs(name_dirs, exist_ok=True)
+        os.replace(source, destination)
