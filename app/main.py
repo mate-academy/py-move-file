9f709fd2be74f6941481df3c432a383ca4668949
Dede <specialized8393@gmail.com>
2024-03-25 20:54:47 +0000

    Update script for Windows

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def move_file(command: str) -> None:
     """ That will move a file from one location to another """
     # Ex: mv file.txt first_dir/second_dir/third_dir/file2.txt
 
+    curent_path = os.getcwd().replace("\\", "/") + "/"
+
     (cmd,
      file_in,
      file_out) = command.split(" ")
@@ -13,12 +15,8 @@ def move_file(command: str) -> None:
         raise ValueError("Command mv not found")
 
     if "/" in file_out:
-        dir_path = ""
-        directories, filename = os.path.split(file_out)
-
-        for directory in directories.split("/"):
-            dir_path = os.path.join(dir_path, directory)
-            os.makedirs(dir_path, exist_ok=True)
+        dir_path, file_name = os.path.split(file_out)
+        os.makedirs(curent_path + dir_path, exist_ok=True)
 
     with open(file_in, "r") as f_in, open(file_out, "w") as f_out:
         f_out.write(f_in.read())
