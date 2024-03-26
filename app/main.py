6218f4c93741d8d639d3fbe61bde4c5234b8fe3c
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-03-26 11:50:11 +0200

    Solution with os.path.split and os.makedirs methods

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from os import mkdir, remove, rename, path
+import os
 
 
 def move_file(command: str) -> None:
@@ -6,21 +6,18 @@ def move_file(command: str) -> None:
     if (
             len(command_list) == 3
             and command_list[0] == "mv"
-            and path.isfile(command_list[1])
+            and os.path.isfile(command_list[1])
     ):
         if "/" not in command_list[2]:
-            rename(command_list[1], command_list[2])
+            os.rename(command_list[1], command_list[2])
         else:
-            path_list: list = command_list[2].split("/")
-            full_path: str = ""
-            for directory in path_list[:-1]:
-                full_path = path.join(full_path, directory)
-                if not path.exists(full_path):
-                    mkdir(full_path)
+            destination_path = os.path.split(command_list[2])[0]
+            if not os.path.exists(destination_path):
+                os.makedirs(destination_path, exist_ok=True)
             with (
                 open(command_list[1], "r") as source_file,
                 open(command_list[2], "w") as destination_file
             ):
                 source: str = source_file.read()
                 destination_file.write(source)
-            remove(command_list[1])
+            os.remove(command_list[1])
