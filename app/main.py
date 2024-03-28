e454705697cce586e8d3efc3ec8394261349ab0d
Vadym Pavlenko <pavvadymq@gmail.com>
2024-03-29 00:55:42 +0200

    Second_try

--- app/main.py
+++ app/main.py
@@ -2,19 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    command = command.split()
-    initial_file, destination = command[1], command[2]
-    if "/" not in command[2]:
+    command, initial_file, destination = command.split()
+    if "/" not in destination:
         os.rename(initial_file, destination)
         return
     with open(initial_file, "r") as file_in:
         content = file_in.read()
     os.remove(initial_file)
-    path = destination.split("/")[:-1]
-    step = ""
-    for i in path:
-        step += i + "/"
-        if os.path.exists(step) is False:
-            os.mkdir(step)
-    with open(command[2], "w") as file_out:
+    path = os.path.dirname(destination)
+    if os.path.exists(path) is False:
+        os.makedirs(path)
+    with open(destination, "w") as file_out:
         file_out.write(content)
