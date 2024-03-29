a5b3134627e58175c0fdf68071ddf325011c539f
kekW <panicua12@gmail.com>
2024-03-29 23:55:38 +0200

    Simplified solution a little

--- app/main.py
+++ app/main.py
@@ -2,17 +2,11 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_to_list = command.split(" ")
-    start_file_name, destination_path = command_to_list[1], command_to_list[2]
+    _, start_file_name, destination_path = command.split(" ")
     destination_path_list = destination_path.split("/")
-    path = ""
-
-    for folder_index in range(len(destination_path_list) - 1):
-        path += destination_path_list[folder_index] + "/"
-        try:
-            os.mkdir(path)
-        except FileExistsError:
-            print("Folder already exists")
+    if not len(destination_path_list) == 1:
+        path = "/".join(destination_path_list[:len(destination_path_list) - 1])
+        os.makedirs(path, exist_ok=True)
 
     if destination_path_list[-1] == "":
         os.rename(start_file_name,
