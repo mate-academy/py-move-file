b1ff1cf38ad79d6cfdee62edfc34ccb165178095
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-29 15:27:58 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, source_file, destination_path = command.split()
+    if "/" in destination_path:
+        os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+    with open(source_file, "r") as src, open(destination_path, "w") as dst:
+        dst.write(src.read())
+    os.remove(source_file)
