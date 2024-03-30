63faf8f7f625f096bf347cbc3610bd1b94602633
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-30 12:28:52 +0100

    fixed flake8

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ def move_file(command: str) -> None:
     _, source_file, destination_path = command.split()
     if "/" in destination_path:
         os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-    with open(source_file, "r") as copied_file, open(destination_path, "w") as destination_directory:
+    with (open(source_file, "r") as copied_file,
+          open(destination_path, "w") as destination_directory):
         destination_directory.write(copied_file.read())
     os.remove(source_file)
