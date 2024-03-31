a8de361dd174dd5e62a8dd071cad34816054604c
kekW <panicua12@gmail.com>
2024-03-31 22:07:53 +0300

    removed useless conditions(didn't understand task at first)

--- app/main.py
+++ app/main.py
@@ -8,8 +8,5 @@ def move_file(command: str) -> None:
         path = "/".join(destination_path_list[:len(destination_path_list) - 1])
         os.makedirs(path, exist_ok=True)
 
-    if destination_path_list[-1] == "":
-        os.rename(start_file_name,
-                  destination_path + "new_" + start_file_name)
-    else:
-        os.rename(start_file_name, destination_path)
+    os.rename(start_file_name, destination_path)
+
