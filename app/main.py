1ed4a89a1b5029e7abbd92bfae77079d33fdfe21
Dmytro Hlazyrin <dmytro.hlazyrin@gmail.com>
2024-04-01 21:54:02 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -1,18 +1,22 @@
 import os
+import re
+from shutil import move
 
 
 def move_file(command: str) -> None:
-    move, file_in, file_out = command.split(" ")
+    if not re.match(r"^mv\s+\S+\s+\S+$", command):
+        print("Invalid command format. "
+              "Use 'mv <source_file> <destination_file>'")
+        return
+
+    command, file_in, file_out = command.split()
     folder_out_path = os.path.dirname(file_out)
     folder_in_path = os.path.dirname(file_in)
 
-    if folder_in_path == folder_out_path:
-        os.rename(file_in, file_out)
-    else:
-        if not os.path.exists(folder_out_path):
-            os.makedirs(folder_out_path)
-
-        with (open(file_in, "r") as file_in,
-              open(file_out, "w") as file_out):
-            file_out.write(file_in.read())
-        os.remove(file_in.name)
+    if command == "mv":
+        if folder_in_path == folder_out_path:
+            os.rename(file_in, file_out)
+        else:
+            if not os.path.exists(folder_out_path):
+                os.makedirs(folder_out_path)
+            move(file_in, file_out)
