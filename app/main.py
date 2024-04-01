655cf858f0a8381e40e583e68818bbb2815dd9d0
Dmytro Pukas <pukasdima2002@gmail.com>
2024-04-01 14:06:57 +0200

    used shutil lib, renamed variable

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
+import shutil
 import os
 
 
 def move_file(command: str) -> None:
     command = command.split()
     if len(command) == 3 and command[0] == "mv":
-        _, file_name, new_dir = command
-        dir_path = os.path.dirname(new_dir)
+        _, source_file, destination_path = command
+        dir_path = os.path.dirname(destination_path)
         if dir_path and not os.path.exists(dir_path):
             os.makedirs(dir_path, exist_ok=True)
-        if os.path.exists(new_dir):
-            os.remove(new_dir)
-        os.rename(file_name, new_dir)
+        if os.path.exists(destination_path):
+            os.remove(destination_path)
+        shutil.move(source_file, destination_path)
