d87544b5aa80b68227daf9b5a7f6c6265daea991
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-01 17:06:00 +0200

    fixed code

--- app/main.py
+++ app/main.py
@@ -3,19 +3,18 @@ import shutil
 
 
 def move_file(command: str) -> None:
-    source_file = None
-    try:
-        _, source_file, destination_path = command.split()
+    commands = command.split()
+    if len(commands) != 3 or commands[0] != "mv":
+        raise ValueError("Invalid command format")
 
-        if any(char in source_file + destination_path for char in '<>:"|?*'):
-            raise ValueError("file name contains invalid characters")
-        if "/" in destination_path or "\\" in destination_path:
-            os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+    _, source_file, destination_path = commands
+    try:
+        destination_dir, _ = os.path.split(destination_path)
+        if destination_dir:
+            os.makedirs(destination_dir, exist_ok=True)
         shutil.move(source_file, destination_path)
 
-    except FileNotFoundError:
-        print(f"Error: The file '{source_file}' does not exist.")
-    except ValueError as ve:
-        print(f"Error: {ve}")
+    except OSError as e:
+        print(f"Error: {e.strerror}. Path: '{e.filename}'")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
