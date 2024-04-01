211e1ad6d1e24426a8a41ad9530ccdc8fc7ee78f
Vladyslav Prokopiv <prokopiv.vladyslav@gmail.com>
2024-03-31 18:03:10 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    *_, in_file, out_file = command.split(" ")
+    current_location = os.path.dirname(in_file)
+    destination = os.path.dirname(out_file)
+
+    if current_location == destination:
+        os.rename(in_file, out_file)
+    else:
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+
+        with (open(in_file, "r") as in_file,
+              open(out_file, "w") as out_file):
+            out_file.write(in_file.read())
+        os.remove(in_file.name)
