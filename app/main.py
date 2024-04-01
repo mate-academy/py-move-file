56f7966e64311220caca71bada3e826b7f64b72f
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-01 09:09:09 +0200

    added exceptions

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 import os
+import shutil
 
 
 def move_file(command: str) -> None:
-    _, source_file, destination_path = command.split()
-    if "/" in destination_path:
-        os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-    with (open(source_file, "r") as copied_file,
-          open(destination_path, "w") as destination_directory):
-        destination_directory.write(copied_file.read())
-    os.remove(source_file)
+    source_file = None
+    try:
+        _, source_file, destination_path = command.split()
+
+        if any(char in source_file + destination_path for char in '<>:"|?*'):
+            raise ValueError("file name contains invalid characters")
+        if "/" in destination_path or "\\" in destination_path:
+            os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+        shutil.move(source_file, destination_path)
+
+    except FileNotFoundError:
+        print(f"Error: The file '{source_file}' does not exist.")
+    except ValueError as ve:
+        print(f"Error: {ve}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
