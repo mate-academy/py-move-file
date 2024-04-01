f95a48fc539cb0995b6fa9dcfd74b479ea00771f
Shershen Alexandra <hornet240204@gmail.com>
2024-04-01 14:43:42 +0300

    Solution for task move file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    command_name, source_file, destination_file = command.split()
+    source_directory = os.path.dirname(source_file)
+    destination_directory = os.path.dirname(destination_file)
+
+    if source_directory == destination_directory:
+        os.rename(source_file, destination_file)
+    else:
+        if not os.path.exists(destination_directory):
+            os.makedirs(destination_directory)
+
+        shutil.move(source_file, destination_file)
