4bf6b02b13a388fc5a137d05f84266ad0215e215
Maksym Hekalo <maximgekalo@gmail.com>
2024-04-02 23:11:39 +0300

    delete for loop, add change on easier logic

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from os import mkdir, remove, path
+import os
 
 
 def move_file(command: str) -> None:
@@ -6,15 +6,12 @@ def move_file(command: str) -> None:
     command_parts = command.split()
     if not len(command_parts) == 3 and command_parts[0] == "mv":
         raise Exception(f"Entered incorrect command {command}")
+
     with open(command_parts[1], "r") as main_file:
         content_of_file = main_file.read()
-
-    dirs = command_parts[2]
-    final_path = ""
-    for file_dir in dirs.split("/")[:-1]:
-        final_path += f"{file_dir}/"
-        if not path.exists(final_path) is True:
-            mkdir(final_path)
-    with open(final_path + dirs.split("/")[-1], "w") as copied_file:
+    dir_path, _ = os.path.split(command_parts[2])
+    if not os.path.exists(dir_path) is True and dir_path:
+        os.makedirs(dir_path)
+    with open(command_parts[2], "w") as copied_file:
         copied_file.write(content_of_file)
-        remove(command_parts[1])
+        os.remove(command_parts[1])
