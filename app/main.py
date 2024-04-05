96484b71d4c47d95557a9e493e5f2a44a54c6b90
Shershen Alexandra <hornet240204@gmail.com>
2024-04-05 15:29:15 +0300

    Add check for correct command name

--- app/main.py
+++ app/main.py
@@ -5,14 +5,11 @@ import shutil
 def move_file(command: str) -> None:
     command_parts = command.split()
 
-    if len(command_parts) != 3:
+    if len(command_parts) != 3 and command_parts[0] != "mv":
         raise ValueError("Invalid command format. "
                          "Usage: mv file.txt some_dir/new_file.txt")
 
     command_name, source_file, destination_file = command_parts
-
-    if len(command_parts) != 3:
-        raise ValueError("Invalid move command format")
 
     if not os.path.exists(source_file):
         raise FileNotFoundError(f"Source file not found: {source_file}")
