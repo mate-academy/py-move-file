3f33480980aa8b0361053c2f2a36b05aba2d1e3b
serhii_chaban <boetz1995@gmail.com>
2024-04-08 15:32:10 +0300

    fixed2

--- app/main.py
+++ app/main.py
@@ -5,17 +5,18 @@ def move_file(command: str) -> None:
     mv, file_source, path_another_file, *_ = command.split()
     folders, new_name = os.path.split(path_another_file)
 
-    if mv == "mv" and folders:
-        if file_source == path_another_file:
+    if mv == "mv":
+        if folders:
+            if file_source == new_name:
+                return
+
+            os.makedirs(folders, exist_ok=True)
+
+            with (
+                open(file_source, "r") as source,
+                open(os.path.join(folders, new_name), "w") as moved
+            ):
+                moved.write(source.read())
+            os.remove(file_source)
             return
-
-        os.makedirs(folders, exist_ok=True)
-
-        with (
-            open(file_source, "r") as source,
-            open(os.path.join(folders, new_name), "w") as moved
-        ):
-            moved.write(source.read())
-        os.remove(file_source)
-        return
-    os.rename(file_source, path_another_file)
+        os.rename(file_source, new_name)
