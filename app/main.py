170d391629cd172522d03306974d9ad73c982990
serhii_chaban <boetz1995@gmail.com>
2024-04-08 12:36:55 +0300

    solved

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, file_source, path_another_file = command.split()
+    folders = path_another_file.split("/")
+    curr_folder = ""
+
+    if file_source == path_another_file:
+        return
+
+    if len(folders) == 1:
+        os.rename(file_source, path_another_file)
+        return
+
+    for folder in folders:
+        if "." in folder:
+            break
+        curr_folder += f"{folder}/"
+        if not os.path.exists(curr_folder):
+            os.mkdir(curr_folder)
+
+    with (
+        open(file_source, "r") as source,
+        open(os.path.join(*folders), "w") as moved
+    ):
+        moved.write(source.read())
+    os.remove(file_source)
