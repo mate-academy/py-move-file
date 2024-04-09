18448df9901c362498f8c8be6a792320970a9b37
NioviIki <charmniovi@tutanota.com>
2024-04-09 21:19:40 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+import shlex
+import os
+
+
+def move_file(command: str) -> None:
+    if command[:3] != "mv ":
+        raise ValueError(
+            "Expected 'mv' as the first argument"
+        )
+
+    source_path, destination_path, *extra_path = shlex.split(command[3:])
+    destination_dir, destination_file = os.path.split(destination_path)
+
+    if extra_path:
+        raise ValueError(
+            "Expected 2 arguements"
+        )
+
+    if not os.path.exists(source_path):
+        raise FileExistsError(f"File {source_path} not exists")
+
+    if destination_dir and not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+
+    with (
+        open(source_path, "r") as source_file,
+        open(destination_path, "w") as destination_file
+    ):
+        destination_file.write(source_file.read())
+
+    os.remove(source_path)
