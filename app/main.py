1801b241139bf492991fc6db8fa882108be140be
Maksym Hekalo <maximgekalo@gmail.com>
2024-04-09 09:20:56 +0300

    clear if statement and add key arg to call function

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ def move_file(command: str) -> None:
     with open(command_parts[1], "r") as main_file:
         content_of_file = main_file.read()
     dir_path, _ = os.path.split(command_parts[2])
-    if not os.path.exists(dir_path) is True and dir_path:
-        os.makedirs(dir_path)
+    if dir_path:
+        os.makedirs(dir_path, exist_ok=True)
     with open(command_parts[2], "w") as copied_file:
         copied_file.write(content_of_file)
         os.remove(command_parts[1])
