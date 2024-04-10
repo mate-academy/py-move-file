be259d3d87e4922555f222bad964254bf0eca93c
Yaroslav Ivanov <165368790+nrjss67@users.noreply.github.com>
2024-04-10 21:46:27 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,16 +7,9 @@ def move_file(command: str) -> None:
         comm, file_name, path = command_split
 
     if "/" not in path:
-        os.rename(file_name, path)
-        return
-
-    if path.endswith("/"):
-        os.makedirs(path, exist_ok=True)
         os.replace(file_name, path)
         return
 
-    if path.endswith(".txt"):
-        new_file_name = path.split("/")[-1]
-        path_without_fn = "/".join(path.split("/")[:-1])
-        os.makedirs(path_without_fn, exist_ok=True)
-        os.replace(file_name, f"{path_without_fn}/{new_file_name}")
+    path_without_fn = "/".join(path.split("/")[:-1])
+    os.makedirs(path_without_fn, exist_ok=True)
+    os.replace(file_name, path)
