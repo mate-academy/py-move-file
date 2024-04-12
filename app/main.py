9a143b00ef3fb5b4cbe123a398a14733eda4410f
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-12 23:46:42 +0300

    move_file_v1.2

--- app/main.py
+++ app/main.py
@@ -9,12 +9,7 @@ def move_file(command: str) -> None:
     if "/" not in file_2:
         os.rename(file_1, file_2)
     else:
-        dir_list = file_2.split("/")[0:-1]
-        os.mkdir(dir_list[0])
-        if len(dir_list) >= 2:
-            for i in range(len(dir_list[0:-1])):
-                os.mkdir(f'{"/".join(file_2.split("/")[0:i + 1])}'
-                         f"/{dir_list[i + 1]}")
+        os.makedirs(os.path.dirname(file_2))
         with open(file_1, "r") as f1, open(file_2, "w") as f2:
             f2.write(f1.read())
         os.remove(file_1)
