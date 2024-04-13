5bf9066603f3a567e9138b1e100c7b50e425bd3a
Serhii Sierkov <sserkov84@gmail.com>
2024-04-13 21:10:41 +0300

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -2,19 +2,13 @@ import os
 
 
 def move_file(command: str) -> bool:
-    c_args = command.split()
-    if command == "" or c_args[0] != "mv" or c_args[1] == c_args[2]:
+    cmd, file1, file2 = command.split()
+    if cmd != "mv" or file1 == file2:
         return False
-    file1, file2 = c_args[1], c_args[2]
-    if "/" in c_args[2]:
-        splitter = []
-        path = c_args[2]
-        for num in range(len(path) - 1):
-            if path[num] == "/":
-                splitter.append(num)
-                print(path[:num])
-                if not os.path.isdir(path[:num]):
-                    os.mkdir(path[:num])
+    if "/" in file2:
+        folders = os.path.dirname(file2)
+        if not os.path.isdir(folders):
+            os.makedirs(folders)
     with open(file1, "r") as file_in, open(file2, "w") as file_out:
         file_out.write(file_in.read())
-    os.remove(c_args[1])
+    os.remove(file1)
