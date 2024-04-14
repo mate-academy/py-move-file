e7a26980602d00b3ae2e6a7a61d91c7096f543ec
Oleksandr Moroz <molecxander@gmail.com>
2024-04-14 22:04:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+import shutil
+
+def move_file(command: str) -> bool:
+    if len(command.split()) != 3:
+        raise ValueError
+    cmd, source_file, new_file = command.split()
+    os.makedirs(os.path.dirname(os.path.abspath(new_file)),
+                exist_ok=True)
+    with open(source_file, "r") as file_in, open(new_file, "w") as file_out:
+        file_out.write(file_in.read())
+    os.remove(source_file)
