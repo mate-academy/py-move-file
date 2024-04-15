082c23d594482d45d56c59422048581e0f1bb647
Klishch Nazar <klishch65@gmail.com>
2024-04-15 13:36:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, source_file, moved_file = command.split()
+    if command != "mv":
+        print("Wrong command")
+    if "/" not in moved_file:
+        if not os.path.exists(moved_file):
+            os.rename(source_file, moved_file)
+            return None
+        else:
+            print("File already exists.")
+            return None
+    else:
+        os.makedirs(os.path.dirname(moved_file), exist_ok=True)
+        with (open(source_file, "r") as old_file,
+              open(moved_file, "w") as new_file):
+            new_file.write(old_file.read())
+        os.remove(source_file)
