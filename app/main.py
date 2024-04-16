99c9428f64d9935ccd8df4b4f3887a9a1f9a6f1d
Mikola Rozhansky <bymikola@gmail.com>
2024-04-16 22:12:16 +0300

    all_test_working

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import os
 
 
 def move_file(command: str) -> None:
-    cmd, source_name, destination_name = "","",""
+    cmd, source_name, destination_name = "", "", ""
     try:
         cmd, source_name, destination_name = command.split()
     except Exception:
         print("Error in cmd!")
+    print(cmd, source_name, destination_name)
 
     dir_str = ""
 
@@ -15,11 +16,16 @@ def move_file(command: str) -> None:
             list_dir = destination_name.split("/")
 
             for i in range(len(list_dir) - 1):
-                dir_str += list_dir[i]+"/"
-                print(dir_str)
-                os.mkdir(dir_str)
+                dir_str += list_dir[i] + "/"
 
-        with open(source_name) as f_source, open(destination_name, "w") as f_destination:
+                try:
+                    print(dir_str)
+                    os.mkdir(dir_str)
+                except Exception:
+                    print("Err_dir")
+
+        with (open(source_name) as f_source,
+              open(destination_name, "w") as f_destination):
             buff = f_source.read()
             f_destination.write(buff)
 
