d329fece50355a9d568608b96d27c17cd990e7bf
Mikola Rozhansky <bymikola@gmail.com>
2024-04-16 21:53:15 +0300

    1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    cmd, source_name, destination_name = "","",""
+    try:
+        cmd, source_name, destination_name = command.split()
+    except Exception:
+        print("Error in cmd!")
+
+    dir_str = ""
+
+    if cmd == "mv":
+        if "/" in destination_name:
+            list_dir = destination_name.split("/")
+
+            for i in range(len(list_dir) - 1):
+                dir_str += list_dir[i]+"/"
+                print(dir_str)
+                os.mkdir(dir_str)
+
+        with open(source_name) as f_source, open(destination_name, "w") as f_destination:
+            buff = f_source.read()
+            f_destination.write(buff)
+
+        os.remove(source_name)
