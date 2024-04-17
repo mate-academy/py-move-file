3f8a83d79c328149a04b5f0f1ace3e4beb7dda5f
Marko Panchak <marko.panchak1@gmail.com>
2024-04-17 21:59:51 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    part = command.split()
+    first_file = part[1]
+    second_file = part[2]
+    if first_file != second_file and part[0] == "mv":
+        if os.path.exists(first_file):
+            if "/" in second_file:
+                directories = second_file.split("/")
+                directories = directories[0:len(directories) - 1]
+                current = ""
+                for directory in directories:
+                    current += directory
+                    if not os.path.exists(current):
+                        os.mkdir(current)
+                    current += "/"
+
+            with (open(part[1], "r") as old,
+                  open(part[2], "w") as new):
+                new.write(old.read())
+
+            os.remove(first_file)
