13c71cd0f4321cc574df133542600b4fc7551ccc
Oleh <musiychuk09@gmail.com>
2024-04-20 14:51:33 +0300

    Was added makedirs with exist_ok = True

--- app/main.py
+++ app/main.py
@@ -5,14 +5,8 @@ def move_file(command: str) -> None:
     command = command.split()
     if len(command) != 3 or command[0] != "mv":
         raise Exception("Wrong command")
-    path_to_file = command[2].split("/")
 
-    for num_dir in range(1, len(path_to_file)):
-        if os.path.exists("/".join(path_to_file[:num_dir])):
-            pass
-        else:
-            os.mkdir("/".join(path_to_file[:num_dir]))
-
+    os.makedirs(command[2][:-1], exist_ok=True)
     with open(command[1], "r") as file_in, open(command[2], "w") as file_out:
         file_out.write(file_in.read())
     os.remove(command[1])
