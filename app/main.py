5e672b6a812e1a05f35109f3931653dd66306736
Oleh <musiychuk09@gmail.com>
2024-04-21 10:46:38 +0300

    Was added walidation before defining the variables

--- app/main.py
+++ app/main.py
@@ -3,12 +3,11 @@ import os
 
 def move_file(command: str) -> None:
     command = command.split()
-    mv = command[0]
+
+    if len(command) != 3 or command[0] != "mv":
+        raise Exception("Wrong command")
     old_file = command[1]
     new_file = command[2]
-
-    if len(command) != 3 or mv != "mv":
-        raise Exception("Wrong command")
 
     only_path = os.path.dirname(new_file)    # make path without file name
     if only_path == "":
