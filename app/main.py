733861184e37a5445786b658d49003ade872f8f8
Oleksandr Moroz <molecxander@gmail.com>
2024-04-26 13:38:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 import os
 
 
-def move_file(command: str) -> bool:
-    if len(command.split()) != 3:
-        raise ValueError
-    cmd, source_file, new_file = command.split()
-    os.makedirs(os.path.dirname(os.path.abspath(new_file)),
-                exist_ok=True)
-    with open(source_file, "r") as file_in, open(new_file, "w") as file_out:
-        file_out.write(file_in.read())
-    os.remove(source_file)
+def move_file(command: str) -> None:
+    command_split = command.split()
+    if len(command_split) != 3:
+        raise ValueError(f"Invalid command: {command}, expected 3 arguments")
+    if command_split[0] != "mv":
+        raise ValueError("First argument must be 'mv'")
+    cmd, source_file, new_file = command_split
+    if new_file.endswith("/"):
+        new_dir = new_file
+        new_file = os.path.join(new_dir, os.path.basename(source_file))
+    else:
+        new_dir = os.path.dirname(new_file)
+    os.makedirs(os.path.abspath(new_dir), exist_ok=True)
+    try:
+        with (open(source_file, "r") as file_in,
+              open(os.path.join(new_dir, os.path.basename(new_file)),
+                                                     "w") as file_out):
+            file_out.write(file_in.read())
+        os.remove(source_file)
+    except OSError:
+        raise
