88147471061d7042081e8b404dbedb2a5f47ec6b
Oleksandr Moroz <molecxander@gmail.com>
2024-04-26 13:42:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,8 +16,10 @@ def move_file(command: str) -> None:
     os.makedirs(os.path.abspath(new_dir), exist_ok=True)
     try:
         with (open(source_file, "r") as file_in,
-              open(os.path.join(new_dir, os.path.basename(new_file)),
-                                                     "w") as file_out):
+              open(os.path.join(
+                  new_dir,
+                  os.path.basename(new_file)
+              ), "w") as file_out):
             file_out.write(file_in.read())
         os.remove(source_file)
     except OSError:
