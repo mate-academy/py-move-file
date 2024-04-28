f3455bf4c9993deb46d82aa7ec61471befd06a0f
Oleksandr Moroz <molecxander@gmail.com>
2024-04-28 19:21:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,26 +1,16 @@
 import os
+import shutil
 
 
 def move_file(command: str) -> None:
     command_split = command.split()
-    if len(command_split) != 3:
-        raise ValueError(f"Invalid command: {command}, expected 3 arguments")
-    if command_split[0] != "mv":
-        raise ValueError("First argument must be 'mv'")
-    cmd, source_file, new_file = command_split
-    if new_file.endswith("/"):
-        new_dir = new_file
-        new_file = os.path.join(new_dir, os.path.basename(source_file))
-    else:
-        new_dir = os.path.dirname(new_file)
-    os.makedirs(os.path.abspath(new_dir), exist_ok=True)
+    if len(command_split) != 3 or command_split[0] != "mv":
+        raise ValueError(f"Invalid command: {command}")
+    _, source_file, new_file = command_split
+    new_dir, new_filename = os.path.split(new_file)
+    new_dir = os.path.abspath(new_dir)
+    os.makedirs(new_dir, exist_ok=True)
     try:
-        with (open(source_file, "r") as file_in,
-              open(os.path.join(
-                  new_dir,
-                  os.path.basename(new_file)
-              ), "w") as file_out):
-            file_out.write(file_in.read())
-        os.remove(source_file)
-    except OSError:
-        raise
+        shutil.move(source_file, os.path.join(new_dir, new_filename))
+    except OSError as error:
+        raise OSError(f"Error moving file: {error}")
