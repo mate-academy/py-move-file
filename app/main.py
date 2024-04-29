3bd93af9a88568e0d0b58a472b66922685f213d0
Serhii Marynin <serhii.marynin@gmail.com>
2024-04-29 18:58:00 +0300

    name a file if name does not exist

--- app/main.py
+++ app/main.py
@@ -3,25 +3,21 @@ import os
 
 def move_file(command: str) -> None:
     commands = command.split()
-
     if len(commands) != 3 or commands[0] != "mv":
         raise ValueError(f"'{command}': invalid command input")
+    old_file, new_file = commands[1:]
+    if not os.path.exists(old_file):
+        raise ValueError(f"'{old_file}': file does not exist")
 
-    old_file_name, new_file_name = commands[1:]
-
-    old_file_tail = os.path.split(old_file_name)[1]
-    new_file_tail = os.path.split(new_file_name)[1]
-    if not old_file_tail or not new_file_tail:
-        raise ValueError(f"'{command}' must contain two file names")
-
-    new_file_path = os.path.split(new_file_name)[0]
+    new_file_path, new_file_tail = os.path.split(new_file)
+    if not new_file_tail:
+        new_file = os.path.join(new_file_path, os.path.split(old_file)[1])
     if new_file_path:
         os.makedirs(new_file_path, exist_ok=True)
-
     try:
-        with open(old_file_name) as old, open(new_file_name, "w") as new:
+        with open(old_file) as old, open(new_file, "w") as new:
             new.write(old.read())
     except OSError:
         raise
 
-    os.remove(old_file_name)
+    os.remove(old_file)
