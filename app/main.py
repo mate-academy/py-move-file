39e07260f037ba8c4ab5e85b11838ac53115bee0
Kate <katrusja.ko@gmail.com>
2024-05-04 20:44:58 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import os
 
 def move_file(command: str) -> None:
     data = command.split()
-    if data[0] == "mv" and len(data) == 3:
+    if data[0] == "mv" and len(data) == 3 and data[1] != data[2]:
         if os.path.dirname(os.path.dirname(data[2])):
             os.makedirs(os.path.dirname(data[2]))
         with open(data[1], "r") as r, open(data[2], "w") as f:
