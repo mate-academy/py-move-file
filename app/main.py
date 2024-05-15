c4c0f9c349086e73969fc9768dfe790a979b0da2
Maryna Barannik <sober2night@gmail.com>
2024-05-15 12:02:27 +0200

    'Fix'

--- app/main.py
+++ app/main.py
@@ -3,8 +3,15 @@ import os
 
 def move_file(command: str) -> None:
     parts = command.split(" ")
+
+    if len(parts) != 3 or parts[0] != "mv":
+        raise ValueError("Invalid command")
+
     source_path = parts[1]
     destination_path = parts[2]
+
+    if os.path.abspath(source_path) == os.path.abspath(destination_path):
+        raise ValueError("Source and destination paths are the same.")
 
     destination_dir = os.path.dirname(destination_path)
     if destination_dir:
