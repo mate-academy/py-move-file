1e10040add5a0a913786d188ffd9d845252c4007
haldaniko <haldaniko@gmail.com>
2024-05-19 16:25:41 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split()
+    cmd, old_loc, new_loc = command
+    new_directory = os.path.dirname(new_loc)
+    if new_directory:
+        os.makedirs(new_directory, exist_ok=True)
+    with (open(old_loc, "r") as source_file,
+            open(new_loc, "w") as destination_file):
+        destination_file.write(source_file.read())
+    if os.path.exists(old_loc):
+        os.remove(old_loc)
