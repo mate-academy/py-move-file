4571bac5a684c013d14b192e4a5d60c750f8f390
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-05-20 13:18:57 +0300

    and now?)

--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
 import os
 
 
-def move_file(command: str) -> None:
-    source_file_name, new_path = command.split()[1:]
-    new_path = new_path.split("/")
-    new_file_name = new_path[-1]
-    if len(new_path) == 1:
-        new_path = ""
-    if new_path:
-        new_path = "/".join(new_path[:-1]) + "/"
-    if new_path:
-        if not os.path.exists(new_path):
-            os.makedirs(new_path)
+def move_file(command: str) -> str | None:
+    command, source_file_name, new_path = command.split()
+    if command != "mv":
+        return "Incorrect command"
+    new_file_name = new_path.split("/")[-1]
+    new_path = os.path.join(os.getcwd(), new_path.replace(new_file_name, ""))
+    os.makedirs(new_path, exist_ok=True)
+
     with (
-        open(source_file_name, "r") as source_file,
-        open(new_path + new_file_name, "w") as new_file
+        open(os.path.join(os.getcwd(), source_file_name), "r") as source_file,
+        open(os.path.join(new_path, new_file_name), "w") as new_file
     ):
         new_file.write(source_file.read())
-    os.remove(source_file_name)
+    os.remove(os.path.join(os.getcwd(), source_file_name))
