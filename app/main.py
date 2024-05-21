e405b524dd27d9bc90d4b1c01981fa5a391d9739
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-21 16:43:59 +0300

    resolved remarks

--- app/main.py
+++ app/main.py
@@ -1,35 +1,21 @@
 import os
-
-
-def copy_file(old_file_name: str, new_file_name: str) -> None:
-    with (open(old_file_name, "r") as old_file,
-          open(new_file_name, "w") as new_file):
-        text = old_file.read()
-        new_file.write(text)
+import shutil
 
 
 def move_file(command: str) -> None:
     command_split = command.split(" ")
+    if len(command_split) != 3 or command_split[0] != "mv":
+        raise ValueError("Invalid command format. "
+                         "Expected: mv source_file target_path")
     old_file_name = command_split[1]
     path_and_name = command_split[2]
 
-    if "/" not in path_and_name:
-        os.rename(old_file_name, path_and_name)
-        return
+    target_dir, target_file = os.path.split(path_and_name)
 
-    path_to_file = path_and_name.split("/")
-    path = ""
-    name = path_to_file[-1]
-    for index in range(len(path_to_file) - 1):
-        path += path_to_file[index]
-        if not os.path.isdir(path):
-            os.mkdir(path)
-        path += "/"
-
-    if name:
-        # path = dir/new_name.txt
-        copy_file(old_file_name, path + name)
+    if target_dir:
+        os.makedirs(target_dir, exist_ok=True)
+        target_path = os.path.join(target_dir, target_file)
     else:
-        # path = dir/
-        copy_file(old_file_name, path + old_file_name)
-    os.remove(old_file_name)
+        target_path = target_file
+
+    shutil.move(old_file_name, target_path)
