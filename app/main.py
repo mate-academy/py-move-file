cf3fc7f2f0687ddf48c0ee9b69d996b59a4dcc7d
AndrewwSSS <clash2clans1one@gmail.com>
2024-05-27 10:47:23 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+BUFFER_SIZE = 256
+
+
+def move_file(command: str) -> None:
+    operators = command.split(" ")
+    first_path, second_path = operators[1::]
+
+    # create directory if needed
+    directory_path = os.path.split(second_path)[0]
+    if directory_path:
+        os.makedirs(directory_path, exist_ok=True)
+
+    with (open(first_path, "r") as file_read,
+          open(second_path, "w") as file_write):
+        buffer = file_read.read(BUFFER_SIZE)
+        while buffer:
+            file_write.write(buffer)
+            buffer = file_read.read(BUFFER_SIZE)
+
+    os.remove(first_path)
