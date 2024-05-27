0ce2e063d16ca57e5cef6adda71c61cafbb74aaf
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-05-27 18:05:49 +0300

    combined file operations in a single with statement, and splitted command into different variables

--- app/main.py
+++ app/main.py
@@ -2,21 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    split_command = command.split()
-    original_file_name = split_command[1]
-    if "/" not in split_command[2]:
-        with (open(original_file_name, "r") as original_file,
-              open(split_command[2], "w") as renamed_file):
-            renamed_file.write(original_file.read())
-        os.remove(original_file_name)
-    else:
-        path_list = split_command[2].split("/")
+    command, original_file_name, directory = command.split()
+    if "/" in directory:
+        directory_path = directory.split("/")
 
-        for directory in range(1, len(path_list)):
-            if not os.path.exists("/".join(path_list[0:directory])):
-                os.mkdir("/".join(path_list[0:directory]))
+        for folder in range(1, len(directory_path)):
+            if not os.path.exists("/".join(directory_path[0:folder])):
+                os.mkdir("/".join(directory_path[0:folder]))
 
-        with (open(original_file_name, "r") as original_file,
-              open(split_command[2], "w") as moved_file):
-            moved_file.write(original_file.read())
-        os.remove(original_file_name)
+    with (open(original_file_name, "r") as original_file,
+          open(directory, "w") as moved_file):
+        moved_file.write(original_file.read())
+    os.remove(original_file_name)
