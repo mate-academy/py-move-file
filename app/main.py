456a61201578474b6c7a0cbd0896c81757479be9
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-05-29 23:07:45 +0200

    The solution of the task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, file, direct = command.split(" ")
+    if command == "mv":
+        new_direct = direct.split("/")
+        if len(new_direct) > 1:
+            os.makedirs("/".join(new_direct[:-1]), exist_ok=True)
+    with open(file, "r") as original, open(direct, "w") as moved_file:
+        moved_file.write(original.read())
+    os.remove(file)
