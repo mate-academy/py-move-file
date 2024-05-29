82428beb743d731df243e496a4158164fd0ca74d
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-05-29 17:45:35 +0300

    optimized directory creation

--- app/main.py
+++ app/main.py
@@ -5,10 +5,7 @@ def move_file(command: str) -> None:
     command, original_file_name, directory = command.split()
     if "/" in directory:
         directory_path = directory.split("/")
-
-        for folder in range(1, len(directory_path)):
-            if not os.path.exists("/".join(directory_path[0:folder])):
-                os.mkdir("/".join(directory_path[0:folder]))
+        os.makedirs("/".join(directory_path[:-1]), exist_ok=True)
 
     with (open(original_file_name, "r") as original_file,
           open(directory, "w") as moved_file):
