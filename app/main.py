f316841492fdd9021cc69e6f613ce856796ec2a0
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-05-31 15:30:06 +0200

    The solution of the task

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ import os
 
 
 def move_file(command: str) -> None:
+    if command.split()[0] != "mv" or len(command.split()) != 3:
+        raise ValueError("""
+        The input should look: "mv current_dir desired_dir".
+        """)
     action, file, direct = command.split()
-    if action != "mv" or len(command.split()) != 3:
-        raise ValueError
     new_direct = os.path.split(direct)[0]
     if len(new_direct):
         os.makedirs(new_direct, exist_ok=True)
