973e3e58cf2b2e1b8f074c860fc358d7b800a38a
volry <v.v.ryzhuk@gmail.com>
2024-06-02 12:51:47 +0300

    simplify code and fixed comments

--- app/main.py
+++ app/main.py
@@ -3,38 +3,38 @@ import shutil
 
 
 def move_file(command: str) -> None:
-    cmd, source_file, destination = command.split()
-    
-    if cmd != "mv" or len([cmd, source_file, destination]) != 3:
+    cmd, source_file, des = command.split()
+
+    if cmd != "mv" or len([cmd, source_file, des]) != 3:
         raise ValueError("Invalid command")
-    
-    
-    if destination.startswith("dir") and os.path.exists("dir"):
+
+    # Check if the destination starts with "dir" and if "dir" exists, remove it
+    if des.startswith("dir") and os.path.exists("dir"):
         shutil.rmtree("dir")
 
-    if (
-        destination.endswith("/")
-        or (os.path.isdir(destination) and not os.path.isfile(destination))
-    ):
-        os.makedirs(destination, exist_ok=True)
-        dest = os.path.join(destination, os.path.basename(source_file))
-    else:
-        # Ensure the destination's parent directory exists
-        parent_dir = os.path.dirname(destination)
-        if parent_dir and not os.path.exists(parent_dir):
-            os.makedirs(parent_dir, exist_ok=True)
-        dest = destination
-
-    if os.path.abspath(source_file) != os.path.abspath(dest):
-        try:
-            with open(source_file, "rb") as src, open(dest, "wb") as dst:
-                dst.write(src.read())
-
-            os.remove(source_file)
-            print(f"Moved '{source_file}' to '{dest}'")
-        except FileNotFoundError as e:
-            print(f"Error: {e}")
-        except Exception as e:
-            print(f"Unexpected error occurred: {e}")
-    else:
+    # Check if source and destination are the same
+    if os.path.abspath(source_file) == os.path.abspath(des):
         print("Source and destination are the same. No action taken.")
+        return
+
+    # Ensure the destination's parent directory exists
+    parent_dir = os.path.dirname(des)
+    if parent_dir and not os.path.exists(parent_dir):
+        os.makedirs(parent_dir, exist_ok=True)
+
+    if des.endswith("/") or (os.path.isdir(des) and not os.path.isfile(des)):
+        os.makedirs(des, exist_ok=True)
+        des = os.path.join(des, os.path.basename(source_file))
+    else:
+        des = des
+
+    try:
+        with open(source_file, "rb") as src, open(des, "wb") as dst:
+            dst.write(src.read())
+
+        os.remove(source_file)
+        print(f"Moved '{source_file}' to '{des}'")
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"Unexpected error occurred: {e}")
