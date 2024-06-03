d42fb306c9c803a98d88f16ba51a02578a608d69
volry <v.v.ryzhuk@gmail.com>
2024-06-03 08:19:21 +0300

    delete dir name check

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 
 def move_file(command: str) -> None:
@@ -7,10 +6,6 @@ def move_file(command: str) -> None:
 
     if cmd != "mv" or len([cmd, source_file, des]) != 3:
         raise ValueError("Invalid command")
-
-    # Check if the destination starts with "dir" and if "dir" exists, remove it
-    if des.startswith("dir") and os.path.exists("dir"):
-        shutil.rmtree("dir")
 
     # Check if source and destination are the same
     if os.path.abspath(source_file) == os.path.abspath(des):
