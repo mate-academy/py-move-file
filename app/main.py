77d320ee34ac867dc3753a45b2fffb65eff8e312
lilarin <80650911+lilarin@users.noreply.github.com>
2024-06-04 12:00:23 +0300

    Solution refiniment (PEP8 len)

--- app/main.py
+++ app/main.py
@@ -10,10 +10,14 @@ def move_file(command: str) -> None:
     destination_path = parts[2]
 
     if not os.path.isfile(source_path):
-        raise FileNotFoundError(f"Source file '{source_path}' does not exist.")
+        raise FileNotFoundError(
+            f"Source file '{source_path}' does not exist."
+        )
 
     if destination_path.endswith('/'):
-        destination_path = os.path.join(destination_path, os.path.basename(source_path))
+        destination_path = os.path.join(
+            destination_path, os.path.basename(source_path)
+        )
 
     destination_dir = os.path.dirname(destination_path)
     if not os.path.exists(destination_dir):
