2dcb6f08abbd80c144ba4875a10c4e5f09e89fe7
Alex Bul <tarasov57228@gmail.com>
2024-06-05 20:48:40 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_name, file, direct, *_ = command.split()
+
+    if "/" in direct:
+        directory = direct.split("/")
+        os.makedirs("/".join(directory[:-1]), exist_ok=True)
+
+    with (open(file, "r") as file_in,
+          open(direct, "w") as file_out):
+        file_out.write(file_in.read())
+    os.remove(file)
