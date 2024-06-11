b72160f526f755c8962056196aea433762b48348
timur <stukantimur811@gmail.com>
2024-06-11 09:10:41 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ def move_file(command: str) -> None:
                     and not os.path.exists(path_to_new_file[-2])):
                 os.makedirs(combined_path)
 
-            with open(file_to_move, "r") as file_in, open(new_file, "w") as file_out:
+            with (open(file_to_move, "r") as file_in,
+                  open(new_file, "w") as file_out):
                 whole_data = file_in.read()
                 file_out.write(whole_data)
             os.remove(file_to_move)
