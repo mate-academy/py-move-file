3ab171fec9077a67bc28997b1b4b59608ae820bc
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-06-18 21:10:07 +0300

    Current state

--- app/main.py
+++ app/main.py
@@ -8,30 +8,25 @@ def move_file(command: str) -> None:
     if len(un_command) != 3 and un_command[0] != "mv":
         raise ValueError("The command is incorrect!")
 
-    file = un_command[1]
+    old_file = un_command[1]
     way = un_command[0]
-
     destination_dir = dirname(way)
 
     if exists(destination_dir):
         raise ValueError("The file exists!")
 
     if "/" not in destination_dir:
-        rename(file, destination_dir)
+        rename(old_file, destination_dir)
     else:
-        with open(file, "r") as old_file:
-            content = old_file.read()
-        remove(file)
+        with open(old_file, "r") as current_file:
+            content = current_file.read()
+        remove(old_file)
         files_list = destination_dir.split("/")
-        for file in files_list:
-            if file != files_list[-1] and not exists(file):
-                mkdir(file)
-                chdir(file)
-            if file != files_list[-1] and exists(file):
-                chdir(file)
+        for name_file in files_list:
+            if name_file != files_list[-1] and not exists(name_file):
+                mkdir(name_file)
+                chdir(name_file)
+            if name_file != files_list[-1] and exists(name_file):
+                chdir(name_file)
         with open(files_list[-1], "w") as new_file:
             new_file.write(content)
-
-
-
-
