e39a88d17096c2d6f2aa40c2f11dec3f97da657e
unlie9 <unlie.devops9@gmail.com>
2024-06-18 18:26:19 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ def move_file(command: str) -> None:
     cmd, file_name, direction = command.split()
 
     if direction:
-        os.makedirs(dir_, exist_ok=True)
+        os.makedirs(direction, exist_ok=True)
     os.replace(file_name, direction)
