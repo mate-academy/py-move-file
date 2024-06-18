cd45e79cc02ce34bc7e8ade97674ea9bdab9110e
unlie9 <unlie.devops9@gmail.com>
2024-06-18 18:25:09 +0300

    sol2

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import os
 
 def move_file(command: str) -> None:
     cmd, file_name, direction = command.split()
-    dir_ = direction
-    if dir_:
+
+    if direction:
         os.makedirs(dir_, exist_ok=True)
     os.replace(file_name, direction)
