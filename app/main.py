fefae8b883aaacc62be5b36646dd95dc6d77eded
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2024-06-20 20:15:21 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    elements = command.split()
+    if elements[0] != "mv":
+        raise ValueError
+
+    if "/" not in elements[2]:
+        os.rename(elements[1], elements[2])
+    else:
+        dir_elements = elements[2].split("/")
+        path_creator = []
+        for dirs in dir_elements[:-1]:
+            path_creator.append(dirs)
+            path = os.path.join(*path_creator)
+            if not os.path.exists(path):
+                os.mkdir(path)
+        if dir_elements[-1] == "":
+            dir_elements.append(elements[1])
+        os.rename(elements[1], os.path.join(*dir_elements))
