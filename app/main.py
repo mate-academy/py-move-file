3179336c429dcda741e333f48be6b4aaf0a11fe6
Vlad <vlad@example.com>
2024-06-24 21:38:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    _, file, path = command.split()
+    command, file, path = command.split()
+    if command != "mv":
+        raise Exception("please enter command 'mv'")
     path = path.split("/")
-    new_file = (os.path.join(*path) if len(path) == 1
-                else os.path.join(*path[:-1]))
+    new_file = os.path.dirname(os.path.join(*path))
     if len(path) > 1:
         os.makedirs(new_file, exist_ok=True)
-    with open(file) as file_1, open(new_file if len(path) == 1
-                                    else os.path.join(new_file, path[-1]),
+    with open(file) as file_1, open(os.path.join(new_file, path[-1]),
                                     "w") as file_2:
         text = file_1.read()
         file_2.write(text)
