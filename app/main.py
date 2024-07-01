0e14cbc72b80d6bf35c290eb7cdf32bf00317464
Valeriy Morkvych <morkvych@gmail.com>
2024-07-01 23:32:28 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -2,23 +2,17 @@ import os
 
 
 def move_file(command: str) -> None:
-    ls_command = command.split(" ")
+    ls_command = command.split()
     if len(ls_command) != 3:
         raise RuntimeError("the command syntax is not valid")
     if ls_command[0] != "mv":
         raise NameError("not the expected command")
 
     file_name_in, file_name_out = ls_command[1:]
-    ls_address = file_name_out.split("/")
-    length_address = len(ls_address)
-    ls_path = ls_address[:(length_address - 1)]
+    path_, _ = os.path.split(file_name_out)
 
-    last_catalog = ""
-    for catalog in ls_path:
-        path_join = os.path.join(last_catalog, catalog)
-        if not os.path.exists(path_join):
-            os.mkdir(path_join)
-        last_catalog = path_join
+    if path_ != "" and not os.path.exists(path_):
+        os.makedirs(path_)
 
     with (open(file_name_in, "r") as file_in,
           open(file_name_out, "w") as file_out):
