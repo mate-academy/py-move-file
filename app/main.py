98e7e11c2aaf9a0482dea0d98ec4a32e0ac8a2fa
Stepan <oleksiukstepann@gmail.com>
2024-07-21 19:01:49 +0300

    Implemented main

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parsed_command = command.split()
+    path = parsed_command[-1].split("/")
+    directory = ""
+
+    for i in range(len(path) - 1):
+        directory += path[i]
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+        directory += "/"
+    directory += path[-1]
+
+    with open(parsed_command[1], "r") as f:
+        with open(directory, "w") as c:
+            c.write(f.read())
+        os.remove(parsed_command[1])
