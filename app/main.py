02273c4f2de46b5a57878c15465da970bfbfa4e6
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-07-22 22:00:39 +0300

    Implemented solution (added try except blocks)

--- app/main.py
+++ app/main.py
@@ -7,12 +7,19 @@ def move_file(command: str) -> None:
     if file == new_file:
         return
     if "/" not in new_file:
-        shutil.move(file, new_file)
-        return
+        try:
+            shutil.move(file, new_file)
+        except FileNotFoundError:
+            print(f"{file} does not exists")
+        finally:
+            return
     end_path = new_file.split("/")
     directory = end_path[:-1]
     file_name = end_path[-1]
     path_for_directory = os.path.join(*directory)
     path_for_file = os.path.join(*directory, file_name)
     os.makedirs(path_for_directory, exist_ok=True)
-    shutil.move(file, path_for_file)
+    try:
+        shutil.move(file, path_for_file)
+    except FileNotFoundError:
+        print(f"{file} does not exists")
