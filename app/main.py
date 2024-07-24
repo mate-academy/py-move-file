fc834ef2b70eb61d6aec7e8a1175fd03081dd149
Maksim <golevmaksim52@gmail.com>
2024-07-24 06:04:01 +0300

    create move function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    cmd, source_file, destination_path = command.split()
+
+    with open(source_file, "r") as file:
+        content = file.read()
+
+    destination_dir = os.path.dirname(destination_path)
+
+    os.makedirs(destination_dir, exist_ok=True)
+
+    with open(destination_path, "w") as file2:
+        file2.write(content)
+
+    os.remove(source_file)
