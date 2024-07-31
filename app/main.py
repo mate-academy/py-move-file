205b6f4215cc719a5a11ac2e07a58ad91bfd32ae
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-31 17:14:34 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+from pathlib import Path
+
+
+def move_file(command: str) -> None:
+    command, source, destination = command.split()
+    last_slash = destination.rfind("/")
+
+    Path(destination[:last_slash]).mkdir(parents=True, exist_ok=True)
+
+    with open(source) as src, open(destination, "w") as dst:
+        content = src.read()
+        dst.write(content)
+    os.remove(source)
