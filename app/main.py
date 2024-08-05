a010cba0366bfde1cd8c12ade5dff0f263428158
Stepan <oleksiukstepann@gmail.com>
2024-08-05 13:45:03 +0300

    added some changes

--- app/main.py
+++ app/main.py
@@ -4,13 +4,12 @@ import shutil
 
 def move_file(command: str) -> None:
     try:
-        assert len(command.split()) == 3, "Command should have 3 arguments"
-        assert command.split()[0] == "mv", "First command should be 'mv'"
-    except AssertionError as e:
+        order, old_file, new_location = command.split()
+        assert order == "mv", "First command should be 'mv'"
+    except (AssertionError, ValueError) as e:
         print(e)
         return
 
-    order, old_file, new_location = command.split()
     if os.path.sep in new_location:
         directories = os.path.split(new_location)[0]
         os.makedirs(directories, exist_ok=True)
