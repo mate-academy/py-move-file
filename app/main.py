a0f3096cfc734ed1e0d943e75d85a1051989c4c7
Alexandr Politov <apolitov83@gmail.com>
2024-08-06 19:11:53 +0300

    Solution Politov

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+
+    user_command, source_file, result_file = command.split()
+
+    os.renames(source_file, result_file)
