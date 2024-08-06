8912d1b5d2e0ae86a18e17d212f62bae29acb4e5
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-08-06 13:05:54 +0300

    fix move_file function

--- app/main.py
+++ app/main.py
@@ -3,27 +3,20 @@ import os
 
 def move_file(command: str) -> None:
     command_key_word, file_in_name, dir_destination = command.split()
-    if command_key_word != "mv" or file_in_name == dir_destination:
-        return
 
-    *directories, file_out_name = dir_destination.split("/")
-    if not directories:
-        os.rename(file_in_name, file_out_name)
-        return
+    if command_key_word == "mv" and file_in_name != dir_destination:
+        *directories, file_out_name = dir_destination.split("/")
 
-    parent_dir = ""
-    for directory in directories:
-        path = os.path.join(parent_dir, directory)
-        parent_dir = str(path)
-        try:
-            os.mkdir(path)
-        except OSError:
-            pass
+        if not directories:
+            os.rename(file_in_name, file_out_name)
+        else:
+            path = os.path.join(*directories)
+            os.makedirs(path, exist_ok=True)
 
-    with (
-        open(file_in_name, "r") as file_in,
-        open(dir_destination, "w") as file_out
-    ):
-        file_out.write(file_in.read())
+            with (
+                open(file_in_name, "r") as file_in,
+                open(dir_destination, "w") as file_out
+            ):
+                file_out.write(file_in.read())
 
-    os.remove(file_in_name)
+            os.remove(file_in_name)
