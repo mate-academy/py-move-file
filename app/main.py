ba801308fa62e7b0690d7b0d3af11587a60e9768
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-08-10 10:42:42 +0300

    solution 1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    command_list = command.split()
+    if len(command_list) == 3:
+        _, source_file, destination_file = command_list
+        if not os.path.isdir(destination_file):
+            if (os.path.dirname(destination_file)
+                    == os.path.dirname(source_file)):
+                os.rename(source_file, destination_file)
+            else:
+                os.makedirs(os.path.dirname(destination_file), exist_ok=True)
+                shutil.move(source_file, destination_file)
