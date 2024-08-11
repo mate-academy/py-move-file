80d4191d98993d3da1ba759a4b3ab8d987946694
pavel_t <{89582095}+{PavelTym}@users.noreply.github.com>
2024-08-11 22:12:36 +0300

    made changes suggested by the mentor

--- app/main.py
+++ app/main.py
@@ -13,11 +13,11 @@ def move_file(command: str) -> None:
         raise FileNotFoundError(f"No such file: '{source}'")
 
     if destination.endswith("/"):
-        os.makedirs(destination, exist_ok=True)
         destination = os.path.join(destination, os.path.basename(source))
-    else:
-        dir_path = os.path.dirname(destination)
-        if dir_path:
-            os.makedirs(dir_path, exist_ok=True)
+
+    dir_path = os.path.dirname(destination)
+
+    if dir_path:
+        os.makedirs(dir_path, exist_ok=True)
 
     os.rename(source, destination)
