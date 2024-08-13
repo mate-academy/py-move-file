1425cdac0b02842808b1ced84648c5ec4caaaaae
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-08-13 18:12:33 +0300

    Solution 1.3

--- app/main.py
+++ app/main.py
@@ -3,15 +3,26 @@ import os
 
 def move_file(command: str) -> None:
     command_list = command.split()
-    if len(command_list) == 3 and command_list[0] == "mv":
-        _, source_file, destination_file = command_list
-        if not os.path.isdir(destination_file):
-            if (os.path.dirname(destination_file)
-                    == os.path.dirname(source_file)):
-                os.rename(source_file, destination_file)
-            else:
-                os.makedirs(os.path.dirname(destination_file), exist_ok=True)
-                with (open(source_file, "r") as source,
-                      open(destination_file, "w") as destiny):
-                    destiny.write(source.read())
-                os.remove(source_file)
+    if len(command_list) != 3 or command_list[0] != "mv":
+        raise Exception("Invalid command's format")
+
+    _, source_file, destination = command_list
+
+    if destination.endswith("/"):
+        destination = os.path.join(destination, os.path.basename(source_file))
+
+    destination_dir = os.path.dirname(destination)
+
+    if destination_dir:
+        os.makedirs(destination_dir, exist_ok=True)
+
+    if os.path.dirname(destination) == os.path.dirname(source_file):
+        os.rename(source_file, destination)
+    else:
+        try:
+            with (open(source_file, "r") as source,
+                  open(destination, "w") as dest):
+                dest.write(source.read())
+            os.remove(source_file)
+        except OSError as ex:
+            print(f"Error while moving file: {ex}")
