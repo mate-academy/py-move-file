8f534568fd885a9118db93672e86987e1a707708
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-08-14 19:22:37 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_list = command.split()
+
+    if len(command_list) != 3 or command_list[0] != "mv":
+        raise ValueError("Something wrong. "
+                         "Command example: "
+                         "'mv source destination'")
+
+    source = command_list[1]
+    dest = command_list[2]
+
+    if not os.path.isfile(source):
+        raise FileNotFoundError(f"File {source} not found.")
+
+    if dest.endswith("/"):
+        os.makedirs(dest, exist_ok=True)
+        dest = os.path.join(dest, os.path.basename(source))
+    else:
+        dest_dir = os.path.dirname(dest)
+        if dest_dir:
+            os.makedirs(dest_dir, exist_ok=True)
+
+    os.rename(source, dest)
