4f541cdcf7006dd45d784070bb0aec4b30b09531
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2024-08-17 18:31:47 +0300

    add my solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+from os import path, makedirs
+from shutil import move
+
+
+def move_file(command: str) -> None:
+    _, source, destination = command.split()
+
+    if (not (command.startswith("mv "))
+            or not (source.endswith(".txt"))
+            or not (destination.endswith(".txt"))):
+        return
+
+    if not (path.dirname(destination) == ''):
+        makedirs(path.dirname(destination), exist_ok=True)
+    move(source, destination)
