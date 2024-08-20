6c834de6770c94c3bf517be919d90d643f1dc873
ulichka <ulichka7615@gmail.com>
2024-08-20 17:17:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,22 +3,21 @@ import os
 
 def move_file(name: str) -> None:
     command = name.split()
-    if command[0] != "mv":
-        print("The command must have mv at the beginning")
+    if command[0] != "mv" or len(command) != 3:
         return
-    target_file = command[2]
-    target_path = target_file.split("/")
-    print(target_path)
+
     source_file = command[1]
-    if len(target_path) == 1:
-        os.rename(source_file, target_file)
-    else:
-        path = ""
-        for folder in target_path[:-1]:
-            path += folder + "/"
-            os.makedirs(path, exist_ok=True)
+    target_file = command[2]
+    target_path = os.path.dirname(target_file)
+
+    if target_path != "" and not os.path.exists(target_path):
+        os.makedirs(target_path, exist_ok=True)
 
         with (open(source_file, "r") as file_in,
               open(target_file, "w") as file_out):
             file_out.write(file_in.read())
         os.remove(source_file)
+
+    else:
+        if source_file != target_file:
+            os.rename(source_file, target_file)
