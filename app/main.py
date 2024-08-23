8ffc129b2c6ccf722fa13c8d3bc358d03854a386
Kulib <kulibchenkovalera@gmail.com>
2024-08-23 22:55:01 +0300

    first commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+
+def move_file(file_names: str) -> None:
+    names = file_names.split()
+    if names[0] != "mv" or names[1] == names[2]:
+        return
+    if "/" in names[2]:
+        os.makedirs(os.path.dirname(names[2]), exist_ok=True)
+    with open(names[1], "r") as file_in, open(names[2], "w") as file_out:
+        file_out.writelines(file_in.read())
+    os.remove(names[1])
