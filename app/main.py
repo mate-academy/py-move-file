a6704c8e4b07fe2c69eb25e3c8cb46106fc1f722
Illia Khomutov <ilyakhomutov613@gmail.com>
2024-08-26 18:09:13 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
-from os import makedirs, rename, remove, chdir, getcwd, path
+import os.path
+from os import makedirs, rename, remove
 
 
 def move_file(command: str) -> None:
-    command_name = command.split()[0]
-    source_file_name = command.split()[1]
-    destination_name = command.split()[2]
-    if command_name == "mv":
+    if command.split()[0] == "mv" and len(command.split()) == 3:
+        command_name, source_file_name, destination_name = command.split()
 
-        if "/" in destination_name:
+        if os.path.dirname(destination_name):
             with open(source_file_name) as source:
                 source_text = source.read()
 
-            init_dir = getcwd()
+            destination_path = "/".join(destination_name.split("/"))
             destination_folders = "/".join(destination_name.split("/")[:-1])
-            destination_file_name = destination_name.split("/")[-1]
-            if not path.exists(destination_folders):
-                makedirs(destination_folders)
-            chdir(destination_folders)
 
-            with open(destination_file_name, "w") as destination:
+            makedirs(destination_folders, exist_ok=True)
+
+            with open(destination_path, "w") as destination:
                 destination.write(source_text)
 
-            chdir(init_dir)
             remove(source_file_name)
         else:
             rename(source_file_name, destination_name)
