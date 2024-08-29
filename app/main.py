c83e2fb0647a9fe2a9ac9608454f5a5318ec9950
Arsen <arsen.test.mess@gmail.com>
2024-08-29 13:47:41 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    files_list = command.split()[1:]
+    dirs = []
+    second_file = files_list[1]
+
+    if "/" in files_list[1]:
+        dirs = files_list[1].split("/")[:-1]
+        second_file = files_list[1].split("/")[-1]
+    try:
+        with open(files_list[0], "r") as first_file:
+            content = first_file.read()
+
+        if len(dirs) == 0:
+            with open(second_file, "w") as file:
+                file.write(content)
+        else:
+            os.makedirs("/".join(dirs), exist_ok=True)
+
+            with open("/".join(dirs) + "/" + second_file, "w") as file:
+                file.write(content)
+    finally:
+        os.remove(files_list[0])
