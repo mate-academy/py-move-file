da1a64dcd04e21305024cb073c7572bf8655271a
Arthur <avers.target@gmail.com>
2024-08-30 14:16:47 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os.path
+from os import makedirs
+
+
+def move_file(command: str) -> None:
+    list_command = command.split(" ")[1:]
+    file_origin, file_to_create = list_command
+
+    if os.path.dirname(file_to_create):
+        with open(file_origin) as f_origin:
+            file_data = f_origin.read()
+
+        destination = os.path.dirname(file_to_create)
+        makedirs(destination, exist_ok=True)
+
+        with open(file_to_create, "w") as f_target:
+            f_target.write(file_data)
+
+        os.remove(file_origin)
+    else:
+        os.rename(file_origin, file_to_create)
