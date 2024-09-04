2d04afe10270fcaca275faa8395e6e2f96551425
Ihor Bachuk <igor.bachuk04@gmail.com>
2024-09-04 10:20:30 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+
+    _, file_name, copy_path = command.split()
+    copy_path = copy_path.split("/")
+
+    if copy_path[:-1]:
+        os.makedirs(os.path.join(
+            *copy_path[:-1]
+        ), exist_ok=True)
+
+    with (open(file_name, "r") as file,
+          open(os.path.join(*copy_path), "w") as copy):
+        copy.write(file.read())
+    os.remove(file_name)
