061758616c79a4fb1900c4476747539e2dfc5342
Daria Rozumovska <dr.dariapushkar@gmail.com>
2024-09-04 14:26:49 +0300

    Solution_fixed

--- app/main.py
+++ app/main.py
@@ -7,23 +7,15 @@ def move_file(command: str) -> None:
             and len(command_list) == 3
             and "/" in command_list[2]):
         path_directions = command_list[2].split("/")
-        make_direction = path_directions[0]
-        if not os.path.exists(make_direction):
-            os.mkdir(make_direction)
-        if len(path_directions) > 2:
-            for i in range(1, len(path_directions) - 1):
-                make_direction += "/" + path_directions[i]
-                if not os.path.exists(make_direction):
-                    os.mkdir(make_direction)
-        path = os.path.join(make_direction, path_directions[-1])
-        with (open(command_list[1], "r") as file_in,
-                open(path, "w") as file_out):
-            for line in file_in:
-                file_out.write(line)
-        os.remove(command_list[1])
+        path = path_directions[0]
+        for i in range(1, len(path_directions)):
+            if not os.path.exists(path):
+                os.mkdir(path)
+            path = os.path.join(path, path_directions[i])
     elif command_list[0] == "mv" and len(command_list) == 3:
-        with (open(command_list[1], "r") as file_in,
-                open(command_list[2], "w") as file_out):
-            for line in file_in:
-                file_out.write(line)
-        os.remove(command_list[1])
+        path = command_list[2]
+    with (open(command_list[1], "r") as file_in,
+            open(path, "w") as file_out):
+        for line in file_in:
+            file_out.write(line)
+    os.remove(command_list[1])
