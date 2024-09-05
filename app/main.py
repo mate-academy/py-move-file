174881f083f3ea28827100fe62ad0918ded0782a
Bogdan <bogdan.rylov422@gmail.com>
2024-09-05 13:54:44 +0300

    Reformat code according to mentor's requirements

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 
 def move_file(command: str) -> None:
@@ -9,25 +8,16 @@ def move_file(command: str) -> None:
         print("Wrong command format")
     else:
         if command == "mv" and source_path != destination_path:
-            if not os.path.isfile(source_path):
-                raise ValueError(f"File '{source_path}' doesn't exist")
-            source_path = os.path.join(*re.split(r"[\\/]", source_path))
-            destination_path = os.path.join(
-                *re.split(r"[\\/]", destination_path)
-            )
-            if (
-                    source_path.endswith(os.sep)
-                    or destination_path.endswith(os.sep)
-            ):
-                return
-
-            if os.sep in destination_path:
+            if os.path.split(destination_path)[0]:
                 os.makedirs(os.path.dirname(destination_path), exist_ok=True)
 
-            with (
-                open(source_path) as source,
-                open(destination_path, "w") as destination
-            ):
-                destination.write(source.read())
+            try:
+                with (
+                    open(source_path) as source,
+                    open(destination_path, "w") as destination
+                ):
+                    destination.write(source.read())
 
-            os.remove(source_path)
+                os.remove(source_path)
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
