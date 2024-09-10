5d1270b05d8af2f1ab1a23e87c858551535ea8f6
Deadmarvald <deadmarvald@gmail.com>
2024-09-10 22:06:31 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -3,24 +3,18 @@ import os
 
 def move_file(command: str) -> None:
     command_list = command.split(" ")
-    if command_list[0] != "mv" or len(command_list) < 2:
+    cmd, source_file_name, destination_path = command_list
+    destination_dir = os.path.dirname(destination_path)
+
+    if cmd != "mv" or len(command_list) < 2:
         return
 
-    source_file_name = command_list[1]
-    list_of_dirs = command_list[2].split("/")
-    list_of_dirs.pop()
-    destination_file_name = command_list[2]
-    current_path = os.getcwd()
+    if not os.path.exists(destination_dir) and "/" in destination_dir:
+        os.makedirs(destination_dir, exist_ok=True)
 
-    if list_of_dirs:
-        for directory in list_of_dirs:
-            new_directory_path = os.path.join(current_path, directory)
-            os.makedirs(new_directory_path, exist_ok=True)
-            current_path = new_directory_path
-
-    if source_file_name != destination_file_name:
+    if source_file_name != destination_path:
         with (open(source_file_name, "r") as file_in,
-              open(destination_file_name, "w") as file_out):
+              open(destination_path, "w") as file_out):
             content = file_in.read()
             file_out.write(content)
 
