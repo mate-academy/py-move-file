97bfe4fe1b3918cd7b6b423515e061886645374c
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-09-12 22:46:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,13 +6,9 @@ def move_file(command: str) -> None:
     if len(split_command) != 3 or split_command[0] != "mv":
         print("The format is incorrect")
         return
-    source, destination = split_command[1], split_command[2]
-    parts = destination.split("/")
-    file_name = parts.pop()
-    current_path = ""
-    for part in parts:
-        current_path = os.path.join(current_path, part)
-        if not os.path.exists(current_path):
-            os.mkdir(current_path)
+    _, source, destination = split_command
+    current_path, file_name = os.path.split(destination)
+    if current_path:
+        os.makedirs(current_path, exist_ok=True)
     new_file_path = os.path.join(current_path, file_name)
     os.rename(source, new_file_path)
