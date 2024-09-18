44398ee83a2d23afa87f38ba050bc122dbb52f98
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-09-18 10:42:41 +0300

    solution for move file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    mv, source_file, result = command.split()
+    if "/" in result:
+        destination, file = os.path.split(result)
+    else:
+        destination, file = "", result
+
+    if destination:
+        os.makedirs(destination, exist_ok=True)
+
+    with (
+        open(source_file, "r") as file_in,
+        open(os.path.join(destination, file), "w")
+        as file_out
+    ):
+        file_out.write(file_in.read())
+
+    os.remove(source_file)
