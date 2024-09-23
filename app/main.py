956612d3796afcd5c3d78ae055651959f0235b50
Viacheslav417865 <sfinks417865@gmail.com>
2024-09-23 13:53:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "mv":
+        raise ValueError("Invalid command format")
+    source_file = parts[1]
+    destination_path = parts[2]
+    if not os.path.isfile(source_file):
+        raise FileNotFoundError(f"Source file '{source_file}' does not exist")
+    if destination_path.endswith("/"):
+        raise (ValueError
+               ("Destination path cannot end with a '/' "
+                "as it should be a file path"))
+    destination_dir = os.path.dirname(destination_path)
+    if destination_dir:
+        os.makedirs(destination_dir, exist_ok=True)
+    shutil.move(source_file, destination_path)
