f7398965ece739bd77e6b38f3123f1ada0d03c41
yura <pollo7964132@gmail.com>
2024-09-27 12:42:33 +0300

    made the code short

--- app/main.py
+++ app/main.py
@@ -4,15 +4,9 @@ import os
 def move_file(command: str) -> None:
     command, file_name, location = command.split()
 
-    if "/" not in location and file_name != location:
-        os.rename(file_name, location)
-        return
+    os.makedirs(location[:location.rfind("/")], exist_ok=True)
 
-    if not os.path.isfile(location):
-        directories = location[:location.rfind("/")]
-        os.makedirs(directories, exist_ok=True)
+    with open(file_name) as file_in, open(location, "w") as file_out:
+        file_out.write(file_in.read())
 
-        with open(file_name) as file_in, open(location, "w") as file_out:
-            file_out.write(file_in.read())
-
-        os.remove(file_name)
+    os.remove(file_name)
