b7e11890e3aede767b43a26f2494b86be6e81871
user <nikitaskrypets@gmail.com>
2024-10-04 14:04:41 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command, file, path = command.split(" ")
+    path_list = path.split("/")
+    for i, directory in enumerate(path_list):
+        if "." not in directory:
+            if not os.path.exists(directory):
+                os.makedirs("/".join(path_list[:i + 1]), exist_ok=True)
+        else:
+            with open(file, "r") as source, open(path, "w") as moved:
+                moved.write(source.read())
+            os.remove(file)
