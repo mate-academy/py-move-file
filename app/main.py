0a994c7748fa273e142df842d828d8125eb65e37
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2024-10-06 12:57:05 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 import os
 
 
+def copying_file(name: str, new_name: str) -> None:
+    with (
+            open(name, "r") as source,
+            open(new_name, "w") as copy_file
+    ):
+        copy_file.write(source.read())
+    os.remove(name)
+
+
 def move_file(command: str) -> None:
     _command = command.split()
     if len(_command) != 3:
@@ -12,29 +21,14 @@ def move_file(command: str) -> None:
         return
     *path, file_name = new_file.split("/")
 
-    if len(path) == 0:
-        with (
-            open(source_file, "r") as source,
-            open(file_name, "w") as copy_file
-        ):
-            copy_file.write(source.read())
-        os.remove(source_file)
+    if not path:
+        copying_file(source_file, file_name)
         return
 
     path = "/".join(path)
     if os.path.exists(path):
-        with (
-            open(source_file, "r") as source,
-            open(path + "/" + file_name, "w") as copy_file
-        ):
-            copy_file.write(source.read())
+        copying_file(source_file, path + "/" + file_name)
     else:
         new_path = os.path.join(path)
         os.makedirs(new_path)
-        with (
-            open(source_file, "r") as source,
-            open(new_path + "/" + file_name, "w") as copy_file
-        ):
-            copy_file.write(source.read())
-
-    os.remove(source_file)
+        copying_file(source_file, new_path + "/" + file_name)
