55f7d7d636fb42994abfa72e6cdedc448ce52ea9
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-10-07 12:57:16 +0300

    rename

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_name, file_name, file_path, *_ = command.split()
+    command_name, file_name, file_name_out, *_ = command.split()
 
-    if command_name != "mv":
+    if command_name != "mv" and file_name == file_name_out:
         return
 
-    directory = os.path.dirname(file_path)
+    directory = os.path.dirname(file_name_out)
     if directory:
         os.makedirs(directory, exist_ok=True)
 
-    os.rename(file_name, file_path)
+    os.rename(file_name, file_name_out)
