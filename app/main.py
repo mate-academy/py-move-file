5283d030c2ae64d6fffc0eb43669d303235c9432
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-10-07 12:43:38 +0300

    New Solution

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_name, file_name, file_path = command.split()
+    command_name, file_name, file_path, *_ = command.split()
 
-    if command_name != "mv" or file_path[-1] == "/":
+    if command_name != "mv":
         return
 
     directory = os.path.dirname(file_path)
