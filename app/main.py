a0bd9bc1053336091a6e37f32d4bd6f85e500d1e
Dima Havrylenko <94nj111@gmail.com>
2024-10-09 17:35:51 +0300

    move-file solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command = command.split(" ")
+    mv = command[0]
+    file_to_move = command[1]
+    destination = command[2].split("/")
+    if mv == "mv" and file_to_move != "".join(destination):
+        try:
+            with open(file_to_move, "r") as f:
+                data = f.read()
+            os.remove(file_to_move)
+        except FileNotFoundError:
+            print("File not found")
+        path = ""
+        if len(destination) == 1:
+            path = destination[0]
+        else:
+            for i in range(len(destination) - 1):
+                path += destination[i] + "/"
+                if not os.path.exists(path):
+                    os.mkdir(path)
+            path += destination[-1]
+        with open(path, "w") as f:
+            f.write(data)
