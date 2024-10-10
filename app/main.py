b743631cf4cf37ad51d872a067bca87589042ad5
Aleksandr Mazur <a9p9m9@gmail.com>
2024-10-10 13:31:26 +0300

    Solution_2

--- app/main.py
+++ app/main.py
@@ -3,22 +3,15 @@ import os
 
 def move_file(command: str) -> None:
     split_command = command.split()
-    mv, file_1, path_file_2 = split_command
-
-    if "/" in command:
-        path = path_file_2[0:path_file_2.rfind("/")]
-
-        if not os.path.exists(path):
-            os.makedirs(path)
-        with (open(file_1, "r") as file_in,
-              open(path_file_2, "w") as file_out):
-            file_out.write(file_in.read())
-        os.remove(file_1)
-    else:
-        if not os.path.exists(path_file_2):
-            with (open(file_1, "r") as file_in,
-                  open(path_file_2, "w") as file_out):
+    mv, source_file, destination_file = split_command
+    path = destination_file[0:destination_file.rfind("/")]
+    if mv == "mv":
+        if "/" in command:
+            if not os.path.exists(os.path.join(path)):
+                os.makedirs(os.path.join(path))
+            with (open(source_file, "r") as file_in,
+                  open(destination_file, "w") as file_out):
                 file_out.write(file_in.read())
-            os.remove(file_1)
+            os.remove(source_file)
         else:
-            os.rename(file_1, path_file_2)
+            os.rename(source_file, destination_file)
