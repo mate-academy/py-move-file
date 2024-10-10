95482c082ac36a5805e760ecbf5a64eb3b643c41
Dima Havrylenko <94nj111@gmail.com>
2024-10-10 18:07:48 +0300

    move-file fix

--- app/main.py
+++ app/main.py
@@ -6,21 +6,23 @@ def move_file(command: str) -> None:
     mv = command[0]
     file_to_move = command[1]
     destination = command[2].split("/")
-    if mv == "mv" and file_to_move != "".join(destination):
+    if mv != "mv":
+        raise SyntaxError(f"Command {mv} not found")
+    if file_to_move != "".join(destination):
         try:
-            with open(file_to_move, "r") as f:
-                data = f.read()
+            with open(file_to_move, "r") as source_file:
+                data = source_file.read()
             os.remove(file_to_move)
         except FileNotFoundError:
-            print("File not found")
+            raise FileNotFoundError(f"File {file_to_move} not found")
         path = ""
         if len(destination) == 1:
-            path = destination[0]
+            path = os.path.join(path, destination[0])
         else:
             for i in range(len(destination) - 1):
-                path += destination[i] + "/"
+                path = os.path.join(path, destination[i])
                 if not os.path.exists(path):
                     os.mkdir(path)
-            path += destination[-1]
-        with open(path, "w") as f:
-            f.write(data)
+            path = os.path.join(path, destination[-1])
+        with open(path, "w") as destination_file:
+            destination_file.write(data)
