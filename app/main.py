221ce395b921bc28dc91e1f96a14f0ab3eab7bd7
Aleksandr Mazur <a9p9m9@gmail.com>
2024-10-10 13:38:09 +0300

    Solution_3

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def move_file(command: str) -> None:
     path = destination_file[0:destination_file.rfind("/")]
     if mv == "mv":
         if "/" in command:
-            if not os.path.exists(os.path.join(path)):
-                os.makedirs(os.path.join(path))
+            if not os.path.exists(path):
+                os.makedirs(path)
             with (open(source_file, "r") as file_in,
                   open(destination_file, "w") as file_out):
                 file_out.write(file_in.read())
