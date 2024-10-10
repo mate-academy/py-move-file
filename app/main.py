cf50c68be1a1196276176fada18e38d064464bf1
Dima Havrylenko <94nj111@gmail.com>
2024-10-10 20:10:11 +0300

    move-file fix1

--- app/main.py
+++ app/main.py
@@ -3,26 +3,17 @@ import os
 
 def move_file(command: str) -> None:
     command = command.split(" ")
-    mv = command[0]
     file_to_move = command[1]
     destination = command[2].split("/")
-    if mv != "mv":
-        raise SyntaxError(f"Command {mv} not found")
     if file_to_move != "".join(destination):
-        try:
-            with open(file_to_move, "r") as source_file:
-                data = source_file.read()
-            os.remove(file_to_move)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"File {file_to_move} not found")
+        if not os.path.exists(file_to_move):
+            raise FileNotFoundError
         path = ""
-        if len(destination) == 1:
-            path = os.path.join(path, destination[0])
-        else:
-            for i in range(len(destination) - 1):
-                path = os.path.join(path, destination[i])
-                if not os.path.exists(path):
-                    os.mkdir(path)
-            path = os.path.join(path, destination[-1])
-        with open(path, "w") as destination_file:
-            destination_file.write(data)
+        path = os.path.join(path, *destination[:-1])
+        if path:
+            os.makedirs(path, exist_ok=True)
+        path = os.path.join(path, destination[-1])
+        with (open(file_to_move, "r") as file,
+              open(path, "w") as destination_file):
+            destination_file.write(file.read())
+        os.remove(file_to_move)
