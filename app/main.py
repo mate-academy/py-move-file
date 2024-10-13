d220fd3dd2aff0a4b0aba61d8b8447153d6b7684
Max <mxbely98@gmail.com>
2024-10-13 13:03:00 +0300

    simplified solution

--- app/main.py
+++ app/main.py
@@ -1,26 +1,12 @@
 import os
+import shutil
 
 
 def move_file(command: str) -> None:
-    command = command.split()
-    route = command[2].split("/")
-    if command[0] != "mv":
+    command, source, destination = command.split()
+    if command != "mv":
         return
-    if len(route) > 1:
-        route.pop()
-    else:
-        new_file_name = route[0]
-        with (
-            open(command[1], "r") as file_in,
-            open(new_file_name, "x") as file_out,
-        ):
-            file_out.write(file_in.read())
-        os.remove(command[1])
-        return
-    for directory in route:
-        if not os.path.exists(directory):
-            os.mkdir(directory)
-        os.chdir(directory)
-    for _ in range(len(route)):
-        os.chdir("..")
-    os.rename(command[1], command[2])
+    path_, new_file_name = os.path.split(destination)
+    if path_ and not os.path.exists(path_):
+        os.makedirs(path_)
+    shutil.move(source, destination)
