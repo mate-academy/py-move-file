4b7437ca09197325082de9e9fe0748bc0b027c70
Dmytro Stepanenko <dmytrostepanenkoua@gmail.com>
2024-10-14 19:57:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,15 +4,19 @@ import shutil
 
 def move_file(command: str) -> None:
     parsed_command = command.split()
+    cmd_name = parsed_command[0]
     src_file = parsed_command[1]
     dst_file = parsed_command[2]
 
-    src_file_path = os.path.abspath(src_file)
-    dst_file_path = os.path.abspath(dst_file)
+    if cmd_name == "mv":
+        src_file_path = os.path.abspath(src_file)
+        dst_file_path = os.path.abspath(dst_file)
 
-    if src_file_path != dst_file_path:
-        dst_dir = os.path.dirname(dst_file_path)
-        if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)
+        if src_file_path != dst_file_path:
+            dst_dir = os.path.dirname(dst_file_path)
+            if not os.path.exists(dst_dir):
+                os.makedirs(dst_dir)
 
-        shutil.move(src_file_path, dst_file_path)
+            shutil.move(src_file_path, dst_file_path)
+    else:
+        "Command name is not correct!"
