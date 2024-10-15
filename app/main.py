db04500ab478324d1f712a7876f5e9035be5fad5
Dmytro Stepanenko <dmytrostepanenkoua@gmail.com>
2024-10-15 13:22:51 +0300

    Solution fix 2

--- app/main.py
+++ app/main.py
@@ -4,21 +4,20 @@ import shutil
 
 def move_file(command: str) -> None:
     parsed_command = command.split()
-    cmd_name = parsed_command[0]
-    src_file = parsed_command[1]
-    dst_file = parsed_command[2]
+    command_name = parsed_command[0]
+    source_file = parsed_command[1]
+    destination_file = parsed_command[2]
 
-    src_file_path = os.path.abspath(src_file)
-    dst_file_path = os.path.abspath(dst_file)
+    src_file_path = os.path.abspath(source_file)
+    dst_file_path = os.path.abspath(destination_file)
 
-    if cmd_name == "mv":
+    if command_name == "mv":
         if os.path.exists(src_file_path) and not os.path.exists(dst_file_path):
             dir_path = os.path.dirname(dst_file_path)
-            if not os.path.exists(dir_path):
-                os.makedirs(dir_path, exist_ok=True)
+            os.makedirs(dir_path, exist_ok=True)
             shutil.move(src_file_path, dst_file_path)
         else:
-            print("""Check:
+            raise Exception("""Check:
             - is source file exist
             - destination file not exist in destination directory
             - destination directory is exist""")
