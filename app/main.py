8bcaa2aff2d0821907e36a812898fb9eaf9a30c1
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2024-10-20 15:13:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, old_name, new_name = command.split()
+    try:
+        os.renames(old_name, new_name)
+    except OSError as err:
+        print("An error occurred:", err)
