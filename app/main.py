733a5f0a60f58e8e0a2900665baaa53cd74e750f
Oleksandr <rigby8806@gmail.com>
2024-10-27 21:45:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, src, dest = command.split()
+
+    if dest.endswith("/"):
+        os.makedirs(dest, exist_ok=True)
+        dest = os.path.join(dest, os.path.basename(src))
+    else:
+        dest_dir = os.path.dirname(dest)
+        if dest_dir:
+            os.makedirs(dest_dir, exist_ok=True)
+
+    os.rename(src, dest)
