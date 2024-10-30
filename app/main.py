5a10eeed3d3bac73b3f1d9464382fb1a31301aed
Eldar <eldardemchenko19@mail.com>
2024-10-30 21:17:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import os
+import shutil
+
+
+def move_file(command: str) -> None:
+    parts: list[str] = command.split(" ")
+    if parts[0] == "mv" and len(parts) == 3:
+        source_file: str = parts[1]
+        destination: str = parts[2]
+
+        if not os.path.exists(source_file):
+            raise FileNotFoundError(f"The source file {source_file} "
+                                    f"does not exist.")
+
+        if destination.endswith("/"):
+            if not os.path.exists(destination):
+                os.makedirs(destination, exist_ok=True)
+            destination = os.path.join(destination,
+                                       os.path.basename(source_file))
+        else:
+            destination_dir: str = os.path.dirname(destination)
+            if destination_dir:
+                os.makedirs(destination_dir, exist_ok=True)
+
+        shutil.move(source_file, destination)
