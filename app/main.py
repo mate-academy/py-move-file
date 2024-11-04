79667df57b8feb2193255a80d09888ef5a71b590
Dmytro <labtech.kh@gmail.com>
2024-11-04 22:02:27 +0200

    'Solution_v1.00'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+
+    mv, source_file, destination = command.split()
+    if mv != "mv":
+        return
+
+    if "/" in destination:
+        if destination[-1] == "/":
+            destination += source_file
+
+        dir_list = destination.split("/")
+        dir_list.pop()
+        directory = ""
+        for dirs in dir_list:
+            directory += dirs
+            directory += "/"
+
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+
+    sf = open(source_file, "r")
+    source_text = sf.read()
+    cf = open(destination, "w")
+    cf.write(source_text)
+    sf.close()
+    cf.close()
+    os.remove(source_file)
