c24e4df988fa96f12f4b1bd12610d64397432a73
Dmytro <labtech.kh@gmail.com>
2024-11-05 11:51:43 +0200

    'Solution_v1.06'

--- app/main.py
+++ app/main.py
@@ -1,34 +1,26 @@
 import os
+import shutil
 
 
 def move_file(command: str) -> None:
-    lcommand = command.split()
+    parts = command.split()
 
-    if len(lcommand) != 3 or lcommand[0] != "mv":
-        raise ValueError(
-            "Invalid command format! Use: mv <source_file> <destination_path>"
-        )
+    if len(parts) != 3 or parts[0] != "mv":
+        raise ValueError("Invalid command. "
+                         "Please use the format 'mv source destination'.")
 
-    _, source_file, destination_file = lcommand
+    _, source, destination = parts
 
-    if not os.path.isfile(source_file):
-        raise FileNotFoundError(f"File {source_file} does not exist!")
+    if not os.path.isfile(source):
+        raise FileNotFoundError(f"The source file '{source}' does not exist.")
 
-    destination_file_dir = os.path.dirname(destination_file)
-    if destination_file_dir:
-        os.makedirs(destination_file_dir, exist_ok=True)
+    if destination.endswith("/"):
+        os.makedirs(destination, exist_ok=True)
+        destination = os.path.join(destination, os.path.basename(source))
+    else:
+        destination_dir = os.path.dirname(destination)
 
-    with open(source_file, "r") as src_file:
-        with open(destination_file, "w") as dst_file:
-            dst_file.write(src_file.read())
+        if destination_dir and not os.path.exists(destination_dir):
+            os.makedirs(destination_dir, exist_ok=True)
 
-# f = open("file.txt", "w")
-# f.write("Some\nText")
-# f.close()
-#
-# move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt")
-# if os.path.isfile("first_dir/second_dir/third_dir/file2.txt"):
-#     print(open("first_dir/second_dir/third_dir/file2.txt").read())
-#
-# if os.path.isfile("file.txt"):
-#     os.remove("file.txt")
+    shutil.move(source, destination)
