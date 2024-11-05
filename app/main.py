11e8996f6cb1e18370ce5654851b3fa676ea83db
Dmytro <labtech.kh@gmail.com>
2024-11-05 10:46:10 +0200

    'Solution_v1.05'

--- app/main.py
+++ app/main.py
@@ -2,39 +2,25 @@ import os
 
 
 def move_file(command: str) -> None:
+    lcommand = command.split()
 
-    if len(command.split()) != 3:
-        print("Invalid command")
-        return
+    if len(lcommand) != 3 or lcommand[0] != "mv":
+        raise ValueError(
+            "Invalid command format! Use: mv <source_file> <destination_path>"
+        )
 
-    mv, source_file, destination = command.split()
-    directory = ""
+    _, source_file, destination_file = lcommand
 
-    if mv != "mv":
-        return
+    if not os.path.isfile(source_file):
+        raise FileNotFoundError(f"File {source_file} does not exist!")
 
-    if "/" in destination:
-        if destination[-1] == "/":
-            destination += source_file
+    destination_file_dir = os.path.dirname(destination_file)
+    if destination_file_dir:
+        os.makedirs(destination_file_dir, exist_ok=True)
 
-        dir_list = destination.split("/")
-        dir_list.pop()
-#        print("dir_list = ", dir_list)
-        for dirs in dir_list:
-            directory = os.path.join(directory, dirs)
-
-#        print("directory = ", directory)
-
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
-
-#    print("destination = ", destination, os.path.isfile(destination))
-    if not os.path.isfile(destination):
-        with open(source_file, "r") as sf:
-            source_text = sf.read()
-        with open(destination, "a") as cf:
-            cf.write(source_text)
-        os.remove(source_file)
+    with open(source_file, "r") as src_file:
+        with open(destination_file, "w") as dst_file:
+            dst_file.write(src_file.read())
 
 # f = open("file.txt", "w")
 # f.write("Some\nText")
