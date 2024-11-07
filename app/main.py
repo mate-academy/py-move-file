1f3995ae2480d1ac8673a5a540d2b6ca84b08e73
Dmytro <dimontus02140@gmail.com>
2024-11-07 18:51:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import os
 
 def move_file(command: str) -> None:
     command_parts = command.split(" ")
+    if len(command_parts) != 3 or command_parts != "mv":
+        return
     path_2 = command_parts[2].split("/")
     with open(command_parts[1], "r") as file_read:
         files = file_read.read()
