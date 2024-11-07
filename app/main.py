a4a4e352bd77793193d871ca8307c6eb00d03f0b
Dmytro <dimontus02140@gmail.com>
2024-11-07 18:45:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_parts = command.split(" ")
+    path_2 = command_parts[2].split("/")
+    with open(command_parts[1], "r") as file_read:
+        files = file_read.read()
+        if len(path_2) > 1:
+            path = ""
+            for part in path_2[:-1]:
+                path += part + "/"
+                if not os.path.isdir(path):
+                    os.mkdir(path)
+        with open(command_parts[2], "w") as file_write:
+            file_write.write(files)
+    os.remove(command_parts[1])
