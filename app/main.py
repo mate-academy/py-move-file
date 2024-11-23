7eed220eab8a7c9c46af60a0a9798db31b5cb3b5
Volodymyr Povroznyk <vpovroznyk1998@gmail.com>
2024-11-23 18:11:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_parts = command.split()
+    mv_command, file_name, file_path = command_parts
+    if mv_command == "mv" and len(command_parts) == 3:
+        path_parts = file_path.split("/")
+        current_dir = path_parts[0]
+        if len(path_parts) == 1:
+            os.rename(file_name, file_path)
+            return
+        elif len(path_parts) == 2:
+            if not os.path.exists(current_dir):
+                os.mkdir(current_dir)
+        else:
+            if not os.path.exists(current_dir):
+                os.mkdir(current_dir)
+            for next_path_part in path_parts[1:-1]:
+                current_dir = os.path.join(current_dir, next_path_part)
+                if not os.path.exists(current_dir):
+                    os.mkdir(current_dir)
+
+        with open(file_name) as source_file:
+            source_file_content = source_file.read()
+        final_file_path = os.path.join(current_dir, path_parts[-1])
+        with open(final_file_path, "w") as destination_file:
+            destination_file.write(source_file_content)
+            os.remove(file_name)
