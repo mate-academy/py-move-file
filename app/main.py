46d9eb3793d25d3685e502793ec809e8c0ccca01
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-12-12 18:54:18 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,19 @@ import os
 
 def move_file(string: str) -> None:
     words = string.split()
+    if len(words) != 3:
+        raise TypeError
+    elif words[0] != "mv":
+        raise ValueError
+
     old_file_name = words[1]
-    destination_path = os.path.abspath(words[-1])
+
+    if words[-1][-1] == "\\" or words[-1][-1] == "/":
+        destination_path = os.path.join(*words[-1])
+        os.makedirs(destination_path, exist_ok=True)
+        return None
+    else:
+        destination_path = os.path.join(*words[-1].split("/"))
 
     new_dir = os.path.dirname(destination_path)
 
@@ -13,7 +24,11 @@ def move_file(string: str) -> None:
 
     os.remove(old_file_name)
 
-    os.makedirs(new_dir, exist_ok=True)
+    if new_dir:
+        os.makedirs(new_dir, exist_ok=True)
+    else:
+        with open(destination_path, "w") as file:
+            file.write(text)
 
     with open(destination_path, "w") as file:
         file.write(text)
