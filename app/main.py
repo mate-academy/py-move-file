2a6e843ceb1f7f98e0691f83b32c3dbbc74f1ee7
Julia Alexeeva <julia4406@gmail.com>
2024-12-14 12:26:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    if len(command.split()) == 3 and command.split()[0] == "mv":
+        mv, file1_name, destination = command.split()
+        dirs = destination.split("/")[:-1]
+        file2_name = destination.split("/")[-1]
+
+        dir_path = ""
+        for folder in dirs:
+            dir_path = os.path.join(dir_path, folder)
+            if not os.path.exists(dir_path):
+                os.mkdir(dir_path)
+
+        dir_path = os.path.join(dir_path, file2_name)
+        with open(file1_name, "r") as source, open(dir_path, "w") as clone:
+            clone.write(source.read())
+        os.remove(file1_name)
