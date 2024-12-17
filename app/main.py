a3416b6fa90c4abdef04650341d2e18df3f23b6a
Trukhanov Danylo <trukhanov.danya@gmail.com>
2024-12-17 09:35:40 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    instructions = command.split()
+    source = instructions[1]
+    destination = instructions[2]
+
+    if len(instructions) != 3 or instructions[0] != "mv":
+        raise ValueError("Incorrect command")
+
+    if os.path.exists(destination):
+        raise ValueError("Already exists!")
+
+    path = "/".join(destination.split("/")[:-1])
+    new_name = "".join(destination.split("/")[-1])
+
+    if path and not os.path.exists(path):
+        os.makedirs(path)
+    if path:
+        new_name = path + "/" + new_name
+
+    with open(source, "r") as old, open(new_name, "w") as new:
+        new.write(old.read())
+
+    os.remove(source)
