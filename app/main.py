a348bcf605339fefc64a6da1a3fe054eb5342082
Trukhanov Danylo <trukhanov.danya@gmail.com>
2024-12-17 18:10:20 +0200

    changed error type, added using os.path.join

--- app/main.py
+++ app/main.py
@@ -3,22 +3,21 @@ import os
 
 def move_file(command: str) -> None:
     instructions = command.split()
-    source = instructions[1]
-    destination = instructions[2]
+    _, source, destination = instructions
 
     if len(instructions) != 3 or instructions[0] != "mv":
         raise ValueError("Incorrect command")
 
     if os.path.exists(destination):
-        raise ValueError("Already exists!")
+        raise FileExistsError("Already exists!")
 
-    path = "/".join(destination.split("/")[:-1])
-    new_name = "".join(destination.split("/")[-1])
+    path = os.path.join("/".join(destination.split("/")[:-1]))
+    new_name = destination.split("/")[-1]
 
     if path and not os.path.exists(path):
         os.makedirs(path)
     if path:
-        new_name = path + "/" + new_name
+        new_name = os.path.join(path + "/" + new_name)
 
     with open(source, "r") as old, open(new_name, "w") as new:
         new.write(old.read())
