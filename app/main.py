ac38b34d9fa23eb4e97deb6a6d09601bcecb8e61
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-17 12:53:02 +0200

    Changed

--- app/main.py
+++ app/main.py
@@ -12,7 +12,6 @@ def move_file(command: str) -> None | str:
         new_directories = command_list[2][:index_new_directories]
         os.makedirs(new_directories, exist_ok=True)
 
-
     if command_list[0] == "mv":
         try:
             with (
@@ -27,13 +26,3 @@ def move_file(command: str) -> None | str:
             return f"No such file: {command_list[1]}"
     else:
         raise CommandError
-
-# print(open("file.txt").read())
-# Some
-# Text
-move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt")
-print(open("first_dir/second_dir/third_dir/file2.txt").read())
-# # Some
-# # Text
-# open("file.txt")
-# FileNotFoundError: [Errno 2] No such file or directory: 'file.txt'
