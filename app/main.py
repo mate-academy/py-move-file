754d54886da619ca2955c05ec5410efde7155b1c
Ruslan <ruslanyemets@gmail.com>
2024-12-22 20:29:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ def move_file(command: str) -> None:
                     command_list[1], "r"
                 ) as old_file,
                 open(
-                    f"{path}/{command_list[2].split("/")[-1]}", "w"
+                    f"{path}/{command_list[2].split('/')[-1]}", "w"
                 ) as new_file
             ):
                 new_file.write(old_file.read())
@@ -44,7 +44,7 @@ def move_file(command: str) -> None:
                     command_list[1], "r"
                 ) as old_file,
                 open(
-                    f"{command_list[2].split("/")[-1]}", "w"
+                    f"{command_list[2].split('/')[-1]}", "w"
                 ) as new_file
             ):
                 new_file.write(old_file.read())
