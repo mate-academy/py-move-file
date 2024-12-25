7b38ee300ed4fa2f585b2020fc99cea5f434255a
Taras <t.kudinov90@gmail.com>
2024-12-26 00:47:27 +0200

    Solved task with passed tests and added changes after AI review.

--- app/main.py
+++ app/main.py
@@ -10,27 +10,20 @@ def move_file(command: str) -> None:
         path = third_part
         dirs = third_part.split("/")
 
-        if len(dirs) > 1 and len(dirs) != 2:
+        if len(dirs) > 1:
             current_path = dirs[0]
-            if not os.path.exists(current_path):
-                os.mkdir(current_path)
+            os.makedirs(current_path, exist_ok=True)
 
             for folder in dirs[1:-1]:
                 if folder is not None:
                     current_path = os.path.join(current_path, folder)
-                    if not os.path.exists(current_path):
-                        os.mkdir(current_path)
+                    os.makedirs(current_path, exist_ok=True)
 
             with open(file_name, "r") as file_in, open(path, "w") as file_out:
                 content = file_in.read()
                 file_out.write(content)
             os.remove(file_name)
-        elif len(dirs) == 2:
-            current_path = os.path.join(dirs[0], dirs[1])
-            with (open(file_name, "r") as file_in,
-                  open(current_path, "w") as file_out):
-                file_out.write(file_in.read())
-            os.remove(file_name)
+
         elif len(dirs) == 1:
             with (open(file_name, "r") as file_i,
                   open(path, "w") as file_o) :
