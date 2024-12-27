af18f8bb9f82a7844576e4b7360ca4a92187b46e
Liza Shulika <liza.shulika2009@gmail.com>
2024-12-27 22:09:02 +0200

    file1

--- app/main.py
+++ app/main.py
@@ -2,22 +2,20 @@ from os import mkdir, replace
 import os
 
 
-def move_file(command):
+def move_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "mv":
-        raise ValueError("Invalid command format. Use: mv <source> <destination>")
+        raise ValueError(
+            "Invalid command format. Use: mv <source> <destination>")
 
     source = parts[1]
     destination = parts[2]
 
     three = destination.split("/")
     array = []
-    for i in range(0, len(three)-1):
+    for i in range(0, len(three) - 1):
         array.append(three[i])
         if not os.path.exists("/".join(array)):
             mkdir("/".join(array))
 
     replace(source, destination)
-
-
-#move_file('mv file.txt first_dir/second_dir/third_dir/file2.txt')
