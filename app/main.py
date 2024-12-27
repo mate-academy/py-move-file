27086a8c6d0efa83a5fe8b05ec289adca94081a3
D Petetskyi <4477617@gmail.com>
2024-12-27 15:08:45 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    file_name, new_path = command.split(" ")[1:]
+
+    with open(file_name, "r") as sorce:
+        text = sorce.read()
+
+    os.remove(file_name)
+    dir_list = new_path.split("/")
+    directory = ""
+
+    for index in range(len(dir_list) - 1):
+        if index:
+            directory += "/" + dir_list[index]
+        else:
+            directory += dir_list[index]
+        if os.path.exists(directory):
+            continue
+        os.mkdir(directory)
+
+    with open(new_path, "w") as new_file:
+        new_file.write(text)
