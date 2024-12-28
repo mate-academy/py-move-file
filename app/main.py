546a810193a012960d72c4669cd1f78c786e57b8
Oksana Dzeryn <oks.dzeryn@gmail.com>
2024-12-28 14:22:03 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 import os
+import shutil
 
 
 def move_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "mv":
-        raise ValueError("Invalid command format."
-                         " Use: mv source_file destination_path")
+        raise ValueError("Invalid command format.")
     source, destination = parts[1], parts[2]
     if not os.path.isfile(source):
         raise FileNotFoundError(f"Source file '{source}' does not exist.")
@@ -16,4 +16,5 @@ def move_file(command: str) -> None:
         dir_path = os.path.dirname(destination)
         if dir_path:
             os.makedirs(dir_path, exist_ok=True)
-    os.rename(source, destination)
+    shutil.copy2(source, destination)
+    os.remove(source)
