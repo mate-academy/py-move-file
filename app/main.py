e80eb230c06e71974b604b06138f00b0ae77e6b2
Psychox1k <success01001011@gmail.com>
2024-12-29 15:46:55 +0100

    solutionFinal

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) < 3 or parts[0] != "mv":
+        raise ValueError("Invalid command format. Use"
+                         " 'mv source_path destination_path'.")
+
+    source_path = parts[1]
+    destination_path = parts[2]
+
+    if not os.path.isfile(source_path):
+        raise FileNotFoundError(f"Source file '{source_path}' "
+                                f"does not exist.")
+
+    directory_path = os.path.dirname(destination_path)
+
+    if directory_path and not os.path.exists(directory_path):
+        os.makedirs(directory_path, exist_ok=True)
+
+    with (open(source_path, "r") as src_file,
+          open(destination_path, "w") as dest_file):
+        dest_file.write(src_file.read())
+
+    os.remove(source_path)
