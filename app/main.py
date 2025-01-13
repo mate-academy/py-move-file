1c5eaf6dfe34634a97d4317f077ef109dceee3f4
Leo <leonsiypopov@gmail.com>
2025-01-13 02:19:17 +0200

    done2

--- app/main.py
+++ app/main.py
@@ -3,16 +3,16 @@ import os
 
 
 def move_file(command: str) -> None:
-    if command.startswith("mv "):
-        blocks = command.split()
-        if len(blocks) > 2:
-            current_file_name = blocks[1]
-            new_file_name = blocks[2]
+    blocks = command.split()
+    if len(blocks) > 2 and blocks[0] == "mv":
+        current_file_name = blocks[1]
+        new_file_name = blocks[2]
 
-            if "/" in new_file_name:
+        if "/" in new_file_name:
 
-                new_file_blocks = new_file_name.split("/")
+            new_file_blocks = new_file_name.split("/")
 
-                if not os.path.isdir("/".join(new_file_blocks[:-1])):
-                    os.makedirs("/".join(new_file_blocks[:-1]))
-            os.rename(current_file_name, new_file_name)
+            if not os.path.isdir("/".join(new_file_blocks[:-1])):
+                os.makedirs("/".join(new_file_blocks[:-1]))
+
+        os.rename(current_file_name, new_file_name)
