804d572cde469f3e3ad9ecaf7bdf94fc18486a4f
Dmytro <dimkanividimka4@gmail.com>
2025-01-16 18:37:57 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,23 +6,31 @@ def move_file(command: str) -> None:
     if len(our_list) == 3:
         if our_list[0] == "mv":
             if our_list[2][-1] != "/":
-                name = ""
+                destination_path = ""
                 if "/" in our_list[2]:
                     our_dir_list = our_list[2].split("/")
                     counter = 0
                     while counter < len(our_dir_list) - 1:
                         if counter == 0:
-                            name += our_dir_list[counter]
+                            destination_path += our_dir_list[counter]
                         else:
-                            name += ("/" + our_dir_list[counter])
-                        if not os.path.exists(name):
-                            os.mkdir(name)
+                            destination_path = os.path.join(
+                                destination_path,
+                                our_dir_list[counter]
+                            )
+                        if not os.path.exists(destination_path):
+                            os.mkdir(destination_path)
+                        else:
+                            pass
                         if counter == len(our_dir_list) - 2:
-                            name += ("/" + our_dir_list[counter + 1])
+                            destination_path = os.path.join(
+                                destination_path,
+                                our_dir_list[counter + 1]
+                            )
                         counter += 1
                 else:
-                    name += our_list[2]
+                    destination_path += our_list[2]
                 with (open(our_list[1], "r") as read_file,
-                      open(name, "w") as write_file):
+                      open(destination_path, "w") as write_file):
                     write_file.write(read_file.read())
                 os.remove(our_list[1])
