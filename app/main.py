66758bde87b9c31d84f5896177ea9018d4144f36
Andrii Sokolan <your@mail.com>
2025-01-18 02:25:27 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, source, destination = command.split()
+
+    if not os.path.exists(source):
+        raise FileNotFoundError(f"{source} does not exist")
+
+    dest_dir = os.path.dirname(destination)
+
+    if dest_dir and not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    os.rename(source, destination)
+
+    if os.path.exists(source):
+        os.remove(source)
