6737ab1c7a98c2713a80976670453f8a1a8df063
Yura Chornyi <chorny.yura@gmail.com>
2025-01-20 01:30:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    cmd_name, file, destination = command.split()
+
+    if cmd_name == "mv" and len(command.split()) == 3:
+        if destination.endswith("/"):
+            os.makedirs(destination, exist_ok=True)
+            os.rename(file, f"{destination}{file}")
+        elif "/" not in destination:
+            os.rename(file, destination)
+        else:
+            *dirs, new_file = destination.split("/")
+            path = "/".join(dirs)
+            os.makedirs(path, exist_ok=True)
+            os.rename(file, destination)
