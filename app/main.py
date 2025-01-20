71bc2c930eacf3aecb803e92b42b3e40efdd9e2e
Yura Chornyi <chorny.yura@gmail.com>
2025-01-20 02:58:25 +0200

    Solution - refactor code

--- app/main.py
+++ app/main.py
@@ -2,20 +2,18 @@ import os
 
 
 def move_file(command: str) -> None:
-    cmd_name, file, destination = command.split()
+    command_list = command.split()
 
-    if cmd_name == "mv" and len(command.split()) == 3:
-        cmd_name, file, destination = command.split()
-        if destination.endswith("/"):
-            os.makedirs(destination, exist_ok=True)
-            os.rename(file, f"{destination}{file}")
-            os.remove(file)
-        elif "/" not in destination:
-            os.rename(file, destination)
-            os.remove(file)
-        else:
-            *dirs, new_file = destination.split("/")
-            path = "/".join(dirs)
-            os.makedirs(path, exist_ok=True)
-            os.rename(file, destination)
-            os.remove(file)
+    if len(command_list) != 3 or command_list[0] != "mv":
+        raise ValueError("Invalid command format")
+
+    _, file, path = command_list
+
+    if path.endswith("/"):
+        os.makedirs(os.path.normpath(path), exist_ok=True)
+        os.rename(file, f"{os.path.join(path + file)}")
+    elif "/" not in path:
+        os.rename(file, path)
+    else:
+        os.makedirs(os.path.normpath(os.path.dirname(path)), exist_ok=True)
+        os.rename(file, path)
