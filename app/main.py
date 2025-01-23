50367b497dd74bc776e943b6b06d5eee358c1374
Oksana <oksana.zhornyk@gmail.com>
2025-01-23 14:00:57 +0100

    Solution py-move-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    (linux_command,
+     old_file, new_file) = command.split(" ")
+    if linux_command != "mv":
+        raise f"Invalid command: {linux_command}"
+    if "/" in new_file:
+        path_parts = new_file.split("/")
+        new_file_path = "/".join(path_parts[:-1])
+    else:
+        os.rename(old_file, new_file)
+        return
+
+    if new_file_path and not os.path.exists(new_file_path):
+        try:
+            os.makedirs(new_file_path)
+        except FileExistsError:
+            pass
+
+    with open(old_file, "r") as old_file, open(new_file, "w") as new_file:
+        new_file.write(old_file.read())
+
+    os.remove(old_file.name)
