78b9b0cd59f81574b3e92bf195879e810eca19d1
Bohdan <khubashvili12@gmail.com>
2025-01-23 19:42:23 +0200

    Done!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "mv":
+        print("Invalid command")
+        return
+
+    source_file = parts[1]
+    destination_path = parts[2]
+
+    if not os.path.isfile(source_file):
+        print(f"Error: {source_file} does not exist.")
+        return
+
+    if destination_path.endswith("/"):
+        print("Error: Destination must include a file name,"
+              " not just a directory path.")
+        return
+
+    destination_dir = os.path.dirname(destination_path)
+
+    if destination_dir and not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+
+    try:
+        os.rename(source_file, destination_path)
+        print(f"Moved {source_file} to {destination_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
