d311834441ab59a92e9553da9d69481c85cf16a0
Oleksii Churbanov <lyoshatop@gmail.com>
2025-02-03 02:56:54 +0200

    implement py-move-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    source_file = parts[1]
+    dest_path = parts[2]
+
+    if os.path.dirname(source_file) == os.path.dirname(dest_path):
+        os.rename(source_file, dest_path)
+    else:
+        dest_dir = os.path.dirname(dest_path)
+        os.makedirs(dest_dir, exist_ok=True)
+
+        with open(source_file, "r") as f1:
+            text = f1.read()
+
+        with open(dest_path, "w") as f2:
+            f2.write(text)
+
+        os.remove(source_file)
