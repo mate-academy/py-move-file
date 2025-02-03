8f6a0654fbe46de21cb2a56d1cd75e73050a8c6c
RuslanTopal <baranotik15@gmail.com>
2025-02-03 09:51:53 +0000

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    data = command.split()
+
+    if len(data) != 3 or data[0] != "mv":
+        raise ValueError("Uncorrected command")
+
+    source = data[1]
+    destination = data[2]
+
+    if not os.path.isfile(source):
+        raise FileNotFoundError
+
+    if destination.endswith("/"):
+        os.makedirs(destination, exist_ok=True)
+        destination = os.path.join(destination, os.path.basename(source))
+
+    else:
+        dir_path = os.path.dirname(destination)
+        if dir_path:
+            os.makedirs(dir_path, exist_ok=True)
+
+    with open(source, "r") as f:
+        file_data = f.read()
+
+    with open(destination, "w") as f:
+        f.write(file_data)
+
+    os.remove(source)
