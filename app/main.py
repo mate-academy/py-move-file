8ce0c4305d618cfe2af409c9bee6a24333819dd4
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-02-03 16:01:08 +0200

    Complete function move file.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
-# write your code here
+from pathlib import Path
+
+
+def move_file(command_file1_file2: str) -> None:
+    data = command_file1_file2.split()
+    if len(data) != 3:
+        print("Invalid command format. Use: mv source destination")
+        return
+
+    cmd, src, dest = data
+    src_file = Path(src)
+    dest_file = Path(dest)
+    dest_file.parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        if cmd != "mv":
+            raise ValueError(f"command: -->> {cmd} <<-- Allowed only: mv")
+
+        src_file.rename(dest_file)
+
+    except FileNotFoundError:
+        print("There not exist file to move")
+    except ValueError as e_info:
+        print(e_info)
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
