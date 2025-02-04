54d77866b2a906c88217b37bb343acf7f8e7160f
Andrii <andrijgordienko67@gmail.com>
2025-02-05 01:07:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    moving_file = command.split()[1]
+    path = command.split()[-1]
+    new_path = ""
+    for i in range(len(path.split("/"))-1):
+        new_path = os.path.join(new_path, path.split("/")[i])
+        os.makedirs(new_path, exist_ok=True)
+
+    with open(moving_file, "r") as file, open(path, "w") as new_file:
+        new_file.write(file.read())
+    os.remove(moving_file)
