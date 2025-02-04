476ab6450a2eaacb21882627a63f7b7f73199c53
Andrii <andrijgordienko67@gmail.com>
2025-02-05 01:09:58 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def move_file(command: str) -> None:
     moving_file = command.split()[1]
     path = command.split()[-1]
     new_path = ""
-    for i in range(len(path.split("/"))-1):
+    for i in range(len(path.split("/")) - 1):
         new_path = os.path.join(new_path, path.split("/")[i])
         os.makedirs(new_path, exist_ok=True)
 
