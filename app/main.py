47c867abf161e02c69f02fe04057481a3a55437b
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-02-05 12:10:52 +0200

    Solution 2

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from os import path, remove, makedirs, rename
 
 
 def move_file(command: str) -> None:
-    command_list = command.split(" ")
+    command_list = command.split()
 
     if len(command_list) == 3 and command_list[0] == "mv":
         action, source_file_name, new_file_name = command_list
@@ -13,9 +13,7 @@ def move_file(command: str) -> None:
                 open(source_file_name) as source_file,
                 open(new_file_name, "w") as new_file
             ):
-                new_file.write(
-                    source_file.read()
-                )
+                new_file.write(source_file.read())
             remove(source_file_name)
         else:
             rename(source_file_name, new_file_name)
