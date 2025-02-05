96239fb23ca2d7ee78cd63b131ef7585993d2e16
Ruslan Priadko <r.priadko@ukr.net>
2025-02-05 10:17:42 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    if len(command.split()) != 3 or command.split()[0] != "mv":
+        print("invalid command")
+        return
+    cmd, source_file, destination = command.split()
+    if len(os.path.split(destination)[0]) == 0:
+        print("simple source")
+        os.rename(source_file, destination)
+        return
+    path_to_create = os.path.split(destination)[0]
+    os.makedirs(path_to_create, exist_ok=True)
+    os.rename(source_file, destination)
+    return
