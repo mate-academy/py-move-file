70f85cdb3b6ac55d61b6d6f8ece87f3d052b9362
Kateryna <kateryna.kotyh@gmail.com>
2025-02-06 13:47:58 +0200

    Updated

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ import os
 # write your code here
 def move_file(command: str):
     command_list = command.split()
-    if len(command_list) != 3:
+    if len(command_list) != 3 or command_list[0] != "mv":
         return "The command should be: mv source destination"
     source = command_list[1]
     destination = command_list[2]
     destination_list = destination.split("/")
-    destination_str = "/".join(destination_list[0:-1])
+    # destination_str = "/".join(destination_list[0:-1])
+    if len(destination_list) > 1:
+        destination_str = os.path.join(*destination_list[:-1])
     if len(destination_list) > 1:
         if not os.path.exists(destination_str):
             if destination[-1] != "/":
