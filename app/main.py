f1e6fa2f98f86cfb6c2d663d3c9730d81a024787
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-02-11 21:30:14 +0200

    refactor

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def move_file(command_file1_file2: str) -> None:
     data = command_file1_file2.split()
-    if len(data) != 3:
+    if len(data) != 3 and data[0] != "cmd":
         print("Invalid command format. Use: mv source destination")
         return
 
@@ -13,20 +13,7 @@ def move_file(command_file1_file2: str) -> None:
     dest_file.parent.mkdir(parents=True, exist_ok=True)
 
     try:
-        if cmd != "mv":
-            raise ValueError(f"command: -->> {cmd} <<-- Allowed only: mv")
-
         src_file.rename(dest_file)
 
     except FileNotFoundError:
         print("There not exist file to move")
-    except ValueError as e_info:
-        print(e_info)
-
-
-def main() -> None:
-    pass
-
-
-if __name__ == "__main__":
-    main()
