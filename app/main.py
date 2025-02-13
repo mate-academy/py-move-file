9698865009f18f204d06e7de78da288cb5c99198
Vik <viktoria.om@gmail.com>
2025-02-13 20:37:46 +0200

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -3,15 +3,19 @@ import os
 
 def move_file(command: str) -> None:
     command_list = command.split()
-    if command_list[2].find("/") != -1:
-        dir_list = command_list[2].split("/")
-        folders_path = "/".join(dir_list[:-1])
-        os.makedirs(folders_path, exist_ok=True)
-        file_name = dir_list[-1]
-        file_path = os.path.join(folders_path, file_name)
-        with (open(command_list[1], "r") as file_to_read,
-              open(file_path, "w") as file_to_write):
-            file_to_write.write(file_to_read.read())
-        os.remove(command_list[1])
-    else:
-        os.rename(command_list[1], command_list[2])
+    if len(command_list) == 3 and command_list[0] == "mv":
+        from_file = command_list[1]
+        to_file = command_list[2]
+        print(to_file)
+        if to_file.find("/") != -1:
+            dir_list = to_file.split("/")
+            folders_path = os.path.join("/".join(dir_list[:-1]))
+            os.makedirs(folders_path, exist_ok=True)
+            file_name = dir_list[-1]
+            file_path = os.path.join(folders_path, file_name)
+            with (open(from_file, "r") as file_to_read,
+                  open(file_path, "w") as file_to_write):
+                file_to_write.write(file_to_read.read())
+            os.remove(command_list[1])
+        else:
+            os.rename(from_file, to_file)
