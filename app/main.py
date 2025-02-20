0decdf31d407b073fe7bfc0eafe6d9415aab3f39
CryptoQwest <morokhovgleb@gmail.com>
2025-02-20 19:06:01 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    c_info = list(command.split())
+    file_default = c_info[1]
+    file_moved = c_info[2]
+
+    with open(file_default, "r") as file_reader:
+        reader = file_reader.read()
+
+    if "/" not in file_moved:
+        os.rename(file_default, file_moved)
+    else:
+        os.makedirs(os.path.dirname(file_moved), exist_ok=True)
+
+        with open(file_moved, "w") as filee:
+            filee.write(reader)
+
+        os.remove(file_default)
