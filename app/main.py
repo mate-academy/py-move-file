96ea0c08a4a1a8533c8c1eb7383e444a8f95f220
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-02-21 20:32:17 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ import os
 def move_file(command: str) -> None:
     commands = command.split(" ")
     if len(commands) != 3:
-        return "wrong command"
+        return None
     comm, original_file, target_file = commands
     if comm != "mv":
-        return "wrong command"
+        return None
     try:
         with open(original_file, "r") as file:
             data = file.read()
     except FileNotFoundError:
-        return "No original file in directory"
+        return None
 
     if "/" in target_file:
         *directories, target_file_name = target_file.split("/")
@@ -24,10 +24,9 @@ def move_file(command: str) -> None:
                 continue
             finally:
                 path += directory + "/"
-        with open(path + target_file_name, "w") as file:
+        with open(os.path.join(path + target_file_name), "w") as file:
             file.write(data)
     else:
         with open(target_file, "w") as file:
             file.write(data)
-
     os.remove(original_file)
