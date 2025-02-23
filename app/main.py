8cefa313d1134868e7ae5b8772de59a3672226ed
Nikita-pyth <nicksan2002@gmail.com>
2025-02-23 19:19:56 +0200

    Issues addressed

--- app/main.py
+++ app/main.py
@@ -2,24 +2,19 @@ import os
 
 
 def move_file(command: str) -> None:
-    command, from_f, to_f = command.split(" ")
-    directories = to_f.split("/")
+    parts = command.split()
+    if len(parts) != 3:
+        raise ValueError("Command must be in the format: 'move from_f to_f'")
 
-    if to_f[-1] == "/":
-        directories = directories[:-1]
-        file_name_moved = from_f.split("/")[-1]
-    else:
-        directories, file_name_moved = directories[:-1], directories[-1]
+    _, from_f, to_f = parts
+    current_dir = os.path.dirname(to_f)
 
-    current_dir = ""
-    for directory in directories:
-        current_dir = os.path.join(current_dir, directory)
-        if not os.path.exists(current_dir):
-            os.mkdir(current_dir)
+    if current_dir and not os.path.exists(current_dir):
+        os.makedirs(current_dir, exist_ok=True)
 
-    new_file_path = os.path.join(current_dir, file_name_moved)
-    with open(from_f, "r") as src_file:
-        with open(new_file_path, "w") as dst_file:
-            dst_file.write(src_file.read())
+    new_file_path = os.path.join(current_dir, os.path.basename(to_f))
+
+    with open(from_f, "r") as src_file, open(new_file_path, "w") as dst_file:
+        dst_file.write(src_file.read())
 
     os.remove(from_f)
