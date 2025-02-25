ad30a95fbbbd7f8959b543cce5f94ac0ac2cec88
HP <ngayka@gmail.com>
2025-02-25 14:20:36 +0200

    finally solution

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
 import os
+import shutil
 
 
 def move_file(command: str) -> None:
     parts = command.split()
-    if parts[0] != "mv" or len(parts) != 3:
+    if parts[0] != "mv" and len(parts) != 3:
         print("Invalid format!")
         return
 
     destination_path = parts[2]
     source_file = parts[1]
 
-    """"якщо немає директорії призначення, створюємо її """
+    """"if there is no destination directory, create it """
     dest_dir = os.path.dirname(destination_path)
     if dest_dir and not os.path.exists(dest_dir):
         os.makedirs(dest_dir, exist_ok=True)
 
     try:
-        os.rename(source_file, destination_path)
+        shutil.move(source_file, destination_path)
         print(f"File moved to {destination_path}")
     except Exception as e:
         print(f"Error: {e}")
