8bf675a826098f5cf0963983e312d0f88f21bde0
Andrii <yurkivandriy02@gmail.com>
2025-02-27 18:30:12 +0200

    fix problem

--- app/main.py
+++ app/main.py
@@ -10,14 +10,14 @@ def move_file(command: str) -> None:
 
     _, source_file, destination = part_of_command
 
-    if '/' not in destination:
+    if "/" not in destination:
         os.rename(source_file, destination)
         print(f"File renamed to {destination}")
         return
 
-    if '/' in destination:
-        path_parts = destination.split('/')
-        folder_path = '/'.join(path_parts[:-1])
+    if "/" in destination:
+        path_parts = destination.split("/")
+        folder_path = "/".join(path_parts[:-1])
 
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
@@ -28,5 +28,5 @@ def move_file(command: str) -> None:
         if not os.path.exists(source_file):
             print(f"{source_file} successfully removed after moving.")
 
-        with open(destination, 'r') as destination_object:
+        with open(destination, "r") as destination_object:
             print(destination_object.read())
