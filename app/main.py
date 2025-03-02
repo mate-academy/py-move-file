5f42a029af6ea49f486e55eb71dd6e3790a26490
PrimeGlorious <TheComboPunk@gmail.com>
2025-03-02 20:30:09 +0200

    Implement move_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+
+    if parts[0] != "mv" or len(parts) != 3:
+        return
+
+    old_file = parts[1]
+    new_file = parts[2]
+
+    path_parts = new_file.split("/")
+    if len(path_parts) > 1:
+        os.makedirs("/".join(path_parts[:-1]), exist_ok=True)
+
+    with open(old_file, "r") as old, open(new_file, "w") as new:
+        content = old.read()
+        new.write(content)
+
+    os.remove(old_file)
