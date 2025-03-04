017e5e7b0d2cfee7f0f2c28a8efc4a2c4a471533
AndyStar <andy_star@ukr.net>
2025-03-04 17:48:34 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+import os
+
+
+def move_file(move_command: str) -> None:
+
+    command_list = move_command.split()
+    file_command, file_source, file_target = command_list
+
+    if file_command != "mv":
+        print("Incorrect command")
+        return
+    with open(file_source, "r") as f_source:
+        # full_path = file_target
+        create_directory(file_target)
+        with open(file_target, "w") as f_target:
+            for line in f_source:
+                f_target.write(line)
+    os.remove(file_source)
+    try:
+        open(file_source, "r")
+    except FileNotFoundError:
+        print(f"File {file_source} successfully deleted.")
+
+
+def create_directory(full_path: str) -> None:
+    path_parts = full_path.split("/")
+    if len(path_parts) < 2:
+        return
+
+    path_seek = ""
+    for i in range(len(path_parts) - 1):
+        path_seek += path_parts[i] + "/"
+        if not os.path.isdir(path_seek):
+            os.mkdir(path_seek)
