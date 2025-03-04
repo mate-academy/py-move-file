e0120d8fc2105821b40b29e08cd125d8b373f6eb
Taras Pavlovskyi <taras.pavlovskyi@gmail.com>
2025-03-04 21:31:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,28 +1,31 @@
 import os
-from os.path import dirname
+from os.path import dirname, join
 
 
 def move_file(command: str) -> None:
     split_command = command.split(" ")
+
+    if split_command[0] != "mv" or len(split_command) != 3:
+        return
+
     source_file = split_command[1]
     destination = split_command[2]
 
-    if split_command[0] == "mv" and len(split_command) == 3:
-        if "/" not in command:
-            with (open(source_file, "r") as file_in,
-                  open(destination, "w") as file_out):
-                file_out.write(file_in.read())
-                os.remove(source_file)
+    if destination.endswith("/"):
+        destination = join(destination, os.path.basename(source_file))
 
-        else:
-            try:
-                directory = dirname(destination)
-                if not os.path.exists(directory):
-                    os.makedirs(directory)
-            except FileExistsError:
-                pass
+    if "/" not in command:
+        with (open(source_file, "r") as file_in,
+                open(destination, "w") as file_out):
+            file_out.write(file_in.read())
+            os.remove(source_file)
 
-            with (open(source_file, "r") as file_in,
-                  open(destination, "w") as file_out):
-                file_out.write(file_in.read())
-                os.remove(source_file)
+    else:
+        directory = dirname(destination)
+        if not os.path.exists(directory):
+            os.makedirs(directory, exist_ok=True)
+
+        with (open(source_file, "r") as file_in,
+              open(destination, "w") as file_out):
+            file_out.write(file_in.read())
+        os.remove(source_file)
