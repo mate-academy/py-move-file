14c5c9b4479e47e61f859bbf6d056f934995708c
Taras Pavlovskyi <taras.pavlovskyi@gmail.com>
2025-03-04 21:18:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import os
+from os.path import dirname
+
+
+def move_file(command: str) -> None:
+    split_command = command.split(" ")
+    source_file = split_command[1]
+    destination = split_command[2]
+
+    if split_command[0] == "mv" and len(split_command) == 3:
+        if "/" not in command:
+            with (open(source_file, "r") as file_in,
+                  open(destination, "w") as file_out):
+                file_out.write(file_in.read())
+                os.remove(source_file)
+
+        else:
+            try:
+                directory = dirname(destination)
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+            except FileExistsError:
+                pass
+
+            with (open(source_file, "r") as file_in,
+                  open(destination, "w") as file_out):
+                file_out.write(file_in.read())
+                os.remove(source_file)
