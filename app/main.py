f1de84655800e65f2d9ce8bd35bb7aa75dbbafd3
Dmytro Stehantsev <stegan296@gmail.com>
2025-03-04 18:16:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "mv":
+        return
+
+    _, input_file, output_file = parts
+    output_dir = os.path.dirname(output_file)
+
+    if output_dir and not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    try:
+        with (open(input_file, "r") as file_in,
+              open(output_file, "w") as file_out):
+            file_out.write(file_in.read())
+        os.remove(input_file)
+    except FileNotFoundError as e:
+        print(e)
