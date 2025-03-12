3ae4fce91f7f1165b7d147c8acac65cf3db53eab
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-03-12 21:29:23 +0200

    fixet bug1

--- app/main.py
+++ app/main.py
@@ -1,33 +1,38 @@
 import os
 
 
-def move_file(command: str) -> None:
+def create_file(path_creating_file: str,
+                name_future_file: str) -> None:
+    with (open(path_creating_file, "r") as file_read,
+          open(name_future_file, "w") as file_write):
+        file_write.write(file_read.read())
+    if os.path.exists(os.path.abspath(f"{name_future_file}")):
+        os.remove(path_creating_file)
+
+
+def move_file(command: str) -> None | str:
     result = command.split(" ")
-    if result[0] == "mv":
+    if len(result) >= 3 and result[0] == "mv":
         directory = result[-1].split("/")
         name_future_file = directory[-1]
         path_creating_file = os.path.abspath(f"{result[1]}")
         if len(directory) > 1:
-            path_for_mkdir = "/".join(directory[:len(directory) - 1])
+            path_for_mkdir = os.path.join(
+                "/".join(directory[:len(directory) - 1]))
             if not os.path.isdir(path_for_mkdir):
                 try:
                     os.makedirs(path_for_mkdir)
-                except OSError:
+                except FileNotFoundError:
                     pass
             named_directory = os.path.join(path_for_mkdir, name_future_file)
-            with (open(path_creating_file, "r") as file_read,
-                  open(named_directory, "w") as file_write):
-                file_write.write(file_read.read())
-            os.remove(path_creating_file)
+            try:
+                create_file(path_creating_file, named_directory)
+            except FileNotFoundError as error:
+                return f"{error}"
         else:
-            with (open(path_creating_file, "r") as file_read,
-                  open(name_future_file, "w") as file_write):
-                file_write.write(file_read.read())
-            os.remove(path_creating_file)
+            try:
+                create_file(path_creating_file, name_future_file)
+            except FileNotFoundError as error:
+                return f"{error}"
     else:
-        return
-# path_creating_file = os.path.abspath("file.txt")
-# with open(path_creating_file, "r") as file:
-#     file.read()
-
-# print(move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt"))
+        return "check your command"
