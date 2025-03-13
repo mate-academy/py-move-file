998dbddb0c31270fae99fe629cb96f783e0fbfdb
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2025-03-13 21:04:02 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+from os import mkdir, path, remove
+
+
+def move_file(command: str) -> None:
+    source, target = command.split()[1:]
+
+    if path.dirname(target):
+        path_folders = path.dirname(target).split("/")
+        for i in range(len(path_folders)):
+            path_temp = path.join(*path_folders[:i + 1])
+            if not path.exists(path_temp):
+                mkdir(path_temp)
+
+    if not path.basename(target):
+        target += source
+
+    with (
+        open(source, "r") as source_file,
+        open(target, "w") as target_file
+    ):
+        target_file.write(source_file.read())
+
+    remove(source)
