8e74ec912e8640ea51fb5a7e1cdd765a155bfa78
Iryna <irakirvas78@gmail.com>
2025-03-14 01:17:11 +0300

    Implement py-move-file

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ import os
 
 
 def move_file(command: str) -> None:
-    ls = command.split()
-    if len(ls) == 3 and ls[0] == "mv":
-        source_file = ls[1]
-        destination_path = ls[2]
+    split_list = command.split()
+    if len(split_list) == 3 and split_list[0] == "mv":
+        source_file = split_list[1]
+        destination_path = split_list[2]
 
         if not os.path.exists(source_file):
             return
@@ -13,16 +13,18 @@ def move_file(command: str) -> None:
         try:
             if "/" not in destination_path:
                 os.replace(source_file, destination_path)
+                os.remove(source_file)
             else:
                 new_list = destination_path.split("/")
                 file_name = new_list[-1]
                 directory_path = "/".join(new_list[:-1])
 
                 if not os.path.exists(directory_path):
-                    os.makedirs(directory_path)
+                    os.makedirs(directory_path, exist_ok=True)
 
                 destination_file = os.path.join(directory_path, file_name)
                 os.replace(source_file, destination_file)
+                os.remove(source_file)
 
         except Exception as e:
             print(f"An error occurred {e}")
