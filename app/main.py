e9473c0d79d378a2579f22ec3fea8af36f62b11d
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-03-26 17:19:30 +0100

    del unnecessary commands

--- app/main.py
+++ app/main.py
@@ -2,15 +2,14 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) != 3:
+    command_parts = command.split()
+    mv, file_name, destination = command_parts
+
+    if len(command_parts) != 3:
         raise ValueError("Command should contains 3 elements "
                          "separated by a space")
-    elif command.split()[0] != "mv":
+    elif mv != "mv":
         raise ValueError("First element should be \'mv\'")
-
-    command = command.split()
-    file_name = command[1]
-    destination = command[2]
 
     if "/" not in destination:
         os.rename(file_name, destination)
@@ -18,7 +17,7 @@ def move_file(command: str) -> None:
         directory = os.path.dirname(destination)
         os.makedirs(os.path.join(os.getcwd(), directory), exist_ok=True)
 
-        new_file_path = os.path.join(os.getcwd(), destination)
+        new_file_path = destination
 
         with open(file_name, "r") as old_file:
             with open(new_file_path, "w") as new_file:
