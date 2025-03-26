65d5de6de18608f5d1ecc77191f7499f45122c58
Danylo Melnychuk <danyakid2005@gmail.com>
2025-03-26 19:35:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def move_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3 or parts[0] != "mv":
-        raise ValueError("Invalid command format. Use: 'mv source destination'")
+        raise ValueError("Invalid command format. Use: mv source destination")
 
     source_path, destination_path = parts[1], parts[2]
 
