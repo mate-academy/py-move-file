36c92e53516867cfcf71d4b9e0fbc9eaf24f71e0
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-03-26 17:11:07 +0100

    fix creating of path

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ import os
 
 
 def move_file(command: str) -> None:
+    if len(command.split()) != 3:
+        raise ValueError("Command should contains 3 elements separated by a space")
+    elif command.split()[0] != "mv":
+        raise ValueError("First element should be \'mv\'")
+
     command = command.split()
     file_name = command[1]
     destination = command[2]
@@ -10,9 +15,11 @@ def move_file(command: str) -> None:
         os.rename(file_name, destination)
     else:
         directory = os.path.dirname(destination)
-        os.makedirs(directory, exist_ok=True)
+        os.makedirs(os.path.join(os.getcwd(), directory), exist_ok=True)
+
+        new_file_path = os.path.join(os.getcwd(), destination)
 
         with open(file_name, "r") as old_file:
-            with open(destination, "w") as new_file:
+            with open(new_file_path, "w") as new_file:
                 new_file.write(old_file.read())
         os.remove(file_name)
