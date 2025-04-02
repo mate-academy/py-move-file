6ae22885fbf12a60428234f6d623066677f8d677
Alexandr <oleksandrglushen2005@gmail.com>
2025-04-02 20:29:57 +0300

    broken code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+def move_file(command: str) -> None:
+    my_split = command.split()
+    if my_split[0] != "mv":
+        return
+    if len(my_split) != 3:
+        return
+    if my_split[2].endswith("/"):
+        os.makedirs(my_split[2], exist_ok=True)
+        destination = my_split[2] + os.path.basename(my_split[1])
+        os.rename(my_split[1], destination)
