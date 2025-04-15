9eef43d476751a9ce57cff649b1b96fd12839599
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-04-15 21:11:52 +0200

    fix variables names

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ import os
 def move_file(command: str) -> None:
     command_list = command.split(" ")
     if len(command_list) == 3 and command_list[0] == "mv":
-        move, cur_file, new_file_path = command_list
+        move, current_file, destination_path = command_list
 
-        directory = os.path.dirname(new_file_path)
+        directory = os.path.dirname(destination_path)
 
         if directory:
             os.makedirs(directory, exist_ok=True)
 
-        with open(cur_file, "r") as file_read, \
-                open(new_file_path, "w") as file_write:
+        with open(current_file, "r") as file_read, \
+                open(destination_path, "w") as file_write:
             file_write.write(file_read.read())
-            os.remove(cur_file)
+            os.remove(current_file)
