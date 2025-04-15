988f84446ca5cb5dbef83dadc788782bcab73fbe
LassisterV16 <lassisterv16@gmail.com>
2025-04-15 21:43:20 +0200

    fixed splits and deleted duplicated code

--- app/main.py
+++ app/main.py
@@ -10,19 +10,14 @@ def move_file(command: str) -> None:
     if mv != "mv":
         return
 
-    second_file = second_file.rsplit("/")
-    if len(second_file) == 1:
-        with (open(first_file, "r") as file_in,
-              open("".join(second_file), "w") as file_out):
-            file_out.write(file_in.read())
-        os.remove(first_file)
-        return
+    dir_path = os.path.dirname(second_file)
+    file_name = os.path.basename(second_file)
+    file_path = os.path.join(dir_path, file_name) if dir_path else file_name
 
-    dir_path = "/".join(second_file[:-1])
-    file_path = os.path.join(dir_path, second_file[-1])
-    if not os.path.exists(dir_path):
+    if dir_path and not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
     with open(first_file, "r") as file_in, open(file_path, "w") as file_out:
         file_out.write(file_in.read())
+
     os.remove(first_file)
