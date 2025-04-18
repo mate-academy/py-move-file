6a0697033356936b0d45bee90fd46f0ecb28fd23
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-04-18 15:13:12 +0100

    v3

--- app/main.py
+++ app/main.py
@@ -5,8 +5,10 @@ def move_file(command: str) -> None:
     if command[:2] == "mv":
         names = command.split()
         if len(names) == 3:
+            # inside len(names) == 3
             if "/" in names[2]:
 
                 os.makedirs(os.path.dirname(names[2]), exist_ok=True)
 
+            # still inside len(names) == 3
             os.rename(names[1], names[2])
