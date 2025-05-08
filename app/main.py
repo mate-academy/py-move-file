ed6711a040ed2e8c1eb4bdaf7cf17984f4fbb79b
Vlad <7nomer7@gmail.com>
2025-05-08 18:04:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,11 +8,14 @@ def move_file(command: str) -> None:
         file_path = "/".join(command_list[-1].split("/")[:-1])
         os.makedirs(file_path, exist_ok=True)
         destination_file = f"{file_path}/{command_list[-1].split("/")[-1]}"
-        with open(
-                destination_file, "w") as f, open(
-                first_file, "r") as src_file:
-            f.write(src_file.read())
+        try:
+            with open(
+                    destination_file, "w") as f, open(
+                    first_file, "r") as src_file:
+                f.write(src_file.read())
             os.remove(first_file)
+        except FileNotFoundError:
+            print(f"Error: File {first_file} not found.")
     else:
         second_file = command_list[2]
         os.rename(first_file, second_file)
