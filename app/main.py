c92cb5d329754391afd5c66463256b4b27b7c859
Vlad <7nomer7@gmail.com>
2025-05-16 21:22:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def move_file(command: str) -> None:
     if "/" in command:
         file_path = "/".join(command_list[-1].split("/")[:-1])
         os.makedirs(file_path, exist_ok=True)
-        with (open(f"{file_path}/{command_list[-1].split("/")[-1]}", "w") as f,
+        with (open(f"{file_path}/{command_list[-1].split('/')[-1]}", "w") as f,
               open(first_file, "r") as src_file):
             f.write(src_file.read())
         os.remove(first_file)
