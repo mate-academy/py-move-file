fa813237938ed4a2e0f6e4bc0ce4ec89a80bf3a0
Serhii Kermas <serhii.kermas@gmail.com>
2025-05-29 15:04:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,13 +6,15 @@ def move_file(command: str) -> None:
     src_file = command.split()[1]
     destination = command.split()[2]
     cmd = command.split()[0]
-    des_path = os.path.dirname(destination)
 
     if (cmd == "mv" and os.path.exists(src_file)):
-        if "/" not in destination:
+        if destination == os.path.basename(destination):
             os.rename(src_file, destination)
         else:
-            os.makedirs(des_path, exist_ok=True)
+            os.makedirs(os.path.dirname(destination), exist_ok=True)
             if not os.path.exists(destination):
-                shutil.copy(src_file, destination)
-                os.remove(src_file)
+                try:
+                    shutil.copy(src_file, destination)
+                    os.remove(src_file)
+                except Exception as e:
+                    print(f"Unexpected error: {e}")
