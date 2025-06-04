0745d291517909dc8eea1881676fc42179e8bbbd
Yevhen Nastoiashchyi <Nastoyashiy@i.ua>
2025-06-04 16:56:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ def move_file(command: str) -> None:
         return
     if destination.endswith("/"):
         destination = os.path.join(destination, os.path.basename(source))
+    if os.path.exists(destination):
+        return
     dir_path = os.path.dirname(destination)
     if dir_path and not os.path.exists(dir_path):
         os.makedirs(dir_path)
