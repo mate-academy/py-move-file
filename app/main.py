741f9b571cf93c9eeb2941daf67c5fc05c4ad48a
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-07-06 14:28:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,34 +5,24 @@ def move_file(command: str) -> None:
     commands = command.split()
     if len(commands) < 3:
         return None
-    move_command, input_file_name, output_file_name_with_path = commands
+    move_command, input_file_name, output_file_path_with_name = commands
     if move_command != "mv":
         return None
 
-    file_path_list = output_file_name_with_path.split("/")
+    file_path_list = output_file_path_with_name.split("/")
     output_file_name = file_path_list[-1]
-    universal_output_file_path = output_file_name
 
     if len(file_path_list) > 1:
-        universal_output_file_path = file_path_list[0]
-        for i in range(1, len(file_path_list) - 1):
-            directory = file_path_list[i]
-            universal_output_file_path = os.path.join(
-                universal_output_file_path, directory
-            )
-
-        os.makedirs(universal_output_file_path, exist_ok=True)
-        universal_output_file_path = os.path.join(
-            universal_output_file_path, output_file_name
-        )
+        output_file_path = output_file_path_with_name.rstrip(output_file_name)
+        os.makedirs(output_file_path, exist_ok=True)
 
     try:
         with open(input_file_name, "r") as file_in, open(
-            universal_output_file_path, "w"
+            output_file_path_with_name, "w"
         ) as file_out:
             content = file_in.read()
             file_out.write(content)
     except FileNotFoundError:
-        print("Enter file not found!")
+        print("Input file not found!")
     else:
         os.remove(input_file_name)
