1d17eb21d5f363cfd7fd982dd91840302f21056e
helix09825 <maksilczuk@gmail.com>
2025-07-07 19:18:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,29 @@ import os
 
 
 def move_file(command: str) -> None:
-    if not command.startswith("mv"):
+    if not command.startswith("mv "):
         return
     splitted_command = command.split()
     if len(splitted_command) != 3:
         return
+
     source = splitted_command[1]
     destination = splitted_command[2]
+
+    if not os.path.isfile(source):
+        raise FileNotFoundError(f"Source file '{source}' does not exist.")
+
+    abs_source = os.path.abspath(source)
+    if destination.endswith("/"):
+        abs_destination = os.path.abspath(
+            os.path.join(destination, os.path.basename(source)))
+    else:
+        abs_destination = os.path.abspath(destination)
+
+    if abs_source == abs_destination:
+        raise ValueError(
+            "Source and destination paths are the same. "
+            "Move operation aborted.")
 
     if destination.endswith("/"):
         os.makedirs(destination, exist_ok=True)
