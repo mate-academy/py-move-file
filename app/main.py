3af95b2e43d17c3712df4a8432ac76df64b73f15
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2025-07-09 16:24:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-# app/move_file.py
 import os
 import shutil
 
-def move_file(command: str):
+
+def move_file(command: str) -> None:
     parts = command.strip().split()
     if parts[0] != "mv" or len(parts) != 3:
         raise ValueError("Invalid command format")
@@ -10,12 +10,12 @@ def move_file(command: str):
     src = parts[1]
     dst = parts[2]
 
-    # Якщо шлях закінчується на / — це директорія
+    # Якщо шлях закінчується на слеш — це директорія
     if dst.endswith("/"):
         os.makedirs(dst, exist_ok=True)
         dst = os.path.join(dst, os.path.basename(src))
     else:
-        # Створити всі проміжні директорії
+        # Якщо в шляху є проміжні директорії — створити їх
         dir_path = os.path.dirname(dst)
         if dir_path:
             os.makedirs(dir_path, exist_ok=True)
