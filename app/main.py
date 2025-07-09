17cd73bfa927ce63598e329beaa870aba176e27f
Mykola Totsevych <totsevich02@gmail.com>
2025-07-10 00:54:47 +0300

    Fixed issues

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ import os
 def move_file(command: str) -> None:
     parts = command.strip().split()
     if len(parts) != 3 or parts[0] != "mv":
-        raise ValueError("""Invalid command:
-        use 'mv source_path destination_path'""")
+        raise ValueError("""Invalid command: use
+        'mv source_path destination_path'""")
 
     source_path, destination_path = parts[1], parts[2]
 
@@ -16,7 +16,6 @@ def move_file(command: str) -> None:
         )
 
     destination_dir = os.path.dirname(destination_path)
-
     if destination_dir:
         if (
                 os.path.exists(destination_dir)
@@ -24,13 +23,12 @@ def move_file(command: str) -> None:
         ):
             raise NotADirectoryError(f"{destination_dir} exists "
                                      f"and is not a directory")
-
         os.makedirs(destination_dir, exist_ok=True)
 
-    with open(source_path, "r") as src_file:
+    with open(source_path, "rb") as src_file:
         content = src_file.read()
 
-    with open(destination_path, "w") as dst_file:
+    with open(destination_path, "wb") as dst_file:
         dst_file.write(content)
 
     os.remove(source_path)
