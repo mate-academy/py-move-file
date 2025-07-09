4bb02bab618fd66419d3d676e3c9ae3bbe642f06
Mykola Totsevych <totsevich02@gmail.com>
2025-07-10 00:22:47 +0300

    Added solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.strip().split()
+    if len(parts) != 3 or parts[0] != "mv":
+        raise ValueError("""Invalid command:
+        use 'mv source_path destination_path'""")
+
+    source_path, destination_path = parts[1], parts[2]
+
+    if destination_path.endswith("/"):
+        destination_path = os.path.join(
+            destination_path,
+            os.path.basename(source_path)
+        )
+
+    destination_dir = os.path.dirname(destination_path)
+
+    if destination_dir:
+        if (
+                os.path.exists(destination_dir)
+                and not os.path.isdir(destination_dir)
+        ):
+            raise NotADirectoryError(f"{destination_dir} exists "
+                                     f"and is not a directory")
+
+        os.makedirs(destination_dir, exist_ok=True)
+
+    with open(source_path, "r") as src_file:
+        content = src_file.read()
+
+    with open(destination_path, "w") as dst_file:
+        dst_file.write(content)
+
+    os.remove(source_path)
