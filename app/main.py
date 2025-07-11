386e4eaddfa77d1a5110e4c80ba939ffd743bcd1
Bohdan Yagginov <bhders62@gmail.com>
2025-07-11 10:54:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    try:
+        action, src_file, dest_file = command.split()
+    except ValueError:
+        return
+
+    if src_file == dest_file or action != "mv":
+        return
+
+    try:
+        with open(src_file, "r") as f:
+            data = f.read()
+    except FileNotFoundError:
+        return
+    os.remove(src_file)
+
+    path = "/".join(dest_file.split("/")[:-1])
+    try:
+        # raise FileNotFoundError if path is clear
+        # (additional dictionary is not needed)
+        os.makedirs(path, exist_ok=True)
+    except FileNotFoundError:
+        pass
+
+    with open(dest_file, "w") as f:
+        f.write(data)
