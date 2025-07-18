562e1c7d084c2251acaf29830ca6bd33df484933
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-07-18 17:15:16 +0700

    updated move_file if it'll contain whitespace separated path

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
 import os
 
 
-def move_file(command: str = "mv file.txt some_dir/new_file.txt") -> None:
-    file, dest = command.split()[1:]
-    orig_dir = os.getcwd()
+def move_file(command: str) -> None:
+    file, *dest = command.split()[1:]
+    original_directory = os.getcwd()
     if not os.path.exists(file):
         with open(file, "w") as file_:
             file_.write("")
     with open(file, "r") as inp_file:
         orig_file = inp_file.read()
     way = None
-    to_file = dest
-    if "/" in dest:
-        dest = dest.split("/")
+    to_file = dest[-1]
+    if len(dest) == 1 and "/" in dest[0]:
+        dest = dest[0].split("/")
+        to_file = dest[-1]
+        way = dest[:-1]
+    else:
         to_file = dest[-1]
         way = dest[:-1]
     if way:
@@ -24,8 +27,8 @@ def move_file(command: str = "mv file.txt some_dir/new_file.txt") -> None:
                 os.chdir(folder)
     with open(to_file, "w") as new_file:
         new_file.write(orig_file)
-    os.chdir(orig_dir)
-    os.remove(f"./{file}")
+    os.chdir(original_directory)
+    os.remove(f"{file}")
 
 
 if __name__ == "__main__":
