6429148bdb0536cbc832c06867a4aa3a6bb44b5c
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-07-18 20:45:45 +0700

    Used Errors and os.makedir

--- app/main.py
+++ app/main.py
@@ -1,34 +1,27 @@
 import os
+from shutil import copy
 
 
 def move_file(command: str) -> None:
-    file, *dest = command.split()[1:]
-    original_directory = os.getcwd()
-    if not os.path.exists(file):
-        with open(file, "w") as file_:
-            file_.write("")
-    with open(file, "r") as inp_file:
-        orig_file = inp_file.read()
-    way = None
-    to_file = dest[-1]
-    if len(dest) == 1 and "/" in dest[0]:
-        dest = dest[0].split("/")
-        to_file = dest[-1]
-        way = dest[:-1]
-    else:
-        to_file = dest[-1]
-        way = dest[:-1]
-    if way:
-        for folder in way:
-            if os.path.exists(folder):
-                os.chdir(folder)
-            else:
-                os.mkdir(folder)
-                os.chdir(folder)
-    with open(to_file, "w") as new_file:
-        new_file.write(orig_file)
-    os.chdir(original_directory)
-    os.remove(f"{file}")
+    starting_dir = os.getcwd()
+    cmnd = command.split()
+    if len(cmnd) < 3 or cmnd[1].endswith("/"):
+        raise ValueError("Wrong format of the command. "
+                         "It should be like "
+                         "'mv file.txt some_dir/new_file.txt'")
+    source, destination = cmnd[1], cmnd[2]
+    if not os.path.isfile(source):
+        raise ValueError("The source file doesn't exist!")
+    dest_path, dest_file = os.path.split(destination)
+    if dest_path:
+        os.makedirs(dest_path, exist_ok=True)
+    try:
+        copy(source, destination)
+    except Exception as e:
+        print(e)
+    os.chdir(starting_dir)
+    os.system("rm -v !('main.py')")
+    os.remove(source)
 
 
 if __name__ == "__main__":
