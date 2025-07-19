1884fbf0e13700011445255102e3f5ae6a2bfe34
Vasyl <jomajok7777@gmail.com>
2025-07-19 03:21:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_words = command.strip().split()
+
+    if command_words[0] == "mv":
+        try:
+            os.rename(command_words[1], command_words[2])
+        except FileNotFoundError:
+            os.makedirs("/".join(command_words[2].split("/"))[:-1])
+            os.rename(command_words[1], command_words[2])
