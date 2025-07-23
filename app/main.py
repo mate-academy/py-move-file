260a699c372bc98a74d5908e9fabb634da00c204
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-07-23 21:52:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_lst = command.split()
+    if len(command_lst) != 3:
+        return
+    mv, old_file, directory = command_lst
+
+    if mv != "mv":
+        return
+    if old_file == directory:
+        return
+
+    if "/" not in directory:
+        copy_file(old_file, directory)
+    else:
+        directory_list = directory.split("/")
+        for i in range(1, len(directory_list)):
+            try:
+                os.mkdir("/".join(directory_list[:i]))
+            except FileExistsError:
+                pass
+        copy_file(old_file, directory)
+
+
+def copy_file(old_file: str, new_file: str) -> None:
+    with open(old_file, "r") as old , open(new_file, "w") as new:
+        new.write(old.read())
+    os.remove(old_file)
