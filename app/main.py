36a5b0f252f458f64a89a234540de4f764dcbb5a
Oleksandr Skoromnov <solalex005@gmail.com>
2025-08-05 02:36:34 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -6,17 +6,13 @@ def move_file(command: str) -> None:
     if len(content) < 3:
         return "Not enough params"
     mv, old_path, new_path = content
-    if mv != "mv" or old_path == new_path:
+    if mv != "mv":
+        return "Wrong command"
+    if old_path == new_path:
         return
     directories = new_path.split("/")[:-1]
-    os.makedirs("/".join(directories), exist_ok=True)
-    # for i in range(len(directories)):
-    #     if not os.path.exists("/".join(directories[:i+1])):
-    #
+    if directories:
+        os.makedirs("/".join(directories), exist_ok=True)
     with open(old_path, "r") as f1, open(new_path, "w") as f2:
         f2.write(f1.read())
     os.remove(old_path)
-
-
-if __name__ == '__main__':
-    move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt")
