249094b164977f8749e07a14d302d83e3be68b1f
Maksym Pakula <gargantadoto@gmail.com>
2025-08-06 00:03:29 +0300

    added solution for move file task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    commands = command.split()
+    if commands[0] != "mv":
+        print(f"Error: '{commands[0]}' should be 'mv'")
+        return
+    source_file = commands[1]
+    if not os.path.exists(source_file):
+        print(f"Error: file with name '{source_file}' does not exist")
+    path = "/".join(commands[2].split("/")[0:-1])
+    try:
+        os.makedirs(path)
+        print(f"Directory '{path}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{path}' already exists.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    with open(commands[2], "w") as file, open(source_file, "r") as source:
+        file.write(source.read())
+    os.remove(source_file)
