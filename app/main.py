478453f9a11182297e9b4948b5f56e8ba51bf11e
Konstantin S. <kostaho16@gmail.com>
2025-08-09 00:29:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,19 +5,13 @@ def move_file(command: str) -> None:
     command_line = command.split()
     if len(command_line) != 3 or command_line[0] != "mv":
         return
-    source_file = command_line[1]
-    command_line = command_line[2].split("/")
-    destination_file = command_line.pop(-1)
-    destination_path = ""
-    for command in command_line:
-        try:
-            destination_path += command
-            os.mkdir(destination_path)
-        except FileExistsError:
-            continue
-        finally:
-            destination_path += "/"
-    destination_path += destination_file
+    _, source_file, destination = command_line
+    path, destination_file = os.path.split(destination)
+    try:
+        os.makedirs(path, exist_ok=True)
+    except FileNotFoundError:
+        pass
+    destination_path = os.path.join(path, destination_file)
     try:
         with open(source_file, "r") as source, open(
             destination_path, "w"
