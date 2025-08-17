11ead206fe85865edc8532d84e061cfce78e3998
Hunach <sasha2001sek@gmail.com>
2025-08-17 17:19:33 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,26 +1,39 @@
 import os
-import shutil
 
 
 def move_file(command: str) -> None:
-
     parts = command.split()
-    source = parts[1]
-    destination = parts[2]
+    if len(parts) != 3 or parts[0] != "mv":
+        raise ValueError("Невірний формат команди. "
+                         "Використовуйте 'mv <source> <destination>'.")
+
+    _, source, destination = parts
+
+    if not os.path.exists(source):
+        raise FileNotFoundError(f"Файл '{source}' не знайдено.")
+    if os.path.isdir(source):
+        raise IsADirectoryError(f"Джерело '{source}' є "
+                                "каталогом, а не файлом.")
 
     if destination.endswith("/"):
         final_destination = os.path.join(destination, os.path.basename(source))
-        os.makedirs(destination, exist_ok=True)
     else:
-
         final_destination = destination
-        destination_dir = os.path.dirname(final_destination)
-        if destination_dir:
-            os.makedirs(destination_dir, exist_ok=True)
 
-    try:
-        shutil.copyfile(source, final_destination)
-        os.remove(source)
-        print(f"Файл '{source}' успішно переміщено в '{final_destination}'")
-    except FileNotFoundError:
-        print(f"Помилка: Файл '{source}' не знайдено.")
+    destination_dir = os.path.dirname(final_destination)
+    if destination_dir:
+        path_parts = []
+        while destination_dir and not os.path.exists(destination_dir):
+            path_parts.append(os.path.basename(destination_dir))
+            destination_dir = os.path.dirname(destination_dir)
+
+        for part in reversed(path_parts):
+            destination_dir = os.path.join(destination_dir, part)
+            os.mkdir(destination_dir)
+
+    with open(source, "r") as infile:
+        content = infile.read()
+    with open(final_destination, "w") as outfile:
+        outfile.write(content)
+
+    os.remove(source)
