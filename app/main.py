b1216baa89434c2453f7bfcb1957751d3c4f5dfe
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-08-29 19:09:30 -0300

    resolution

--- app/main.py
+++ app/main.py
@@ -1,25 +1,19 @@
 import os
+import shutil
+from pathlib import Path
 
 
 def move_file(command: str) -> None:
-    commands = command.split()
-    cmd_size = len(commands) == 3
-    cmd1 = commands[0] == "mv"
-    cmd2 = commands[1][len(commands[1]) - 4:] == ".txt"
-    cmd3 = commands[2][len(commands[2]) - 4:] == ".txt"
+    command = command.strip().split(" ")
+    if (command[0] == "mv"):
+        file_origin = Path(str(command[1]))
+        if file_origin.is_file():
+            name_dir_2 = os.path.dirname(command[2])
+            parts_dir_2 = Path(name_dir_2).parts
+            atual = ""
+            for path in parts_dir_2:
+                atual = os.path.join(atual, path)
+                if not os.path.exists(atual):
+                    os.mkdir(atual)
 
-    if (cmd_size and cmd1 and cmd2 and cmd3):
-        dir_move_file = commands[2].split("/")
-        data_file = None
-        try:
-            with open(commands[1], "r") as file:
-                data_file = file.read()
-        except FileNotFoundError:
-            raise FileNotFoundError
-        if len(dir_move_file) > 1:
-            diretorio = "/".join(dir_move_file[:-1])
-            if not os.path.exists(diretorio):
-                os.makedirs(diretorio)
-        with open(commands[2], "w") as move_file:
-            move_file.write(data_file)
-            os.remove(commands[1])
+            shutil.move(command[1], command[2])
