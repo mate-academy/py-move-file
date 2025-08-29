cb63a1ff9df8c20730cb768c6b4f1860aa43f5d0
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-08-29 17:57:10 -0300

    resolution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    commands = command.split()
+    cmd_size = len(commands) == 3
+    cmd1 = commands[0] == "mv"
+    cmd2 = commands[1][len(commands[1]) - 4:] == ".txt"
+    cmd3 = commands[2][len(commands[2]) - 4:] == ".txt"
+
+    if (cmd_size and cmd1 and cmd2 and cmd3):
+        dir_move_file = commands[2].split("/")
+        data_file = None
+        try:
+            with open(commands[1], "r") as file:
+                data_file = file.read()
+        except FileNotFoundError:
+            raise FileNotFoundError
+        if len(dir_move_file) > 1:
+            diretorio = "/".join(dir_move_file[:-1])
+            if not os.path.exists(diretorio):
+                os.makedirs(diretorio)
+        with open(commands[2], "w") as move_file:
+            move_file.write(data_file)
+            os.remove(commands[1])
+
+
