477165c3b1d702ce0c0474e1c8c3cca8c2197a21
Oleksii-Hrechukha <Oleksii_Hrechukha@outlook.com>
2025-08-30 20:59:43 +0300

    SolutionNew

--- app/main.py
+++ app/main.py
@@ -3,24 +3,17 @@ import os.path
 
 def move_file(command: str) -> None:
 
-    command_list = []
-    path = ""
-    for com in command.split():
-        command_list.extend(com.split("/"))
-
+    command_list = command.split()
     if command_list[0] != "mv":
         return
+    if len(command_list) != 3:
+        return
+
     origin_file = command_list[1]
-    moving_file = command_list[-1]
-
-    for directory in command_list[2:-1]:
-        if not directory:
-            continue
-        path += f"{directory}/"
-        if not os.path.exists(path):
-            os.mkdir(path)
-
+    moving_file = command_list[2]
+    if os.path.dirname(moving_file) != "":
+        os.makedirs(os.path.dirname(moving_file), exist_ok=True)
     with (open(origin_file) as file_or,
-          open(f"{path}{moving_file}", "w") as file_mv):
+          open(f"{moving_file}", "w") as file_mv):
         file_mv.write(file_or.read())
     os.remove(origin_file)
