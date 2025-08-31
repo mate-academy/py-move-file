f3405f9ba1afc736b6919b20faaf478c22150ec2
Oleksii-Hrechukha <Oleksii_Hrechukha@outlook.com>
2025-08-31 14:58:15 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -9,18 +9,21 @@ def move_file(command: str) -> None:
     if len(command_list) != 3:
         return
     origin_file = command_list[1]
+    path = os.path.normpath(os.path.dirname(command_list[2]))
     destination = ""
 
-    for direct in os.path.dirname(command_list[2]).split("/"):
+    for direct in path.split(os.path.sep):
         if direct == "":
             continue
-        destination += os.path.join(f"{direct}/")
+        destination = os.path.join(destination,
+                                   direct).replace(os.path.sep, "/")
+
         if not os.path.exists(destination):
             os.mkdir(destination)
 
-    with (open(origin_file) as file_or,
+    with (open(origin_file, "rb") as file_or,
           open(os.path.join(destination,
-                            os.path.basename(command_list[2])), "w")
+                            os.path.basename(command_list[2])), "wb")
           as file_mv):
         file_mv.write(file_or.read())
     os.remove(origin_file)
