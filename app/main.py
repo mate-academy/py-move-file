f1e386df28f7e16e5e6210bf624d7d789a39c8d3
Sabina Gamidova <sabina.gamidova.dev@gmail.com>
2025-09-04 20:31:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_parts = command.split()
+    if len(command_parts) == 3:
+        operation, filename, destination_to_new_file = command_parts
+        if (
+                operation == "mv"
+                and os.path.exists(filename)
+        ):
+            with open(filename, "r") as f:
+                file_content = f.read()
+
+            os.remove(filename)
+
+            folder = os.path.dirname(destination_to_new_file)
+
+            if folder:
+                os.makedirs(folder, exist_ok=True)
+
+            with open(destination_to_new_file, "w") as f:
+                f.write(file_content)
