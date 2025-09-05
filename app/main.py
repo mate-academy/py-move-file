47a7124858df19b34f4c36e7b924d880d8af9a9e
Tanya <tanakutova74@gmail.com>
2025-09-05 10:07:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ import os.path
 
 def move_file(command: str) -> None:
     commands = command.split()
+    if len(commands) != 3 or commands[0] != "mv":
+        return None
     start_file = commands[1]
-    if commands[2][-1] == "/":
+    if os.path.isdir(commands[2]):
         os.makedirs(commands[2], exist_ok=True)
         file_path = os.path.join(commands[2], os.path.basename(start_file))
         with (open(start_file, "r") as file_in,
@@ -12,7 +14,7 @@ def move_file(command: str) -> None:
             content = file_in.read()
             file_out.write(content)
         os.remove(start_file)
-    elif "/" in commands[2]:
+    elif os.path.dirname(commands[2]):
         file_path = commands[2]
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
