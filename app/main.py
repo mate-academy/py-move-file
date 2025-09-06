93c7c15e11d15067e2f09e2ceb4760a7f03818a3
Yurii <yurij.kenu@gmail.com>
2025-09-06 20:28:44 +0200

    move-file: renamed variables

--- app/main.py
+++ app/main.py
@@ -6,23 +6,23 @@ def move_file(command: str) -> None:
     if len(parts) != 3 or parts[0] != "mv":
         raise ValueError()
 
-    func, source, target = parts
+    command_name, source, destination = parts
 
     source_path = Path(source)
-    if target.endswith("/"):
-        target += source_path.name
-    target_path = Path(target)
+    if destination.endswith("/"):
+        destination += source_path.name
+    destination_path = Path(destination)
 
-    if source_path.parent == target_path.parent:
-        source_path.rename(target_path)
+    if source_path.parent == destination_path.parent:
+        source_path.rename(destination_path)
         return
 
     with open(source, "r") as f:
         content = f.read()
 
-    target_path.parent.mkdir(parents=True, exist_ok=True)
+    destination_path.parent.mkdir(parents=True, exist_ok=True)
 
-    with open(target_path, "w") as f:
+    with open(destination_path, "w") as f:
         f.write(content)
 
     source_path.unlink()
