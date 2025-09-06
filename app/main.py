9edbd69ec1ed71ad6290c3af96ec25d0493975fb
Yurii <yurij.kenu@gmail.com>
2025-09-06 19:59:42 +0200

    move-file: implemented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+from pathlib import Path
+
+
+def move_file(command: str) -> None:
+    func, source, target = command.split()
+
+    source_path = Path(source)
+    target_path = Path(target)
+
+    if source_path.parent == target_path.parent:
+        source_path.rename(target_path)
+        return
+
+    with open(source, "r") as f:
+        content = f.read()
+
+    source_path.unlink()
+
+    target_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(target_path, "w") as f:
+        f.write(content)
