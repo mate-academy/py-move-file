93fc233ebe95f83ceb0e336a0116001350259105
Yurii <yurij.kenu@gmail.com>
2025-09-06 20:16:09 +0200

    move-file: moved deleting file, added validation of command

--- app/main.py
+++ app/main.py
@@ -2,9 +2,15 @@ from pathlib import Path
 
 
 def move_file(command: str) -> None:
-    func, source, target = command.split()
+    parts = command.strip().split()
+    if len(parts) != 3 or parts[0] != "mv":
+        raise ValueError()
+
+    func, source, target = parts
 
     source_path = Path(source)
+    if target.endswith("/"):
+        target += source_path.name
     target_path = Path(target)
 
     if source_path.parent == target_path.parent:
@@ -14,9 +20,9 @@ def move_file(command: str) -> None:
     with open(source, "r") as f:
         content = f.read()
 
-    source_path.unlink()
-
     target_path.parent.mkdir(parents=True, exist_ok=True)
 
     with open(target_path, "w") as f:
         f.write(content)
+
+    source_path.unlink()
