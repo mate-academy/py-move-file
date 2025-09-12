1283c21e03ec47ddbc06940006b393f5602e7aa2
Partarum <email@partarum.de>
2025-09-12 15:04:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+from pathlib import Path
+import os
+
+
+def move_file(command: str) -> None:
+    command_parts = command.split()
+
+    if len(command_parts) != 3:
+        return
+
+    if command_parts[0] == "mv":
+
+        if command_parts[1] == command_parts[2]:
+            return
+
+        source_file_name = command_parts[1]
+        destination_file_name = command_parts[2]
+
+        if not Path(source_file_name).is_file():
+            return
+
+        if not Path(destination_file_name).parent.is_dir():
+            os.makedirs(Path(destination_file_name).parent)
+
+        with (open(source_file_name, "r") as source_file_object,
+              open(destination_file_name, "w") as destination_file_object):
+            destination_file_object.write(source_file_object.read())
+
+        os.remove(source_file_name)
