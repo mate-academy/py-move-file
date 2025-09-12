d1783ccaf5c9e0ecb5b1e74a201e757d06ff6dee
Partarum <email@partarum.de>
2025-09-12 17:06:32 +0200

    Solution with dir_sep

--- app/main.py
+++ app/main.py
@@ -14,11 +14,10 @@ def move_file(command: str) -> None:
         if source_file_name == destination_file_name:
             return
 
-        if not Path(source_file_name).is_file():
-            return
+        dir_sep = os.sep if os.sep in source_file_name else "/"
 
         is_destination_directory = (Path(destination_file_name).is_dir()
-                                    or destination_file_name.endswith(os.sep))
+                                    or destination_file_name.endswith(dir_sep))
 
         if is_destination_directory:
             final_destination = os.path.join(
@@ -31,8 +30,8 @@ def move_file(command: str) -> None:
         if parent_dir and not os.path.exists(parent_dir):
             os.makedirs(parent_dir)
 
-        with (open(source_file_name, "r") as source_file_object,
-              open(final_destination, "w") as destination_file_object):
+        with (open(source_file_name, "rb") as source_file_object,
+              open(final_destination, "wb") as destination_file_object):
             destination_file_object.write(source_file_object.read())
 
         os.remove(source_file_name)
