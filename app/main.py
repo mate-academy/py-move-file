56b9ed3e9c7bd05d1a52b84e292b3d310a21224e
Radoslaw Ptak <radek1026@interi.pl>
2025-09-14 19:32:20 +0200

    Implement py-move-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(filename: str) -> None:
+    parts = filename.split()
+    if parts[0] == "mv":
+        source_path = parts[1]
+        dest_path = parts[2]
+
+        if dest_path.endswith("/"):
+            final_dest = os.path.join(dest_path, os.path.basename(source_path))
+        else:
+            final_dest = dest_path
+
+        os.makedirs(os.path.dirname(final_dest), exist_ok=True)
+        os.rename(source_path, final_dest)
