09c4d174197a25e4d94beae9c2d180af634e6082
0lex1y <kotov.oleksiy@icloud.com>
2025-09-17 18:42:00 +0300

    Add move_file implementation

--- app/main.py
+++ app/main.py
@@ -3,28 +3,28 @@ import os
 
 def move_file(command: str) -> None:
     parts = command.split()
+
     if len(parts) != 3 or parts[0] != "mv":
         raise ValueError("Invalid command format")
-    cmd, source_file, destination_file = parts
+    cmd, source_path, dest_path = parts
 
-    if destination_file.endswith(os.sep):
-        destination_file = os.path.join(destination_file,
-                                        os.path.basename(source_file))
+    if (
+            dest_path.endswith(os.path.sep)
+            or (os.path.altsep and dest_path.endswith(os.path.altsep))
+    ):
+        final_dest = os.path.join(dest_path, os.path.basename(source_path))
+    else:
+        final_dest = dest_path
 
-    dest_dir = os.path.dirname(destination_file)
-    if dest_dir:
-        dest_dir = os.path.normpath(dest_dir)
-        parts = dest_dir.split(os.sep)
-        path_accumulate = os.getcwd()  # стартуємо з поточної папки
-        for part in parts:
-            path_accumulate = os.path.join(path_accumulate, part)
-            if not os.path.exists(path_accumulate):
-                os.mkdir(path_accumulate)
-    try:
-        with (open(source_file, "rb") as src,
-              open(destination_file, "wb") as dst):
-            dst.write(src.read())
-
-        os.remove(source_file)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Source file {source_file} does not exist")
+    dir_name = os.path.dirname(final_dest)
+    if dir_name:
+        components = dir_name.split(os.path.sep)
+        prefix = ""
+        for component in components:
+            prefix = os.path.join(prefix, component)
+            if not os.path.exists(prefix):
+                os.mkdir(prefix)
+    with open(source_path, "rb") as source:
+        with open(final_dest, "wb") as dest:
+            dest.write(source.read())
+    os.remove(source_path)
