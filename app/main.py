f11f4411d19b37fc82c36b0ce80fd04f856f6885
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-23 15:13:18 +0300

    fix a problem1

--- app/main.py
+++ app/main.py
@@ -10,12 +10,14 @@ def move_file(command: str) -> None:
     """
     parts = command.split()
     if len(parts) != 3 or parts[0] != "mv":
-        raise ValueError("Command must be in format: mv source_file destination_path")
+        raise ValueError(
+            "Command must be in format: mv source_file destination_path"
+        )
 
     src = parts[1]
     dst = parts[2]
 
-    # Якщо кінцевий шлях закінчується на '/', вважаємо його директорією і беремо ім'я оригінального файлу
+    # Якщо кінцевий шлях закінчується на '/', вважаємо його директорією
     if dst.endswith("/"):
         dst = os.path.join(dst, os.path.basename(src))
 
@@ -26,7 +28,8 @@ def move_file(command: str) -> None:
         os.makedirs(dst_dir)
 
     # Копіюємо вміст у нове місце
-    with open(src, "r", encoding="utf-8") as f_src, open(dst, "w", encoding="utf-8") as f_dst:
+    with open(src, "r", encoding="utf-8") as f_src, \
+         open(dst, "w", encoding="utf-8") as f_dst:
         f_dst.write(f_src.read())
 
     # Видаляємо оригінальний файл
