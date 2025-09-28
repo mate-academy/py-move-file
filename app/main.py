689a7c1a71207711854ee3decfe59d5181645b9c
Kacper Kogut <kogutkacper7@gmail.com>
2025-09-28 13:40:05 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+from __future__ import annotations
+from types import TracebackType
+from typing import Optional, Type
+import os
+
+
+class Cleaner:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __enter__(self) -> Cleaner:
+        return self
+
+    def __exit__(self,
+                 exc_type: Optional[Type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[TracebackType]
+                 ) -> None:
+        os.remove(self.name)
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) == 3:
+        action, source_file, destination = parts
+        if os.path.exists(source_file):
+            if not os.path.exists(destination):
+                path = os.path.dirname(destination)
+                if path:
+                    os.makedirs(path, exist_ok=True)
+            with (Cleaner(source_file)):
+                with open(source_file, "r"
+                          ) as source, open(destination, "w"
+                                            ) as destination:
+                    destination.write(source.read())
