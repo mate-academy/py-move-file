0550b780de331c2122bf361760533fed5c12573a
Nazar <your@mail.com>
2025-09-28 16:07:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_name, source_file_name, destination_file_name = command.split()
+    if command_name != "mv":
+        return
+
+    if "/" in destination_file_name:
+        path_list = destination_file_name.split("/")[:-1]
+        check_folder = ""
+
+        for folder in path_list:
+            check_folder = os.path.join(check_folder, folder)
+            if not os.path.exists(check_folder):
+                os.mkdir(check_folder)
+
+        try:
+            with (open(source_file_name, "r") as file_in,
+                  open(destination_file_name, "w") as file_out):
+                content = file_in.read()
+                file_out.write(content)
+
+            os.remove(source_file_name)
+        except FileNotFoundError:
+            return
+
+    else:
+        os.rename(source_file_name, destination_file_name)
