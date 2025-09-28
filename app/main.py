499646fcc141e3be1a65a4ebb5dbfd6353b7912e
Nazar <your@mail.com>
2025-09-28 22:58:53 +0300

    Solution_3

--- app/main.py
+++ app/main.py
@@ -2,31 +2,40 @@ import os
 
 
 def move_file(command: str) -> None:
-    parts = (command_name, source_file_name,
-             destination_file_name) = command.split()
-
+    parts = command.split()
     if len(parts) != 3 or parts[0] != "mv":
         return
     cmd, source_file_name, destination_file_name = parts
 
+    if (destination_file_name.endswith(os.path.sep) or
+            (os.path.altsep and destination_file_name.endswith(os.path.altsep))):
+        destination_file_name = os.path.join(destination_file_name,
+                                             os.path.basename(source_file_name))
+
     destination_file_name = os.path.normpath(destination_file_name)
 
-    if os.path.sep not in destination_file_name:
-        if os.path.altsep not in destination_file_name:
-            os.rename(source_file_name, destination_file_name)
-            return
+    if os.path.dirname(destination_file_name) == "":
+        os.rename(source_file_name, destination_file_name)
+        return
+
     else:
         folder_path = os.path.dirname(destination_file_name)
+        folder_path_list = folder_path.split(os.path.sep)
         name_patch = os.path.basename(destination_file_name)
-        os.makedirs(folder_path, exist_ok=True)
-        path = os.path.join(folder_path, name_patch)
+        folder_result = ""
+        for folder in folder_path_list:
+            folder_result = os.path.join(folder_result, folder)
+            if not os.path.exists(folder_result):
+                os.mkdir(folder_result)
+
+        path = os.path.join(folder_result, name_patch)
 
         try:
-            with (open(source_file_name, "r") as file_in,
-                  open(path, "w") as file_out):
+            with (open(source_file_name, "rb") as file_in,
+                  open(path, "wb") as file_out):
                 content = file_in.read()
                 file_out.write(content)
 
             os.remove(source_file_name)
         except FileNotFoundError:
-            raise FileNotFoundError
+            return
