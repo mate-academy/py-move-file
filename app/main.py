2ffe8287f0e9f1222a5958d50e9270ab5fd837d8
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-10-01 01:05:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+from os import mkdir, remove, getcwd, path
+
+
+def move_file(command: str) -> None:
+    active_path = getcwd()
+    items_command = command.split(" ")
+    old_file = items_command[1]
+    new_file = items_command[2].split("/")[-1]
+    path_new_file = items_command[2].split("/")[0:-1]
+
+    if path_new_file:
+        for item in path_new_file:
+            active_path += f"\\{item}"
+            if not path.isdir(active_path):
+                mkdir(active_path)
+
+    with open(old_file, "r",) as old:
+        with open(f"{active_path}\\{new_file}", "w",) as new:
+            new.write(old.read())
+
+    remove(old_file)
