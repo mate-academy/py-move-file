9e7aa98206ca0677f36c7577232cdd51c8d3757d
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-10-02 23:52:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    commands = command.split()
+    if len(commands) == 3 and commands[0] == "mv":
+
+        #with open(commands[1]) as file_to_read:
+
