5e0481bb714d4197736bb0c60341b2b7bdeec1b3
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-10 09:21:32 +0200

    fixing logic

--- app/main.py
+++ app/main.py
@@ -2,26 +2,52 @@ import os
 
 
 def move_file(command: str) -> None:
-    arguments = command.split()
-    if len(arguments) != 3 or arguments[0] != "mv":
-        return
-    src, dest = arguments[1], arguments[2]
-
-    if dest == "." or dest == "./":
-        dest = os.getcwd()
-
-    if not os.path.isfile(src):
+    cmd, source_path, destination_path = command.split()
+    if cmd == "mv":
         return
 
-    is_dest_dir = os.path.isdir(dest) or dest.endswith(os.sep)
+
+    if destination_path == "." or destination_path ==  "./":
+        destination_path = os.getcwd()
+
+    if not os.path.isfile(source_path):
+        return
+
+    is_dest_dir = os.path.isdir(destination_path) or destination_path.endswith(os.sep)
+
     if is_dest_dir:
-        dest_dir = dest.rstrip(os.sep)
-        dest_path = os.path.join(dest_dir, os.path.basename(src))
+        dest_dir = destination_path.rstrip(os.sep)
+        dest_path = os.path.join(dest_dir, os.path.basename(source_path))
     else:
-        dest_dir = os.path.dirname(dest)
-        dest_path = dest
+        dest_dir = os.path.dirname(destination_path)
+        dest_path = destination_path.rstrip(os.sep)
 
     if dest_dir:
-        os.makedirs(dest_dir, exist_ok=True)
+        normalized_dir = os.path.normpath(source_path)
+        path_segments = normalized_dir.split(os.path.sep)
+        current_path = ""
 
-    os.replace(src, dest_path)
+        for segment in path_segments:
+            if not segment:
+                continue
+
+            current_path = os.path.join(current_path, segment)
+
+            if os.path.isdir(current_path):
+                continue
+            try:
+                os.makedirs(current_path)
+            except FileExistsError:
+                pass
+
+    try:
+        with open(source_path, "r") as source_file:
+            content = source_file.read()
+
+        with open(dest_path, "w") as destination_file:
+            destination_file.write(content)
+
+        os.remove(source_path)
+
+    except FileNotFoundError:
+        pass
