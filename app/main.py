9f4a2261018b13cc475cc4262b06f9365898264b
Karina Kryzh <krizanivskak@gmail.com>
2025-10-19 18:43:51 +0100

    fix

--- app/main.py
+++ app/main.py
@@ -2,16 +2,24 @@ import os
 
 
 def move_file(command: str) -> None:
-    if not command.startswith("mv "):
+    parts = command.strip().split(maxsplit=2)
+    if len(parts) != 3 or parts[0] != "mv":
         return
 
-    _, source_file, target_path = command.split()
+    _, source_file, target_path = parts
+    if not os.path.isfile(source_file):
+        return
+
     if target_path.endswith("/"):
         target_path = os.path.join(target_path, os.path.basename(source_file))
 
     directory = os.path.dirname(target_path)
     if directory and not os.path.exists(directory):
-        os.makedirs(directory, exist_ok=True)
+        path_part = ""
+        for folder in directory.split("/"):
+            path_part = os.path.join(path_part, folder)
+            if not os.path.exists(path_part):
+                os.mkdir(path_part)
 
     with open(source_file, "r") as file_in, open(target_path, "w") as file_out:
         for line in file_in:
