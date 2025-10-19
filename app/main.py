785233fc197503df0135060951730177fc4470f7
Karina Kryzh <krizanivskak@gmail.com>
2025-10-19 19:11:40 +0100

    fix-2

--- app/main.py
+++ app/main.py
@@ -10,15 +10,15 @@ def move_file(command: str) -> None:
     if not os.path.isfile(source_file):
         return
 
-    if target_path.endswith("/"):
+    if target_path[-1] in ("/", "\\"):
         target_path = os.path.join(target_path, os.path.basename(source_file))
 
     directory = os.path.dirname(target_path)
     if directory and not os.path.exists(directory):
         path_part = ""
-        for folder in directory.split("/"):
+        for folder in os.path.normpath(directory).split(os.path.sep):
             path_part = os.path.join(path_part, folder)
-            if not os.path.exists(path_part):
+            if not os.path.exists(path_part) and folder != "":
                 os.mkdir(path_part)
 
     with open(source_file, "r") as file_in, open(target_path, "w") as file_out:
