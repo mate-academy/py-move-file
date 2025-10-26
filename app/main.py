062021faf57e8da604e9d430a07916f34565d9ab
Anton Tsikhanovich <alluspeh@gmail.com>
2025-10-26 23:04:26 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,18 +4,24 @@ import os
 def move_file(command: str) -> None:
     if command == "":
         return
-    list_of_files = command.strip().split()
-    if len(list_of_files) != 3 or list_of_files[0] != "mv":
-        return
-    mv, source, path_to_file = list_of_files
-    new_file = path_to_file.split("/")[-1]
-    path = "/".join(path_to_file.split("/")[:-1])
-    if "/" not in path_to_file:
-        os.rename(source, path_to_file)
-        return
-    os.makedirs(path, exist_ok=True)
-    dest_path = os.path.join(path, new_file)
-    if os.path.exists(dest_path):
-        os.remove(dest_path)
 
-    os.rename(source, dest_path)
+    list_of_files = command.strip().split()
+    if list_of_files[0] != "mv" or len(list_of_files) != 3:
+        return
+    _, source, destination = list_of_files
+
+    if not os.path.isfile(source):
+        return
+
+    if destination.endswith(os.sep):
+        os.makedirs(destination, exist_ok=True)
+        destination = os.path.join(destination, os.path.basename(source))
+    else:
+        dest_dir = os.path.dirname(destination)
+        if dest_dir:
+            os.makedirs(dest_dir, exist_ok=True)
+
+    if os.path.exists(destination):
+        os.remove(destination)
+
+    os.rename(source, destination)
