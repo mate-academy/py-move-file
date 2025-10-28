2b25562fc0c2e471b4fb2c0b4b758eca9dfc1ce4
venlivan <venlivandovskyi@gmail.com>
2025-10-28 17:52:57 +0100

    fixed issues

--- app/main.py
+++ app/main.py
@@ -3,16 +3,19 @@ import os
 
 def move_file(command: str) -> None:
     command_split = command.split()
-    if len(command_split) != 3:
+    if len(command_split) == 3 and command_split[0] == "mv":
+        mv, source_file, destination_file = command_split
+    else:
         raise ValueError("Invalid command")
-    src = command_split[1]
-    dst = command_split[2]
-    if dst.endswith("/"):
-        dst = os.path.join(dst, os.path.basename(src))
-    dir_name = os.path.dirname(dst)
+    if destination_file.endswith("/"):
+        destination_file = (os.path.join
+                            (destination_file,
+                             os.path.basename(source_file))
+                            )
+    dir_name = os.path.dirname(destination_file)
     if dir_name:
         os.makedirs(dir_name, exist_ok=True)
-    with open(src, "r") as f1, open(dst, "w") as f2:
-        file_content = f1.read()
-        f2.write(file_content)
-    os.remove(src)
+    with open(source_file, "r") as file1, open(destination_file, "w") as file2:
+        file_content = file1.read()
+        file2.write(file_content)
+    os.remove(source_file)
