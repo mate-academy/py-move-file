478772174e561018b96f27d9767c0d40b02c86a9
venlivan <venlivandovskyi@gmail.com>
2025-10-29 14:48:51 +0100

    variable names 'file1' and 'file2' have been changed

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def move_file(command: str) -> None:
     dir_name = os.path.dirname(destination_file)
     if dir_name:
         os.makedirs(dir_name, exist_ok=True)
-    with open(source_file, "r") as file1, open(destination_file, "w") as file2:
-        file_content = file1.read()
-        file2.write(file_content)
+    with open(source_file, "r") as source_file_object, open(destination_file, "w") as destination_file_object:
+        file_content = source_file_object.read()
+        destination_file_object.write(file_content)
     os.remove(source_file)
