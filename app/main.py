006a9f3aa750ecdc0f01e85022c77a8e53b64347
Kyrylo Kilin <killin@ukr.net>
2025-11-01 12:25:32 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def move_file(command: str) -> None:
     if cmd != "mv":
         return
 
-    if destination_path.endswith(os.path.sep):
+    if destination_path.endswith(("/", "\\")):
         destination_path = os.path.join(
             destination_path, os.path.basename(source_file_name)
         )
@@ -20,15 +20,32 @@ def move_file(command: str) -> None:
 
     destination_dir = os.path.dirname(destination_path)
     if destination_dir:
-        if not os.path.isdir(destination_dir):
-            os.makedirs(destination_dir, exist_ok=True)
 
-    with (open(source_file_name, "rb") as source_file,
-          open(destination_path, "wb") as destination_file):
+        norm_dir = os.path.normpath(destination_dir)
+        drive, tail = os.path.splitdrive(norm_dir)
+        root_prefix = os.path.sep if tail.startswith(os.path.sep) else ""
+        tail = tail.lstrip(os.path.sep)
+
+        current_path = drive + root_prefix
+        for part in tail.split(os.path.sep):
+            if not part:
+                continue
+            current_path = (os
+                            .path
+                            .join(current_path, part)
+                            ) if current_path else part
+            if not os.path.isdir(current_path):
+                try:
+                    os.mkdir(current_path)
+                except FileExistsError:
+                    pass
+
+    with (open(source_file_name, "rb") as src_f,
+          open(destination_path, "wb") as dst_f):
         while True:
-            chunk = source_file.read(1024 * 1024)
+            chunk = src_f.read(1024 * 1024)
             if not chunk:
                 break
-            destination_file.write(chunk)
+            dst_f.write(chunk)
 
     os.remove(source_file_name)
