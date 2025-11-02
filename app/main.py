cf18c1870ec85bc1a714decbfcfe884cf4385871
Wojciech Witkowski <witus85@gmail.com>
2025-11-02 16:05:07 +0100

    fix path issue and assigning variables

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 from os import remove, mkdir, path
 
 
-def move_file(command: str) -> None | list:
+def move_file(command: str) -> None:
     command_parts = command.split(" ")
     if command_parts[0] == "mv" and len(command_parts) == 3:
-        file_in = command_parts[1]
-        file_out = command_parts[2]
+        file_in, file_out = command_parts[1:]
         if file_in != file_out:
             if "/" in file_out:
                 full_path = file_out.split("/")[0:-1]
                 directory = ""
                 for catalogue in full_path:
-                    directory += catalogue
+                    directory = path.join(directory, catalogue)
                     if not path.exists(directory):
                         mkdir(directory)
-                    directory += "/"
             try:
                 with (
                     open(file_in) as source_file,
