de4f2262ad40820aa9a16279af04396ba7422db2
Wojciech Witkowski <witus85@gmail.com>
2025-11-02 18:51:51 +0100

    directory path update

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
-from os import remove, mkdir, path
+from os import remove, mkdir, path, sep
 
 
 def move_file(command: str) -> None:
     command_parts = command.split(" ")
-    if command_parts[0] == "mv" and len(command_parts) == 3:
+    if len(command_parts) == 3 and command_parts[0] == "mv":
         file_in, file_out = command_parts[1:]
         if file_in != file_out:
-            if "/" in file_out:
-                full_path = file_out.split("/")[0:-1]
-                directory = ""
-                for catalogue in full_path:
-                    directory = path.join(directory, catalogue)
-                    if not path.exists(directory):
-                        mkdir(directory)
+            dir_path, file_name = path.split(file_out)
+            if not file_name:
+                file_out = path.join(file_out, file_in)
+            if dir_path:
+                path_parts = dir_path.split(sep)
+                make_dir = ""
+                for catalogue in path_parts:
+                    make_dir = path.join(make_dir, catalogue)
+                    if not path.exists(make_dir):
+                        mkdir(make_dir)
             try:
                 with (
                     open(file_in) as source_file,
@@ -22,3 +25,6 @@ def move_file(command: str) -> None:
                     remove(file_in)
             except FileNotFoundError:
                 pass
+
+
+move_file("mv new/dir/test.txt /")
