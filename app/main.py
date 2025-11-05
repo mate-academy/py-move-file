628a6450197c2c2d8d5bfe2eb7174f21f4242d4f
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2025-11-05 17:51:54 +0200

    Added suggested changes

--- app/main.py
+++ app/main.py
@@ -6,22 +6,22 @@ def move_file(command: str) -> None:
             len(cmd_chain := command.split(" ")) == 3
             and cmd_chain[0] == "mv"
             and os.path.exists(cmd_chain[1])
-            and not cmd_chain[2].startswith(("--", "/", "../"))
+            and not cmd_chain[2].startswith("-")
     ):
-        src, dst = cmd_chain[1], cmd_chain[2]
-        *parent, base = dst.split("/")
+        source, destination = cmd_chain[1], cmd_chain[2]
+        parent = os.path.dirname(destination)
+        basename = os.path.basename(source)
 
         if parent:
-            os.makedirs("/".join(parent), exist_ok=True)
+            os.makedirs(parent, exist_ok=True)
 
-        if not base or os.path.isdir(dst):
-            dst = os.path.join(dst, os.path.basename(src))
+        if not basename or os.path.isdir(destination):
+            destination = os.path.join(destination, os.path.basename(source))
 
         with (
-            open(src, "rb") as file_in,
-            open(dst, "wb") as file_out
+            open(source, "rb") as file_in,
+            open(destination, "wb") as file_out
         ):
             file_out.write(file_in.read())
 
-        os.remove(src)
-        print("File moved successfully.")
+        os.remove(source)
