2ee8969c7b2a679cdeb0ba28ccb40845f980c7ea
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-11-06 22:09:34 +0100

    solved py-move-file

--- app/main.py
+++ app/main.py
@@ -12,7 +12,12 @@ def move_file(command: str) -> None:
     if source_file_name == destination_file_name:
         return
 
+    if destination_file_name.endswith('/') or destination_file_name.endswith('\\'):
+        source_file_name = os.path.basename(source_file_name)
+        destination_file_name = os.path.join(destination_file_name, source_file_name)
+
     try:
+        os.path.join(destination_file_name)
         destination_dir = os.path.dirname(destination_file_name)
         if destination_dir:
             os.makedirs(destination_dir, exist_ok=True)
@@ -25,4 +30,4 @@ def move_file(command: str) -> None:
         os.remove(source_file_name)
 
     except FileNotFoundError:
-        print("File not found.")
+        return
