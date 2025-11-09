bcb7515de9ea337dd92974294bf298cc3777a602
Oleksii Mishchenko <alex3hard@gmail.com>
2025-11-09 22:36:42 +0200

    move_file implementation

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_parts = command.split()
+    if len(command_parts) != 3 or command_parts[0] != "mv":
+        return
+    file_name_origin = command_parts[1]
+    destination = command_parts[2]
+
+    if destination.endswith("/"):
+        destination = os.path.join(
+            destination,
+            os.path.basename(file_name_origin)
+        )
+
+    dest_dir = os.path.dirname(destination)
+
+    if dest_dir:
+        os.makedirs(dest_dir, exist_ok=True)
+
+    os.rename(file_name_origin, destination)
