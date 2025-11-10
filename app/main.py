9ba5e513623710dea5b991999723b422335cb21d
Mike <tradewcs@gmail.com>
2025-11-10 16:42:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, src, dest = command.split()
+
+    if dest.endswith("/"):
+        os.makedirs(dest, exist_ok=True)
+        dest = os.path.join(dest, os.path.basename(src))
+    else:
+        dir_path = os.path.dirname(dest)
+        if dir_path:
+            os.makedirs(dir_path, exist_ok=True)
+
+    with open(src, "rb") as src_file, open(dest, "wb") as dest_file:
+        dest_file.write(src_file.read())
+
+    os.remove(src)
