06dd63e99e5b282b24c662dd650ed507c18f9508
Oleg <oleggavelya@gmail.com>
2025-11-12 19:10:17 +0200

    refactoring function move file

--- app/main.py
+++ app/main.py
@@ -3,22 +3,27 @@ import os
 
 def move_file(mv_file: str) -> None:
     create_list = mv_file.split()
-    if len(create_list) < 3 or create_list[0] != "mv":
+    if len(create_list) != 3 or create_list[0] != "mv":
         raise FileNotFoundError
-    if "/" not in mv_file:
-        with open(create_list[1], "r") as read_file, open(create_list[-1], "w") as create_file:
-            create_file.write(read_file.read())
-            if create_list[1] != create_list[-1]:
-                os.remove(create_list[1])
+    src = create_list[1]
+    dst = create_list[-1]
+    create_folder = dst.strip("/").split("/")
+    if dst.endswith("/"):
+        path_folder = create_folder
+        file_name = os.path.basename(src)
     else:
-        create_folder = create_list[-1].replace("/", ",").split(",")
-        current_path = ""
-        for folder in create_folder[:-1]:
-            current_path = os.path.join(current_path, folder)
-            if not os.path.exists(current_path):
-                os.mkdir(current_path)
-        if os.path.exists(current_path):
-            if create_folder[-1].endswith(".txt"):
-                with open(create_list[1], "r") as read_file, open(f"{current_path}/{create_folder[-1]}", "w") as create_file:
-                    create_file.write(read_file.read())
-                    os.remove(create_list[1])
+        path_folder = create_folder[:-1]
+        file_name = create_folder[-1]
+    current_path = ""
+    for folder in path_folder:
+        current_path = os.path.join(current_path, folder)
+        if not os.path.exists(current_path):
+            os.mkdir(current_path)
+    if os.path.exists(current_path):
+        with open(src, "r") as read_file, open(current_path + "/" + file_name, "w") as create_file:
+            create_file.write(read_file.read())
+            os.remove(src)
+    with open(src, "r") as read_file, open(dst, "w") as create_file:
+        create_file.write(read_file.read())
+        if src != dst:
+            os.remove(src)
