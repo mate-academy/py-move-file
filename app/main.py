c74f1d35bb49d20da42a7e1057cb579315b77130
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2025-11-13 15:37:44 +0200

    Cleared

--- app/main.py
+++ app/main.py
@@ -2,35 +2,29 @@ from os import remove, rename, makedirs, path
 
 
 def move_file(command: str) -> None:
-    commands = command.split()
-    if len(commands) == 3 and commands[0] == "mv":
-        command, file_to_copy, new_file = commands[0], commands[1], commands[2]
+    parts = command.split()
+    if parts[0] != "mv" or len(parts) < 3:
+        return
 
-        cleared_path = commands[2].split("/")
-        directory = "/".join(cleared_path[:-1])
-        if not cleared_path[-1]:
-            new_file = file_to_copy
-        else:
-            new_file = cleared_path[-1]
+    if "/" not in parts[2]:
+        rename(parts[1], parts[2])
+        return
 
-        full_path = path.join(directory, new_file)
+    file_to_copy_name, directory = parts[1], path.dirname(parts[2])
 
-        if not directory:
-            try:
-                rename(file_to_copy, new_file)
-                return
-            except FileExistsError:
-                raise FileExistsError
+    if parts[2].endswith("/"):
+        full_path = path.join(directory, path.basename(file_to_copy_name))
+    else:
+        full_path = path.join(directory, path.basename(parts[2]))
 
-        makedirs(directory, exist_ok=True)
+    if not path.exists(directory):
+        makedirs(directory)
 
-        try:
-            with open(file_to_copy, "r") as to_copy, \
-                 open(full_path, "w") as to_write:
-                file_data = to_copy.readlines()
-                to_write.write("".join(file_data))
+    with (
+        open(file_to_copy_name) as file_to_read,
+        open(full_path, "w") as file_to_write
+    ):
+        for line in file_to_read:
+            file_to_write.write(line)
 
-            remove(file_to_copy)
-
-        except FileNotFoundError:
-            raise FileNotFoundError("[Errno 2] No such file or directory")
+    remove(file_to_copy_name)
