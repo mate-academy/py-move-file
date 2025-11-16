f1b3ca59b6d89319052bcabac661848d873357a1
Andrii Chychkan <mywork1728@gmail>
2025-11-16 15:19:51 +0200

    fixed bugs with existing path

--- app/main.py
+++ app/main.py
@@ -1,27 +1,28 @@
 import os
-from os import makedirs, path
 
 
 def move_file(command: str) -> None:
 
     command_list = command.split(" ")
 
-    destination_path = path.dirname(command_list[2])
-    create_file_name = path.basename(command_list[2])
-    result_path = create_file_name
-    file_to_copy = command_list[1]
+    if len(command_list) != 3 or command_list[0] != "mv":
+        raise ValueError("Invalid command fomat.")
 
-    if destination_path != "":
-        makedirs(destination_path, exist_ok=True)
+    _, file_to_copy, destination_path = command_list
 
-        result_path = path.join(destination_path, create_file_name)
+    if destination_path.endswith("/"):
+        source_filename = os.path.basename(file_to_copy)
+        destination_path = os.path.join(destination_path, source_filename)
 
-    data = ""
+    destination_dir = os.path.dirname(destination_path)
+
+    if destination_dir and not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
 
     with open(file_to_copy, "r") as file:
         data = file.read()
 
-    with open(result_path, "w") as file:
+    with open(destination_path, "w") as file:
         file.write(data)
 
     os.remove(file_to_copy)
