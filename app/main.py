06abd72e4df1f1a133bc51ccad9bd852b3e10a35
Petro Kurinnyi <petro.kurinnyi@gmail.com>
2025-11-23 16:10:52 +0200

    PR fixes 2

--- app/main.py
+++ app/main.py
@@ -10,17 +10,14 @@ def move_file(input_command: str) -> None:
         return
 
     directories, new_file_name = os.path.split(new_file_path)
-    directives_list = directories.strip().split("/")
-
-    new_path = ""
-    for directory in directives_list:
-        if directory != "":
-            new_path = os.path.join(new_path, directory)
-            if not os.path.exists(new_path):
-                os.mkdir(new_path)
+    if directories != "":
+        os.makedirs(directories, exist_ok=True)
 
     if not new_file_name:
-        new_file_path += old_file_path
+        new_file_path = os.path.join(
+            new_file_path,
+            os.path.basename(old_file_path)
+        )
 
     with (open(old_file_path, "r") as old_file,
           open(new_file_path, "w") as new_file):
