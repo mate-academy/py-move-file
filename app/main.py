191048fd0268cb1776ecb0f5a0f94ce1d41df435
Petro Kurinnyi <petro.kurinnyi@gmail.com>
2025-11-23 16:00:11 +0200

    PR fixes

--- app/main.py
+++ app/main.py
@@ -9,14 +9,18 @@ def move_file(input_command: str) -> None:
     except ValueError:
         return
 
-    directories = new_file_path.split("/")
-    directories.pop()
+    directories, new_file_name = os.path.split(new_file_path)
+    directives_list = directories.strip().split("/")
 
     new_path = ""
-    for directory in directories:
-        new_path = os.path.join(new_path, directory)
-        if not os.path.exists(new_path):
-            os.mkdir(new_path)
+    for directory in directives_list:
+        if directory != "":
+            new_path = os.path.join(new_path, directory)
+            if not os.path.exists(new_path):
+                os.mkdir(new_path)
+
+    if not new_file_name:
+        new_file_path += old_file_path
 
     with (open(old_file_path, "r") as old_file,
           open(new_file_path, "w") as new_file):
