7a047f8a41d94fbb03cfacd74122240d6365f19f
Ксюша <mykola.petrenko@gmail.com>
2025-12-09 18:49:57 +0200

    temp

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def move_file(command: str) -> None:
+    file_names = command.split()
+    if len(file_names) != 3 or file_names[0] != "mv":
+        return
+    if file_names[2].endswith("/"):
+        return
+    try:
+        with (open(file_names[1], "r")
+              as file_in, open(file_names[2], "w") as file_out):
+            file_out.write(file_in.read())
+    except OSError:
+        return
