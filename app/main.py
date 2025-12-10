d5471537e088cc1ccd63fdae55a303584805f0ec
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-12-10 10:10:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,21 +5,24 @@ def move_file(command: str) -> None:
     file_names = command.split()
     if len(file_names) != 3 or file_names[0] != "mv":
         return
-    if file_names[2].endswith("/"):
-        return
-    path = file_names[2].split("/")
+    source_file = file_names[1]
+    destination_path = file_names[2]
+    if destination_path.endswith(os.sep) or os.path.isdir(destination_path):
+        destination_path = os.path.join(destination_path, os.path.basename(source_file))
+    normalized_path = os.path.normpath(destination_path)
+    path_parts = normalized_path.split(os.sep)
     current_path = ""
-    for part in path[:-1]:
-        current_path += part + "/"
+    for part in path_parts[:-1]:
+        current_path = os.path.join(current_path, part)
         try:
             os.mkdir(current_path)
         except FileExistsError:
             pass
     try:
         with (open(file_names[1], "r")
-              as file_in, open(file_names[2], "w") as file_out):
+              as file_in, open(destination_path, "w") as file_out):
             file_out.write(file_in.read())
-        os.remove(file_names[1])
+        os.remove(source_file)
     except OSError:
         return
 
