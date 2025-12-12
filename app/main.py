f87aaf7ad734a0e7fa4c7e8f9f69d40b6fad9957
Kyrylo Cherdakliiev <yungkeryaa@gmail.com>
2025-12-12 20:53:46 +0100

    solved all the cross platform problems

--- app/main.py
+++ app/main.py
@@ -3,36 +3,30 @@ import os
 
 def move_file(command: str) -> None:
     split_command = command.split()
-    if len(split_command) == 3 and split_command[0] == "mv": #Check if it is a command format
-        source_file, new_file = split_command[1:]
-        if "/" in new_file: #checkig if it is a path
-            if new_file[-1] == "/":
-                new_file += source_file
-            path_list = new_file.split("/")
-            full_path = path_list[0]
-            for i in range(len(path_list) - 1):
-                try:
-                    os.mkdir(full_path)
-                except FileExistsError:
-                    continue
-                finally:
-                    full_path = os.path.join(full_path, path_list[i + 1])
-            try:
-                with (open(source_file, "r") as file_in,
-                      open(full_path, "w") as file_out):
-                    file_out.write(file_in.read())
-                os.remove(source_file)
-            except FileNotFoundError:
-                pass
+    if len(split_command) == 3 and split_command[0] == "mv":
+        source_file, target_path = split_command[1:]
+        if target_path.endswith(os.path.sep):
+            directory_to_create = target_path
+            final_destination = os.path.join(target_path,
+                                             os.path.basename(source_file))
+        elif os.path.dirname(target_path):
+            directory_to_create = os.path.dirname(target_path)
+            final_destination = target_path
         else:
-            os.rename(source_file, new_file)
 
+            directory_to_create = ""
+            final_destination = target_path
 
+        if directory_to_create:
 
+            os.makedirs(directory_to_create, exist_ok=True)
 
+        try:
+            with (open(source_file, "r") as file_in,
+                  open(final_destination, "w") as file_out):
+                file_out.write(file_in.read())
 
-# file_r = open("file.txt", "w")
-# # file_r.write("Hello!\nI am John")
-# # print(open("file.txt").read())
-# move_file("mv file.txt first_dir/second_dir/third_dir/")
-# # print(open("first_dir/second_dir/third_dir/file2.txt").read())
+            os.remove(source_file)
+
+        except FileNotFoundError:
+            pass
