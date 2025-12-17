99ed5cfab3f7702976bfa34e88c7a43a3a487dcb
Andrii <a17drigs@gmail.com>
2025-12-17 17:30:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    list_command = command.split(" ")
+
+    try:
+        new_file = list_command[2]
+        old_file = list_command[1]
+        try:
+            if "/" in new_file:
+                list_new_file = new_file.split("/")
+                path = os.path.join(*list_new_file)
+                os.makedirs(os.path.dirname(path), exist_ok=True)
+            elif "\\" in new_file:
+                list_new_file = new_file.split("\\")
+                path = os.path.join(*list_new_file)
+                os.makedirs(os.path.dirname(path), exist_ok=True)
+            else:
+                path = new_file
+
+            with open(old_file, "r") as old, open(path, "w") as new:
+                new.write(old.read())
+                os.remove(old_file)
+        except FileNotFoundError:
+            pass
+    except IndexError:
+        pass
