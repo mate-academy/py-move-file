b14caa90b2efa537b26cb2e4feecef13f2421dee
Artem <artem90078@gmail.com>
2025-12-23 17:56:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import os
+
+
+def move_file(command):
+    parts = command.split()
+    src = parts[1]
+    dest = parts[2]
+
+    if dest.endswith('/'):
+        dest = os.path.join(dest, os.path.basename(src))
+
+    dest_dir = os.path.dirname(dest)
+
+    if dest_dir:
+        path_acc = ""
+        for folder in dest_dir.split('/'):
+            if not folder:
+                continue
+            path_acc = os.path.join(path_acc, folder)
+            if not os.path.exists(path_acc):
+                os.mkdir(path_acc)
+
+    with open(src, 'r') as f_src:
+        content = f_src.read()
+
+    with open(dest, 'w') as f_dest:
+        f_dest.write(content)
+
+    os.remove(src)
