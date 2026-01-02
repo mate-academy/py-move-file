ea45aba222fda1e838097e4b07453c33415e8b9d
Danylo Moroz <danylomoroz1989@gmail.com>
2026-01-02 12:28:43 +0100

    solution_moroz_extra_task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    commands = command.split()
+    if len(commands) == 3 and commands[0] == "mv":
+        mv, source, path_destination = commands
+        if not os.path.isfile(source):
+            raise FileNotFoundError(f"File not found: {source}")
+        # if path_destination[-1] == "/" or path_destination[-1] == os.sep:
+        if (path_destination.endswith(("/", os.sep))
+                or os.path.isdir(path_destination)):
+            os.makedirs(path_destination, exist_ok=True)
+            with (open(source, "rb") as original_file,
+                  open(os.path.join(path_destination,
+                                    os.path.basename(source)),
+                       "wb") as new_file):
+                for line in original_file:
+                    new_file.write(line)
+            os.remove(source)
+
+        else:
+            final_path = os.path.dirname(path_destination)
+            if final_path:
+                os.makedirs(final_path, exist_ok=True)
+
+            with (open(source, "rb") as original_file,
+                  open(path_destination, "wb") as new_file):
+                for line in original_file:
+                    new_file.write(line)
+            os.remove(source)
